0606da72cd2808ac7a0c5cfaab0f1799a7a460e5
Sam Minnee <sam@silverstripe.com>
2007-08-27 05:06:14 +0000

    Updated license notice after emailing the author of this profiler, and received an email allowing us to release the code under a BSD license
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/sapphire/trunk@40906 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- profiler/Profiler.php
+++ profiler/Profiler.php
@@ -2,20 +2,7 @@
 /********************************************************************************\
  * Copyright (C) Carl Taylor (cjtaylor@adepteo.com)                             *
  * Copyright (C) Torben Nehmer (torben@nehmer.net) for Code Cleanup             *
- *                                                                              *
- * This program is free software; you can redistribute it and/or                *
- * modify it under the terms of the GNU General Public License                  *
- * as published by the Free Software Foundation; either version 2               *
- * of the License, or (at your option) any later version.                       *
- *                                                                              *
- * This program is distributed in the hope that it will be useful,              *
- * but WITHOUT ANY WARRANTY; without even the implied warranty of               *
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                *
- * GNU General Public License for more details.                                 *
- *                                                                              *
- * You should have received a copy of the GNU General Public License            *
- * along with this program; if not, write to the Free Software                  *
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.  *
+ * Licensed under the BSD license upon request                                  *
 \********************************************************************************/
 
 /// Enable multiple timers to aid profiling of performance over sections of code
