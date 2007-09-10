a672aa74747b435d4e11ec1e4fee33a4b242dc0d
Jeremy Shipman <jeremy@burnbright.co.nz>
2007-09-10 00:21:54 +0000

    removed call by reference because it was causing a warning that it is depricated use. Refer to 41021
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/sapphire/trunk@41510 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- core/model/Hierarchy.php
+++ core/model/Hierarchy.php
@@ -334,7 +334,7 @@ class Hierarchy extends DataObjectDecorator {
 					continue;
 				}
 				$idList[] = $child->ID;
-				$child->loadDescendantIDListInto(&$idList);
+				$child->loadDescendantIDListInto($idList);
 			}
 		}
 	}
