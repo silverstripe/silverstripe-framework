b978d6ca94a3034450b55a9df01898d1625a113b
Jeremy Shipman <jeremy@burnbright.co.nz>
2007-09-14 02:18:48 +0000

    Pear bbcode parser no longer requires work.
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/sapphire/branches/2.1.0@41722 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- _config.php
+++ _config.php
@@ -3,7 +3,7 @@
 
 define('MCE_ROOT', 'jsparty/tiny_mce2/');
 
-$path = Director::baseFolder().'/sapphire/pear/';
+$path = Director::baseFolder().'/sapphire/parsers/';
 set_include_path(get_include_path() . PATH_SEPARATOR . $path);
 
 ?>
