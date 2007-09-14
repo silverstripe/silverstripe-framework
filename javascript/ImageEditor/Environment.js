fcf8302bbaff55cc2208f1ad106982fc8041a492
Ingo Schommer <ingo@silverstripe.com>
2007-09-14 01:48:04 +0000

    mujma: All ImageEditor? files will be placed in cms part rather than in sapphire.  (merged from branches/gsoc)
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/sapphire/trunk@41715 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- javascript/ImageEditor/Environment.js
+++
@@ -1,9 +0,0 @@
-/**
- * @author Mateusz
- */
-var Environment = {
-	initialize: function (imageFile) {
-		imageBox = new ImageBox.initialize();
-		image = new Image.initialize(imageFile);		
-	},		
-}
