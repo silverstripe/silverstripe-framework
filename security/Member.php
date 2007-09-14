f96879bb13e05cd6a4db1377a6f5d4ff37b6c262
Ingo Schommer <ingo@silverstripe.com>
2007-09-14 19:14:35 +0000

    elofgren: In Member::autoLogin() suppress errors in the call to list() so that I don't get this error if _COOKIEalc_enc? does not have the expected parts: Notice: Undefined offset: 1 in /var/www/silverstripe-gsoc/sapphire/security/Member.php on line 69 Which causes this error: FATAL ERROR: session_regenerate_id() [function.session-regenerate-id]: Cannot regenerate session id - headers already sent At line 40 in /var/www/silverstripe-gsoc/sapphire/security/Member.php (merged from branches/gsoc)
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/sapphire/trunk@41788 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- security/Member.php
+++ security/Member.php
@@ -66,7 +66,7 @@ class Member extends DataObject {
 	static function autoLogin() {
 		if(isset($_COOKIE['alc_enc']) && !Session::get("loggedInAs")) {
 
-			list($uid, $token) = explode(':', $_COOKIE['alc_enc'], 2);
+			@list($uid, $token) = explode(':', $_COOKIE['alc_enc'], 2);
 			$uid = Convert::raw2sql($uid);
 			$token = Convert::raw2sql($token);
 
