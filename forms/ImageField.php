44a74482e0fe84f76fe5fbd76b615dee2850f329
Ingo Schommer <ingo@silverstripe.com>
2007-09-15 21:40:28 +0000

    mlanthaler: Bugfix: Fixed E_NOTICE error "Trying to get property of non-object". (merged from branches/gsoc)
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/sapphire/trunk@41955 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- forms/ImageField.php
+++ forms/ImageField.php
@@ -4,20 +4,28 @@
  * A field that will upload attached images.
  */
 class ImageField extends FileField {
-	
+
 	public function Field($id = null) {
 		$data = $this->form->getRecord();
 		
-		$parentID = ($id && is_numeric($id)) ? $id : (isset($data) ? $data->ID : 0);
+		if($id && is_numeric($id)) {
+			$parentID = $id;
+		} elseif($data) {
+			$parentID = $data->ID;
+		} else {
+			$parentID = null;
+		}
+
 		if($data && $parentID && is_numeric($parentID)) {
 			$idxField = $this->name . 'ID';
-			$hiddenField = "<input class=\"hidden\" type=\"hidden\" id=\"" . $this->id() . "\" name=\"$idxField\" value=\"" . $this->attrValue() . "\" />";
+			$hiddenField = "<input class=\"hidden\" type=\"hidden\" id=\"" .
+				$this->id() . "\" name=\"$idxField\" value=\"" . $this->attrValue() . "\" />";
 
 			$parentClass = $data->class;
 			$parentField = $this->name;
-						
+
 			$iframe = "<iframe name=\"{$this->name}_iframe\" src=\"images/iframe/$parentClass/$parentID/$parentField\" style=\"height: 152px; width: 600px; border-style: none;\"></iframe>";
-	
+
 			return $iframe . $hiddenField;
 		} else {
 			$this->value = 'You can add images once you have saved for the first time.';
@@ -25,21 +33,27 @@ class ImageField extends FileField {
 		}
 	}
 
+
 	public function saveInto($record) {
 		$data = $this->form->getRecord();
-		// if the record was written for the first time (has an arbitrary "new"-ID), update the imagefield to enable uploading
-		if($record->ID && substr($data->ID,0,3) == 'new') {
+		// if the record was written for the first time (has an arbitrary "new"-ID),
+		// update the imagefield to enable uploading
+		if($record->ID && substr($data->ID, 0, 3) == 'new') {
 			FormResponse::update_dom_id($this->id(), $this->Field($record->ID));
 		}
 	}
-	
+
+
 	/**
 	 * Returns a readonly version of this field
 	 */
 	function performReadonlyTransformation() {
-		$field = new SimpleImageField_Disabled($this->name, $this->title, $this->value);
+		$field = new SimpleImageField_Disabled($this->name, $this->title,
+																					 $this->value);
 		$field->setForm($this->form);
 		return $field;
 	}
 }
+
+
 ?>
