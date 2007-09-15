35c5b08b37cf2f22e67ac1cb4770a20bd8a2e292
Ingo Schommer <ingo@silverstripe.com>
2007-09-15 21:39:23 +0000

    mlanthaler: Bugfix: Fixed E_NOTICE error due to uninitialized variable. (merged from branches/gsoc)
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/sapphire/trunk@41954 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- forms/LookupField.php
+++ forms/LookupField.php
@@ -1,6 +1,7 @@
 <?php
 
 class LookupField extends DropdownField {
+
 	/**
 	 * Returns a readonly span containing the correct value.
 	 */
@@ -11,13 +12,24 @@ class LookupField extends DropdownField {
 			else $mappedValue = $this->source->getItem($this->value);
 		}
 		
-		if(!isset($mappedValue)) $mappedValue = "<i>(none)</i>";
-		
-		if($this->value) $val = $this->dontEscape ? ($this->reserveNL?Convert::raw2xml($this->value):$this->value) : Convert::raw2xml($this->value);
-		else $val = '<i>(none)</i>';
-		$valforInput = $this->value ? Convert::raw2att($val) : "";
-		
-		return "<span class=\"readonly\" id=\"" . $this->id() . "\">$mappedValue</span><input type=\"hidden\" name=\"".$this->name."\" value=\"".$valforInput."\" />";
+		if(!$mappedValue)
+			$mappedValue = "<i>(none)</i>";
+
+		if($this->value) {
+			$val = $this->dontEscape
+				? ($this->reserveNL?Convert::raw2xml($this->value):$this->value)
+				: Convert::raw2xml($this->value);
+		} else {
+			$val = '<i>(none)</i>';
+		}
+
+		$valforInput = $this->value
+			? Convert::raw2att($val)
+			: "";
+
+		return "<span class=\"readonly\" id=\"" . $this->id() .
+			"\">$mappedValue</span><input type=\"hidden\" name=\"" . $this->name .
+			"\" value=\"" . $valforInput . "\" />";
 	}
 	function performReadonlyTransformation() {
 		return $this;
