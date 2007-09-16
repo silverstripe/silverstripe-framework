f5e24e3512625749b74e9a108b1035c1f2a00563
Ingo Schommer <ingo@silverstripe.com>
2007-09-16 01:55:04 +0000

    mlanthaler: Bugfix: Initialized variable to prevent E_NOTICE error. (merged from branches/gsoc)
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/sapphire/trunk@42026 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- forms/CheckboxFieldDisabled.php
+++ forms/CheckboxFieldDisabled.php
@@ -1,5 +1,6 @@
 <?php
 
+
 /**
  * Single checkbox field.
  */
@@ -8,9 +9,13 @@ class CheckboxFieldDisabled extends CheckboxField {
 	 * Returns a single checkbox field - used by templates.
 	 */
 	function Field() {
-		if($this->value) $checked = "checked = \"checked\"";
-		return "<input class=\"checkbox\" disabled=\"disabled\" type=\"checkbox\" id=\"" . $this->id() . "\" name=\"{$this->name}\" $checked />";
+		$checked = '';
+		if($this->value)
+			$checked = " checked = \"checked\"";
+		return "<input class=\"checkbox\" disabled=\"disabled\" type=\"checkbox\" id=\"" .
+			$this->id() . "\" name=\"{$this->name}\"$checked />";
 	}
 }
+
 
 ?>
