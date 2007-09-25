805bcb4bb52692108d39ee33571446d56e8d87a6
Romain Louis <romalouis@gmail.com>
2007-09-25 05:02:16 +0000

    DataObject merging error solving
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/sapphire/branches/2.1.0@42559 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- security/Member.php
+++ security/Member.php
@@ -23,7 +23,9 @@ class Member extends DataObject {
 	static $has_many = array(
 		'UnsubscribedRecords' => 'Member_UnsubscribeRecord'
 	);
-  
+  	static $many_many = array();
+  	static $belongs_many_many = array();
+  	static $many_many_extraFields = array();
 	static $default_sort = "Surname, FirstName";
 	
 	static $indexes = array(
