04fea245d8254c7dec8e63bb5288af39ef276f6d
Romain Louis <romalouis@gmail.com>
2007-11-04 23:01:55 +0000

    PopupDateTimeField adding
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/sapphire/trunk@44226 467b73ca-7a2a-4603-9d3b-597d59a354a9

---
+++ forms/PopupDateTimeField.php
@@ -0,0 +1,78 @@
+<?php
+
+class PopupDateTimeField extends CalendarDateField {
+	
+	function Field() {
+		
+		Requirements::css( 'sapphire/css/PopupDateTimeField.css' );
+		
+		$field = parent::Field();
+		
+		DropdownTimeField::Requirements();
+		
+		$id = $this->id();
+		$val = $this->attrValue();
+		
+		$date = $this->attrValueDate();
+		$time = $this->attrValueTime();
+		
+		$futureClass = $this->futureOnly ? ' futureonly' : '';
+		
+		$innerHTMLDate = parent::HTMLField( $id . '_Date', $this->name . '[Date]', $date );
+		$innerHTMLTime = DropdownTimeField::HTMLField( $id . '_Time', $this->name . '[Time]', $time );
+				
+		return <<<HTML
+			<div class="popupdatetime">
+				<ul>
+					<li class="calendardate$futureClass">$innerHTMLDate</li>
+					<li class="dropdowntime">$innerHTMLTime</li>
+				</ul>
+			</div>
+HTML;
+	}
+	
+	function attrValueDate() {
+		if( $this->value )
+			return date( 'd/m/Y', strtotime( $this->value ) );	
+		else
+			return '';
+	}
+	
+	function attrValueTime() {
+		if( $this->value )
+			return date( 'h:i a', strtotime( $this->value ) );	
+		else
+			return '';
+	}
+	
+	function setValue( $val ) {
+		if( is_array( $val ) ) {	
+			
+			// 1) Date
+			
+			if( $val[ 'Date' ] && preg_match( '/^([\d]{1,2})\/([\d]{1,2})\/([\d]{2,4})/', $val[ 'Date' ], $parts ) )
+				$date = "$parts[3]-$parts[2]-$parts[1]";
+			else
+				$date = null;
+			
+			// 2) Time
+			
+			$time = $val[ 'Time' ] ? date( 'H:i:s', strtotime( $val[ 'Time' ] ) ) : null;
+	
+			if( $date == null )
+				$this->value = $time;
+			else if( $time == null )
+				$this->value = $date;
+			else
+				$this->value = $date . ' ' . $time;
+		}
+		else
+			$this->value = $val;
+	}
+
+	function dataValue() {
+		return $this->value;
+	}
+}
+
+?>
