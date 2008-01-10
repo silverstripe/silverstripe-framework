9750028e8d694bc85399df6275167bf87e12678e
Normann Lou <normann@silverstripe.com>
2008-01-10 22:42:36 +0000

    fix wrong warning info: Director -> Debug
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/sapphire/trunk@47851 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- core/Debug.php
+++ core/Debug.php
@@ -234,7 +234,7 @@ class Debug {
 	 * @deprecated Use send_errors_to() instead.
 	 */
 	static function sendLiveErrorsTo($emailAddress) {
-		user_error('Debug::sendLiveErrorsTo() is deprecated. Use Director::send_errors_to() instead.', E_USER_NOTICE);
+		user_error('Debug::sendLiveErrorsTo() is deprecated. Use Debug::send_errors_to() instead.', E_USER_NOTICE);
 		if(!Director::isDev()) self::send_errors_to($emailAddress, true);
 	}
 	
