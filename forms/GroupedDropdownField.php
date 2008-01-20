e28361a3137e83604bbb62aa4532c0cecbc6d12e
Sean Harvey <sean@silverstripe.com>
2008-01-20 08:58:04 +0000

    Fixed formatting of code, and added some documentation on what the source for this field should be
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/sapphire/trunk@48319 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- forms/GroupedDropdownField.php
+++ forms/GroupedDropdownField.php
@@ -6,36 +6,48 @@
  */
 
 /**
- * Grouped dropdown, using <optgroup>s.
- * Returns a <select> tag containing all the appropriate <option> tags, with <optgroup> tags around the <option> tags as required
+ * Grouped dropdown, using <optgroup> tags.
+ * 
+ * $source parameter (from DropdownField) must be a two dimensional array.
+ * The first level of the array is used for the <optgroup>, and the second
+ * level are the <options> for each group.
+ * 
+ * Returns a <select> tag containing all the appropriate <option> tags, with
+ * <optgroup> tags around the <option> tags as required.
+ * 
  * @package forms
  * @subpackage fields-basic
  */
 class GroupedDropdownField extends DropdownField {
-  function Field() {
+
+	function Field() {
 		// Initialisations
 		$options = '';
 		$classAttr = '';
-    if($extraClass = trim($this->extraClass())) {
+
+		if($extraClass = trim($this->extraClass())) {
 			$classAttr = "class=\"$extraClass\"";
 		}
-		foreach($this->source as $value => $title) {
-			if(is_array($title)) {
-        $options .= "<optgroup label=\"$value\">";
-        foreach($title as $value2 => $title2) {
-          $selected = $value2 == $this->value ? " selected=\"selected\"" : ""; 
-			    $options .= "<option$selected value=\"$value2\">$title2</option>";
-        }
-        $options .= "</optgroup>";
-      } else { // Fall back to the standard dropdown field
-        $selected = $value == $this->value ? " selected=\"selected\"" : ""; 
-			  $options .= "<option$selected value=\"$value\">$title</option>";
-      }
+		if($this->source) {
+			foreach($this->source as $value => $title) {
+				if(is_array($title)) {
+					$options .= "<optgroup label=\"$value\">";
+					foreach($title as $value2 => $title2) {
+						$selected = $value2 == $this->value ? " selected=\"selected\"" : "";
+						$options .= "<option$selected value=\"$value2\">$title2</option>";
+					}
+					$options .= "</optgroup>";
+				} else { // Fall back to the standard dropdown field
+					$selected = $value == $this->value ? " selected=\"selected\"" : "";
+					$options .= "<option$selected value=\"$value\">$title</option>";
+				}
+			}
 		}
 
 		$id = $this->id();
 
 		return "<select $classAttr name=\"$this->name\" id=\"$id\">$options</select>";
-  }
+	}
 }
+
 ?>
