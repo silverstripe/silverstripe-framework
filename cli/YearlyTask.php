03cabb839d8af979a82e0c0bb0c2bf173d719b16
Normann Lou <normann@silverstripe.com>
2008-01-31 03:46:21 +0000

    For some project we need server run some scheduled task yearly, such as upgrade high school students for GSO.
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/sapphire/trunk@48899 467b73ca-7a2a-4603-9d3b-597d59a354a9

---
+++ cli/YearlyTask.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @package sapphire
+ * @subpackage cron
+ */
+
+/**
+ * Classes that must be run yearly extend this class
+ * @package sapphire
+ * @subpackage cron
+ */
+class YearlyTask extends ScheduledTask {
+
+}
+?>
