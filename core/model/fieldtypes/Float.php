ddd3ef3c3412a28d803da0e5a2413731181a3c1f
Ingo Schommer <ingo@silverstripe.com>
2008-04-04 22:58:58 +0000

    Merged revisions 46596 via svnmerge from svn://svn.silverstripe.com/silverstripe/modules/sapphire/branches/2.2.0-mesq
    ........
      r46596 | ischommer | 2007-12-11 15:28:11 +1300 (Tue, 11 Dec 2007) | 1 line
      
      reverted precision extension (non-standard SQL for MySQL)
    ........
    
    
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/sapphire/trunk@52153 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- core/model/fieldtypes/Float.php
+++ core/model/fieldtypes/Float.php
@@ -12,36 +12,8 @@
  */
 class Float extends DBField {
 	
-	/**
-	 * Allows for setting the digits before decimal point
-	 * e.g. a value of 7 can be -99999.99
-	 *
-	 * @var int
-	 */
-	protected $countTotalDigits;
-	
-	/**
-	 * Allows for setting the digits before decimal point
-	 * e.g. a value of 4 can be -999.9999
-	 *
-	 * @var int
-	 */
-	protected $countDigitsAfterDecimal;
-	
-	function __construct($name, $countTotalDigits, $countDigitsAfterDecimal) {
-		$this->countTotalDigits = $countTotalDigits;
-		$this->countDigitsAfterDecimal = $countDigitsAfterDecimal;
-		
-		parent::__construct($name);
-	}
-	
 	function requireField() {
-		if($this->countTotalDigits && $this->countDigitsAfterDecimal) {
-			$sql = "float({$this->countTotalDigits},{$this->countDigitsAfterDecimal})"; 
-		} else {
-			$sql = "float";
-		}
-		DB::requireField($this->tableName, $this->name, $sql);
+		DB::requireField($this->tableName, $this->name, "float");
 	}
 	
 	function Nice() {
