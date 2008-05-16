bd93c623fccbd4b63e0e82a7097dfe271e89bf9c
Sam Minnee <sam@silverstripe.com>
2008-05-16 04:41:34 +0000

    Reverted build-breaking change
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/sapphire/trunk@54674 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- tests/i18nTest.php
+++ tests/i18nTest.php
@@ -2,7 +2,6 @@
 
 class i18nTest extends SapphireTest {
 	function testGetExistingTranslations() {
-		$this->assertTrue(false);
 		$translations = i18n::get_existing_translations();
 		$this->assertTrue(isset($translations['en_US']), 'Checking for en_US translation');
 		$this->assertTrue(isset($translations['de_DE']), 'Checking for de_DE translation');
