4619d7d466316123ede31a1883f8f851f772819c
Mark Rickerby <me@maetl.net>
2008-06-06 05:06:53 +0000

    removed redundant reporter class
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/sapphire/trunk@55814 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- dev/Debug.php
+++ dev/Debug.php
@@ -452,33 +452,4 @@ function errorHandler($errno, $errstr, $errfile, $errline, $errcontext) {
 	}
 }
 
-/**
- * Interface for rendering a debug info report.
- * 
- */
-class DebugReporter {
-	
-	function writeHeader() {
-		echo '<!DOCTYPE html><html><head><title>'. $_SERVER['REQUEST_METHOD'] . ' ' .$_SERVER['REQUEST_URI'] .'</title>';
-		echo '<style type="text/css">';
-		echo 'body { background-color:#eee; margin:0; padding:0; font-family:Helvetica,Arial,sans-serif; }';
-		echo '.info { border-bottom:1px dotted #333; background-color:#ccdef3; margin:0; padding:6px 12px; }';
-		echo '.info h1 { margin:0; padding:0; color:#333; letter-spacing:-2px; }';
-		echo '.header { margin:0; border-bottom:6px solid #ccdef3; height:23px; background-color:#666673; padding:4px 0 2px 6px; background-image:url('.Director::absoluteBaseURL().'cms/images/mainmenu/top-bg.gif); }';
-		echo '.trace { padding:6px 12px; }';
-		echo '.trace li { font-size:14px; margin:6px 0; }';
-		echo 'pre { margin-left:18px; }';
-		echo 'pre span { color:#999;}';
-		echo 'pre .error { color:#f00; }';
-		echo '</style></head>';
-		echo '<body>';
-		echo '<div class="header"><img src="'. Director::absoluteBaseURL() .'cms/images/mainmenu/logo.gif" width="26" height="23"></div>';
-	}
-	
-	function writeFooter() {
-		echo "</body></html>";		
-	}
-	
-}
-
 ?>
