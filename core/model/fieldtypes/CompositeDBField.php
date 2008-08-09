a0f61006d011d58981b25fd88f3af49c15b5490b
Ingo Schommer <ingo@silverstripe.com>
2008-08-09 05:22:38 +0000

    FEATURE Merged in CompositeDBField
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/sapphire/trunk@60220 467b73ca-7a2a-4603-9d3b-597d59a354a9

---
+++ core/model/fieldtypes/CompositeDBField.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * Apply this interface to any {@link DBField} that doesn't have a 1-1 mapping with a database field.
+ * This includes multi-value fields and transformed fields
+ */
+interface CompositeDBField {
+	
+}
