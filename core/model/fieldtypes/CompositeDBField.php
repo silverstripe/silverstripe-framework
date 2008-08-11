3d28c5a23c73bc36c42495c57cda0a3fbf93f132
Ingo Schommer <ingo@silverstripe.com>
2008-08-11 04:13:06 +0000

    MINOR Documentation
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/sapphire/trunk@60371 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- core/model/fieldtypes/CompositeDBField.php
+++ core/model/fieldtypes/CompositeDBField.php
@@ -1,8 +1,12 @@
 <?php
-
 /**
  * Apply this interface to any {@link DBField} that doesn't have a 1-1 mapping with a database field.
  * This includes multi-value fields and transformed fields
+ *
+ * @todo Unittests for loading and saving composite values (see GIS module for existing similiar unittests)
+ *
+ * @package sapphire
+ * @subpackage model
  */
 interface CompositeDBField {
 	
