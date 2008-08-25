8dff62d3cb2b705ddffad38c34ce5ba8e577ec4a
Sean Harvey <sean@silverstripe.com>
2008-08-25 08:58:38 +0000

    API CHANGE Deprecated GroupedDropdownField, DropdownField should now be used instead ENHANCEMENT DropdownField now allows for <optgroup> elements in the field source by passing in a two dimensional array - this was taken from GroupedDropdownField
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/sapphire/trunk@61485 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- forms/GroupedDropdownField.php
+++ forms/GroupedDropdownField.php
@@ -1,6 +1,9 @@
 <?php
+
 /**
  * Grouped dropdown, using <optgroup> tags.
+ * 
+ * @deprecated - Please use DropdownField instead!
  * 
  * $source parameter (from DropdownField) must be a two dimensional array.
  * The first level of the array is used for the <optgroup>, and the second
@@ -15,32 +18,9 @@
 class GroupedDropdownField extends DropdownField {
 
 	function Field() {
-		// Initialisations
-		$options = '';
-		$classAttr = '';
+		user_error('GroupedDropdownField is deprecated. Please use DropdownField instead.', E_USER_NOTICE);
 
-		if($extraClass = trim($this->extraClass())) {
-			$classAttr = "class=\"$extraClass\"";
-		}
-		if($this->source) {
-			foreach($this->source as $value => $title) {
-				if(is_array($title)) {
-					$options .= "<optgroup label=\"$value\">";
-					foreach($title as $value2 => $title2) {
-						$selected = $value2 == $this->value ? " selected=\"selected\"" : "";
-						$options .= "<option$selected value=\"$value2\">$title2</option>";
-					}
-					$options .= "</optgroup>";
-				} else { // Fall back to the standard dropdown field
-					$selected = $value == $this->value ? " selected=\"selected\"" : "";
-					$options .= "<option$selected value=\"$value\">$title</option>";
-				}
-			}
-		}
-
-		$id = $this->id();
-
-		return "<select $classAttr name=\"$this->name\" id=\"$id\">$options</select>";
+		return parent::Field();
 	}
 }
 
