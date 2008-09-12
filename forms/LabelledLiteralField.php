07f40d2cc68644715b4a33148d4f5e424fe2bb83
Sean Harvey <sean@silverstripe.com>
2008-09-12 03:48:13 +0000

    API CHANGE Deprecated LabelledLiteralField by adding a @deprecated note with alternate approaches, and added a PHP notice for people currently using it
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/sapphire/trunk@62316 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- forms/LabelledLiteralField.php
+++ forms/LabelledLiteralField.php
@@ -1,13 +1,20 @@
 <?php
 /**
  * It's a LiteralField ... with a Label
+ * 
  * @package forms
  * @subpackage fields-dataless
+ * 
+ * @deprecated If you need to have a label for your literal field, just put the
+ * HTML into a LiteralField, or use a custom form template to separate your
+ * presentation/content from the data fields.
+ * {@see http://doc.silverstripe.com/doku.php?id=form#using_a_custom_template}
  */
 class LabelledLiteralField extends LiteralField {
 	
 	function __construct( $name, $title, $content ) {
-		parent::__construct( $name, $content );
+		parent::__construct($name, $content);
+		user_error('LabelledLiteralField is deprecated. Please see the @deprecated note in LabelledLiteralField.php', E_USER_NOTICE);
 		$this->setTitle( $title );
 	}
 	
