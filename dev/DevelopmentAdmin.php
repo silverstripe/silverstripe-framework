f47d5481eee43083a7a76ba5973baacdf108766e
Sam Minnee <sam@silverstripe.com>
2008-09-25 00:36:20 +0000

    ENHANCEMENT: Better CLI output of 'sake dev'
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/sapphire/trunk@63025 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- dev/DevelopmentAdmin.php
+++ dev/DevelopmentAdmin.php
@@ -17,21 +17,36 @@ class DevelopmentAdmin extends Controller {
 	);
 	
 	function index() {
-		$renderer = new DebugView();
-		$renderer->writeHeader();
-		$renderer->writeInfo("Sapphire Development Tools", Director::absoluteBaseURL());
-		$base = Director::baseURL();
-		echo <<<HTML
-			<div class="options">
-			<ul>
-				<li style="margin-bottom: 1em"><a href="{$base}dev/build"><b>/dev/build:</b> Build/rebuild this environment (formerly db/build).  Call this whenever you have updated your project sources</a></li>
-				<li><a href="{$base}dev/tests"><b>/dev/tests:</b> See a list of unit tests to run</a></li>
-				<li><a href="{$base}dev/tasks"><b>/dev/tasks:</b> See a list of build tasks to run</a></li>
-				<li><a href="{$base}dev/viewcode"><b>/dev/viewcode:</b> Read source code in a literate programming style</a></li>
-			</ul>
-			</div>
-HTML;
-		$renderer->writeFooter();
+		$actions = array(
+			"build" => "Build/rebuild this environment (formerly db/build).  Call this whenever you have updated your project sources",
+			"tests" => "See a list of unit tests to run",
+			"tasks" => "See a list of build tasks to run",
+			"viewcode" => "Read source code in a literate programming style",
+		);
+		
+		// Web mode
+		if(!Director::is_cli()) {
+			$renderer = new DebugView();
+			$renderer->writeHeader();
+			$renderer->writeInfo("Sapphire Development Tools", Director::absoluteBaseURL());
+			$base = Director::baseURL();
+
+			echo '<div class="options"><ul>';
+			foreach($actions as $action => $description) {
+				echo "<li><a href=\"{$base}dev/$action\"><b>/dev/$action:</b> $description</a></li>\n";
+			}
+
+			$renderer->writeFooter();
+		
+		// CLI mode
+		} else {
+			echo "SAPPHIRE DEVELOPMENT TOOLS\n--------------------------\n\n";
+			echo "You can execute any of the following commands:\n\n";
+			foreach($actions as $action => $description) {
+				echo "  sake dev/$action: $description\n";
+			}
+			echo "\n\n";
+		}
 	}
 	
 	function tests($request) {
