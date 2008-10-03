fbd37c897efe369d3ac2e9bbf57e4dcc555d956c
Ingo Schommer <ingo@silverstripe.com>
2008-10-03 01:14:45 +0000

    BUGFIX typo in js file
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/sapphire/trunk@63570 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- javascript/lang/de_DE.js
+++ javascript/lang/de_DE.js
@@ -2,6 +2,6 @@ if(typeof(ss) == 'undefined' || typeof(ss.i18n) == 'undefined') {
 	console.error('Class ss.i18n not defined');
 } else {
 	ss.i18n.addDictionary('de_DE', {
-		'VALIDATOR.FIELDREQUIRED': '"%s" wird benötigt',
+		'VALIDATOR.FIELDREQUIRED': '"%s" wird benötigt'
 	});
 }
