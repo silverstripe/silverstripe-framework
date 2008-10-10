silverstripe/silverstripe-framework

commit a32b42fe1979824fdf204c19eaaef149d6649bb6

BUGFIX: fix the bug that add some rules for summary columns even when they are not there.
git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/sapphire/trunk@64049 467b73ca-7a2a-4603-9d3b-597d59a354a9

javascript/TableListField.js
@@ -41,7 +41,10 @@ TableListField.prototype = {
 		// TODO Breaks with nested divs
 		var summaryCols = $$('tfoot tr.summary td', this);
 		this._summaryDefs = [];
-		if(summaryCols) {
+		
+		//if(summaryCols) {
+		//should check summaryCols.length, cos summaryCols will be alway an array, though its length could be 0.
+		if(summaryCols.length) {
 			rules['#'+this.id+' table.data tbody input'] = {
 				onchange: function(e) {
 					if (!e) e = window.event; // stupid IE
@@ -63,7 +66,10 @@ TableListField.prototype = {
 		}
 		
 		Behaviour.register('TableListField_'+this.id,rules);
-		if(summaryCols) {
+		
+		//if(summaryCols) {
+		//should check summaryCols.length, cos summaryCols will be alway an array, though its length could be 0.
+		if(summaryCols.length) {
 			//this._getSummaryDefs(summaryCols);
 		}
 	},

commit b562fcc934fc14625c0cb6b85af9b6a804386fd9

BUGFIX Fixed AssetTableField javascript errors caused by r64049
git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/sapphire/trunk@64074 467b73ca-7a2a-4603-9d3b-597d59a354a9

javascript/TableListField.js
@@ -42,9 +42,8 @@ TableListField.prototype = {
 		var summaryCols = $$('tfoot tr.summary td', this);
 		this._summaryDefs = [];
 		
-		//if(summaryCols) {
-		//should check summaryCols.length, cos summaryCols will be alway an array, though its length could be 0.
-		if(summaryCols.length) {
+		//should check summaryCols.length, because summaryCols will always be an array, though its length could be 0.
+		if(summaryCols && summaryCols.length) {
 			rules['#'+this.id+' table.data tbody input'] = {
 				onchange: function(e) {
 					if (!e) e = window.event; // stupid IE
@@ -67,11 +66,11 @@ TableListField.prototype = {
 		
 		Behaviour.register('TableListField_'+this.id,rules);
 		
-		//if(summaryCols) {
-		//should check summaryCols.length, cos summaryCols will be alway an array, though its length could be 0.
+		/*
 		if(summaryCols.length) {
-			//this._getSummaryDefs(summaryCols);
+			this._getSummaryDefs(summaryCols);
 		}
+		*/
 	},
 	
 	/**