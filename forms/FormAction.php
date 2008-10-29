silverstripe/silverstripe-framework

commit 3e13bd8307ae9037c2ff0b43999c5318d9058d2d

ENHANCEMENT Made use of createTag() on FormAction->Field()
git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/sapphire/trunk@64410 467b73ca-7a2a-4603-9d3b-597d59a354a9

forms/FormAction.php
@@ -55,14 +55,35 @@ class FormAction extends FormField {
 		return $this->extraData;
 	}
 	
+	/**
+	 * Create a submit input, or button tag
+	 * using {@link Form->createTag()} functionality.
+	 * 
+	 * @return HTML code for the input OR button element
+	 */
 	function Field() {
-		$titleAttr = $this->description ? "title=\"" . Convert::raw2att($this->description) . "\"" : '';
 		if($this->useButtonTag) {
-			return "<button class=\"action " . $this->extraClass() . "\" id=\"" . $this->id() . "\" type=\"submit\" name=\"$this->action\" $titleAttr />" . $this->attrTitle() . "</button>\n";
+			$attributes = array(
+				'class' => 'action' . ($this->extraClass() ? (' ' . $this->extraClass()) : ''),
+				'id' => $this->id(),
+				'type' => 'submit',
+				'name' => $this->action
+			);
+			
+			return $this->createTag('button', $attributes, $this->attrTitle());
 		} else {
-			return "<input class=\"action " . $this->extraClass() . "\" id=\"" . $this->id() . "\" type=\"submit\" name=\"$this->action\" value=\"" . $this->attrTitle() . "\" $titleAttr />\n";
+			$attributes = array(
+				'class' => 'action' . ($this->extraClass() ? (' ' . $this->extraClass()) : ''),
+				'id' => $this->id(),
+				'type' => 'submit',
+				'name' => $this->action,
+				'value' => $this->attrTitle()
+			);
+
+			if($this->description) $attributes['title'] = Convert::raw2att($this->description);
+			
+			return $this->createTag('input', $attributes);
 		}
-		
 	}
 	
 	/**

commit 41075078fde0fd0a89c689665e2ccde47c056911

BUGFIX Fixed title-handling in FormAction, regression from r64410
git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/sapphire/trunk@64879 467b73ca-7a2a-4603-9d3b-597d59a354a9

forms/FormAction.php
@@ -80,7 +80,7 @@ class FormAction extends FormField {
 				'value' => $this->attrTitle()
 			);
 
-			if($this->description) $attributes['title'] = $this->description;
+			$attributes['title'] = ($this->description) ? $this->description : $this->attrTitle();
 			
 			return $this->createTag('input', $attributes);
 		}