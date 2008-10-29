f5e636ac43132ee96278dcfd7088b8b897b1bf79
Sam Minnee <sam@silverstripe.com>
2008-10-29 02:39:09 +0000

    Changed default # of rows on HTMLEditorField from 15 to 30
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/sapphire/trunk@64863 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- forms/HtmlEditorField.php
+++ forms/HtmlEditorField.php
@@ -15,7 +15,7 @@ class HtmlEditorField extends TextareaField {
 	/**
 	 * Construct a new HtmlEditor field
 	 */
-	function __construct($name, $title = "", $rows = 15, $cols = 20, $value = "", $form = null) {
+	function __construct($name, $title = "", $rows = 30, $cols = 20, $value = "", $form = null) {
 		parent::__construct($name, $title, $rows, $cols, $value, $form);
 		$this->extraClass = 'typography';
 	}
