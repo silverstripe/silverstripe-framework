2381e480c30ae90550cbda26dd39940dc382767c
Ingo Schommer <ingo@silverstripe.com>
2008-12-03 22:27:23 +0000

    ENHANCEMENT Allowing custom methods in DataObjectSet->column()
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/sapphire/branches/2.3@67345 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- core/model/DataObjectSet.php
+++ core/model/DataObjectSet.php
@@ -581,7 +581,7 @@ class DataObjectSet extends ViewableData implements IteratorAggregate {
 	public function column($value = "ID") {
 		$list = array();
 		foreach($this->items as $item ){
-			$list[] = $item->$value;
+			$list[] = ($item->hasMethod($value)) ? $item->$value() : $item->$value;
 		}
 		return $list;
 	}
