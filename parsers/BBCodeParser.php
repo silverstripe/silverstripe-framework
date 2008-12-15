9b85835604eb42ccb02f47e37a3926597f3288d0
Will Rossiter <will@silverstripe.com>
2008-12-15 05:07:38 +0000

    MINOR: reorganized layout of bbcode list elements to move longer ones to bottom row for BBCode popouts
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/sapphire/branches/2.3@68973 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- parsers/BBCodeParser.php
+++ parsers/BBCodeParser.php
@@ -51,8 +51,32 @@ class BBCodeParser extends TextParser {
 			new ArrayData(array(
 				"Title" => _t('BBCodeParser.ALIGNEMENT', 'Alignment'),
 				"Example" => '[align=right]'._t('BBCodeParser.ALIGNEMENTEXAMPLE', 'right aligned').'[/align]'
+			)),	
+			new ArrayData(array(
+				"Title" => _t('BBCodeParser.CODE', 'Code Block'),
+				"Description" => _t('BBCodeParser.CODEDESCRIPTION', 'Unformatted code block'),
+				"Example" => '[code]'._t('BBCodeParser.CODEEXAMPLE', 'Code block').'[/code]'
 			)),
-						
+			new ArrayData(array(
+				"Title" => _t('BBCodeParser.EMAILLINK', 'Email link'),
+				"Description" => _t('BBCodeParser.EMAILLINKDESCRIPTION', 'Create link to an email address'),
+				"Example" => "[email]you@yoursite.com[/email]"
+			)),
+			new ArrayData(array(
+				"Title" => _t('BBCodeParser.EMAILLINK', 'Email link'),
+				"Description" => _t('BBCodeParser.EMAILLINKDESCRIPTION', 'Create link to an email address'),
+				"Example" => "[email=you@yoursite.com]Email[/email]"
+			)),
+			new ArrayData(array(
+				"Title" => _t('BBCodeParser.UNORDERED', 'Unordered list'),
+				"Description" => _t('BBCodeParser.UNORDEREDDESCRIPTION', 'Unordered list'),
+				"Example" => '[ulist][*]'._t('BBCodeParser.UNORDEREDEXAMPLE1', 'unordered item 1').'[/ulist]'
+			)),			
+			new ArrayData(array(
+				"Title" => _t('BBCodeParser.IMAGE', 'Image'),
+				"Description" => _t('BBCodeParser.IMAGEDESCRIPTION', 'Show an image in your post'),
+				"Example" => "[img]http://www.website.com/image.jpg[/img]"
+			)),
 			new ArrayData(array(
 				"Title" => _t('BBCodeParser.LINK', 'Website link'),
 				"Description" => _t('BBCodeParser.LINKDESCRIPTION', 'Link to another website or URL'),
@@ -61,37 +85,8 @@ class BBCodeParser extends TextParser {
 			new ArrayData(array(
 				"Title" => _t('BBCodeParser.LINK', 'Website link'),
 				"Description" => _t('BBCodeParser.LINKDESCRIPTION', 'Link to another website or URL'),
-				"Example" => "[url=http://www.website.com/]Some website[/url]"
-			)),			
-			new ArrayData(array(
-				"Title" => _t('BBCodeParser.EMAILLINK', 'Email link'),
-				"Description" => _t('BBCodeParser.EMAILLINKDESCRIPTION', 'Create link to an email address'),
-				"Example" => "[email]you@yoursite.com[/email]"
-			)),
-				new ArrayData(array(
-				"Title" => _t('BBCodeParser.EMAILLINK', 'Email link'),
-				"Description" => _t('BBCodeParser.EMAILLINKDESCRIPTION', 'Create link to an email address'),
-				"Example" => "[email=you@yoursite.com]email me[/email]"
-			)),		
-
-			new ArrayData(array(
-				"Title" => _t('BBCodeParser.IMAGE', 'Image'),
-				"Description" => _t('BBCodeParser.IMAGEDESCRIPTION', 'Show an image in your post'),
-				"Example" => "[img]http://www.website.com/image.jpg[/img]"
-			)),
-			
-			new ArrayData(array(
-				"Title" => _t('BBCodeParser.CODE', 'Code Block'),
-				"Description" => _t('BBCodeParser.CODEDESCRIPTION', 'Unformatted code block'),
-				"Example" => '[code]'._t('BBCodeParser.CODEEXAMPLE', 'Code block').'[/code]'
-			)),
-			new ArrayData(array(
-				"Title" => _t('BBCodeParser.UNORDERED', 'Unordered list'),
-				"Description" => _t('BBCodeParser.UNORDEREDDESCRIPTION', 'Unordered list'),
-				"Example" => '[ulist][*]'._t('BBCodeParser.UNORDEREDEXAMPLE1', 'unordered item 1').'[*]'._t('BBCodeParser.UNORDEREDEXAMPLE2', 'unordered item 2').'[/ulist]'
-			))						
-			
-					
+				"Example" => "[url=http://www.website.com/]Website[/url]"
+			))		
 		);
 	}
 	
