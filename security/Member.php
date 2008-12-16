eebab1df715d74e6ee1a0c43db96f95be18857c8
Ingo Schommer <ingo@silverstripe.com>
2008-12-16 07:00:40 +0000

    BUGFIX Making Password formfield in Member->getCMSFields() translatable ENHANCEMENT Hiding Member->BlacklistedEmail field in getCMSFields() by default - only relevant in newsletter/bounce context at the moment
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/sapphire/branches/2.3@69204 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- security/Member.php
+++ security/Member.php
@@ -821,12 +821,13 @@ class Member extends DataObject {
 		
 		$password = new ConfirmedPasswordField(
 			'Password', 
-			'Password', 
+			null, 
 			null, 
 			null, 
 			true // showOnClick
 		);
 		$password->setCanBeEmpty(true);
+		if(!$this->ID) $password->showOnClick = false;
 		$mainFields->replaceField('Password', $password);
 		
 		$mainFields->insertBefore(
@@ -862,6 +863,7 @@ class Member extends DataObject {
 		$mainFields->removeByName('Salt');
 		$mainFields->removeByName('NumVisit');
 		$mainFields->removeByName('LastVisited');
+		$mainFields->removeByName('BlacklistedEmail');
 	
 		$fields->removeByName('Subscriptions');
 		$fields->removeByName('UnsubscribedRecords');
