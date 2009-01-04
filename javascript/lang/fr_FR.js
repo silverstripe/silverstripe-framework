7a6cca4d35747a75a9f5bdcfcae475d1e1cc9510
Ingo Schommer <ingo@silverstripe.com>
2009-01-04 21:56:19 +0000

    MINOR Added french translation (see #3290)
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/sapphire/branches/2.3@69592 467b73ca-7a2a-4603-9d3b-597d59a354a9

---
+++ javascript/lang/fr_FR.js
@@ -0,0 +1,15 @@
+if(typeof(ss) == 'undefined' || typeof(ss.i18n) == 'undefined') {
+	console.error('Class ss.i18n not defined');
+} else {
+	ss.i18n.addDictionary('fr_FR', {
+		'VALIDATOR.FIELDREQUIRED': 'Veuillez remplir "%s", c\'est un champ requis.',
+		'HASMANYFILEFIELD.UPLOADING': 'Uploading... %s',
+		'TABLEFIELD.DELETECONFIRMMESSAGE': 'Etes-vous sûr de vouloir supprimer cet enregistrement ?',
+		'LOADING': 'chargement...',
+		'UNIQUEFIELD.SUGGESTED': "Changez la valeur de '%s' : %s",
+		'UNIQUEFIELD.ENTERNEWVALUE': 'You devez saisir une nouvelle valeur pou ce champ',
+		'UNIQUEFIELD.CANNOTLEAVEEMPTY': 'Ce champ ne peut être laissé vide',
+		'RESTRICTEDTEXTFIELD.CHARCANTBEUSED': "Le character '%s' ne peut être utilisé dans ce champ",
+		'UPDATEURL.CONFIRM': 'Voulez-vous que je change l\'URL en:\n\n%s/\n\nCliquez Ok pour changer l\'URL, cliquez Annuler pour la laisser à:\n\n%s'
+	});
+}
