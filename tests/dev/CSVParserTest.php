b87a0ae1075f5d989628f7ff5cc197d0c58ec8d3
Sean Harvey <sean@silverstripe.com>
2009-02-02 05:50:04 +0000

    MINOR Fixed CSVParserTest with encoding issues
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/sapphire/branches/2.3@71194 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- tests/dev/CSVParserTest.php
+++ tests/dev/CSVParserTest.php
@@ -5,22 +5,25 @@ class CSVParserTest extends SapphireTest {
 		/* By default, a CSV file will be interpreted as having headers */
 		$csv = new CSVParser('sapphire/tests/dev/CsvBulkLoaderTest_PlayersWithHeader.csv');
 		
-		$firstNames = $birthdays = $biographies = array();
+		$firstNames = $birthdays = $biographies = $registered = array();
 		foreach($csv as $record) {
 			/* Each row in the CSV file will be keyed with the header row */
-			$this->assertEquals(array('FirstName','Biography','Birthday'), array_keys($record));
+			$this->assertEquals(array('FirstName','Biography','Birthday','IsRegistered'), array_keys($record));
 			$firstNames[] = $record['FirstName'];
 			$biographies[] = $record['Biography'];
 			$birthdays[] = $record['Birthday'];
+			$registered[] = $record['IsRegistered'];
 		}
 		
 		$this->assertEquals(array('John','Jane','Jamie','Järg'), $firstNames);
+		
 		$this->assertEquals(array(
 			"He's a good guy",
 			"She is awesome.\nSo awesome that she gets multiple rows and \"escaped\" strings in her biography",
 			"Pretty old, with an escaped comma",
 			"Unicode FTW"), $biographies);
 		$this->assertEquals(array("31/01/1988","31/01/1982","31/01/1882","31/06/1982"), $birthdays);
+		$this->assertEquals(array('1', '0', '1', '1'), $registered);
 	}
 
 	function testParsingWithHeadersAndColumnMap() {
@@ -33,13 +36,14 @@ class CSVParserTest extends SapphireTest {
 			'bIoGrApHy' => '__BG',
 		));
 		
-		$firstNames = $birthdays = $biographies = array();
+		$firstNames = $birthdays = $biographies = $registered = array();
 		foreach($csv as $record) {
 			/* Each row in the CSV file will be keyed with the renamed columns.  Any unmapped column names will be left as-is. */
-			$this->assertEquals(array('__fn','__BG','Birthday'), array_keys($record));
+			$this->assertEquals(array('__fn','__BG','Birthday','IsRegistered'), array_keys($record));
 			$firstNames[] = $record['__fn'];
 			$biographies[] = $record['__BG'];
 			$birthdays[] = $record['Birthday'];
+			$registered[] = $record['IsRegistered'];
 		}
 		
 		$this->assertEquals(array('John','Jane','Jamie','Järg'), $firstNames);
@@ -49,21 +53,23 @@ class CSVParserTest extends SapphireTest {
 			"Pretty old, with an escaped comma",
 			"Unicode FTW"), $biographies);
 		$this->assertEquals(array("31/01/1988","31/01/1982","31/01/1882","31/06/1982"), $birthdays);
+		$this->assertEquals(array('1', '0', '1', '1'), $registered);
 	}
 
 	function testParsingWithExplicitHeaderRow() {
 		/* If your CSV file doesn't have a header row */
 		$csv = new CSVParser('sapphire/tests/dev/CsvBulkLoaderTest_PlayersWithHeader.csv');
 		
-		$csv->provideHeaderRow(array('__fn','__bio','__bd'));
+		$csv->provideHeaderRow(array('__fn','__bio','__bd','__reg'));
 		
-		$firstNames = $birthdays = $biographies = array();
+		$firstNames = $birthdays = $biographies = $registered = array();
 		foreach($csv as $record) {
 			/* Each row in the CSV file will be keyed with the header row that you gave */
-			$this->assertEquals(array('__fn','__bio','__bd'), array_keys($record));
+			$this->assertEquals(array('__fn','__bio','__bd','__reg'), array_keys($record));
 			$firstNames[] = $record['__fn'];
 			$biographies[] = $record['__bio'];
 			$birthdays[] = $record['__bd'];
+			$registered[] = $record['__reg'];
 		}
 		
 		/* And the first row will be returned in the data */		
@@ -75,6 +81,7 @@ class CSVParserTest extends SapphireTest {
 			"Pretty old, with an escaped comma",
 			"Unicode FTW"), $biographies);
 		$this->assertEquals(array("Birthday","31/01/1988","31/01/1982","31/01/1882","31/06/1982"), $birthdays);
+		$this->assertEquals(array('IsRegistered', '1', '0', '1', '1'), $registered);
 	}
 	
 }
