f8df717e48ef3463cd3b8db76331e9080eccb61b
Andrew O'Neil <andy@andyofniall.net>
2009-03-10 21:20:28 +0000

    FEATURE: #3614 - Allow spell checking in HTMLEditorField when used outside of the CMS
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/sapphire/branches/2.3@72791 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- javascript/HtmlEditorField.js
+++ javascript/HtmlEditorField.js
@@ -13,11 +13,11 @@ if((typeof tinyMCE != 'undefined')) {
 		theme_advanced_toolbar_location : "top",
 		theme_advanced_toolbar_align : "left",
 		theme_advanced_toolbar_parent : "right",
-		plugins : "blockquote,contextmenu,table,emotions,paste,../../tinymce_advcode",	
+		plugins : "blockquote,contextmenu,table,emotions,paste,../../tinymce_advcode,spellchecker",	
 		blockquote_clear_tag : "p",
 		table_inline_editing : true,
 		theme_advanced_buttons1 : "bold,italic,underline,strikethrough,separator,justifyleft,justifycenter,justifyright,justifyfull,formatselect,separator,bullist,numlist,outdent,indent,blockquote,hr,charmap",
-		theme_advanced_buttons2 : "undo,redo,separator,cut,copy,paste,pastetext,pasteword,separator,advcode,search,replace,selectall,visualaid,separator,tablecontrols",
+		theme_advanced_buttons2 : "undo,redo,separator,cut,copy,paste,pastetext,pasteword,spellchecker,separator,advcode,search,replace,selectall,visualaid,separator,tablecontrols",
 		theme_advanced_buttons3 : "",
 
 		safari_warning : false,
