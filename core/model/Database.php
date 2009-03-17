silverstripe/silverstripe-framework

commit ac06b8dd20be351ae27cb306c5e2aaf6d7dbe6e0

API CHANGE: index logic changed for MSSQL compatibility
git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/sapphire/trunk@73214 467b73ca-7a2a-4603-9d3b-597d59a354a9

core/model/Database.php
@@ -299,18 +299,20 @@ abstract class Database extends Object {
 			$this->indexList[$table] = $this->indexList($table);
 		}
 		
+		$index_alt=DB::getConn()->indexOrIndexAlt($index);
+				
 		//Fix up the index for database purposes
 		$index=DB::getConn()->getDbSqlDefinition($table, $index, null, true);
 		
 		if(!$newTable) {
-			if(is_array($this->indexList[$table][$index])) {
-				$array_spec = $this->indexList[$table][$index]['spec'];
+			if(is_array($this->indexList[$table][$index_alt])) {
+				$array_spec = $this->indexList[$table][$index_alt]['spec'];
 			} else {
-				$array_spec = $this->indexList[$table][$index];
+				$array_spec = $this->indexList[$table][$index_alt];
 			}
 		}
-			
-		if($newTable || !isset($this->indexList[$table][$index])) {
+		
+		if($newTable || !isset($this->indexList[$table][$index_alt])) {
 			$this->transCreateIndex($table, $index, $spec);
 			Database::alteration_message("Index $table.$index: created as $spec","created");
 		} else if($array_spec != DB::getConn()->convertIndexSpec($spec)) {

commit 68fc0bab0bbd0b9799b23012b746a0a33c434fe5

MINOR Temporarily disabled broken method reference from r73214
git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/sapphire/trunk@73229 467b73ca-7a2a-4603-9d3b-597d59a354a9

core/model/Database.php
@@ -299,7 +299,9 @@ abstract class Database extends Object {
 			$this->indexList[$table] = $this->indexList($table);
 		}
 		
-		$index_alt=DB::getConn()->indexOrIndexAlt($index);
+		//$index_alt=DB::getConn()->indexOrIndexAlt($index);
+		// @todo Geoff to fix his faulty commit from r73214
+		$index_alt = $index;
 				
 		//Fix up the index for database purposes
 		$index=DB::getConn()->getDbSqlDefinition($table, $index, null, true);