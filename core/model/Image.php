22a1bf8781ffb23846f5a5d0afd15e2eeedb50b1
Sean Harvey <sean@silverstripe.com>
2009-03-18 03:53:39 +0000

    BUGFIX Fixed too strict permission checking on Image::$allowed_actions. Replaced broken * permission check with explicit method names
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/sapphire/branches/2.3@73301 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- core/model/Image.php
+++ core/model/Image.php
@@ -478,7 +478,10 @@ class Image_Uploader extends Controller {
 	);
 	
 	static $allowed_actions = array(
-		'*' => 'CMS_ACCESS_CMSMain'
+		'iframe' => 'CMS_ACCESS_CMSMain',
+		'flush' => 'CMS_ACCESS_CMSMain',
+		'save' => 'CMS_ACCESS_CMSMain',
+		'delete' => 'CMS_ACCESS_CMSMain'
 	);
 	
 	function init() {
