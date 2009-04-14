d7aead3f71d33f285671f61df2b719d624eb4285
Will Rossiter <will@silverstripe.com>
2009-04-14 22:57:06 +0000

    MINOR: refactored dulicate code in ResetFormField, who knew we had a reset form field...
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/sapphire/branches/2.3@74462 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- forms/ResetFormAction.php
+++ forms/ResetFormAction.php
@@ -8,37 +8,25 @@
 class ResetFormAction extends FormAction {
 	
 	function Field() {
-		if($this->useButtonTag) {
-			$attributes = array(
-				'class' => 'action' . ($this->extraClass() ? $this->extraClass() : ''),
-				'id' => $this->id(),
-				'type' => 'reset',
-				'name' => $this->action
-			);
+		$attributes = array(
+			'class' => 'action' . ($this->extraClass() ? $this->extraClass() : ''),
+			'id' => $this->id(),
+			'type' => 'reset',
+			'name' => $this->action,
+		);
 			
-			if($this->isReadonly()) {
-				$attributes['disabled'] = 'disabled';
-				$attributes['class'] = $attributes['class'] . ' disabled';
-			}
-			
-			return $this->createTag('button', $attributes, $this->attrTitle());
-		} else {
-			$attributes = array(
-				'class' => 'action' . ($this->extraClass() ? $this->extraClass() : ''),
-				'id' => $this->id(),
-				'type' => 'reset',
-				'name' => $this->action,
-			);
-			
-			if($this->isReadonly()) {
-				$attributes['disabled'] = 'disabled';
-				$attributes['class'] = $attributes['class'] . ' disabled';
-			}
-			
-			$attributes['title'] = ($this->description) ? $this->description : ($this->dontEscape) ? $this->Title() : $this->attrTitle();
-			
-			return $this->createTag('input', $attributes);
+		if($this->isReadonly()) {
+			$attributes['disabled'] = 'disabled';
+			$attributes['class'] = $attributes['class'] . ' disabled';
 		}
+			
+		$attributes['title'] = ($this->description) ? $this->description : ($this->dontEscape) ? $this->Title() : $this->attrTitle();
+		
+		if($this->$useButtonTag) {
+			return $this->createTag('button', $attributes, $this->attrTitle());
+		}
+
+		return $this->createTag('input', $attributes);
 	}
 	
 }
