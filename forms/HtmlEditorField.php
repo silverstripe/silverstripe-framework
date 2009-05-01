928e937ffc0dfadc140533b2900103f34baf1388
Sean Harvey <sean@silverstripe.com>
2009-05-01 03:02:53 +0000

    Reverted r69824
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/sapphire/branches/2.3@75816 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- forms/HtmlEditorField.php
+++ forms/HtmlEditorField.php
@@ -430,7 +430,6 @@ class HtmlEditorField_Toolbar extends RequestHandler {
 				new ThumbnailStripField('Image', 'FolderID', 'getimages'),
 				new TextField('AltText', _t('HtmlEditorField.IMAGEALTTEXT', 'Alternative text (alt) - shown if image cannot be displayed'), '', 80),
 				new TextField('ImageTitle', _t('HtmlEditorField.IMAGETITLE', 'Title text (tooltip) - for additional information about the image')),
-				new CheckboxField('Caption', _t('HtmlEditorField.CAPTION', 'Include as Caption')),
 				new DropdownField(
 					'CSSClass',
 					_t('HtmlEditorField.CSSCLASS', 'Alignment / style'),
