8c9b7b282cb7fc2588958f8a1e05b72f14c97b64
Sam Minnee <sam@silverstripe.com>
2009-05-04 23:57:12 +0000

    BUGFIX: Fixed extraFilter argument for SiteTree::get_by_url() when translatable is enabled
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/sapphire/branches/2.3@76036 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- core/model/Translatable.php
+++ core/model/Translatable.php
@@ -723,12 +723,13 @@ class Translatable extends DataObjectDecorator {
 	 * @return DataObject
 	 */
 	function alternateGetByUrl($urlSegment, $extraFilter, $cache = null, $orderby = null) {
-		$SQL_URLSegment = Convert::raw2sql($urlSegment);
+		$filter = sprintf("`SiteTree`.`URLSegment` = '%s'", Convert::raw2sql($urlSegment));
+		if($extraFilter) $filter .= " AND $extraFilter";
 
 		self::$enable_lang_filter = false;
-		$record = DataObject::get_one('SiteTree', "`URLSegment` = '{$SQL_URLSegment}'");
+		$record = DataObject::get_one('SiteTree', $filter);
 		self::$enable_lang_filter = true;
-		
+
 		return $record;
 	}
 
