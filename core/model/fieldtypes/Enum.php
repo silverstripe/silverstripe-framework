b0a56e7d374c73e9019fae357905bd71e342137e
Geoff Munn <geoff@silverstripe.com>
2009-05-18 21:24:02 +0000

    Enum values now include table name
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/sapphire/trunk@77098 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- core/model/fieldtypes/Enum.php
+++ core/model/fieldtypes/Enum.php
@@ -42,7 +42,7 @@ class Enum extends DBField {
 	}
 	
 	function requireField(){
-		$parts=Array('datatype'=>'enum', 'enums'=>$this->enum, 'character set'=>'utf8', 'collate'=> 'utf8_general_ci', 'default'=>$this->default);
+		$parts=Array('datatype'=>'enum', 'enums'=>$this->enum, 'character set'=>'utf8', 'collate'=> 'utf8_general_ci', 'default'=>$this->default, 'table'=>$this->tableName);
 		$values=Array('type'=>'enum', 'parts'=>$parts);
 		DB::requireField($this->tableName, $this->name, $values);
 }
