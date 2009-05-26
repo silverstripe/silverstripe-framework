5448a29835ab0cfcbe6aea163771ff50d182afca
Will Rossiter <will@silverstripe.com>
2009-05-26 01:56:21 +0000

    FEATURE: added ability to display comments even when comments have been disabled. #2081
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/sapphire/trunk@77807 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- core/control/ContentController.php
+++ core/control/ContentController.php
@@ -297,7 +297,8 @@ HTML;
 	 * Returns a page comment system
 	 */
 	function PageComments() {
-		if($this->data() && $this->data()->ProvideComments) {
+		$hasComments = DB::query("SELECT COUNT(*) FROM `PageComment` WHERE `PageComment`.`ParentID` ='". Convert::raw2sql($this->ID)."'")->value();
+		if(($this->data() && $this->data()->ProvideComments) || ($hasComments > 0 && PageCommentInterface::$show_comments_when_disabled)) {
 			return new PageCommentInterface($this, 'PageComments', $this->data());
 		} else {
 			if(isset($_REQUEST['executeForm']) && $_REQUEST['executeForm'] == 'PageComments.PostCommentForm') {
