9c1ef3cd156f914b2cc1e80ff34754405b311f71
Sean Harvey <sean@silverstripe.com>
2009-06-15 05:02:00 +0000

    ENHANCEMENT Added caption field to HtmlEditorField for settting caption text (optional) on an image
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/sapphire/trunk@79206 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- forms/HtmlEditorField.php
+++ forms/HtmlEditorField.php
@@ -439,6 +439,7 @@ class HtmlEditorField_Toolbar extends RequestHandler {
 				new ThumbnailStripField('FolderImages', 'FolderID', 'getimages'),
 				new TextField('AltText', _t('HtmlEditorField.IMAGEALTTEXT', 'Alternative text (alt) - shown if image cannot be displayed'), '', 80),
 				new TextField('ImageTitle', _t('HtmlEditorField.IMAGETITLE', 'Title text (tooltip) - for additional information about the image')),
+				new TextField('CaptionText', _t('HtmlEditorField.CAPTIONTEXT', 'Caption text')),
 				new DropdownField(
 					'CSSClass',
 					_t('HtmlEditorField.CSSCLASS', 'Alignment / style'),
