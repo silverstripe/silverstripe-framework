a81f3879450747a3033eb1f65f7dafce53b27143
Tom Rix <tom@rixth.org>
2009-07-13 20:28:31 +0000

    MINOR smarter static-main.php, now detects automatically if you are using domain based cacheing
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/sapphire/trunk@81712 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- static-main.php
+++ static-main.php
@@ -1,18 +1,22 @@
 <?php
 
-$domainBasedCaching = true;
+/**
+ * This file is designed to be the new 'server' of sites using StaticPublisher.
+ * to use this, you need to modify your .htaccess to point all requests to
+ * static-main.php, rather than main.php. This file also allows for using
+ * static publisher with the subsites module.
+ *
+ * If you are using StaticPublisher+Subsites, set the following in _config.php:
+ *   FilesystemPublisher::$domain_based_caching = true;
+ */
 
-if ($domainBasedCaching) {
-	// Host -> cache dir mapping
-	if (file_exists('../subsites/host-map.php')) {
-		include_once '../subsites/host-map.php';
-	} else {
-		$subsiteHostmap = array();
-	}
+if (file_exists('../subsites/host-map.php')) {
+	include_once '../subsites/host-map.php';
+	$siteHostmap['default'] = isset($siteHostmap['default']) ? $siteHostmap['default'] : '';
 	
 	// Look for the host, and find the cache dir
 	$host = str_replace('www.', '', $_SERVER['HTTP_HOST']);
-	$cacheDir = isset($siteHostmap[$host]) ? $siteHostmap[$host] : $siteHostmap['default'];
+	$cacheDir = (isset($siteHostmap[$host]) ? $siteHostmap[$host] : $siteHostmap['default']) . '/';
 } else {
 	$cacheDir = '';
 }
@@ -21,10 +25,10 @@ if ($domainBasedCaching) {
 $file = preg_replace('/[^a-zA-Z0-9]/si', '_', trim($_SERVER['REQUEST_URI'], '/'));
 $file = $file ? $file : 'index';
 	
-if (file_exists('../cache/'.$cacheDir.'/'.$file.'.html')) {
-	echo file_get_contents('../cache/'.$cacheDir.'/'.$file.'.html');
-} elseif (file_exists('../cache/'.$cacheDir.'/'.$file.'.php')) {
-	include_once '../cache/'.$cacheDir.'/'.$file.'.php';
+if (file_exists('../cache/'.$cacheDir.$file.'.html')) {
+	echo file_get_contents('../cache/'.$cacheDir.$file.'.html');
+} elseif (file_exists('../cache/'.$cacheDir.$file.'.php')) {
+	include_once '../cache/'.$cacheDir.$file.'.php';
 } else {
 	// No cache hit... fallback!!!
 	include 'main.php';
