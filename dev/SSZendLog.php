e13316d0dc80164cc25c28901a22da8970e424a2
Sean Harvey <sean@silverstripe.com>
2009-08-19 05:16:58 +0000

    MINOR Documentation updates to SSZendLog
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/sapphire/trunk@84805 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- dev/SSZendLog.php
+++ dev/SSZendLog.php
@@ -1,9 +1,10 @@
 <?php
 /**
  * Modifications to Zend_Log to make it work nicer
- * with SSErrorLog. Specifically, this includes removing
+ * with {@link SSLog}. Specifically, this includes removing
  * writers that have been added to the logger, as well as
- * listing which ones are currently in use.
+ * listing which ones are currently in use since the Zend_Log
+ * API does not support these methods.
  * 
  * @package sapphire
  * @subpackage dev
@@ -12,11 +13,11 @@
 require_once 'Zend/Log.php';
 
 class SSZendLog extends Zend_Log {
-	
+
 	public function getWriters() {
 		return $this->_writers;
 	}
-	
+
 	/**
 	 * Remove a writer instance that exists in
 	 * the current writers collection for this logger.
@@ -30,5 +31,5 @@ class SSZendLog extends Zend_Log {
 			}
 		}
 	}
-	
+
 }
