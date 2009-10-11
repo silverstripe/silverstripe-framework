d81d7593b32141105701455c3307bef3b95c72c2
Andrew Short <andrewjshort@gmail.com>
2009-10-11 00:06:53 +0000

    MINOR ajshort: Deprecated un-used HTTPResponse->getLinks() method that duplicated functionality.
    From: Andrew Short <andrewjshort@gmail.com>
    
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/sapphire/trunk@88467 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- core/control/HTTPResponse.php
+++ core/control/HTTPResponse.php
@@ -209,28 +209,29 @@ class HTTPResponse extends Object {
 	function isFinished() {
 		return in_array($this->statusCode, array(301, 302, 401, 403));
 	}
-    
-    /**
-     * Return all the links in the body as an array.
-     * @returns An array of maps.  Each map will contain 'id', 'class', and 'href', representing the HTML attributes of the link.
-     */
-    function getLinks() {
-        $attributes = array('id', 'href', 'class');
-        $links = array();
-		$results = array();
-        
-        preg_match_all('/<a[^>]+>/i', $this->body, $links);
-        // $links[0] contains the actual matches
-        foreach($links[0] as $link) {
+	
+	/**
+	 * @deprecated 2.4 Use {@link HTTP::getLinksIn()} on DOMDocument.
+	 */
+	public function getLinks() {
+		user_error (
+			'HTTPResponse->getLinks() is deprecated, please use HTTP::getLinksIn() or DOMDocument.', E_USER_NOTICE
+		);
+		
+		$attributes = array('id', 'href', 'class');
+		$links      = array();
+		$results    = array();
+		
+		if(preg_match_all('/<a[^>]+>/i', $this->body, $links)) foreach($links[0] as $link) {
 			$processedLink = array();
-            foreach($attributes as $attribute) {
-                $matches = array();
-                if(preg_match('/' . $attribute  . '\s*=\s*"([^"]+)"/i', $link, $matches)) {
-                    $processedLink[$attribute] = $matches[1];
-                }                
-            }
+			foreach($attributes as $attribute) {
+				$matches = array();
+				if(preg_match('/' . $attribute  . '\s*=\s*"([^"]+)"/i', $link, $matches)) {
+					$processedLink[$attribute] = $matches[1];
+				}
+			}
 			$results[] = $processedLink;
-        }
+		}
 		
 		return $results;
     }
