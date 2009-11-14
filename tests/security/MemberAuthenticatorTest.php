2a1abcae2ad3c7ef7e643d341eef62266443c79a
Ingo Schommer <ingo@silverstripe.com>
2009-11-14 00:42:18 +0000

    BUGFIX Legacy password hash migration in MemberAuthenticator::authenticate() which fixes the precision problems mentioned in #3004 when a user logs in (from r90950)
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/sapphire/branches/2.4@91572 467b73ca-7a2a-4603-9d3b-597d59a354a9

---
+++ tests/security/MemberAuthenticatorTest.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * @package sapphire
+ * @subpackage tests
+ */
+class MemberAuthenticatorTest extends SapphireTest {
+	function testLegacyPasswordHashMigrationUponLogin() {
+		$member = new Member();
+		$member->Email = 'test@test.com';
+		$member->PasswordEncryption = "sha1";
+		$member->Password = "mypassword";
+		$member->write();
+		
+		$data = array(
+			'Email' => $member->Email,
+			'Password' => 'mypassword'
+		);
+		MemberAuthenticator::authenticate($data);
+		
+		$member = DataObject::get_by_id('Member', $member->ID);
+		$this->assertEquals($member->PasswordEncryption, "sha1_v2.4");
+		$this->assertTrue($member->checkPassword('mypassword'));
+	}
+	
+	function testNoLegacyPasswordHashMigrationOnIncompatibleAlgorithm() {
+		PasswordEncryptor::register('crc32', 'PasswordEncryptor_PHPHash("crc32")');
+		
+		$member = new Member();
+		$member->Email = 'test@test.com';
+		$member->PasswordEncryption = "crc32";
+		$member->Password = "mypassword";
+		$member->write();
+		
+		$data = array(
+			'Email' => $member->Email,
+			'Password' => 'mypassword'
+		);
+		MemberAuthenticator::authenticate($data);
+		
+		$member = DataObject::get_by_id('Member', $member->ID);
+		$this->assertEquals($member->PasswordEncryption, "crc32");
+		$this->assertTrue($member->checkPassword('mypassword'));
+	}
+}
