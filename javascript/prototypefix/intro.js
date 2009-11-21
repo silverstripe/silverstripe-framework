32c44b071ddae5a2d5521ef654cdf9ac92960b01
Ingo Schommer <ingo@silverstripe.com>
2009-11-21 02:32:19 +0000

    MINOR Added prototypefix helper scripts from jquery13 module to fix document.getElementsByClassName() override by prototypejs
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/sapphire/trunk@92548 467b73ca-7a2a-4603-9d3b-597d59a354a9

---
+++ javascript/prototypefix/intro.js
@@ -0,0 +1,1 @@
+var browserGetElementsByClassName = document.getElementsByClassName;
---
+++ javascript/prototypefix/outro.js
@@ -0,0 +1,1 @@
+if (browserGetElementsByClassName) document.getElementsByClassName = browserGetElementsByClassName;
