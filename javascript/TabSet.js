1afe18a268038e59b19dc8520d5d11647f1001cd
Ingo Schommer <ingo@silverstripe.com>
2009-11-21 02:32:38 +0000

    MINOR Formatting in TabSet.js
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/sapphire/trunk@92554 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- javascript/TabSet.js
+++ javascript/TabSet.js
@@ -6,27 +6,29 @@
 	 * their height explicitly set. This is important
 	 * for forms inside the CMS layout.
 	 */
-	$('.ss-tabset').concrete({
-		onmatch: function() {
-			this.rewriteHashlinks();
+	$('.ss-tabset').concrete('ss',  function($){
+		return {
+			onmatch: function() {
+				this.rewriteHashlinks();
 
-			// Initialize jQuery UI tabs
-			this.tabs({
-				cookie: $.cookie ? { expires: 30, path: '/', name: 'ui-tabs-' + this.attr('id') } : false
-			});
-		},
+				// Initialize jQuery UI tabs
+				this.tabs({
+					cookie: $.cookie ? { expires: 30, path: '/', name: 'ui-tabs-' + this.attr('id') } : false
+				});
+			},
 			
-		/**
-		 * Replace prefixes for all hashlinks in tabs.
-		 * SSViewer rewrites them from "#Root_MyTab" to
-		 * e.g. "/admin/#Root_MyTab" which makes them
-		 * unusable for jQuery UI.
-		 */
-		rewriteHashlinks: function() {
-			$(this).find('ul a').each(function() {
-				var href = $(this).attr('href').replace(/.*(#.*)/, '$1');
-				if(href) $(this).attr('href', href);
-			});
-		}
+			/**
+			 * Replace prefixes for all hashlinks in tabs.
+			 * SSViewer rewrites them from "#Root_MyTab" to
+			 * e.g. "/admin/#Root_MyTab" which makes them
+			 * unusable for jQuery UI.
+			 */
+			rewriteHashlinks: function() {
+				$(this).find('ul a').each(function() {
+					var href = $(this).attr('href').replace(/.*(#.*)/, '$1');
+					if(href) $(this).attr('href', href);
+				});
+			}
+		};
 	});
 })(jQuery);
