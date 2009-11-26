7ca85245d57dfa317a1352959f07c97d0e8f0666
Ingo Schommer <ingo@silverstripe.com>
2009-11-26 03:19:21 +0000

    MINOR Moved jsparty/tiny_mce to sapphire/thirdparty/tinymce
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/sapphire/trunk@93561 467b73ca-7a2a-4603-9d3b-597d59a354a9

---
+++ thirdparty/tinymce/3.2.4.1/plugins/emotions/langs/ja_dlg.js
@@ -0,0 +1,20 @@
+tinyMCE.addI18n('ja.emotions_dlg',{
+title:"\u30B9\u30DE\u30A4\u30EA\u30FC\u306E\u633F\u5165",
+desc:"\u30B9\u30DE\u30A4\u30EA\u30FC",
+cool:"Cool",
+cry:"Cry",
+embarassed:"Embarassed",
+foot_in_mouth:"Foot in mouth",
+frown:"Frown",
+innocent:"Innocent",
+kiss:"Kiss",
+laughing:"Laughing",
+money_mouth:"Money mouth",
+sealed:"Sealed",
+smile:"Smile",
+surprised:"Surprised",
+tongue_out:"Tongue out",
+undecided:"Undecided",
+wink:"Wink",
+yell:"Yell"
+});
---
+++ thirdparty/tinymce/3.2.4.1/plugins/template/langs/ca_dlg.js
@@ -0,0 +1,15 @@
+tinyMCE.addI18n('ca.template_dlg',{
+title:"Plantilles",
+label:"Plantilla",
+desc_label:"Descripci\u00F3",
+desc:"Insereix contingut predefinit de plantilla",
+select:"Selecciona una plantilla",
+preview:"Vista pr\u00E8via",
+warning:"Atenci\u00F3: Actualitzar una plantilla amb una de diferent pot provocar p\u00E8rdua de dades.",
+mdate_format:"%d-%m-%Y %H:%M:%S",
+cdate_format:"%d-%m-%Y %H:%M:%S",
+months_long:"Gener,Febrer,Mar\u00E7,Abril,Maig,Juny,Juliol,Agost,Setembre,Octubre,Novembre,Desembre",
+months_short:"Gen,Feb,Mar,Abr,Mai,Jun,Jul,Ago,Set,Oct,Nov,Des",
+day_long:"Diumenge,Dilluns,Dimarts,Dimecres,Dijous,Divendres,Dissabte,Diumenge",
+day_short:"Dmg,Dll,Dmt,Dmc,Djs,Dvs,Dss,Dmg"
+});
