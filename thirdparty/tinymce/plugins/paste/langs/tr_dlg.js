e550de71e3cf355e741750f39b22e06092173a86
Ingo Schommer <ingo@silverstripe.com>
2009-11-26 04:58:17 +0000

    MINOR Moved jsparty/tiny_mce to sapphire/thirdparty/tinymce
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/sapphire/branches/2.4@93610 467b73ca-7a2a-4603-9d3b-597d59a354a9

---
+++ thirdparty/tinymce/plugins/paste/langs/tr_dlg.js
@@ -0,0 +1,5 @@
+tinyMCE.addI18n('tr.paste_dlg',{
+text_title:"Pencereye metin yap\u0131\u015Ft\u0131rmak i\u00E7in CTRL+V tu\u015Funu kullan\u0131n\u0131z.",
+text_linebreaks:"Sat\u0131r k\u0131r\u0131l\u0131mlar\u0131n\u0131 koru",
+word_title:"Pencereye metin yap\u0131\u015Ft\u0131rmak i\u00E7in CTRL+V tu\u015Funu kullan\u0131n\u0131z."
+});
