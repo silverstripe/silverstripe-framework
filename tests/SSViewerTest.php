ae04c15e355b208255bcb2b495708ddf75da5552
Sam Minnee <sam@silverstripe.com>
2009-11-27 00:24:57 +0000

    MINOR: Refactor the SSViewerTest to make it easier to add more tests.
    From: Sam Minnee <sam@silverstripe.com>
    
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/sapphire/trunk@93718 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- tests/SSViewerTest.php
+++ tests/SSViewerTest.php
@@ -13,6 +13,16 @@ class SSViewerTest extends SapphireTest {
 		$this->assertEquals('Test partial template: var value', trim(preg_replace("/<!--.*-->/U",'',$result)));
 	}
 	
+	
+	/**
+	 * Small helper to render templates from strings
+	 */
+	function render($templateString, $data = null) {
+		$t = SSViewer::fromString($templateString);
+		if(!$data) $data = new SSViewerTestFixture();
+		return $t->process($data);
+	}
+	
 	function testRequirements() {
 		$requirements = $this->getMock("Requirements_Backend", array("javascript", "css"));
 		$jsFile = 'sapphire/tests/forms/a.js';
@@ -23,92 +33,124 @@ class SSViewerTest extends SapphireTest {
 		
 		Requirements::set_backend($requirements);
 		
-		$data = new ArrayData(array());
-		
-		$viewer = SSViewer::fromString(<<<SS
-		<% require javascript($jsFile) %>
-		<% require css($cssFile) %>
-SS
-);
-		$template = $viewer->process($data);
+		$template = $this->render("<% require javascript($jsFile) %>
+		<% require css($cssFile) %>");
+
 		$this->assertFalse((bool)trim($template), "Should be no content in this return.");
 	}
 	
 	function testComments() {
-		$viewer = SSViewer::fromString(<<<SS
+		$output = $this->render(<<<SS
 This is my template<%-- this is a comment --%>This is some content<%-- this is another comment --%>This is the final content
 SS
 );
-		$output = $viewer->process(new ArrayData(array()));
 		
 		$this->assertEquals("This is my templateThis is some contentThis is the final content", preg_replace("/\n?<!--.*-->\n?/U",'',$output));
 	}
 	
 	function testObjectDotArguments() {
-		// one argument
-		$viewer = SSViewer::fromString(<<<SS
-\$TestObject.methodWithOneArgument(one)
-SS
-);
-		$obj = new SSViewerTest_ViewableData();
 		$this->assertEquals(
-			$viewer->process(new ArrayData(array('TestObject'=>$obj))),
-			"arg1:one",
+			$this->render("\$TestObject.methodWithOneArgument(one)"),
+			"TestObject.methodWithOneArgument(one)",
 			"Object method calls in dot notation work with one argument"
 		);
 		
 		// two arguments
-		$viewer = SSViewer::fromString(<<<SS
-\$TestObject.methodWithTwoArguments(one,two)
-SS
-);
-		$obj = new SSViewerTest_ViewableData();
 		$this->assertEquals(
-			$viewer->process(new ArrayData(array('TestObject'=>$obj))),
-			"arg1:one,arg2:two",
+			$this->render("\$TestObject.methodWithTwoArguments(one,two)"),
+			"TestObject.methodWithTwoArguments(one,two)",
 			"Object method calls in dot notation work with two arguments"
 		);
 	}
 	
 	function testBaseTagGeneration() {
 		// XHTML wil have a closed base tag
-		$tmpl1 = SSViewer::fromString('<?xml version="1.0" encoding="UTF-8"?>
+		$tmpl1 = '<?xml version="1.0" encoding="UTF-8"?>
 			<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 			<html>
 				<head><% base_tag %></head>
 				<body><p>test</p><body>
-			</html>');
-		$this->assertRegExp('/<head><base href=".*"><\/base><\/head>/', $tmpl1->process(new ViewableData()));
+			</html>';
+		$this->assertRegExp('/<head><base href=".*"><\/base><\/head>/', $this->render($tmpl1));
 			
 		// HTML4 and 5 will only have it for IE
-		$tmpl2 = SSViewer::fromString('<!DOCTYPE html>
+		$tmpl2 = '<!DOCTYPE html>
 			<html>
 				<head><% base_tag %></head>
 				<body><p>test</p><body>
-			</html>');
-		$this->assertRegExp('/<head><base href=".*"><!--\[if lte IE 6\]><\/base><!\[endif\]--><\/head>/', $tmpl2->process(new ViewableData()));
+			</html>';
+		$this->assertRegExp('/<head><base href=".*"><!--\[if lte IE 6\]><\/base><!\[endif\]--><\/head>/', $this->render($tmpl2));
 			
 			
-		$tmpl3 = SSViewer::fromString('<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+		$tmpl3 = '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
 			<html>
 				<head><% base_tag %></head>
 				<body><p>test</p><body>
-			</html>');
-		$this->assertRegExp('/<head><base href=".*"><!--\[if lte IE 6\]><\/base><!\[endif\]--><\/head>/', $tmpl3->process(new ViewableData()));
+			</html>';
+		$this->assertRegExp('/<head><base href=".*"><!--\[if lte IE 6\]><\/base><!\[endif\]--><\/head>/', $this->render($tmpl3));
 
 		// Check that the content negotiator converts to the equally legal formats
 		$negotiator = new ContentNegotiator();
 		
-		$response = new SS_HTTPResponse($tmpl1->process(new ViewableData()));
+		$response = new SS_HTTPResponse($this->render($tmpl1));
 		$negotiator->html($response);
 		$this->assertRegExp('/<head><base href=".*"><!--\[if lte IE 6\]><\/base><!\[endif\]--><\/head>/', $response->getBody());
 
-		$response = new SS_HTTPResponse($tmpl1->process(new ViewableData()));
+		$response = new SS_HTTPResponse($this->render($tmpl1));
 		$negotiator->xhtml($response);
 		$this->assertRegExp('/<head><base href=".*"><\/base><\/head>/', $response->getBody());
 	}
 }
 
+/**
+ * A test fixture that will echo back the template item
+ */
+class SSViewerTestFixture extends ViewableData {
+	protected $name;
+
+	function __construct($name = null) {
+		$this->name = $name;
+	}
+	
+
+	private function argedName($fieldName, $arguments) {
+		$childName = $this->name ? "$this->name.$fieldName" : $fieldName;
+		if($arguments) return $childName . '(' . implode(',', $arguments) . ')';
+		else return $childName;
+	}
+	function obj($fieldName, $arguments=null, $forceReturnedObject=true, $cache=false, $cacheName=null) {
+		$childName = $this->argedName($fieldName, $arguments);
+
+		// Special field name Loop### to create a list
+		if(preg_match('/^Loop([0-9]+)$/', $fieldName, $matches)) {
+			$output = new DataObjectSet();
+			for($i=0;$i<$matches[1];$i++) $output->push(new SSViewerTestFixture($childName));
+			return $output;
+
+		} else if(preg_match('/NotSet/i', $fieldName)) {
+			return new ViewableData();
+
+		} else {
+			return new SSViewerTestFixture($childName);
+		}
+	}
+	
+
+	function XML_val($fieldName, $arguments = null) {
+		if(preg_match('/NotSet/i', $fieldName)) {
+			return '';
+		} else if(preg_match('/Raw/i', $fieldName)) {
+			return $fieldName;
+		} else {
+			return '[out:' . $this->argedName($fieldName, $arguments) . ']';
+		}
+	}
+
+	function hasValue($fieldName, $arguments = null) {
+		return (bool)$this->XML_val($fieldName, $arguments);
+	}
+}
+/*
 class SSViewerTest_ViewableData extends ViewableData implements TestOnly {
 	function methodWithOneArgument($arg1) {
 		return "arg1:{$arg1}";
@@ -118,3 +160,4 @@ class SSViewerTest_ViewableData extends ViewableData implements TestOnly {
 		return "arg1:{$arg1},arg2:{$arg2}";
 	}
 }
+*/
