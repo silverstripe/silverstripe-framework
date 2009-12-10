55ded86c8f8f1dbdf52f913c8884403042072d95
Mateusz Uzdowski <mateusz@silverstripe.com>
2009-12-10 20:03:50 +0000

    FEATURE: added several tests for PermissionCheckboxSetField, PermissionRole and Group
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/sapphire/branches/2.4@94887 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- tests/security/PermissionCheckboxSetFieldTest.php
+++ tests/security/PermissionCheckboxSetFieldTest.php
@@ -4,6 +4,8 @@
  * @subpackage tests
  */
 class PermissionCheckboxSetFieldTest extends SapphireTest {
+	static $fixture_file = 'sapphire/tests/security/PermissionCheckboxSetFieldTest.yml';
+	
 	function testHiddenPermissions() {
 		$f = new PermissionCheckboxSetField(
 			'Permissions',
@@ -20,5 +22,66 @@ class PermissionCheckboxSetFieldTest extends SapphireTest {
 		);
 		$this->assertContains('CMS_ACCESS_CMSMain', $f->Field());
 		$this->assertNotContains('CMS_ACCESS_ReportAdmin', $f->Field());
+	}
+	
+	function testSaveInto() {
+		$group = $this->objFromFixture('Group', 'group');  // tested group
+		$untouchable = $this->objFromFixture('Group', 'untouchable');  // group that should not change
+		
+		$field = new PermissionCheckboxSetField(
+			'Permissions',
+			'Permissions',
+			'Permission',
+			'GroupID',
+			$group
+		);
+
+		// get the number of permissions before we start
+		$baseCount = DataObject::get('Permission')->Count();
+		
+		// there are currently no permissions, save empty checkbox
+		$field->saveInto($group);
+		$group->flushCache();
+		$untouchable->flushCache();
+		$this->assertEquals($group->Permissions()->Count(), 0, 'The tested group has no permissions');
+
+		$this->assertEquals($untouchable->Permissions()->Count(), 1, 'The other group has one permission');
+		$this->assertEquals($untouchable->Permissions("Code='ADMIN'")->Count(), 1, 'The other group has ADMIN permission');
+
+		$this->assertEquals(DataObject::get('Permission')->Count(), $baseCount, 'There are no orphaned permissions');
+				
+		// add some permissions
+		$field->setValue(array(
+			'ADMIN'=>true,
+			'CMS_ACCESS_AssetAdmin'=>true
+		));
+
+		$field->saveInto($group);
+		$group->flushCache();
+		$untouchable->flushCache();
+		$this->assertEquals($group->Permissions()->Count(), 2, 'The tested group has two permissions permission');
+		$this->assertEquals($group->Permissions("Code='ADMIN'")->Count(), 1, 'The tested group has ADMIN permission');
+		$this->assertEquals($group->Permissions("Code='CMS_ACCESS_AssetAdmin'")->Count(), 1, 'The tested group has CMS_ACCESS_AssetAdmin permission');
+
+		$this->assertEquals($untouchable->Permissions()->Count(), 1, 'The other group has one permission');
+		$this->assertEquals($untouchable->Permissions("Code='ADMIN'")->Count(), 1, 'The other group has ADMIN permission');
+
+		$this->assertEquals(DataObject::get('Permission')->Count(), $baseCount+2, 'There are no orphaned permissions');
+		
+		// remove permission
+		$field->setValue(array(
+			'ADMIN'=>true,
+		));
+
+		$field->saveInto($group);
+		$group->flushCache();
+		$untouchable->flushCache();
+		$this->assertEquals($group->Permissions()->Count(), 1, 'The tested group has 1 permission');
+		$this->assertEquals($group->Permissions("Code='ADMIN'")->Count(), 1, 'The tested group has ADMIN permission');
+
+		$this->assertEquals($untouchable->Permissions()->Count(), 1, 'The other group has one permission');
+		$this->assertEquals($untouchable->Permissions("Code='ADMIN'")->Count(), 1, 'The other group has ADMIN permission');
+
+		$this->assertEquals(DataObject::get('Permission')->Count(), $baseCount+1, 'There are no orphaned permissions');
 	}
 }
---
+++ tests/security/PermissionRoleTest.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @package sapphire
+ * @subpackage tests
+ */
+class PermissionRoleTest extends FunctionalTest {
+	static $fixture_file = 'sapphire/tests/security/PermissionRoleTest.yml';
+	
+	function testDelete() {
+		$role = $this->objFromFixture('PermissionRole', 'role');
+		
+		$role->delete();
+		
+		$this->assertNull(DataObject::get('PermissionRole', "ID={$role->ID}"), 'Role is removed');
+		$this->assertNull(DataObject::get('PermissionRoleCode',"RoleID={$role->ID}"), 'Permissions removed along with the role');
+	}
+}
