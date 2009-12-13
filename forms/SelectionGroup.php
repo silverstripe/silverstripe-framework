d5e2db7f5ec8577121cba4870e672eba2332fe77
Andrew Short <andrewjshort@gmail.com>
2009-12-13 05:22:31 +0000

    BUGFIX: Included jQuery in SelectionGroup->FieldHolder(), and used new jQuery.live syntax.
    From: Andrew Short <andrewjshort@gmail.com>
    
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/sapphire/trunk@95033 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- forms/SelectionGroup.php
+++ forms/SelectionGroup.php
@@ -79,10 +79,8 @@ class SelectionGroup extends CompositeField {
 	}
 	
 	function FieldHolder() {
-		Requirements::javascript(SAPPHIRE_DIR . '/thirdparty/prototype/prototype.js');
-		Requirements::javascript(SAPPHIRE_DIR . '/thirdparty/behaviour/behaviour.js');
-		Requirements::javascript(SAPPHIRE_DIR . '/javascript/prototype_improvements.js');
-		Requirements::javascript(SAPPHIRE_DIR . '/javascript/SelectionGroup.js');
+		Requirements::javascript(THIRDPARTY_DIR .'/jquery/jquery.js');
+		Requirements::javascript(SAPPHIRE_DIR   . '/javascript/SelectionGroup.js');
 		Requirements::css(SAPPHIRE_DIR . '/css/SelectionGroup.css');
 		
 		return $this->renderWith("SelectionGroup");
--- javascript/SelectionGroup.js
+++ javascript/SelectionGroup.js
@@ -1,15 +1,15 @@
 (function($) {
-$(document).ready(function() {
-	$('ul.SelectionGroup input.selector').livequery('click', function(){
-		var li = $(this).parent('li')
-		li.addClass('selected');
-		
-		var prev = li.prevAll('li.selected');
-		if(prev.length) prev.removeClass('selected');
-		var next = li.nextAll('li.selected');
-		if(next.length) next.removeClass('selected');
-		
-		$(this).focus();
-	});
-})
+	$(document).ready(function() {
+		$('ul.SelectionGroup input.selector').live('click', function() {
+			var li = $(this).parent('li')
+			li.addClass('selected');
+
+			var prev = li.prevAll('li.selected');
+			if(prev.length) prev.removeClass('selected');
+			var next = li.nextAll('li.selected');
+			if(next.length) next.removeClass('selected');
+
+			$(this).focus();
+		});
+	})
 })(jQuery);
