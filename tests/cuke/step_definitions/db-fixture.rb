d2d32dafa50e3c57281d6258a41f032d23e9dc4f
Sam Minnee <sam@silverstripe.com>
2010-01-12 23:43:02 +0000

    MINOR: Ensure that cuke can work with --dry-run (from r94819)
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/sapphire/branches/2.4@96772 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- tests/cuke/step_definitions/db-fixture.rb
+++ tests/cuke/step_definitions/db-fixture.rb
@@ -1,3 +1,5 @@
 # Reset database
-$browser.goto $baseURL + 'dev/tests/endsession'
-$browser.goto $baseURL + 'dev/tests/startsession?fixture=sapphire/tests/Bare.yml&flush=1'
+if $baseURL and $browser then
+  $browser.goto $baseURL + 'dev/tests/endsession'
+  $browser.goto $baseURL + 'dev/tests/startsession?fixture=sapphire/tests/Bare.yml&flush=1'
+end
