deae632bbfeb13c4abc1caba244ea1141a888611
Sam Minnee <sam@silverstripe.com>
2010-01-12 23:40:33 +0000

    BUGFIX: Don't let users edit the location in the Behaviour tab if they don't have sitetree reorganise permission (from r94609)
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/sapphire/branches/2.4@96767 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- core/model/SiteTree.php
+++ core/model/SiteTree.php
@@ -1692,6 +1692,16 @@ class SiteTree extends DataObject implements PermissionProvider,i18nEntityProvid
 			//new NamedLabelField("Status", $message, "pageStatusMessage", true)
 		);
 		
+		// Make page location fields read-only if the user doesn't have the appropriate permission
+		if(!Permission::check("SITETREE_REORGANISE")) {
+			$fields->makeFieldReadonly('ParentType');
+			if($this->ParentType == 'root') {
+				$fields->removeByName('ParentID');
+			} else {
+				$fields->makeFieldReadonly('ParentID');
+			}
+		}
+		
 		$viewersOptionsSource = array();
 		if($this->Parent()->ID || $this->CanViewType == 'Inherit') $viewersOptionsSource["Inherit"] = _t('SiteTree.INHERIT', "Inherit from parent page");
 		$viewersOptionsSource["Anyone"] = _t('SiteTree.ACCESSANYONE', "Anyone");
