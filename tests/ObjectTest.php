5a68d0f3b568ce44d13f05b190fa787805ddbb40
Sam Minnee <sam@silverstripe.com>
2010-03-16 02:16:59 +0000

    BUGFIX: Update Object::parse_class_spec() to handle arrays.
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/sapphire/branches/2.4@101105 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- tests/ObjectTest.php
+++ tests/ObjectTest.php
@@ -337,17 +337,30 @@ class ObjectTest extends SapphireTest {
 	}
 
 	public function testParseClassSpec() {
+		// Simple case
 		$this->assertEquals(
 			array('Versioned',array('Stage', 'Live')),
 			Object::parse_class_spec("Versioned('Stage','Live')")
 		);
+		// String with commas
 		$this->assertEquals(
 			array('Versioned',array('Stage,Live', 'Stage')),
 			Object::parse_class_spec("Versioned('Stage,Live','Stage')")
 		);
+		// String with quotes
 		$this->assertEquals(
 			array('Versioned',array('Stage\'Stage,Live\'Live', 'Live')),
 			Object::parse_class_spec("Versioned('Stage\'Stage,Live\'Live','Live')")
+		);
+		// Array
+		$this->assertEquals(
+			array('Enum',array(array('Accepted', 'Pending', 'Declined', 'Unsubmitted'), 'Unsubmitted')),
+			Object::parse_class_spec("Enum(array('Accepted', 'Pending', 'Declined', 'Unsubmitted'), 'Unsubmitted')")
+		);
+		// Nested array
+		$this->assertEquals(
+			array('Enum',array(array('Accepted', 'Pending', 'Declined', array('UnsubmittedA','UnsubmittedB')), 'Unsubmitted')),
+			Object::parse_class_spec("Enum(array('Accepted', 'Pending', 'Declined', array('UnsubmittedA','UnsubmittedB')), 'Unsubmitted')")
 		);
 	}
 }
