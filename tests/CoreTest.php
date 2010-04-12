29ea7e0b982e2acc9f444ee3632aa96cb32f6ca7
Ingo Schommer <ingo@silverstripe.com>
2010-04-12 23:15:04 +0000

    BUGFIX Use BASE_PATH and BASE_URL instead of data from $_SERVER. API CHANGE: Determine default BASE_PATH/BASE_URL from the __FILE__ content, so that the script that initiated the Sapphire process doesn't matter.  This means that index.php doesn't need to manipulate those variables. (from r97731)
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/sapphire/trunk@102528 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- tests/CoreTest.php
+++ tests/CoreTest.php
@@ -20,23 +20,14 @@ class CoreTest extends SapphireTest {
 		if(file_exists($this->tempPath)) {
 			$this->assertEquals(getTempFolder(), $this->tempPath);
 		} else {
-			// Store the original variable so we know what to change it back to
-			$old = $_SERVER['SCRIPT_FILENAME'];
-			
 			// A typical Windows location for where sites are stored on IIS
-			$_SERVER['SCRIPT_FILENAME'] = 'C:\\inetpub\\wwwroot\\silverstripe\\sapphire\\main.php';
-			$this->assertEquals(getTempFolder(), getSysTempDir() . '/silverstripe-cacheC--inetpub-wwwroot-silverstripe');
+			$this->assertEquals(getTempFolder('C:\\inetpub\\wwwroot\\silverstripe'), getSysTempDir() . '/silverstripe-cacheC--inetpub-wwwroot-silverstripe');
 			
 			// A typical Mac OS X location for where sites are stored
-			$_SERVER['SCRIPT_FILENAME'] = '/Users/joebloggs/Sites/silverstripe/sapphire/main.php';
-			$this->assertEquals(getTempFolder(), getSysTempDir() . '/silverstripe-cache-Users-joebloggs-Sites-silverstripe');
+			$this->assertEquals(getTempFolder('/Users/joebloggs/Sites/silverstripe'), getSysTempDir() . '/silverstripe-cache-Users-joebloggs-Sites-silverstripe');
 			
 			// A typical Linux location for where sites are stored
-			$_SERVER['SCRIPT_FILENAME'] = '/var/www/silverstripe/sapphire/main.php';
-			$this->assertEquals(getTempFolder(), getSysTempDir() . '/silverstripe-cache-var-www-silverstripe');
-			
-			// Restore the SCRIPT_FILENAME variable back to the original
-			$_SERVER['SCRIPT_FILENAME'] = $old;
+			$this->assertEquals(getTempFolder('/var/www/silverstripe'), getSysTempDir() . '/silverstripe-cache-var-www-silverstripe');
 		}
 	}
 
