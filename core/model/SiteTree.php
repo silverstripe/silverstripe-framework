7b3e45539bfe6accb9f100c3f9655470d1a75328
Ingo Schommer <ingo@silverstripe.com>
2010-04-12 03:28:07 +0000

    MINOR: added hooks for extending duplicate page functionality (from r96877)
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/sapphire/trunk@102410 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- core/model/SiteTree.php
+++ core/model/SiteTree.php
@@ -496,11 +496,14 @@ class SiteTree extends DataObject implements PermissionProvider,i18nEntityProvid
 	 * @return SiteTree The duplicated object.
 	 */
 	 public function duplicate($doWrite = true) {
+		
+		$this->extend('onBeforeDuplicate', $page);
 		$page = parent::duplicate(false);
 		$page->Sort = 0;
 		if($doWrite) {
 			$page->write();
 		}
+		$this->extend('onAfterDuplicate', $page);
 		return $page;
 	}
 
