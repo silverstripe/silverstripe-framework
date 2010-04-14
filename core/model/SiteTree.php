720bcfa61d955b847512d312b923eb2244997fcb
Ingo Schommer <ingo@silverstripe.com>
2010-04-14 04:40:16 +0000

    BUGFIX: batch_permission_check returns null rather than empty array when user has no permissions (from r99443)
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/sapphire/trunk@102863 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- core/model/SiteTree.php
+++ core/model/SiteTree.php
@@ -1121,7 +1121,7 @@ class SiteTree extends DataObject implements PermissionProvider,i18nEntityProvid
 			}*/
 			return $combinedStageResult;
 		} else {
-			array();
+			return array();
 		}
 	}
 	
