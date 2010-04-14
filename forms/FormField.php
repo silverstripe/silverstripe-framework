fed0d1fe4b3aaa6a0fcc01c62667bbfa3966a8d5
Ingo Schommer <ingo@silverstripe.com>
2010-04-14 04:40:04 +0000

    BUGFIX Fixed variable existence checks in setValue() in FormField::__construct() by checking for !== NULL (changed from isset($value) to $value in r99360) (from r99394)
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/sapphire/trunk@102862 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- forms/FormField.php
+++ forms/FormField.php
@@ -75,7 +75,7 @@ class FormField extends RequestHandler {
 		$this->name = $name;
 		$this->title = ($title === null) ? $name : $title;
 
-		if($value) $this->setValue($value);
+		if($value !== NULL) $this->setValue($value);
 		if($form) $this->setForm($form);
 
 		parent::__construct();
