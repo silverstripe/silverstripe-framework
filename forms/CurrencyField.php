46acb962c87b87f986c9a3308fda9bdc23bd524a
Ingo Schommer <ingo@silverstripe.com>
2010-04-25 21:27:19 +0000

    BUGFIX Fixed CurrencyField->jsValidation() regex escaping (fixes #5462, thanks mobiusnz)
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/sapphire/branches/2.4@103451 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- forms/CurrencyField.php
+++ forms/CurrencyField.php
@@ -54,7 +54,7 @@ Behaviour.register({
 			if(!el || !el.value) return true;
 			
 			var value = \$F(el);
-			if(value.length > 0 && !value.match(/^\s*\\$?(\d{1,3}(\,\d{3})*|(\d+))(\.\d{2})?\s*\$/)) {
+			if(value.length > 0 && !value.match(/^\s*\\\\$?(\d{1,3}(\,\d{3})*|(\d+))(\.\d{2})?\s*\$/)) {
 				validationError(el,"$error","validation",false);
 				return false;
 			}
