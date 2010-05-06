21af61e6663535bbca631e6e4f63beca1028f256
Luke Hudson <lukeletters@gmail.com>
2010-05-06 03:37:41 +0000

    FEATURE: Add 'Given I load the fixture file "app/tests/xyz.yml"' step to salad
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/sapphire/branches/2.4@104232 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- tests/salad/step_definitions/db-fixture.rb
+++ tests/salad/step_definitions/db-fixture.rb
@@ -1,5 +1,28 @@
-# Reset database
-if $baseURL and $browser then
-  $browser.goto $baseURL + 'dev/tests/endsession'
-  $browser.goto $baseURL + 'dev/tests/startsession?fixture=sapphire/tests/Bare.yml&flush=1'
+def loadFixture(fileName)
+	if $baseURL and $browser then
+		$browser.goto $baseURL + "dev/tests/sessionloadyml?fixture=" + fileName + "&flush=1"
+	else
+		fail("No \$baseUrl or \$browser found")
+	end
 end
+
+def startSession
+	# Reset database
+	if $baseURL and $browser then
+		$browser.goto $baseURL + 'dev/tests/endsession'
+	else
+		fail("No \$baseUrl or \$browser found")
+	end
+	fileName = 'sapphire/tests/Bare.yml'
+	if $baseURL and $browser then
+		$browser.goto $baseURL + "dev/tests/startsession?fixture=" + fileName + "&flush=1"
+	else
+		fail("No \$baseUrl or \$browser found")
+	end
+end
+
+Given /load the fixture file "([^"]+)"/ do |fileName|
+	loadFixture(fileName)
+end
+
+startSession()
