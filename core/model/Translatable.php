e5eff98e153d7257adad169ba9c6682cd704840c
Ingo Schommer <ingo@silverstripe.com>
2010-05-25 03:44:31 +0000

    BUGFIX: Show Language dropdown in English (#5098) (from r100359)
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/sapphire/trunk@105552 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- core/model/Translatable.php
+++ core/model/Translatable.php
@@ -979,7 +979,7 @@ class Translatable extends DataObjectDecorator implements PermissionProvider {
 						_t('Translatable.NEWLANGUAGE', 'New language'), 
 						$alreadyTranslatedLocales,
 						'SiteTree',
-						'Locale-Native',
+						'Locale-English',
 						$this->owner
 					),
 					$createButton = new InlineFormAction(
