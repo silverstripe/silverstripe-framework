9d2550e223fb856e4fedb5a206b284f11690b5a9
Saophalkun Ponlu <phalkunz@silverstripe.com>
2010-09-27 04:32:37 +0000

    MINOR: Add translation correction for Czech and add Slovakian translation in cms and sapphire (js). Thanks to @Pike
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/sapphire/branches/2.4@111262 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- javascript/lang/cs_CZ.js
+++ javascript/lang/cs_CZ.js
@@ -4,16 +4,16 @@ if(typeof(ss) == 'undefined' || typeof(ss.i18n) == 'undefined') {
 	ss.i18n.addDictionary('cs_CZ', {
 		'VALIDATOR.FIELDREQUIRED': 'Vyplňte "%s", prosím, je vyžadováno.',
 		'HASMANYFILEFIELD.UPLOADING': 'Nahrávání... %s',
-		'TABLEFIELD.DELETECONFIRMMESSAGE': 'Určitě chceš smazat tento záznam?',
+		'TABLEFIELD.DELETECONFIRMMESSAGE': 'Určitě chcete smazat tento záznam?',
 		'LOADING': 'natahování...',
 		'UNIQUEFIELD.SUGGESTED': "Hodnota změněna na '%s' : %s",
-		'UNIQUEFIELD.ENTERNEWVALUE': 'Pro toto pole musíš zadat novou hodnotu',
+		'UNIQUEFIELD.ENTERNEWVALUE': 'Pro toto pole musíte zadat novou hodnotu',
 		'UNIQUEFIELD.CANNOTLEAVEEMPTY': 'Toto pole nesmí být prázdné',
 		'RESTRICTEDTEXTFIELD.CHARCANTBEUSED': "V tomto poli znak '%s' nesmí být použit",
-		'UPDATEURL.CONFIRM': 'Dovoliš mi změnit URL na:\n\n%s\n\nKlikni OK pro změnu URL, klikni Cancel pro ponechání původního:\n\n%s',
+		'UPDATEURL.CONFIRM': 'Dovolite mi změnit URL na:\n\n%s\n\nKlikněte OK pro změnu URL, klikněte Cancel pro ponechání původního:\n\n%s',
 		'FILEIFRAMEFIELD.DELETEFILE': 'Smazat soubor',
 		'FILEIFRAMEFIELD.UNATTACHFILE': 'Odpojit soubor',
 		'FILEIFRAMEFIELD.DELETEIMAGE': 'Smazat obrázek',
-		'FILEIFRAMEFIELD.CONFIRMDELETE': 'Určitě chceš smazat tento soubor?'
+		'FILEIFRAMEFIELD.CONFIRMDELETE': 'Určitě chcete smazat tento soubor?'
 	});
 }
