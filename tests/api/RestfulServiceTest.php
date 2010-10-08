eec37c26946f101da1ef25d162f5cffb2e45f8af
Ingo Schommer <ingo@silverstripe.com>
2010-10-08 23:04:38 +0000

    BUGFIX Using mock controller in RestfulServiceTest to avoid problems with missing require() calls for PHPUnit/Framework.php (performed in recently merged PHPUnitWrapper::init() which is never called for "nested" true HTTP calls within unit tests). Mostly merged from branches/2.4.
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/sapphire/branches/2.3@111837 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- tests/api/RestfulServiceTest.php
+++ tests/api/RestfulServiceTest.php
@@ -1,35 +1,40 @@
 <?php
 
 class RestfulServiceTest extends SapphireTest {
+	
 	function testGetData() {
-		$connection = new RestfulService(Director::absoluteBaseURL());
+		$connection = new RestfulServiceTest_MockRestfulService(Director::absoluteBaseURL());
+		$test1url = 'RestfulServiceTest_Controller/';
 		$test1params = array(
 			'test1a' => 4352655636.76543, // number test
 			'test1b' => '$&+,/:;=?@#"\'%', // special char test. These should all get encoded
 			'test1c' => 'And now for a string test' // string test
 		);
 		$connection->setQueryString($test1params);
-		$test1 = $connection->request('RestfulServiceTest_Controller?usetestmanifest=1&flush=1')->getBody();
+		$test1 = $connection->request($test1url)->getBody();
 		foreach ($test1params as $key => $value) {
 			$this->assertContains("<request_item name=\"$key\">$value</request_item>", $test1);
 			$this->assertContains("<get_item name=\"$key\">$value</get_item>", $test1);
 		}
+		
 		$connection->setQueryString(array());
 		$test2params = array(
 			'test2a' => 767545678.76887, // number test
 			'test2b' => '%\'"@?=;:/,$', // special character checks
 			'test2c' => 'And now for a string test', // string test
 		);
-		$test2suburl = 'RestfulServiceTest_Controller/?usetestmanifest=1&flush=1&';
+		$test2suburl = 'RestfulServiceTest_Controller/?';
 		foreach ($test2params as $key=>$value) {
 			$test2suburl .= "$key=$value&";
 		}
+		
 		$test2suburl = substr($test2suburl, 0, -1);
 		$test2 = $connection->request($test2suburl)->getBody();
 		foreach ($test2params as $key => $value) {
 			$this->assertContains("<request_item name=\"$key\">$value</request_item>", $test2);
 			$this->assertContains("<get_item name=\"$key\">$value</get_item>", $test2);
 		}
+		
 		$test3params = array_merge($test1params, $test2params); // We want to check using setQueryString() and hard coded
 		$connection->setQueryString($test1params);
 		$test3 = $connection->request($test2suburl)->getBody();
@@ -40,13 +45,14 @@ class RestfulServiceTest extends SapphireTest {
 	}
 	
 	function testPostData() {
-		$connection = new RestfulService(Director::absoluteBaseURL(), 0);
+		$connection = new RestfulServiceTest_MockRestfulService(Director::absoluteBaseURL());
+		$url = 'RestfulServiceTest_Controller/';
 		$test1params = array(
 			'test1a' => mktime(),
 			'test1b' => mt_rand(),
 			'test1c' => 'And now for a string test'
 		);
-		$test1 = $connection->request('RestfulServiceTest_Controller/?usetestmanifest=1&flush=1', 'POST', $test1params)->getBody();
+		$test1 = $connection->request($url, 'POST', $test1params)->getBody();
 		foreach ($test1params as $key => $value) {
 			$this->assertContains("<request_item name=\"$key\">$value</request_item>", $test1);
 			$this->assertContains("<post_item name=\"$key\">$value</post_item>", $test1);
@@ -56,8 +62,9 @@ class RestfulServiceTest extends SapphireTest {
 	 * @expectedException PHPUnit_Framework_Error 
 	 */
 	function testIncorrectData() {
-		$connection = new RestfulService(Director::absoluteBaseURL(), 0);
-		$test1 = $connection->request('RestfulServiceTest_Controller/invalid?usetestmanifest=1&flush=1');
+		$connection = new RestfulServiceTest_MockRestfulService(Director::absoluteBaseURL());
+		$url = 'RestfulServiceTest_Controller/';
+		$test1 = $connection->request($url);
 		$test1->xpath("\\fail");
 	}
 }
@@ -92,8 +99,11 @@ $get	</get>
 $post	</post>
 </test>
 XML;
-		header('Content-type: text/xml');
-		echo $out;
+		
+		$this->response->setBody($out);
+		$this->response->addHeader('Content-type', 'text/xml');
+		
+		return $this->response;
 	}
 	
 	public function invalid() {
@@ -110,4 +120,70 @@ XML;
 	}
 }
 
-?>
+/**
+ * Mock implementation of {@link RestfulService}, which uses {@link Director::test()}
+ * instead of direct curl system calls.
+ * 
+ * @todo Less overloading of request()
+ * @todo Currently only works with relative (internal) URLs
+ * 
+ * @package sapphire
+ * @subpackage tests
+ */
+class RestfulServiceTest_MockRestfulService extends RestfulService {
+	
+	public $session = null;
+	
+	public function request($subURL = '', $method = "GET", $data = null, $headers = null) {
+		
+		if(!$this->session) {
+			$this->session = new Session(array());
+		}
+		
+		$url = $this->baseURL . $subURL; // Url for the request
+
+		if($this->queryString) {
+			if(strpos($url, '?') !== false) {
+				$url .= '&' . $this->queryString;
+			} else {
+				$url .= '?' . $this->queryString;
+			}
+		}
+		$url = str_replace(' ', '%20', $url); // Encode spaces
+	
+		// Custom for mock implementation: Director::test() doesn't cope with absolute URLs
+		$url = Director::makeRelative($url);
+		
+		$method = strtoupper($method);
+		
+		assert(in_array($method, array('GET','POST','PUT','DELETE','HEAD','OPTIONS')));
+		
+		// Add headers
+		if($this->customHeaders) {
+			$headers = array_merge((array)$this->customHeaders, (array)$headers);
+		}
+	
+		// Add authentication
+		if($this->authUsername) {
+			$headers[] = "Authorization: Basic " . base64_encode(
+				$this->authUsername.':'.$this->authPassword
+			);
+		}
+
+		// Custom for mock implementation: Use Director::test()
+		$body = null;
+		$postVars = null;
+		
+		if($method!='POST') $body = $data;
+		else $postVars = $data;
+
+		$responseFromDirector = Director::test($url, $postVars, $this->session, $method, $body, $headers);
+		
+		$response = new RestfulService_Response(
+			$responseFromDirector->getBody(), 
+			$responseFromDirector->getStatusCode()
+		);
+
+		return $response;
+	}
+}
