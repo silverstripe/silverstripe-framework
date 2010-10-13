268800dec614d78f5a5b0d34e503c4e353bbe5ef
Sam Minnee <sam@silverstripe.com>
2010-10-13 01:43:58 +0000

    BUGFIX #5316 Float and Double should never try to save NULL as the "null" value (from r102460)
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/sapphire/trunk@112067 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- core/model/fieldtypes/Double.php
+++ core/model/fieldtypes/Double.php
@@ -19,6 +19,33 @@ class Double extends DBField {
 	
 	function Nice() {
 		return number_format($this->value, 2);
-	}	
+	}
+	
+	/**
+	 * Returns the value to be set in the database to blank this field.
+	 * Usually it's a choice between null, 0, and ''
+	 */
+	function nullValue() {
+		return 0;
+	}
+
+	/**
+	 * Return an encoding of the given value suitable for inclusion in a SQL statement.
+	 * If necessary, this should include quotes.
+	 */
+	function prepValueForDB($value) {
+		if($value === true) {
+			return 1;
+		}
+		if(!$value || !is_numeric($value)) {
+			if(strpos($value, '[') === false) {
+				return '0';
+			} else {
+				return Convert::raw2sql($value);
+			}
+		} else {
+			return Convert::raw2sql($value);
+		}
+	}
+
 }
-?>
