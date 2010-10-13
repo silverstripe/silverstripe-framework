650a9f62c0a84e5a71df3bd87c1017ad112f8c6a
Sam Minnee <sam@silverstripe.com>
2010-10-13 03:33:16 +0000

    BUGFIX: default sort column now quoted (from r103182)
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/sapphire/trunk@112121 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- filesystem/Folder.php
+++ filesystem/Folder.php
@@ -11,7 +11,7 @@ class Folder extends File {
 
 	static $plural_name = "Folders";
 
-	static $default_sort = "Sort";
+	static $default_sort = "\"Sort\"";
 	
 	function populateDefaults() {
 		parent::populateDefaults();
