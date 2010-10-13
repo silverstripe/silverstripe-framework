94158efc45447e5f2a28733e72a743d47593b935
Sam Minnee <sam@silverstripe.com>
2010-10-13 04:06:50 +0000

    MINOR Removed mysql specific functionality from DataObjectSchemaGenerationTest and moved it to a new MySQLDatabaseTest (from r103799)
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/sapphire/trunk@112160 467b73ca-7a2a-4603-9d3b-597d59a354a9

---
+++ tests/model/MySQLDatabaseTest.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * @package sapphire
+ * @subpackage testing
+ */
+
+class MySQLDatabaseTest extends SapphireTest {
+	protected $extraDataObjects = array(
+		'MySQLDatabaseTest_DO',
+	);
+	
+	function setUp() {
+		if(DB::getConn() instanceof MySQLDatabase) {
+			MySQLDatabaseTest_DO::$db = array(
+				'MultiEnum1' => 'MultiEnum("A, B, C, D","")',
+				'MultiEnum2' => 'MultiEnum("A, B, C, D","A")',
+				'MultiEnum3' => 'MultiEnum("A, B, C, D","A, B")',
+			);
+		}
+			
+		parent::setUp();
+	}
+		
+	/**
+	 * Check that once a schema has been generated, then it doesn't need any more updating
+	 */
+	function testFieldsDontRerequestChanges() {
+		// These are MySQL specific :-S
+		if(DB::getConn() instanceof MySQLDatabase) {
+		
+			$db = DB::getConn();
+			DB::quiet();
+		
+			// Verify that it doesn't need to be recreated
+			$db->beginSchemaUpdate();
+			$obj = new MySQLDatabaseTest_DO();
+			$obj->requireTable();
+			$needsUpdating = $db->doesSchemaNeedUpdating();
+			$db->cancelSchemaUpdate();
+
+			$this->assertFalse($needsUpdating);
+		}
+	}
+}
+
+class MySQLDatabaseTest_DO extends DataObject implements TestOnly {
+	static $db = array();
+
+}
