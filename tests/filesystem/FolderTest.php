5ba7760f357557409a2a59d7b3a31d55e19aeea5
Sam Minnee <sam@silverstripe.com>
2010-10-13 03:48:33 +0000

    MINOR: Compiled duplicate tests
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/sapphire/trunk@112138 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- tests/filesystem/FolderTest.php
+++ tests/filesystem/FolderTest.php
@@ -33,4 +33,48 @@ class FolderTest extends SapphireTest {
 			'Path information is correctly saved to database (without trailing slash)'
 		);
 	}
+	
+	function testFilesystemFolderIsCreatedOnFirstWrite() {
+		$parentFolder = new Folder();
+		$parentFolder->Name = '__FolderTest';
+		$parentFolder->write();
+		$this->assertEquals(
+			$parentFolder->getFullPath(),
+			ASSETS_PATH . '/' . $parentFolder->Name . '/',
+			'Folder record creates matching path on filesystem on first write'
+		);
+		$this->assertFileExists(
+			$parentFolder->getFullPath(),
+			'Folder record without ParentID creates a folder in the $base_dir on filesystem on first write'
+		);
+		
+		$childFolder = new Folder();
+		$childFolder->ParentID = $parentFolder->ID;
+		$childFolder->Name = 'child';
+		$childFolder->write();
+		$this->assertEquals(
+			$childFolder->getFullPath(),
+			ASSETS_PATH . '/' . $parentFolder->Name . '/' . $childFolder->Name . '/',
+			'Folder record creates matching path on filesystem on first write'
+		);
+		$this->assertFileExists(
+			$childFolder->getFullPath(),
+			'Folder record without ParentID creates a folder on filesystem on first write'
+		);
+	}
+	
+	function testFolderNameCantDuplicate() {
+		$folder = new Folder();
+		$folder->Name = 'myfolder';
+		$folder->write();
+		
+		$folder2 = new Folder();
+		$folder2->Name = 'myfolder';
+		$folder2->write();
+		$this->assertNotEquals(
+			$folder->Name,
+			$folder2->Name,
+			'Folder write renames to avoid duplicates on filesystem'
+		);
+	}
 }
