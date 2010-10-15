ad7267aa4c6e8a984323c1455c7d9e975050555b
Ingo Schommer <ingo@silverstripe.com>
2010-10-15 03:23:43 +0000

    ENHANCEMENT Member_DatetimeOptionsetField toggle text is now translatable (from r107365)
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/sapphire/trunk@112570 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- security/Member.php
+++ security/Member.php
@@ -1872,7 +1872,7 @@ class Member_DatetimeOptionsetField extends OptionsetField {
 			_t('MemberDatetimeOptionsetField.Preview', 'Preview'),
 			Zend_Date::now()->toString($value)
 		) : '';
-		$options .= "<a class=\"formattingHelpToggle\" href=\"#\">toggle formatting help</a>";
+		$options .= "<a class=\"formattingHelpToggle\" href=\"#\">" . _t('MemberDatetimeOptionsetField.TOGGLEHELP', 'Toggle formatting help') . "</a>";
 		$options .= "<div class=\"formattingHelpText\">";
 		$options .= $this->getFormattingHelpText();
 		$options .= "</div>";
