ca7edd5ca5c31ef14f91def1ca20ce3be2a7e13b
Sam Minnee <sam@silverstripe.com>
2010-10-18 22:53:59 +0000

    BUGFIX Undefined variable when calling DataObject::many_many_extraFields() and relation name couldn't be found for the component (from r108400)
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/sapphire/trunk@112726 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- core/model/DataObject.php
+++ core/model/DataObject.php
@@ -1686,7 +1686,8 @@ class DataObject extends ViewableData implements DataObjectInterface, i18nEntity
 
 		foreach($classes as $class) {
 			if(in_array($class, array('ViewableData', 'Object', 'DataObject'))) continue;
-			
+			$relationName = null;
+
 			// Find extra fields for one component
 			if($component) {
 				$SNG_class = singleton($class);
@@ -1704,7 +1705,6 @@ class DataObject extends ViewableData implements DataObjectInterface, i18nEntity
 					$candidateManyMany = $SNG_candidate->stat('belongs_many_many');
 					
 					// Find the relation given the class
-					$relationName = null;
 					if($candidateManyMany) foreach($candidateManyMany as $relation => $relatedClass) {
 						if($relatedClass == $class) {
 							$relationName = $relation;
