889f888c1ad40d0316d6176232f3794ce0eea126
Paul Meyrick <paul@silverstripe.com>
2010-12-02 23:37:59 +0000

    FIX: Revert last commit
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/sapphire/branches/2.4@114463 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- core/model/MySQLDatabase.php
+++ core/model/MySQLDatabase.php
@@ -823,11 +823,8 @@ class MySQLDatabase extends SS_Database {
 		$records = DB::query($fullQuery);
 
 		foreach($records as $record)
-			if(!$record['ClassName']){
-				throw new Exception("ClassName can not be null, ClassName not set for ". $record['Title']);
-			}else{
 				$objects[] = new $record['ClassName']($record);
-			}
+
 
 		if(isset($objects)) $doSet = new DataObjectSet($objects);
 		else $doSet = new DataObjectSet();
