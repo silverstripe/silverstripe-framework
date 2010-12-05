f8c970ec0c8548330d342f6ac7b8db000cfc0088
Ingo Schommer <ingo@silverstripe.com>
2010-12-05 00:19:57 +0000

    ENHANCEMENT Added RandomGenerator for more secure CRSF tokens etc. (from r114497)
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/sapphire/branches/2.4@114499 467b73ca-7a2a-4603-9d3b-597d59a354a9

---
+++ tests/security/RandomGeneratorTest.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * @package sapphire
+ * @subpackage tests
+ * @author Ingo Schommer
+ */
+class RandomGeneratorTest extends SapphireTest {
+
+	function testGenerateEntropy() {
+		$r = new RandomGenerator();
+		$this->assertNotNull($r->generateEntropy());
+		$this->assertNotEquals($r->generateEntropy(), $r->generateEntropy());
+	}
+	
+	function testGenerateHash() {
+		$r = new RandomGenerator();
+		$this->assertNotNull($r->generateHash());
+		$this->assertNotEquals($r->generateHash(), $r->generateHash());
+	}
+	
+	function testGenerateHashWithAlgorithm() {
+		$r = new RandomGenerator();
+		$this->assertNotNull($r->generateHash('md5'));
+		$this->assertNotEquals($r->generateHash(), $r->generateHash('md5'));
+	}
+	
+}
