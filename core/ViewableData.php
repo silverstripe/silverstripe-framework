976174d8842b518cda5d10b44343989c0f3d5c4e
Sean Harvey <sean@silverstripe.com>
2010-12-15 05:30:42 +0000

    BUGFIX ViewableData::defineMethods() broken method cache because of parameter mismatch to create_function()
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/sapphire/trunk@115063 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- core/ViewableData.php
+++ core/ViewableData.php
@@ -180,8 +180,8 @@ class ViewableData extends Object implements IteratorAggregate {
 		
 		foreach($this->allMethodNames() as $method) {
 			if($method[0] == '_' && $method[1] != '_') {
-				$this->createMethod (
-					substr($method, 1), "return \$obj->cachedCall('$method', '" . substr($method, 1) . "', \$args);"
+				$this->createMethod(
+					substr($method, 1), "return \$obj->cachedCall('$method', \$args, '" . substr($method, 1) . "');"
 				);
 			}
 		}
