05f1fe2316422cbe697a128ddda86e086ce41371
Geoff Munn <geoff@silverstripe.com>
2011-01-21 03:50:30 +0000

    BUGFIX: transaction function names fixed
    git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/sapphire/branches/2.4@115720 467b73ca-7a2a-4603-9d3b-597d59a354a9

--- tests/TransactionTest.php
+++ tests/TransactionTest.php
@@ -4,60 +4,60 @@
  * @subpackage tests
  */
 class TransactionTest extends SapphireTest {
-	
+
 	function testCreateWithTransaction() {
-		
+
 		if(DB::getConn()->supportsTransactions()==true){
-			DB::getConn()->startTransaction();
+			DB::getConn()->transactionStart();
 			$page=new Page();
 			$page->Title='First page';
 			$page->write();
-			
+
 			$page=new Page();
 			$page->Title='Second page';
 			$page->write();
-			
+
 			//Create a savepoint here:
 			DB::getConn()->transactionSavepoint('rollback');
-			
+
 			$page=new Page();
 			$page->Title='Third page';
 			$page->write();
-			
+
 			$page=new Page();
 			$page->Title='Forth page';
 			$page->write();
-			
+
 			//Revert to a savepoint:
 			DB::getConn()->transactionRollback('rollback');
-			
-			DB::getConn()->endTransaction();
-			
+
+			DB::getConn()->transactionEnd();
+
 			$first=DataObject::get('Page', "\"Title\"='First page'");
 			$second=DataObject::get('Page', "\"Title\"='Second page'");
 			$third=DataObject::get('Page', "\"Title\"='Third page'");
 			$forth=DataObject::get('Page', "\"Title\"='Forth page'");
-			
+
 			//These pages should be in the system
 			$this->assertTrue(is_object($first) && $first->exists());
 			$this->assertTrue(is_object($second) && $second->exists());
-			
+
 			//These pages should NOT exist, we reverted to a savepoint:
 			$this->assertFalse(is_object($third) && $third->exists());
-			$this->assertFalse(is_object($forth) && $forth->exists());		
+			$this->assertFalse(is_object($forth) && $forth->exists());
 		}
 	}
-	
+
 	function testReadOnlyTransaction(){
-		
+
 		if(DB::getConn()->supportsTransactions()==true){
-			
+
 			$page=new Page();
 			$page->Title='Read only success';
 			$page->write();
-			
-			DB::getConn()->startTransaction('READ ONLY');
-			
+
+			DB::getConn()->transactionStart('READ ONLY');
+
 			try {
 				$page=new Page();
 				$page->Title='Read only page failed';
@@ -67,21 +67,21 @@ class TransactionTest extends SapphireTest {
 				//We need to do a rollback or a commit otherwise we'll get error messages
 				DB::getConn()->transactionRollback();
 			}
-			
-			DB::getConn()->endTransaction();
+
+			DB::getConn()->transactionEnd();
 			$success=DataObject::get('Page', "\"Title\"='Read only success'");
 			$fail=DataObject::get('Page', "\"Title\"='Read only failed'");
-			
+
 			//This page should be in the system
 			$this->assertTrue(is_object($success) && $success->exists());
-			
+
 			//This page should NOT exist, we had 'read only' permissions
 			$this->assertFalse(is_object($fail) && $fail->exists());
-					
+
 		}
-		
+
 	}
-	
+
 }
 
 ?>
