silverstripe/silverstripe-framework

commit 470e53f2d1ed1bd349f86fce2acfd1d4f2a337c6

ENHANCEMENT Allowing custom popup requirements in ComplexTableField without subclassing through $requirementsForPopupCallback MINOR Added getParentController() to TableListField_ItemRequest and ComplexTableField_Popup MINOR Extending ComplexTableField_ItemRequest from TableListField_ItemRequest to avoid redundant code
git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/sapphire/branches/2.4@100774 467b73ca-7a2a-4603-9d3b-597d59a354a9

forms/ComplexTableField.php
@@ -35,8 +35,7 @@ class ComplexTableField extends TableListField {
 	 *  - A FieldSet object: Use that field set directly.
 	 *  - A method name, eg, 'getCMSFields': Call that method on the child object to get the fields.
 	 */
-     
-    protected $addTitle;
+	protected $addTitle;
     
 	protected $detailFormFields;
 	
@@ -107,6 +106,13 @@ class ComplexTableField extends TableListField {
 	 * This is set by javascript and used by greybox.
 	 */
 	protected $popupCaption = null;
+	
+	/**
+	 * @var callback A function callback invoked
+	 * after initializing the popup and its base calls to
+	 * the {@link Requirements} class.
+	 */
+	public $requirementsForPopupCallback = null;
 
 	/**
 	 * @var $detailFormValidator Validator
@@ -682,7 +688,7 @@ JS;
 /**
  * @todo Tie this into ComplexTableField_Item better.
  */
-class ComplexTableField_ItemRequest extends RequestHandler {
+class ComplexTableField_ItemRequest extends TableListField_ItemRequest {
 	protected $ctf;
 	protected $itemID;
 	protected $methodName;
@@ -695,14 +701,7 @@ class ComplexTableField_ItemRequest extends RequestHandler {
 	function Link($action = null) {
 		return Controller::join_links($this->ctf->Link(), '/item/', $this->itemID, $action);
 	}
-	
-	function __construct($ctf, $itemID) {
-		$this->ctf = $ctf;
-		$this->itemID = $itemID;
 		
-		parent::__construct();
-	}
-	
 	function index() {
 		return $this->show();
 	}
@@ -1080,11 +1079,24 @@ class ComplexTableField_Popup extends Form {
 		Requirements::add_i18n_javascript(SAPPHIRE_DIR . '/javascript/lang');
 		Requirements::javascript(SAPPHIRE_DIR . "/javascript/ComplexTableField_popup.js");
 
+		// Append requirements from instance callbacks
+		$callback = $this->getParentController()->getParentController()->requirementsForPopupCallback;
+		if($callback) call_user_func($callback, $this);
+
+		// Append requirements from DataObject
+		// DEPRECATED 2.4 Use ComplexTableField->requirementsForPopupCallback
  		if($this->dataObject->hasMethod('getRequirementsForPopup')) {
 			$this->dataObject->getRequirementsForPopup();
 		}
 		
 		return $ret;
+	}
+	
+	/**
+	 * @return ComplexTableField_ItemRequest
+	 */
+	function getParentController() {
+		return $this->controller;
 	}
 }
 

forms/TableListField.php
@@ -1480,5 +1480,12 @@ class TableListField_ItemRequest extends RequestHandler {
 		}
 		return false;
 	}
+	
+	/**
+	 * @return TableListField
+	 */
+	function getParentController() {
+		return $this->ctf;
+	}
 }
 ?>

commit 939ec09536a5e17156d28e5b4c9986f2b121a84d

MINOR Fixed regression from r100774
git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/sapphire/branches/2.4@100775 467b73ca-7a2a-4603-9d3b-597d59a354a9

forms/ComplexTableField.php
@@ -1080,7 +1080,12 @@ class ComplexTableField_Popup extends Form {
 		Requirements::javascript(SAPPHIRE_DIR . "/javascript/ComplexTableField_popup.js");
 
 		// Append requirements from instance callbacks
-		$callback = $this->getParentController()->getParentController()->requirementsForPopupCallback;
+		$parent = $this->getParentController();
+		if($parent instanceof ComplexTableField) {
+			$callback = $parent->requirementsForPopupCallback;
+		} else {
+			$callback = $parent->getParentController()->requirementsForPopupCallback;
+		}
 		if($callback) call_user_func($callback, $this);
 
 		// Append requirements from DataObject