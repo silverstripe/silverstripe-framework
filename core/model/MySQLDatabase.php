silverstripe/silverstripe-framework

commit e81f11cc60cbee7425d27a560e03d499717e2628

ENHANCEMENT MySQLDatabase::renameField() no longer checks that the field exists in fieldList(). alterField() does no such check, so it should be consistent. Removing this should provide a small performance improvement as well
git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/sapphire/branches/2.4@112272 467b73ca-7a2a-4603-9d3b-597d59a354a9

core/model/MySQLDatabase.php
@@ -321,10 +321,7 @@ class MySQLDatabase extends SS_Database {
 	 * @param string $newName The new name of the field
 	 */
 	public function renameField($tableName, $oldName, $newName) {
-		$fieldList = $this->fieldList($tableName);
-		if(array_key_exists($oldName, $fieldList)) {
-			$this->query("ALTER TABLE \"$tableName\" CHANGE \"$oldName\" \"$newName\" " . $fieldList[$oldName]);
-		}
+		$this->query("ALTER TABLE \"$tableName\" CHANGE \"$oldName\" \"$newName\" " . $fieldList[$oldName]);
 	}
 	
 	private static $_cache_collation_info = array();

commit 9f673c1e9ad38c7ba078665ec99d54407acc01ae

MINOR Reverted regression in r112272
git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/sapphire/branches/2.4@112278 467b73ca-7a2a-4603-9d3b-597d59a354a9

core/model/MySQLDatabase.php
@@ -321,7 +321,10 @@ class MySQLDatabase extends SS_Database {
 	 * @param string $newName The new name of the field
 	 */
 	public function renameField($tableName, $oldName, $newName) {
-		$this->query("ALTER TABLE \"$tableName\" CHANGE \"$oldName\" \"$newName\" " . $fieldList[$oldName]);
+		$fieldList = $this->fieldList($tableName); 
+		if(array_key_exists($oldName, $fieldList)) { 
+			$this->query("ALTER TABLE \"$tableName\" CHANGE \"$oldName\" \"$newName\" " . $fieldList[$oldName]); 
+		}
 	}
 	
 	private static $_cache_collation_info = array();