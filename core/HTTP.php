silverstripe/silverstripe-framework

commit 792b4cc1c4dedf8065ef0fbf7d9bd1101ce4d5c6

API CHANGE HTTP::setGetVar() always returns absolute URLs. Use Director::makeRelative() to make them relative again. API CHANGE HTTP::setGetVar() combines any GET parameters in PHP array notation (e.g. "foo[bar]=val") instead of replacing the whole array BUGFIX HTTP::setGetVar() uses parse_url() and http_build_query() to add query parameters to an existing URL, instead of doing its own regex-based parsing. This means existing GET parameters are correctly url encoded.
git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/sapphire/branches/2.4@98373 467b73ca-7a2a-4603-9d3b-597d59a354a9

core/HTTP.php
@@ -73,26 +73,59 @@ class HTTP {
 		return $content;
 	}
 
+	/**
+	 * Will try to include a GET parameter for an existing URL,
+	 * preserving existing parameters and fragments.
+	 * If no URL is given, falls back to $_SERVER['REQUEST_URI'].
+	 * 
+	 * CAUTION: If the URL is determined to be relative,
+	 * it is prepended with Director::absoluteBaseURL().
+	 * This method will always return an absolute URL because
+	 * Director::makeRelative() can lead to inconsistent results.
+	 * 
+	 * @param String $varname
+	 * @param String $varvalue
+	 * @param String $currentURL Relative or absolute URL (Optional).
+	 * @return String Absolute URL
+	 */
 	public static function setGetVar($varname, $varvalue, $currentURL = null) {
-		$scriptbase = $currentURL ? $currentURL : $_SERVER['REQUEST_URI'];
-		
-		$scriptbase = str_replace('&amp;', '&', $scriptbase);
-		$scriptbase = preg_replace('/\?' . quotemeta($varname) . '=([^&]*)&/', '?', $scriptbase);
-		$scriptbase = preg_replace('/([\?&]+)' . quotemeta($varname) . '=([^&]*)/', null, $scriptbase);
-		
-		$suffix = '';
-		if(($hashPos = strpos($scriptbase,'#')) !== false) {
-			$suffix .= substr($scriptbase, $hashPos);
-			$scriptbase = substr($scriptbase, 0, $hashPos);
+		$uri = $currentURL ? $currentURL : Director::makeRelative($_SERVER['REQUEST_URI']);
+
+		// We need absolute URLs for parse_url()
+		if(Director::is_relative_url($uri)) $uri = Director::absoluteBaseURL() . $uri;
+
+		// try to parse uri
+		$parts = parse_url($uri);
+		if(!$parts) {
+			throw new InvalidArgumentException("Can't parse URL: " . $uri);
 		}
 
-		if($varvalue !== null) {
-			$scriptbase .= (strrpos($scriptbase,'?') !== false) ? '&' : '?';
-			$scriptbase .= "$varname=" . (isset($urlEncodeVarValue) ? urlencode($varvalue) : $varvalue);
-		}
+		// Parse params and add new variable
+		$params = array();
+		if(isset($parts['query'])) parse_str($parts['query'], $params);
+		$params[$varname] = $varvalue;
 
-		$scriptbase = str_replace('&','&amp;',$scriptbase);
-		return $scriptbase . $suffix;
+		// Recompile Uri
+		$newUri = $parts['scheme'] . '://' . (
+			isset($parts['user']) && $parts['user'] != '' && isset($parts['pass']) 
+				? $parts['user'] . ':' . $parts['pass'] . '@' 
+				: ''
+			) .
+			$parts['host'] . (
+				isset($parts['path']) && $parts['path'] != ''
+				? $parts['path']
+				: ''
+			) . (
+				($params)
+				? '?' . http_build_query($params)
+				: ''
+			) . (
+				isset($parts['fragment']) && $parts['fragment'] != ''
+				? '#' . $parts['fragment']
+			: ''
+		);
+
+		return $newUri;
 	}
 
 	static function RAW_setGetVar($varname, $varvalue, $currentURL = null) {

tests/HTTPTest.php
@@ -47,19 +47,51 @@ class HTTPTest extends SapphireTest {
 	 * Tests {@link HTTP::setGetVar()}
 	 */
 	public function testSetGetVar() {
-		$expected = array (
-			'/?foo=bar'         => array('foo', 'bar', '/'),
-			'/?baz=buz&foo=bar' => array('foo', 'bar', '/?baz=buz'),
-			'/?buz=baz&foo=baz' => array('foo', 'baz', '/?foo=bar&buz=baz'),
-			'/?foo=var'         => array('foo', 'var', '/?foo=&foo=bar'),
-			'/?foo[test]=var'   => array('foo[test]', 'var', '/?foo[test]=another')
+		// HACK No easy way to get the current URL without the query string or fragment
+		$base = Director::absoluteBaseURL() . 'dev/tests/HTTPTest';
+
+		$this->assertEquals(
+			$base . '?foo=bar',
+			HTTP::setGetVar('foo', 'bar'),
+			'Omitting a URL falls back to current URL'
+	);
+
+		$this->assertEquals(
+			Director::absoluteBaseURL() . 'relative/url?foo=bar',
+			HTTP::setGetVar('foo', 'bar', 'relative/url'),
+			'Relative URL without slash prefix returns URL with absolute base'
 		);
-		
-		foreach($expected as $result => $args) {
-			$this->assertEquals(
-				call_user_func_array(array('HTTP', 'setGetVar'), $args), str_replace('&', '&amp;', $result)
-			);
-		}
+
+		$this->assertEquals(
+			Director::absoluteBaseURL() . '/relative/url?foo=bar',
+			HTTP::setGetVar('foo', 'bar', '/relative/url'),
+			'Relative URL with slash prefix returns URL with absolute base'
+		);
+
+		$this->assertEquals(
+			Director::absoluteBaseURL() . '/relative/url?baz=buz&foo=bar',
+			HTTP::setGetVar('foo', 'bar', '/relative/url?baz=buz'),
+			'Relative URL with existing query params, and new added key'
+		);
+
+		$this->assertEquals(
+			'http://test.com/?foo=new&buz=baz',
+			HTTP::setGetVar('foo', 'new', 'http://test.com/?foo=old&buz=baz'),
+			'Absolute URL without path and multipe existing query params, overwriting an existing parameter'
+		);
+
+		$this->assertEquals(
+			'http://test.com/?foo=new',
+			HTTP::setGetVar('foo', 'new', 'http://test.com/?foo=&foo=old'),
+			'Absolute URL and empty query param'
+		);
+
+		$this->assertEquals(
+			// http_build_query() escapes angular brackets, they should be correctly urldecoded by the browser client
+			'http://test.com/?foo%5Btest%5D=one&foo%5Btest%5D=two',
+			HTTP::setGetVar('foo[test]', 'two', 'http://test.com/?foo[test]=one'),
+			'Absolute URL and PHP array query string notation'
+		);
 	}
 	
 }

commit 0f324c852e0322e73c2e5d5fda9f9d3d234cc931

BUGFIX Produce XHTML compliant URLs in HTTP::setGetVar() by default (regression from r98373, see #5101)
git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/sapphire/branches/2.4@100904 467b73ca-7a2a-4603-9d3b-597d59a354a9

core/HTTP.php
@@ -77,6 +77,9 @@ class HTTP {
 	 * Will try to include a GET parameter for an existing URL,
 	 * preserving existing parameters and fragments.
 	 * If no URL is given, falls back to $_SERVER['REQUEST_URI'].
+	 * Uses parse_url() to dissect the URL, and http_build_query() to reconstruct it
+	 * with the additional parameter. Converts any '&' (ampersand)
+	 * URL parameter separators to the more XHTML compliant '&amp;'.
 	 * 
 	 * CAUTION: If the URL is determined to be relative,
 	 * it is prepended with Director::absoluteBaseURL().
@@ -117,7 +120,8 @@ class HTTP {
 				: ''
 			) . (
 				($params)
-				? '?' . http_build_query($params)
+				// XHTML compliant by default
+				? '?' . http_build_query($params, null, '&amp;')
 				: ''
 			) . (
 				isset($parts['fragment']) && $parts['fragment'] != ''

tests/HTTPTest.php
@@ -71,7 +71,7 @@ class HTTPTest extends SapphireTest {
 			);
 		}
 		
-		foreach(array($expectedBasePath, '/relative/url?baz=buz&foo=bar') as $e) {
+		foreach(array($expectedBasePath, '/relative/url?baz=buz&amp;foo=bar') as $e) {
 			$this->assertContains(
 				$e,
 				HTTP::setGetVar('foo', 'bar', '/relative/url?baz=buz'),
@@ -80,7 +80,7 @@ class HTTPTest extends SapphireTest {
 		}
 
 		$this->assertEquals(
-			'http://test.com/?foo=new&buz=baz',
+			'http://test.com/?foo=new&amp;buz=baz',
 			HTTP::setGetVar('foo', 'new', 'http://test.com/?foo=old&buz=baz'),
 			'Absolute URL without path and multipe existing query params, overwriting an existing parameter'
 		);
@@ -93,7 +93,7 @@ class HTTPTest extends SapphireTest {
 
 		$this->assertEquals(
 			// http_build_query() escapes angular brackets, they should be correctly urldecoded by the browser client
-			'http://test.com/?foo%5Btest%5D=one&foo%5Btest%5D=two',
+			'http://test.com/?foo%5Btest%5D=one&amp;foo%5Btest%5D=two',
 			HTTP::setGetVar('foo[test]', 'two', 'http://test.com/?foo[test]=one'),
 			'Absolute URL and PHP array query string notation'
 		);