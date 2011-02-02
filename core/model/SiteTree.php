silverstripe/silverstripe-framework

commit 76dfdd85ea52dccc7e9a569c6dc8c4e632fcaad6

BUGFIX: Make URLs lowercase
git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/sapphire/branches/2.4@104679 467b73ca-7a2a-4603-9d3b-597d59a354a9

core/model/SiteTree.php
@@ -1507,7 +1507,7 @@ class SiteTree extends DataObject implements PermissionProvider,i18nEntityProvid
 	 */
 	function generateURLSegment($title){
 		$t = mb_strtolower($title);
-		$t = Object::create('Transliterator')->toASCII($title);
+		$t = Object::create('Transliterator')->toASCII($t);
 		$t = str_replace('&amp;','-and-',$t);
 		$t = str_replace('&','-and-',$t);
 		$t = ereg_replace('[^A-Za-z0-9]+','-',$t);

commit d7cb0fc39d9ecaf15d9307da6f8e3eed3a04e19a

MINOR Fixed hidden mbstring reliance in SiteTree->generateURLSegment() (broken in r104679)
git-svn-id: svn://svn.silverstripe.com/silverstripe/open/modules/sapphire/branches/2.4@104883 467b73ca-7a2a-4603-9d3b-597d59a354a9

core/model/SiteTree.php
@@ -1506,7 +1506,7 @@ class SiteTree extends DataObject implements PermissionProvider,i18nEntityProvid
 	 * @return string Generated url segment
 	 */
 	function generateURLSegment($title){
-		$t = mb_strtolower($title);
+		$t = (function_exists('mb_strtolower')) ? mb_strtolower($title) : strtolower($title);
 		$t = Object::create('Transliterator')->toASCII($t);
 		$t = str_replace('&amp;','-and-',$t);
 		$t = str_replace('&','-and-',$t);