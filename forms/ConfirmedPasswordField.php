b088d6b4e72ffc9ffb76d840e9907d4bd7b4814f
Julian Seidenberg <julian@silverstripe.com>
2011-02-25 17:01:46 +1300

    ENHANCEMENT: replaced Prototype show/hide functionality in ConfirmedPasswordField with jQuery. Also, added additional test case.

--- forms/ConfirmedPasswordField.php
+++ forms/ConfirmedPasswordField.php
@@ -94,9 +94,8 @@ class ConfirmedPasswordField extends FormField {
 	}
 	
 	function Field() {
-		Requirements::javascript(SAPPHIRE_DIR . '/thirdparty/prototype/prototype.js');
-		Requirements::javascript(SAPPHIRE_DIR . '/thirdparty/behaviour/behaviour.js');
-		Requirements::javascript(SAPPHIRE_DIR . '/javascript/prototype_improvements.js');
+		Requirements::javascript(SAPPHIRE_DIR . '/thirdparty/jquery/jquery.js');
+		Requirements::javascript(SAPPHIRE_DIR . '/thirdparty/jquery-entwine/dist/jquery.entwine-dist.js');
 		Requirements::javascript(SAPPHIRE_DIR . '/javascript/ConfirmedPasswordField.js');
 		
 		$content = '';
