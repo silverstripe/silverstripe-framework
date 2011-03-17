77fa855cdab748057843214dee9b3c54141dd454
Ingo Schommer <ingo@silverstripe.com>
2011-03-18 10:50:57 +1300

    Allowing success and error callbacks in refresh()

--- thirdparty/jstree/jquery.jstree.js
+++ thirdparty/jstree/jquery.jstree.js
@@ -523,7 +523,7 @@
 				}
 				this.__callback({});
 			},
-			refresh : function (obj) {
+			refresh : function (obj, s_call, e_call) {
 				var _this = this;
 				this.save_opened();
 				if(!obj) { obj = -1; }
@@ -531,7 +531,15 @@
 				if(!obj) { obj = -1; }
 				if(obj !== -1) { obj.children("UL").remove(); }
 				else { this.get_container_ul().empty(); }
-				this.load_node(obj, function () { _this.__callback({ "obj" : obj}); _this.reload_nodes(); });
+				this.load_node(
+					obj, 
+					function () { 
+						_this.__callback({ "obj" : obj}); 
+						_this.reload_nodes(); 
+						if(s_call) s_call.call(this); 
+					},
+					e_call
+				);
 			},
 			// Dummy function to fire after the first load (so that there is a jstree.loaded event)
 			loaded	: function () { 
