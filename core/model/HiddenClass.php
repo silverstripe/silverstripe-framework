1225426e75990771848ba92da617e7c5a0fc998d
Ingo Schommer <ingo@silverstripe.com>
2011-03-23 16:13:51 +1300

    MINOR Fixed @package declarations

--- core/model/HiddenClass.php
+++ core/model/HiddenClass.php
@@ -2,7 +2,7 @@
 /**
  * Anything that implements HiddenClass won't be shown in user-interface elements.  For example,
  * DataObjects that implement HiddenClass won't be showing in the "new page" dropdown.
- * @package cms
+ * @package sapphire
  */
 interface HiddenClass {
 	
