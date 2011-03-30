0fe51da1ab27b0da56561eefc44ed31ef6b03c3d
Julian Seidenberg <julian@silverstripe.com>
2011-03-30 14:25:38 +1300

    BUGFIX: rewriting test to not hard-code URLSegment of homepage. If another previous test creates a page with the same URLSegment, this test's page gets URLSegment of "home-2" and the test breaks. This fix prevents that scenario.

--- tests/model/DbDatetimeTest.php
+++ tests/model/DbDatetimeTest.php
@@ -61,6 +61,8 @@ class DbDatetimeTest extends FunctionalTest {
 
 	function testDbDatetimeFormat() {
 		if($this->supportDbDatetime) {
+			$pageSegment = $this->objFromFixture("Page","first")->URLSegment;
+
 			$query = 'SELECT ' . $this->adapter->formattedDatetimeClause('1973-10-14 10:30:00', '%H:%i, %d/%m/%Y');
 			$result = DB::query($query)->value();
 			$this->matchesRoughly($result, date('H:i, d/m/Y', strtotime('1973-10-14 10:30:00')), 'nice literal time');
@@ -69,15 +71,16 @@ class DbDatetimeTest extends FunctionalTest {
 			$result = DB::query($query)->value();
 			$this->matchesRoughly($result, date('d', $this->getDbNow()), 'todays day');
 
-			$query = 'SELECT ' . $this->adapter->formattedDatetimeClause('"Created"', '%U') . ' AS test FROM "SiteTree" WHERE "URLSegment" = \'home\'';
+			$query = 'SELECT ' . $this->adapter->formattedDatetimeClause('"Created"', '%U') . ' AS test FROM "SiteTree" WHERE "URLSegment" = \''.$pageSegment.'\'';
 			$result = DB::query($query)->value();
 
-			$this->matchesRoughly($result, strtotime(DataObject::get_one('SiteTree',"\"URLSegment\" = 'home'")->Created), 'SiteTree[home]->Created as timestamp');
+			$this->matchesRoughly($result, strtotime(DataObject::get_one('SiteTree',"\"URLSegment\" = '$pageSegment'")->Created), "SiteTree[$pageSegment]->Created as timestamp");
 		}
 	}
-	
+
 	function testDbDatetimeInterval() {
 		if($this->supportDbDatetime) {
+			$pageSegment = $this->objFromFixture("Page","first")->URLSegment;
 
 			$query = 'SELECT ' . $this->adapter->datetimeIntervalClause('1973-10-14 10:30:00', '+18 Years');
 			$result = DB::query($query)->value();
@@ -87,15 +90,17 @@ class DbDatetimeTest extends FunctionalTest {
 			$result = DB::query($query)->value();
 			$this->matchesRoughly($result, date('Y-m-d H:i:s', strtotime('+1 Day', $this->getDbNow())), 'tomorrow');
 
-			$query = 'SELECT ' . $this->adapter->datetimeIntervalClause('"Created"', '-15 Minutes') . ' AS "test" FROM "SiteTree" WHERE "URLSegment" = \'home\'';
+			$firstpage = $this->objFromFixture("Page","first");
+			$query = 'SELECT ' . $this->adapter->datetimeIntervalClause('"Created"', '-15 Minutes') . ' AS "test" FROM "SiteTree" WHERE "URLSegment" = \''.$pageSegment.'\'';
 			$result = DB::query($query)->value();
-			$this->matchesRoughly($result, date('Y-m-d H:i:s', strtotime(Dataobject::get_one('SiteTree',"\"URLSegment\" = 'home'")->Created) - 900), '15 Minutes before creating SiteTree[home]');
+			$this->matchesRoughly($result, date('Y-m-d H:i:s', strtotime(Dataobject::get_one('SiteTree',"\"URLSegment\" = '$pageSegment'")->Created) - 900), "15 Minutes before creating SiteTree[$pageSegment]");
 
 		}
 	}
-	
+
 	function testDbDatetimeDifference() {
 		if($this->supportDbDatetime) {
+			$pageSegment = $this->objFromFixture("Page","first")->URLSegment;
 
 			$query = 'SELECT ' . $this->adapter->datetimeDifferenceClause('1974-10-14 10:30:00', '1973-10-14 10:30:00');
 			$result = DB::query($query)->value();
@@ -109,13 +114,12 @@ class DbDatetimeTest extends FunctionalTest {
 			$result = DB::query($query)->value();
 			$this->matchesRoughly($result, -45 * 60, 'now - 45 minutes ahead');
 
-			$query = 'SELECT ' . $this->adapter->datetimeDifferenceClause('"LastEdited"', '"Created"') . ' AS "test" FROM "SiteTree" WHERE "URLSegment" = \'home\'';
+			$query = 'SELECT ' . $this->adapter->datetimeDifferenceClause('"LastEdited"', '"Created"') . ' AS "test" FROM "SiteTree" WHERE "URLSegment" = \''.$pageSegment.'\'';
 			$result = DB::query($query)->value();
-			$lastedited = Dataobject::get_one('SiteTree',"\"URLSegment\" = 'home'")->LastEdited;
-			$created = Dataobject::get_one('SiteTree',"\"URLSegment\" = 'home'")->Created;
+			$lastedited = Dataobject::get_one('SiteTree',"\"URLSegment\" = '$pageSegment'")->LastEdited;
+			$created = Dataobject::get_one('SiteTree',"\"URLSegment\" = '$pageSegment'")->Created;
 			$this->matchesRoughly($result, strtotime($lastedited) - strtotime($created), 'age of HomePage record in seconds since unix epoc');
 
 		}
-	}
-	
+	}	
 }
