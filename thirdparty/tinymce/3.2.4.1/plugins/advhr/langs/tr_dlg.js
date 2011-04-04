2e71dea3b6d56f26e22315a3426fd4b9c3b4fcf3
phalkunz <phalkun@falkonz.silverstripe.com>
2011-04-05 11:42:57 +1200

    MINOR: Upgrade tinymce to version 3.4.1

--- thirdparty/tinymce/3.2.4.1/plugins/advhr/langs/tr_dlg.js
+++
@@ -1,5 +0,0 @@
-tinyMCE.addI18n('tr.advhr_dlg',{
-width:"Geni\u015Flik",
-size:"Y\u00FCkseklik",
-noshade:"G\u00F6lge yok"
-});
--- thirdparty/tinymce/plugins/advhr/langs/vi_dlg.js
+++
@@ -1,5 +0,0 @@
-tinyMCE.addI18n('vi.advhr_dlg',{
-width:"Chi\u1EC1u r\u1ED9ng",
-size:"Chi\u1EC1u cao",
-noshade:"Kh\u00F4ng \u0111\u1ED5 b\u00F3ng"
-});
