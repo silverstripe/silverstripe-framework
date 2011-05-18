silverstripe/silverstripe-framework

commit a9b13509d2c10054241f68932c1700b8c8c96d5d

MINOR Removed dependency on SiteTree in various unit tests

tests/model/DbDatetimeTest.php
@@ -3,6 +3,8 @@
 class DbDatetimeTest extends FunctionalTest {
 
 	static $fixture_file = 'sapphire/tests/model/DbDatetimeTest.yml';
+	
+	protected $extraDataObjects = array('DbDatetimeTest_Team');
 
 	private static $offset = 0;					// number of seconds of php and db time are out of sync
 	private static $offset_thresholds = array(	// throw an error if the offset exceeds 30 minutes
@@ -69,10 +71,10 @@ class DbDatetimeTest extends FunctionalTest {
 			$result = DB::query($query)->value();
 			$this->matchesRoughly($result, date('d', $this->getDbNow()), 'todays day');
 
-			$query = 'SELECT ' . $this->adapter->formattedDatetimeClause('"Created"', '%U') . ' AS test FROM "SiteTree" WHERE "URLSegment" = \'home\'';
+			$query = 'SELECT ' . $this->adapter->formattedDatetimeClause('"Created"', '%U') . ' AS test FROM "DbDateTimeTest_Team"';
 			$result = DB::query($query)->value();
 
-			$this->matchesRoughly($result, strtotime(DataObject::get_one('SiteTree',"\"URLSegment\" = 'home'")->Created), 'SiteTree[home]->Created as timestamp');
+			$this->matchesRoughly($result, strtotime(DataObject::get_one('DbDateTimeTest_Team')->Created), 'fixture ->Created as timestamp');
 		}
 	}
 	
@@ -87,9 +89,9 @@ class DbDatetimeTest extends FunctionalTest {
 			$result = DB::query($query)->value();
 			$this->matchesRoughly($result, date('Y-m-d H:i:s', strtotime('+1 Day', $this->getDbNow())), 'tomorrow');
 
-			$query = 'SELECT ' . $this->adapter->datetimeIntervalClause('"Created"', '-15 Minutes') . ' AS "test" FROM "SiteTree" WHERE "URLSegment" = \'home\'';
+			$query = 'SELECT ' . $this->adapter->datetimeIntervalClause('"Created"', '-15 Minutes') . ' AS "test" FROM "DbDateTimeTest_Team" LIMIT 1';
 			$result = DB::query($query)->value();
-			$this->matchesRoughly($result, date('Y-m-d H:i:s', strtotime(Dataobject::get_one('SiteTree',"\"URLSegment\" = 'home'")->Created) - 900), '15 Minutes before creating SiteTree[home]');
+			$this->matchesRoughly($result, date('Y-m-d H:i:s', strtotime(Dataobject::get_one('DbDateTimeTest_Team')->Created) - 900), '15 Minutes before creating fixture');
 
 		}
 	}
@@ -109,13 +111,19 @@ class DbDatetimeTest extends FunctionalTest {
 			$result = DB::query($query)->value();
 			$this->matchesRoughly($result, -45 * 60, 'now - 45 minutes ahead');
 
-			$query = 'SELECT ' . $this->adapter->datetimeDifferenceClause('"LastEdited"', '"Created"') . ' AS "test" FROM "SiteTree" WHERE "URLSegment" = \'home\'';
+			$query = 'SELECT ' . $this->adapter->datetimeDifferenceClause('"LastEdited"', '"Created"') . ' AS "test" FROM "DbDateTimeTest_Team" LIMIT 1';
 			$result = DB::query($query)->value();
-			$lastedited = Dataobject::get_one('SiteTree',"\"URLSegment\" = 'home'")->LastEdited;
-			$created = Dataobject::get_one('SiteTree',"\"URLSegment\" = 'home'")->Created;
+			$lastedited = Dataobject::get_one('DbDateTimeTest_Team')->LastEdited;
+			$created = Dataobject::get_one('DbDateTimeTest_Team')->Created;
 			$this->matchesRoughly($result, strtotime($lastedited) - strtotime($created), 'age of HomePage record in seconds since unix epoc');
 
 		}
 	}
 	
+}
+
+class DbDateTimeTest_Team extends DataObject implements TestOnly {
+	static $db = array(
+		'Title' => 'Varchar'
+	);
 }

core/model/Hierarchy.php
@@ -472,6 +472,8 @@ class Hierarchy extends DataObjectDecorator {
 	 * from both stage & live.
 	 */
 	public function AllHistoricalChildren() {
+		if(!$this->owner->hasExtension('Versioned')) throw new Exception('Hierarchy->AllHistoricalChildren() only works with Versioned extension applied');
+		
 		$baseClass=ClassInfo::baseDataClass($this->owner->class);
 		return Versioned::get_including_deleted($baseClass, 
 			"\"ParentID\" = " . (int)$this->owner->ID, "\"$baseClass\".\"ID\" ASC");
@@ -481,6 +483,8 @@ class Hierarchy extends DataObjectDecorator {
 	 * Return the number of children that this page ever had, including pages that were deleted
 	 */
 	public function numHistoricalChildren() {
+		if(!$this->owner->hasExtension('Versioned')) throw new Exception('Hierarchy->AllHistoricalChildren() only works with Versioned extension applied');
+		
 		$query = Versioned::get_including_deleted_query(ClassInfo::baseDataClass($this->owner->class), 
 			"\"ParentID\" = " . (int)$this->owner->ID);
 			
@@ -550,6 +554,8 @@ class Hierarchy extends DataObjectDecorator {
 	 * @return DataObjectSet
 	 */
 	public function liveChildren($showAll = false, $onlyDeletedFromStage = false) {
+		if(!$this->owner->hasExtension('Versioned')) throw new Exception('Hierarchy->liveChildren() only works with Versioned extension applied');
+		
 		if($this->owner->db('ShowInMenus')) {
 			$extraFilter = ($showAll) ? '' : " AND \"ShowInMenus\"=1";
 		} else {

filesystem/Filesystem.php
@@ -145,12 +145,14 @@ class Filesystem extends Object {
 		}
 
 		// Update the image tracking of all pages
-		if(class_exists('Subsite')) Subsite::$disable_subsite_filter = true;
-		foreach(DataObject::get("SiteTree") as $page) {
-			// syncLinkTracking is called by SiteTree::onBeforeWrite()
-			$page->write();
+		if(class_exists('SiteTree')) {
+			if(class_exists('Subsite')) Subsite::$disable_subsite_filter = true;
+			foreach(DataObject::get("SiteTree") as $page) {
+				// syncLinkTracking is called by SiteTree::onBeforeWrite()
+				$page->write();
+			}
+			if(class_exists('Subsite')) Subsite::$disable_subsite_filter = false;
 		}
-		if(class_exists('Subsite')) Subsite::$disable_subsite_filter = false;
 		
 		return _t(
 			'Filesystem.SYNCRESULTS',

search/FulltextSearchable.php
@@ -45,6 +45,8 @@ class FulltextSearchable extends DataObjectDecorator {
 
 		if(!is_array($searchableClasses)) $searchableClasses = array($searchableClasses);
 		foreach($searchableClasses as $class) {
+			if(!class_exists($class)) continue;
+			
 			if(isset($defaultColumns[$class])) {
 				Object::add_extension($class, "FulltextSearchable('{$defaultColumns[$class]}')");
 			} else {

tests/DataObjectDecoratorTest.php
@@ -57,16 +57,16 @@ class DataObjectDecoratorTest extends SapphireTest {
 		
 		$this->assertEquals(0, $parent->Faves()->Count());
 		
-		$homepage = $this->objFromFixture('Page', 'home');
-		$firstpage = $this->objFromFixture('Page', 'page1');
+		$obj1 = $this->objFromFixture('DataObjectDecoratorTest_RelatedObject', 'obj1');
+		$obj2 = $this->objFromFixture('DataObjectDecoratorTest_RelatedObject', 'obj2');
 
-		$parent->Faves()->add($homepage->ID);
+		$parent->Faves()->add($obj1->ID);
 		$this->assertEquals(1, $parent->Faves()->Count());
 		
-		$parent->Faves()->add($firstpage->ID);
+		$parent->Faves()->add($obj2->ID);
 		$this->assertEquals(2, $parent->Faves()->Count());
 		
-		$parent->Faves()->remove($firstpage->ID);
+		$parent->Faves()->remove($obj2->ID);
 		$this->assertEquals(1, $parent->Faves()->Count());
 	}
 	
@@ -296,7 +296,7 @@ class DataObjectDecoratorTest_Faves extends DataObjectDecorator implements TestO
 	public function extraStatics() {
 		return array(
 			'many_many' => array(
-				'Faves' => 'Page'
+				'Faves' => 'DataObjectDecoratorTest_RelatedObject'
 			)
 		);
 	}

tests/DataObjectDecoratorTest.yml
@@ -1,11 +1,8 @@
-Page:
-   home:
-      Title: Home
-   page1:
-      Title: First Page
-      Content: <p>Some test content</p>
-   page2:
-      Title: Second Page
+DataObjectDecoratorTest_RelatedObject:
+   obj1:
+      FieldOne: Obj1
+   obj2:
+      FieldOne: Obj2
 Permission:
    adminpermission:
       Code: ADMIN

tests/DataObjectSetTest.php
@@ -13,7 +13,9 @@ class DataObjectSetTest extends SapphireTest {
 		'DataObjectTest_Team',
 		'DataObjectTest_SubTeam',
 		'DataObjectTest_Player',
-		'DataObjectSetTest_TeamComment'
+		'DataObjectSetTest_TeamComment',
+		'DataObjectSetTest_Base',
+		'DataObjectSetTest_ChildClass',
 	);
 	
 	function testArrayAccessExists() {
@@ -232,14 +234,14 @@ class DataObjectSetTest extends SapphireTest {
 
 		// Ensure that duplicates are removed where the base data class is the same.
 		$mixedSet = new DataObjectSet();
-		$mixedSet->push(new SiteTree(array('ID' => 1)));
-		$mixedSet->push(new Page(array('ID' => 1)));		// dup: same base class and ID
-		$mixedSet->push(new Page(array('ID' => 1)));		// dup: more than one dup of the same object
-		$mixedSet->push(new Page(array('ID' => 2)));		// not dup: same type again, but different
-		$mixedSet->push(new SiteTree(array('ID' => 1)));	// dup: another dup, not consequetive.
+		$mixedSet->push(new DataObjectSetTest_Base(array('ID' => 1)));
+		$mixedSet->push(new DataObjectSetTest_ChildClass(array('ID' => 1)));		// dup: same base class and ID
+		$mixedSet->push(new DataObjectSetTest_ChildClass(array('ID' => 1)));		// dup: more than one dup of the same object
+		$mixedSet->push(new DataObjectSetTest_ChildClass(array('ID' => 2)));		// not dup: same type again, but different
+		$mixedSet->push(new DataObjectSetTest_Base(array('ID' => 1)));	// dup: another dup, not consequetive.
 
 		$mixedSet->removeDuplicates('ID');
-
+		
 		$this->assertEquals($mixedSet->Count(), 2, 'There are 3 unique data objects in a very mixed set');
 	}
 
@@ -422,4 +424,13 @@ class DataObjectSetTest_TeamComment extends DataObject implements TestOnly {
 	static $has_one = array(
 		'Team' => 'DataObjectTest_Team',
 	);
+}
+
+class DataObjectSetTest_Base extends DataObject implements TestOnly {
+	static $db = array(
+		'Name' => 'Varchar'
+	);
+}
+
+class DataObjectSetTest_ChildClass extends DataObjectSetTest_Base implements TestOnly {
 }

tests/DataObjectSetTest.yml
@@ -1,18 +1,9 @@
-Page:
-   home:
-      Title: Home
-   page1:
-      Title: First Page
-      Content: <p>Some test content</p>
-   page2:
-      Title: Second Page
-
 DataObjectTest_Team:
-   team1:
-      Title: Team 1
-   team2:
-      Title: Team 2
-      
+	team1:
+		Title: Team 1
+	team2:
+		Title: Team 2
+		
 DataObjectTest_Player:
    captain1:
       FirstName: Captain 1

commit 11b409aac8cee8a1e1666adf56a965bb5ed1d897

MINOR Using SQLQuery wrapper in DbDatetimeTest to provide better cross-db support, specifically correct LIMIT syntax in MSSQL (previously broken in a9b13509)

tests/model/DbDatetimeTest.php
@@ -54,8 +54,8 @@ class DbDatetimeTest extends FunctionalTest {
 
 	function testCorrectNow() {
 		if($this->supportDbDatetime) {
-			$query = 'SELECT ' . $this->adapter->formattedDatetimeClause('now', '%U');
-			$result = DB::query($query)->value();
+			$query = new SQLQuery($this->adapter->formattedDatetimeClause('now', '%U'));
+			$result = $query->execute()->value();
 			$this->assertRegExp('/^\d*$/', (string) $result);
 			$this->assertTrue($result>0);
 		}
@@ -63,16 +63,16 @@ class DbDatetimeTest extends FunctionalTest {
 
 	function testDbDatetimeFormat() {
 		if($this->supportDbDatetime) {
-			$query = 'SELECT ' . $this->adapter->formattedDatetimeClause('1973-10-14 10:30:00', '%H:%i, %d/%m/%Y');
-			$result = DB::query($query)->value();
+			$query = new SQLQuery($this->adapter->formattedDatetimeClause('1973-10-14 10:30:00', '%H:%i, %d/%m/%Y'));
+			$result = $query->execute()->value();
 			$this->matchesRoughly($result, date('H:i, d/m/Y', strtotime('1973-10-14 10:30:00')), 'nice literal time');
 
-			$query = 'SELECT ' . $this->adapter->formattedDatetimeClause('now', '%d');
-			$result = DB::query($query)->value();
+			$query = new SQLQuery($this->adapter->formattedDatetimeClause('now', '%d'));
+			$result = $query->execute()->value();
 			$this->matchesRoughly($result, date('d', $this->getDbNow()), 'todays day');
 
-			$query = 'SELECT ' . $this->adapter->formattedDatetimeClause('"Created"', '%U') . ' AS test FROM "DbDateTimeTest_Team"';
-			$result = DB::query($query)->value();
+			$query = new SQLQuery($this->adapter->formattedDatetimeClause('"Created"', '%U') . ' AS test FROM "DbDateTimeTest_Team"');
+			$result = $query->execute()->value();
 
 			$this->matchesRoughly($result, strtotime(DataObject::get_one('DbDateTimeTest_Team')->Created), 'fixture ->Created as timestamp');
 		}
@@ -81,16 +81,19 @@ class DbDatetimeTest extends FunctionalTest {
 	function testDbDatetimeInterval() {
 		if($this->supportDbDatetime) {
 
-			$query = 'SELECT ' . $this->adapter->datetimeIntervalClause('1973-10-14 10:30:00', '+18 Years');
-			$result = DB::query($query)->value();
+			$query = new SQLQuery($this->adapter->datetimeIntervalClause('1973-10-14 10:30:00', '+18 Years'));
+			$result = $query->execute()->value();
 			$this->matchesRoughly($result, '1991-10-14 10:30:00', 'add 18 years');
 
-			$query = 'SELECT ' . $this->adapter->datetimeIntervalClause('now', '+1 Day');
-			$result = DB::query($query)->value();
+			$query = new SQLQuery($this->adapter->datetimeIntervalClause('now', '+1 Day'));
+			$result = $query->execute()->value();
 			$this->matchesRoughly($result, date('Y-m-d H:i:s', strtotime('+1 Day', $this->getDbNow())), 'tomorrow');
 
-			$query = 'SELECT ' . $this->adapter->datetimeIntervalClause('"Created"', '-15 Minutes') . ' AS "test" FROM "DbDateTimeTest_Team" LIMIT 1';
-			$result = DB::query($query)->value();
+			$query = new SQLQuery();
+			$query->select($this->adapter->datetimeIntervalClause('"Created"', '-15 Minutes') . ' AS "test"')
+			 	->from("DbDateTimeTest_Team")
+				->limit(1);
+			$result = $query->execute()->value();
 			$this->matchesRoughly($result, date('Y-m-d H:i:s', strtotime(Dataobject::get_one('DbDateTimeTest_Team')->Created) - 900), '15 Minutes before creating fixture');
 
 		}
@@ -99,20 +102,24 @@ class DbDatetimeTest extends FunctionalTest {
 	function testDbDatetimeDifference() {
 		if($this->supportDbDatetime) {
 
-			$query = 'SELECT ' . $this->adapter->datetimeDifferenceClause('1974-10-14 10:30:00', '1973-10-14 10:30:00');
-			$result = DB::query($query)->value();
+			$query = new SQLQuery($this->adapter->datetimeDifferenceClause('1974-10-14 10:30:00', '1973-10-14 10:30:00'));
+			$result = $query->execute()->value();
 			$this->matchesRoughly($result/86400, 365, '1974 - 1973 = 365 * 86400 sec');
 
-			$query = 'SELECT ' . $this->adapter->datetimeDifferenceClause(date('Y-m-d H:i:s', strtotime('-15 seconds')), 'now');
-			$result = DB::query($query)->value();
+			$query = new SQLQuery($this->adapter->datetimeDifferenceClause(date('Y-m-d H:i:s', strtotime('-15 seconds')), 'now'));
+			$result = $query->execute()->value();
 			$this->matchesRoughly($result, -15, '15 seconds ago - now');
 
-			$query = 'SELECT ' . $this->adapter->datetimeDifferenceClause('now', $this->adapter->datetimeIntervalClause('now', '+45 Minutes'));
-			$result = DB::query($query)->value();
+			$query = new SQLQuery($this->adapter->datetimeDifferenceClause('now', $this->adapter->datetimeIntervalClause('now', '+45 Minutes')));
+			$result = $query->execute()->value();
 			$this->matchesRoughly($result, -45 * 60, 'now - 45 minutes ahead');
 
-			$query = 'SELECT ' . $this->adapter->datetimeDifferenceClause('"LastEdited"', '"Created"') . ' AS "test" FROM "DbDateTimeTest_Team" LIMIT 1';
-			$result = DB::query($query)->value();
+			$query = new SQLQuery();
+			$query->select($this->adapter->datetimeDifferenceClause('"LastEdited"', '"Created"') . ' AS "test"')
+				->from("DbDateTimeTest_Team")
+				->limit(1);
+				
+			$result = $query->execute()->value();
 			$lastedited = Dataobject::get_one('DbDateTimeTest_Team')->LastEdited;
 			$created = Dataobject::get_one('DbDateTimeTest_Team')->Created;
 			$this->matchesRoughly($result, strtotime($lastedited) - strtotime($created), 'age of HomePage record in seconds since unix epoc');