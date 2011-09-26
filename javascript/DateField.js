b05e3bd89043191df0ef82705fdb58007fdb5d93
Ingo Schommer <ingo@silverstripe.com>
2011-09-26 16:53:27 +0200

    ENHANCEMENT Enforcing min/max config on DateField in jQuery date picker (AIR-93)

--- javascript/DateField.js
+++ javascript/DateField.js
@@ -16,6 +16,9 @@
 			config = $.extend(config, $.datepicker.regional[config.locale], {});
 		}
 		
+		if(config.min) config.minDate = $.datepicker.parseDate('yy-mm-dd', config.min);
+		if(config.max) config.maxDate = $.datepicker.parseDate('yy-mm-dd', config.max);
+		
 		// Initialize and open a datepicker 
 		// live() doesn't have "onmatch", and jQuery.entwine is a bit too heavyweight for this, so we need to do this onclick.
 		config.dateFormat = config.jqueryDateformat;
