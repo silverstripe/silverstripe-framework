e3cceb60389acd53e2feab64b6faea2afe0d4206
Ingo Schommer <ingo@silverstripe.com>
2011-09-27 12:23:17 +0200

    MINOR Refactored DateField.js from inline jQuery.live() application to a ssDatepicker() plugin which can be applied explicitly as well

--- javascript/DateField.js
+++ javascript/DateField.js
@@ -1,28 +1,33 @@
 (function($) {
-	/**
-	 * Initialize and open a datepicker 
-	 * live() doesn't have "onmatch", and jQuery.entwine is a bit too 
-	 * heavyweight for this, so we need to do this onclick.
-	 */
-	var fields = $('.field.date input.text');
 
-	fields.siblings("button").addClass("ui-icon ui-icon-calendar");
+	$.fn.extend({
+		ssDatepicker: function(opts) {
+			return $(this).each(function() {
+				if($(this).data('datepicker')) return; // already applied
+
+				this.siblings("button").addClass("ui-icon ui-icon-calendar");
+				
+				var holder = $(this).parents('.field.date:first'), 
+					config = $.extend(opts || {}, $(this).metadata({type: 'class'}), {});
+				if(!config.showcalendar) return;
 	
-	fields.live('click', function() {
-		var holder = $(this).parents('.field.date:first'), config = $(this).metadata({type: 'class'});
-		if(!config.showcalendar) return;
-		
-		if(config.locale && $.datepicker.regional[config.locale]) {
-			config = $.extend(config, $.datepicker.regional[config.locale], {});
+				if(config.locale && $.datepicker.regional[config.locale]) {
+					config = $.extend(config, $.datepicker.regional[config.locale], {});
+				}
+
+				if(config.min) config.minDate = $.datepicker.parseDate('yy-mm-dd', config.min);
+				if(config.max) config.maxDate = $.datepicker.parseDate('yy-mm-dd', config.max);
+	
+				// Initialize and open a datepicker 
+				// live() doesn't have "onmatch", and jQuery.entwine is a bit too heavyweight for this, so we need to do this onclick.
+				config.dateFormat = config.jqueryDateformat;
+				$(this).datepicker(config);
+			});
 		}
-		
-		if(config.min) config.minDate = $.datepicker.parseDate('yy-mm-dd', config.min);
-		if(config.max) config.maxDate = $.datepicker.parseDate('yy-mm-dd', config.max);
-		
-		// Initialize and open a datepicker 
-		// live() doesn't have "onmatch", and jQuery.entwine is a bit too heavyweight for this, so we need to do this onclick.
-		config.dateFormat = config.jqueryDateformat;
-		$(this).datepicker(config);
+	});
+
+	$('.field.date input.text').live('click', function() {
+		$(this).ssDatepicker();
 		$(this).datepicker('show');
 	});
 }(jQuery));
