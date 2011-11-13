d30b4b1d0058ed771cdd7ada40d16a0be7719dc7
Ingo Schommer <ingo@silverstripe.com>
2011-11-13 13:54:29 +0100

    BUGFIX Respecting api_access on has_one relations in RestfulServer BUGFIX Limiting fields according to api_access on relation object (rather than the "root" object) in RestfulServer BUGFIX Limit listing of has_one relations in RestfulServer to actual relation (was listing all objects before) BUGFIX Creating correct object instances in RestfulServer->getHandler() for relation queries

--- tests/api/RestfulServerTest.php
+++ tests/api/RestfulServerTest.php
@@ -305,6 +305,7 @@ class RestfulServerTest extends SapphireTest {
 	}
 	
 	public function testApiAccessFieldRestrictions() {
+		$author1 = $this->objFromFixture('RestfulServerTest_Author','author1');
 		$rating1 = $this->objFromFixture('RestfulServerTest_AuthorRating','rating1');
 		
 		$url = "/api/v1/RestfulServerTest_AuthorRating/" . $rating1->ID;
@@ -332,15 +333,40 @@ class RestfulServerTest extends SapphireTest {
 		$this->assertNotContains('<SecretRelation>', $response->getBody(),
 			'"fields" URL parameter filters out disallowed relations from $api_access'
 		);
+		
+		$url = "/api/v1/RestfulServerTest_Author/" . $author1->ID . '/Ratings';
+		$response = Director::test($url, null, null, 'GET');
+		$this->assertContains('<Rating>', $response->getBody(),
+			'Relation viewer shows fields allowed through $api_access'
+		);
+		$this->assertNotContains('<SecretField>', $response->getBody(),
+			'Relation viewer on has-many filters out disallowed fields from $api_access'
+		);
 	}
 	
-	public function testApiAccessRelationRestrictions() {
+	public function testApiAccessRelationRestrictionsInline() {
 		$author1 = $this->objFromFixture('RestfulServerTest_Author','author1');
 		
 		$url = "/api/v1/RestfulServerTest_Author/" . $author1->ID;
 		$response = Director::test($url, null, null, 'GET');
-		$this->assertNotContains('<RelatedPages', $response->getBody());
-		$this->assertNotContains('<PublishedPages', $response->getBody());
+		$this->assertNotContains('<RelatedPages', $response->getBody(), 'Restricts many-many with api_access=false');
+		$this->assertNotContains('<PublishedPages', $response->getBody(), 'Restricts has-many with api_access=false');
+	}
+	
+	public function testApiAccessRelationRestrictionsOnEndpoint() {
+		$author1 = $this->objFromFixture('RestfulServerTest_Author','author1');
+		
+		$url = "/api/v1/RestfulServerTest_Author/" . $author1->ID . "/ProfilePage";
+		$response = Director::test($url, null, null, 'GET');
+		$this->assertEquals(404, $response->getStatusCode(), 'Restricts has-one with api_access=false');
+		
+		$url = "/api/v1/RestfulServerTest_Author/" . $author1->ID . "/RelatedPages";
+		$response = Director::test($url, null, null, 'GET');
+		$this->assertEquals(404, $response->getStatusCode(), 'Restricts many-many with api_access=false');
+		
+		$url = "/api/v1/RestfulServerTest_Author/" . $author1->ID . "/PublishedPages";
+		$response = Director::test($url, null, null, 'GET');
+		$this->assertEquals(404, $response->getStatusCode(), 'Restricts has-many with api_access=false');
 	}
 	
 	public function testApiAccessWithPUT() {
