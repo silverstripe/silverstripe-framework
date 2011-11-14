9b27a4c1be23fdfcdcac84597c337b80718443ba
Ingo Schommer <ingo@silverstripe.com>
2011-11-14 12:26:51 +0100

    ENHANCEMENT More flexible URL filtering through new URLSegmentFilter API.  Support for multibyte URL segments through URLPathFilter::$default_allow_multibyte. Abstraction from Convert::raw2url() (and SiteTree->generateURLSegment())

--- tests/filesystem/FileNameFilterTest.php
+++ tests/filesystem/FileNameFilterTest.php
@@ -8,6 +8,7 @@ class FileNameFilterTest extends SapphireTest {
 	function testFilter() {
 		$name = 'Brötchen  für allë-mit_Unterstrich!.jpg';
 		$filter = new FileNameFilter();
+		$filter->setTransliterator(false);
 		$this->assertEquals(
 			'Brtchen-fr-all-mit-Unterstrich.jpg', 
 			$filter->filter($name)
@@ -27,6 +28,7 @@ class FileNameFilterTest extends SapphireTest {
 	function testFilterWithCustomRules() {
 		$name = 'Brötchen  für allë-mit_Unterstrich!.jpg';
 		$filter = new FileNameFilter();
+		$filter->setTransliterator(false);
 		$filter->setReplacements(array('/[\s-]/' => '_'));
 		$this->assertEquals(
 			'Brötchen__für_allë_mit_Unterstrich!.jpg', 
