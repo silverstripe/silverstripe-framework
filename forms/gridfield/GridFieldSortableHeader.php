1f9032fa14aa57d4bf441a04f8f456e63ffd506c
Stig Lindqvist <stig@silverstripe.com>
2011-12-16 19:18:01 +1300

    MINOR Sorted columns have a visable clue on which is actually sorted

--- forms/gridfield/GridFieldSortableHeader.php
+++ forms/gridfield/GridFieldSortableHeader.php
@@ -13,13 +13,18 @@ class GridFieldSortableHeader extends GridFieldElement {
 		$grid = $this->GridField;
 		$state = $grid->State;
 		$cols = $grid->DisplayFields;
-
+		
+		$order = $state->Sorting->getOrder();
+		
 		foreach ($cols as $col) {
 			$field = new GridField_AlterAction($grid, 'SetOrder'.$col, $col);
 			$field->stateChangeOnTrigger(array(
 				'Sorting.Order' => array($col => $state->Sorting->getToggledOrder($col))
 			));
 			$field->addExtraClass('ss-gridfield-button');
+			if(property_exists($order,$col)){
+				$field->addExtraClass('ss-gridfield-sorted');
+			}
 			$this->push($field);
 		}
 	}
@@ -27,7 +32,7 @@ class GridFieldSortableHeader extends GridFieldElement {
 	function getChildContent() {
 		$content = array();
 		foreach($this->FieldList() as $subfield) $content[] = $subfield->forTemplate();
-		$html = '<tr><th>'.implode("</th>\n<th>", $content).'</th>';
+		$html = '<tr><th class="main">'.implode("</th>\n<th class=\"main\">", $content).'</th>';
 		if($this->gridField->getExtraColumnsCount()) {
 			if($this->gridField->getExtraColumnsCount()) {
 				$html .= '<th colspan="'.$this->gridField->getExtraColumnsCount().'"></th>';
@@ -36,5 +41,4 @@ class GridFieldSortableHeader extends GridFieldElement {
 		$html.= '</tr>';
 		return $html;
 	}
-
 }
