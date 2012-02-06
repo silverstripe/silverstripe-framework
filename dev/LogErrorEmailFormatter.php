9a9e607a891c2000af47f9052c2de8559d316307
Ingo Schommer <ingo@silverstripe.com>
2012-02-06 23:04:42 +0100

    MINOR Rewrite stack trace from "In line <line> of <file>" to a more readable and common "<file>:<line>"

--- dev/LogErrorEmailFormatter.php
+++ dev/LogErrorEmailFormatter.php
@@ -36,7 +36,7 @@ class SS_LogErrorEmailFormatter implements Zend_Log_Formatter_Interface {
 		$errcontext = $event['message']['errcontext'];
 
 		$data = "<div style=\"border: 5px $colour solid\">\n";
-		$data .= "<p style=\"color: white; background-color: $colour; margin: 0\">$errorType: $errstr<br /> At line $errline in $errfile\n<br />\n<br />\n</p>\n";
+		$data .= "<p style=\"color: white; background-color: $colour; margin: 0\">[$errorType] $errstr<br />$errfile:$errline\n<br />\n<br />\n</p>\n";
 
 		// Get a backtrace, filtering out debug method calls
 		$data .= SS_Backtrace::backtrace(true, false, array(
@@ -49,7 +49,7 @@ class SS_LogErrorEmailFormatter implements Zend_Log_Formatter_Interface {
 		$relfile = Director::makeRelative($errfile);
 		if($relfile[0] == '/') $relfile = substr($relfile, 1);
 
-		$subject = "$errorType at $relfile line {$errline} (http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI])";
+		$subject = "[$errorType] in $relfile:{$errline} (http://{$host}{$uri})";
 
 		return array(
 			'subject' => $subject,
