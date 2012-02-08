4bd526fbeeb9a00f38c7d1d3d55d1e217a09fb20
Normann Lou <normannlou@gmail.com>
2012-02-08 19:19:32 +1300

    MINOR Don't follow <a> tag's href if click on page-icon or drag-icon (SSF-109)

--- admin/javascript/LeftAndMain.Content.js
+++ admin/javascript/LeftAndMain.Content.js
@@ -20,7 +20,12 @@
 					// Don't trigger unless coming from a click event.
 					// Avoids problems with automated section switches from tree to detail view
 					// when JSTree auto-selects elements on first load.
-					if(!origEvent) return false;
+					if(!origEvent) {
+						return false;
+					}else if($(origEvent.target).hasClass('jstree-icon') || $(origEvent.target).hasClass('jstree-pageicon')){
+						// in case the click is not on the node title, ie on pageicon or dragicon, 
+						return false;
+					}
 					
 					// Don't allow checking disabled nodes
 					if($(node).hasClass('disabled')) return false;
