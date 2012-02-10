silverstripe/silverstripe-framework

commit 1a10e8bcf55c02a1efa02bfab0706ede5a94e3ac

API CHANGE Removed $rows and $cols constructor arguments on TextareaField and HtmlEditorField, use setRows() and setCols() instead

forms/HtmlEditorField.php
@@ -19,8 +19,10 @@ class HtmlEditorField extends TextareaField {
 	/**
 	 * @see TextareaField::__construct()
 	 */
-	public function __construct($name, $title = null, $rows = 30, $cols = 20, $value = '', $form = null) {
-		parent::__construct($name, $title, $rows, $cols, $value, $form);
+	public function __construct($name, $title = null, $value = '') {
+		if(count(func_get_args()) > 3) Deprecation::notice('3.0', 'Use setRows() and setCols() instead of constructor arguments');
+
+		parent::__construct($name, $title, $value);
 		
 		self::include_js();
 	}

forms/TextareaField.php
@@ -31,15 +31,12 @@ class TextareaField extends FormField {
 	 * 
 	 * @param $name Field name
 	 * @param $title Field title
-	 * @param $rows The number of rows
-	 * @param $cols The number of columns
 	 * @param $value The current value
-	 * @param $form The parent form.  Auto-set when the field is placed in a form.
 	 */
-	function __construct($name, $title = null, $rows = 5, $cols = 20, $value = "", $form = null) {
-		$this->rows = $rows;
-		$this->cols = $cols;
-		parent::__construct($name, $title, $value, $form);
+	function __construct($name, $title = null, $value = '') {
+		if(count(func_get_args()) > 3) Deprecation::notice('3.0', 'Use setRows() and setCols() instead of constructor arguments');
+
+		parent::__construct($name, $title, $value);
 	}
 
 	function getAttributes() {

commit 243d4d45aa5d63aff39275843ff30fabb9125c44

MINOR Restored row/col defaults in TextareaField and HtmlEditorField (broken in 1a10e8bc)

forms/HtmlEditorField.php
@@ -12,6 +12,8 @@ class HtmlEditorField extends TextareaField {
 	 * @var Boolean Use TinyMCE's GZIP compressor
 	 */
 	static $use_gzip = true;
+
+	protected $rows = 30;
 	
 	/**
 	 * Includes the JavaScript neccesary for this field to work using the {@link Requirements} system.

forms/TextareaField.php
@@ -24,7 +24,15 @@ class TextareaField extends FormField {
 
 	protected $template = 'TextareaField';
 
-	protected $rows, $cols;
+	/**
+	 * @var Int Visible number of text lines.
+	 */
+	protected $rows = 5;
+
+	/**
+	 * @var Int Width of the text area (in average character widths)
+	 */
+	protected $cols = 20;
 
 	/**
 	 * Create a new textarea field.