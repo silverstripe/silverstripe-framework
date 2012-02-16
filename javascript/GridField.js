82748a0873718c4be839a369fbb26c5e7b8aea9d
Stig Lindqvist <stig@stojg.net>
2012-02-17 12:38:21 +1300

    MINOR JS functionality on intercepting injected filtering of gridfields in LeftAndMain inherited Controllers.

--- javascript/GridField.js
+++ javascript/GridField.js
@@ -3,6 +3,7 @@
 	$('fieldset.ss-gridfield').entwine({
 		/**
 		 * @param {Object} Additional options for jQuery.ajax() call
+		 * @param {successCallback} callback to call after reloading succeeded.
 		 */
 		reload: function(ajaxOpts, successCallback) {
 			var self = this, form = this.closest('form'), data = form.find(':input').serializeArray();
@@ -25,6 +26,7 @@
 
 					form.removeClass('loading');
 					if(successCallback) successCallback.apply(this, arguments);
+					self.trigger('reload', self);
 				},
 				error: function(e) {
 					alert(ss.i18n._t('GRIDFIELD.ERRORINTRANSACTION', 'An error occured while fetching data from the server\n Please try again later.'));
@@ -65,46 +67,6 @@
 		}
 	});
 	
-	/*
-	 * Upon focusing on a filter <input> element, move "filter" and "reset" buttons and display next to the current <input> element
-	 * ToDo ensure filter-button state is maintained after filtering (see resetState param)
-	 * ToDo get working in IE 6-7
-	 */
-	$('fieldset.ss-gridfield input.ss-gridfield-sort').entwine({
-		onfocusin: function(e) {
-			// Dodgy results in IE <=7 & ignore if only one filter-field
-			countfields = $('fieldset.ss-gridfield input.ss-gridfield-sort').length;
-			if(($.browser.msie && $.browser.version <= 7) || countfields == 1) {
-				return false;
-			}
-			var eleInput = $(this);
-
-			// Remove existing <div> and <button> elements in-lieu of cloning
-			this.getGridField().find('th > div').each(function(i,v) {$(v).remove();});	
-
-			var eleButtonSetFilter = $('#action_filter');
-			var eleButtonResetFilter = $('#action_reset');
-			// Retain current widths to ensure <th>'s don't shift widths
-			var eleButtonWidth = eleButtonSetFilter.width();					
-			// Check <th> doesn't already have an (extra) cloned <button> appended, otherwise clone
-			if(eleInput.closest('th').children().length == 1) {
-				var newButtonCss = {
-					'position':'absolute',
-					'top':'-23px',
-					'left':'0',
-					'border':'#EEE solid 1px',
-					'padding':'0',
-					'margin-left':'0'
-				};	
-				// Append a <div> element used purely for CSS positioning - table elements on their own are untrustworthy to style in this manner
-				$('<div/>').append(
-					eleButtonSetFilter.clone().css(newButtonCss),
-					eleButtonResetFilter.clone().css(newButtonCss).css('left',(eleButtonWidth+4)+'px')
-				).css({'position':'relative','margin':'0 auto','width':'65%'}).appendTo(eleInput.closest('th'));
-			}
-		}
-	});	
-
 	/**
 	 * Allows selection of one or more rows in the grid field.
 	 * Purely clientside at the moment.
