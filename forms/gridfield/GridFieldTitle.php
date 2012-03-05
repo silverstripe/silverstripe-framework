a715785a4280c2c9c72192918883a735792f6f70
Julian Seidenberg <julian@silverstripe.com>
2012-03-06 11:19:46 +1300

    ENHANCEMENT: SSF-106 adding the ability to turn off and on the "add new" button on the GridFieldTitle

--- forms/gridfield/GridFieldTitle.php
+++ forms/gridfield/GridFieldTitle.php
@@ -1,12 +1,24 @@
 <?php
 
 class GridFieldTitle implements GridField_HTMLProvider {
+
+	protected $newEnabled = true;
+
 	function getHTMLFragments($gridField) {
 		return array(
 			'header' => $gridField->customise(array(
-				'NewLink' => Controller::join_links($gridField->Link('item'), 'new')
+				'NewLink' => Controller::join_links($gridField->Link('item'), 'new'),
+				'NewEnabled' => $this->getNewEnabled()
 			))->renderWith('GridFieldTitle')
 		);
+	}
+
+	function getNewEnabled() {
+		return $this->newEnabled;
+	}
+
+	function setNewEnabled($enabled) {
+		$this->newEnabled = $enabled;
 	}
 }
 
