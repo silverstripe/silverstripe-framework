dbc4be3e94aad6dc0097ba46640278cfb75f4bf4
Sean Harvey <sean@silverstripe.com>
2012-03-30 16:23:55 +1300

    API CHANGE Removed Services_JSON library, as we use the built-in json functions instead.

--- core/Convert.php
+++ core/Convert.php
@@ -143,7 +143,7 @@ class Convert {
 	 * Convert a JSON encoded string into an object.
 	 *
 	 * @param string $val
-	 * @return mixed
+	 * @return object|boolean
 	 */
 	static function json2obj($val) {
 		return json_decode($val);
