silverstripe/silverstripe-framework

commit a9509a610de28b96d2439e6d70bf5f3bc421a4fe

MINOR Security::database_is_ready() check are doing duplicate DB queries for Members.
The will make sure that if the database has been ready once, it is ready for the rest of the request

security/Security.php
@@ -105,6 +105,14 @@ class Security extends Controller {
 	static $force_database_is_ready = null;
 	
 	/**
+	 * When the database has once been verified as ready, it will not do the
+	 * checks again.
+	 *
+	 * @var bool
+	 */
+	protected static $database_is_ready = false;
+
+	/**
 	 * Set location of word list file
 	 * 
 	 * @param string $wordListFile Location of word list file
@@ -800,6 +808,8 @@ class Security extends Controller {
 	public static function database_is_ready() {
 		// Used for unit tests
 		if(self::$force_database_is_ready !== NULL) return self::$force_database_is_ready;
+
+		if(self::$database_is_ready) return self::$database_is_ready;
 		
 		$requiredTables = ClassInfo::dataClassesFor('Member');
 		$requiredTables[] = 'Group';
@@ -822,6 +832,7 @@ class Security extends Controller {
 			
 			if($missingFields) return false;
 		}
+		self::$database_is_ready = true;
 		
 		return true;
 	}

commit a3d99b082dc34a9affa0fb2945311c51f32b01d0

MINOR Resetting Security::$database_is_ready state during tests (regression from a9509a610de28)

security/Security.php
@@ -110,7 +110,7 @@ class Security extends Controller {
 	 *
 	 * @var bool
 	 */
-	protected static $database_is_ready = false;
+	static $database_is_ready = false;
 
 	/**
 	 * Set location of word list file

dev/SapphireTest.php
@@ -159,6 +159,7 @@ class SapphireTest extends PHPUnit_Framework_TestCase {
 		if(class_exists('SiteTree')) SiteTree::reset();
 		Hierarchy::reset();
 		if(Controller::has_curr()) Controller::curr()->setSession(new Session(array()));
+		Security::$database_is_ready = null;
 		
 		$this->originalTheme = SSViewer::current_theme();
 		
@@ -770,7 +771,7 @@ class SapphireTest extends PHPUnit_Framework_TestCase {
 	static function create_temp_db() {
 		// Disable PHPUnit error handling
 		restore_error_handler();
-		
+
 		// Create a temporary database, and force the connection to use UTC for time
 		global $databaseConfig;
 		$databaseConfig['timezone'] = '+0:00';