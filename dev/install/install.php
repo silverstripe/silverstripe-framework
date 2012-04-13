8b0dafb30d51573a0d195acf5676314b4c815d3a
Robert Curry <robert@silverstripe.com>
2012-04-13 13:12:48 +1200

    ENHANCEMENT: Change PHP version requirements. Part of #7131.

--- dev/install/install.php
+++ dev/install/install.php
@@ -4,19 +4,16 @@
  ************************************************************************************
  **                                                                                **
  **  If you can read this text in your browser then you don't have PHP installed.  **
- **  Please install PHP 5.3 or higher.                                             **
+ **  Please install PHP 5.3.2 or higher, preferably PHP 5.3.10.                    **
  **                                                                                **
  ************************************************************************************
  ************************************************************************************/
 
 /**
- * PHP version check. Make sure we've got at least PHP 5.3 in the most friendly way possible
+ * PHP version check. Make sure we've got at least PHP 5.3.2 in the most friendly way possible
  */
 
-$majorVersion = strtok(phpversion(),'.');
-$minorVersion = strtok('.');
-
-if($majorVersion < 5 || ($majorVersion == 5 && $minorVersion < 3)) {
+if (version_compare(phpversion(), '5.3.2', '<')) {
 	header("HTTP/1.1 500 Server Error");
 	echo str_replace('$PHPVersion', phpversion(), file_get_contents("sapphire/dev/install/php5-required.html"));
 	die();
