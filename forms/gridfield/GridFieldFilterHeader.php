c21bc6ec8549c043bb5affd92fabb35eef7fb4ca
Naomi Guyer <adrexia@gmail.com>
2012-05-17 12:37:53 +1200

    BUGFIX: Filter IDs
    Change ID of action buttons in the filter panel to be unique

--- forms/gridfield/GridFieldFilterHeader.php
+++ forms/gridfield/GridFieldFilterHeader.php
@@ -105,6 +105,8 @@ class GridFieldFilterHeader implements GridField_HTMLProvider, GridField_DataMan
 		$forTemplate = new ArrayData(array());
 		$forTemplate->Fields = new ArrayList;
 
+	
+
 		$columns = $gridField->getColumns();
 		$filterArguments = $gridField->State->GridFieldFilterHeader->Columns->toArray();
 		$currentColumn = 0;
@@ -129,16 +131,20 @@ class GridFieldFilterHeader implements GridField_HTMLProvider, GridField_DataMan
 					GridField_FormAction::create($gridField, 'reset', false, 'reset', null)
 						->addExtraClass('ss-gridfield-button-reset')
 						->setAttribute('title', _t('GridField.ResetFilter', "Reset"))
+						->setAttribute('id', 'action_reset_'.$gridField->getModelClass().'_'.$columnField)
+
 				);
 			} else {
 				if($currentColumn == count($columns)){
 					$field = new FieldGroup(
 						GridField_FormAction::create($gridField, 'filter', false, 'filter', null)
 							->addExtraClass('ss-gridfield-button-filter')
-							->setAttribute('title', _t('GridField.Filter', "Filter")),
+							->setAttribute('title', _t('GridField.Filter', "Filter"))
+							->setAttribute('id', 'action_filter_'.$gridField->getModelClass().'_'.$columnField),
 						GridField_FormAction::create($gridField, 'reset', false, 'reset', null)
 							->addExtraClass('ss-gridfield-button-close')
-							->setAttribute('title', _t('GridField.ResetFilter', "Reset"))					
+							->setAttribute('title', _t('GridField.ResetFilter', "Reset"))
+							->setAttribute('id', 'action_reset_'.$gridField->getModelClass().'_'.$columnField)					
 					);	
 					$field->addExtraClass('filter-buttons');				
 				}else{
