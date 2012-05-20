silverstripe/silverstripe-framework

commit fdebedd10352a5d3d8f4e101cc36045b5e583f7d

BUGFIX Ensure the form inside htmleditorfield-dialog element is redrawn when the form is lazy loaded by AJAX.

javascript/HtmlEditorField.js
@@ -295,6 +295,7 @@ ss.editorWrappers['default'] = ss.editorWrappers.tinyMCE;
 						url: url,
 						success: function(html) {
 							dialog.html(html);
+							dialog.getForm().redraw();
 						}
 					});
 				}
@@ -330,7 +331,7 @@ ss.editorWrappers['default'] = ss.editorWrappers.tinyMCE;
 			},
 			ondialogopen: function(e) {
 				this.getForm().updateFromEditor();
-				this.redraw();
+				this.getForm().redraw();
 			},
 			ondialogclose: function(e) {
 				this.getForm().resetFields();

forms/HtmlEditorField.php
@@ -313,8 +313,9 @@ class HtmlEditorField_Toolbar extends RequestHandler {
 							'external' => _t('HtmlEditorField.LINKEXTERNAL', 'Another website'),
 							'anchor' => _t('HtmlEditorField.LINKANCHOR', 'Anchor on this page'),
 							'email' => _t('HtmlEditorField.LINKEMAIL', 'Email address'),
-							'file' => _t('HtmlEditorField.LINKFILE', 'Download a file'),			
-						)
+							'file' => _t('HtmlEditorField.LINKFILE', 'Download a file'),
+						),
+						'internal'
 					),
 					new LiteralField('Step2',
 						'<div class="step2">' . sprintf($numericLabelTmpl, '2', _t('HtmlEditorField.DETAILS', 'Details')) . '</div>'

commit 0cbb1b11e395bf46171dc4c464c850f23ec7a763

MINOR Fixed async entwine redraw calls in HtmlEditorField.js (regression from fdebedd1)

javascript/HtmlEditorField.js
@@ -295,7 +295,6 @@ ss.editorWrappers['default'] = ss.editorWrappers.tinyMCE;
 						url: url,
 						success: function(html) {
 							dialog.html(html);
-							dialog.getForm().redraw();
 						}
 					});
 				}
@@ -356,6 +355,8 @@ ss.editorWrappers['default'] = ss.editorWrappers.tinyMCE;
 
 				this.setEditor(ss.editorWrappers['default']());
 				this._super();
+
+				this.redraw();
 			},
 			redraw: function() {
 			},