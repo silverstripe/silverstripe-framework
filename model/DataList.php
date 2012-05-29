909c5bd3b18dec052449f7ae8642c78f10e09f26
Sean Harvey <halkyon@gmail.com>
2012-05-29 20:10:34 +1200

    MINOR Fixed spelling error

--- model/DataList.php
+++ model/DataList.php
@@ -319,7 +319,7 @@ class DataList extends ViewableData implements SS_List, SS_Filterable, SS_Sortab
 		} elseif($numberFuncArgs == 2) {
 			$whereArguments[func_get_arg(0)] = func_get_arg(1);
 		} else {
-			throw new InvalidArgumentException('Incorrect number of arguments pased to exclude()');
+			throw new InvalidArgumentException('Incorrect number of arguments passed to exclude()');
 		}
 
 		$SQL_Statements = array();
