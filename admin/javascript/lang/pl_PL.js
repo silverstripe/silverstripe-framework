f794e98d7e514e683fcb6c39f6295aa5b733d2d1
Ingo Schommer <ingo@silverstripe.com>
2012-06-04 11:52:31 +0200

    BUGFIX Moved localised JS messages in correct module (fixes #7393)

---
+++ admin/javascript/lang/pl_PL.js
@@ -0,0 +1,10 @@
+if(typeof(ss) == 'undefined' || typeof(ss.i18n) == 'undefined') {
+	console.error('Class ss.i18n not defined');
+} else {
+	ss.i18n.addDictionary('pl_PL', {
+		'ModelAdmin.SAVED': "Zapisano",
+		'ModelAdmin.REALLYDELETE': "Napewno usunąć?",
+		'ModelAdmin.DELETED': "Usunięto",
+		'LeftAndMain.PAGEWASDELETED': "Ta strona została usunięta."
+	});
+}
