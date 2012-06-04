5499079c1c4f605cf606ccaf36d4bbaaa5dce31e
Sam Minnee <sam@silverstripe.com>
2012-06-05 11:07:19 +1200

    BUGFIX: Fixed ORM bugs introduced in ec554689325765881a83df6f87c34efe80c92961

--- model/DataQuery.php
+++ model/DataQuery.php
@@ -199,8 +199,8 @@ class DataQuery {
 						$collisionBase = $matches[1];
 						$collisionClasses = ClassInfo::subclassesFor($collisionBase);
 						$collisionClasses = array_map(array(DB::getConn(), 'prepStringForDB'), $collisionClasses);
-						$caseClauses[] = "WHEN \"$baseClass\".\"ClassName\" IN ('"
-							. implode("', '", $collisionClasses) . "') THEN $collision";
+						$caseClauses[] = "WHEN \"$baseClass\".\"ClassName\" IN ("
+							. implode(", ", $collisionClasses) . ") THEN $collision";
 					} else {
 						user_error("Bad collision item '$collision'", E_USER_WARNING);
 					}
@@ -217,7 +217,7 @@ class DataQuery {
 				$classNames = ClassInfo::subclassesFor($this->dataClass);
 				if(!$classNames) user_error("DataList::create() Can't find data sub-classes for '$callerClass'");
 				$classNames = array_map(array(DB::getConn(), 'prepStringForDB'), $classNames);
-				$query->addWhere("\"$baseClass\".\"ClassName\" IN ('" . implode("','", $classNames) . "')");
+				$query->addWhere("\"$baseClass\".\"ClassName\" IN (" . implode(",", $classNames) . ")");
 			}
 		}
 
