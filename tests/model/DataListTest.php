fd881d6a4cdd3df2bbb1a429ecfa1b6af2428805
Sam Minnee <sam@silverstripe.com>
2012-06-25 10:43:37 +1200

    MINOR: Add test using a namespaced class for DataList::filter() and DataList::exclude()

--- tests/model/DataListTest.php
+++ tests/model/DataListTest.php
@@ -14,7 +14,8 @@ class DataListTest extends SapphireTest {
 		'DataObjectTest_FieldlessSubTable',
 		'DataObjectTest_ValidatedObject',
 		'DataObjectTest_Player',
-		'DataObjectTest_TeamComment'
+		'DataObjectTest_TeamComment',
+		'DataObjectTest\NamespacedClass',
 	);
 	
 	public function testSubtract(){
@@ -406,6 +407,15 @@ class DataListTest extends SapphireTest {
 		$list = DataObjectTest_SubTeam::get();
 		$this->assertEquals(3, count($list));
 		$this->assertEquals(2, count($list->exclude('ID', $id)));
+
+		// Check that classes with namespaces work.
+		$obj = new DataObjectTest\NamespacedClass();
+		$obj->Name = "Test";
+		$obj->write();
+
+		$list = DataObjectTest\NamespacedClass::get()->filter('ID', $obj->ID);
+		$this->assertEquals('Test', $list->First()->Name);
+		$this->assertEquals(0, $list->exclude('ID', $obj->ID)->count());
 	}
 
 	/**
