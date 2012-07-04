85480142c133cbeaf844c0dcdd21f06fc53184c7
Ingo Schommer <ingo@silverstripe.com>
2012-07-04 12:05:31 +0200

    CLI bootstrap: Merge existing $_GET and $_REQUEST data
    Necessary to have the <get name="db" /> directive
    working in phpunit.xml definitions, which in turns allows
    us to use GET parameters to switch the database connection
    for running automated tests.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -28,7 +28,8 @@ if(!defined('BASE_PATH')) define('BASE_PATH', dirname($frameworkPath));
 // Copied from cli-script.php, to enable same behaviour through phpunit runner.
 if(isset($_SERVER['argv'][2])) {
     $args = array_slice($_SERVER['argv'],2);
-    $_GET = array();
+    if(!isset($_GET)) $_GET = array();
+    if(!isset($_REQUEST)) $_REQUEST = array();
     foreach($args as $arg) {
        if(strpos($arg,'=') == false) {
            $_GET['args'][] = $arg;
@@ -38,7 +39,7 @@ if(isset($_SERVER['argv'][2])) {
            $_GET = array_merge($_GET, $newItems);
        }
     }
-	$_REQUEST = $_GET;
+	$_REQUEST = array_merge($_REQUEST, $_GET);
 }
 
 // Always flush the manifest for phpunit test runs
