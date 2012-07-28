silverstripe/silverstripe-framework

commit 15e2efb55d982053bb3cf6a1a7472bcf58119e1d

FIX 7590: Image thumbnails broken in gridfield
Delay converting the object to a string and escaping its value until the end of getColumnContent. Call formatValue BEFORE castValue, so that formatValue can create raw HTML by casting to HTMLText afterwards.

forms/gridfield/GridFieldDataColumns.php
@@ -94,7 +94,7 @@ class GridFieldDataColumns implements GridField_ColumnProvider {
 	 * Caution: Make sure to escape special php-characters like in a normal php-statement.
 	 * Example:	"myFieldName" => '<a href=\"custom-admin/$ID\">$ID</a>'.
 	 * Alternatively, pass a anonymous function, which takes two parameters:
-	 *  The value returned by Convert::raw2xml and the original list item.
+	 *  The value and the original list item.
 	 *
 	 * @param array $formatting
 	 */
@@ -126,18 +126,20 @@ class GridFieldDataColumns implements GridField_ColumnProvider {
 		// Allow callbacks
 		if(is_array($columnInfo) && isset($columnInfo['callback'])) {
 			$method = $columnInfo['callback'];
-			$value = Convert::raw2xml($method($record));
+			$value = $method($record);
 		
 		// This supports simple FieldName syntax
 		} else {
-			$value = Convert::raw2xml($gridField->getDataFieldValue($record, $columnName));
+			$value = $gridField->getDataFieldValue($record, $columnName);
 		}
 
-		$value = $this->castValue($gridField, $columnName, $value);
 		$value = $this->formatValue($gridField, $record, $columnName, $value);
+		$value = $this->castValue($gridField, $columnName, $value);
 		$value = $this->escapeValue($gridField, $value);
 
-		return $value;
+		return (is_object($value) && $value instanceof Object && $value->hasMethod('forTemplate')) 
+			? $value->forTemplate()
+			: Convert::raw2xml($value);
 	}
 	
 	/**

commit 9c4e4747c9c39c5753436ec3c124156c4ea6b2d8

BUG 15e2efb55d broke the Page ListView.

forms/gridfield/GridFieldDataColumns.php
@@ -133,13 +133,14 @@ class GridFieldDataColumns implements GridField_ColumnProvider {
 			$value = $gridField->getDataFieldValue($record, $columnName);
 		}
 
-		$value = $this->formatValue($gridField, $record, $columnName, $value);
+		// Turn $value, whatever it is, into a HTML embeddable string
 		$value = $this->castValue($gridField, $columnName, $value);
+		// Make any formatting tweaks
+		$value = $this->formatValue($gridField, $record, $columnName, $value);
+		// Do any final escaping
 		$value = $this->escapeValue($gridField, $value);
 
-		return (is_object($value) && $value instanceof Object && $value->hasMethod('forTemplate')) 
-			? $value->forTemplate()
-			: Convert::raw2xml($value);
+		return $value;
 	}
 	
 	/**
@@ -200,6 +201,7 @@ class GridFieldDataColumns implements GridField_ColumnProvider {
 	}
 	
 	/**
+	 * Casts a field to a string which is safe to insert into HTML
 	 *
 	 * @param GridField $gridField
 	 * @param string $fieldName
@@ -207,11 +209,22 @@ class GridFieldDataColumns implements GridField_ColumnProvider {
 	 * @return string 
 	 */
 	protected function castValue($gridField, $fieldName, $value) {
+		// If a fieldCasting is specified, we assume the result is safe
 		if(array_key_exists($fieldName, $this->fieldCasting)) {
-			return $gridField->getCastedValue($value, $this->fieldCasting[$fieldName]);
-		} elseif(is_object($value) && method_exists($value, 'Nice')) {
-			return $value->Nice();
+			$value = $gridField->getCastedValue($value, $this->fieldCasting[$fieldName]);
 		}
+		// If the value is an object, we do one of two things
+		else if(is_object($value)) {
+			// If it has a "Nice" method, call that & make sure the result is safe
+			if (method_exists($value, 'Nice')) Convert::raw2xml($value->Nice());
+			// Otherwise call forTemplate - the result of this should already be safe
+			else $value = $value->forTemplate();
+		}
+		// Otherwise, just treat as a text string & make sure the result is safe
+		else {
+			$value = Convert::raw2xml($value);
+		}
+
 		return $value;
 	}
 	