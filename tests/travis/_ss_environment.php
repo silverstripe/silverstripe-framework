4d1c2ed4fc46f27da809e17b473f0b13e73e760a
Sam Minnee <sam@silverstripe.com>
2012-08-14 16:44:32 +1200

    Make PHPUnit bootstrap add flush=1

--- tests/travis/_ss_environment.php
+++ tests/travis/_ss_environment.php
@@ -4,7 +4,8 @@ ob_start();
 define('SS_ENVIRONMENT_TYPE', 'dev');
 
 /* Database connection */ 
-define('SS_DATABASE_SERVER', 'localhost'); 
+define('SS_DATABASE_CLASS', 'SQLite3Database');
+define('SS_DATABASE_SERVER', ':memory:'); 
 define('SS_DATABASE_USERNAME', 'root'); 
 define('SS_DATABASE_PASSWORD', '');
 define('SS_DATABASE_CHOOSE_NAME', true);
@@ -12,3 +13,5 @@ define('SS_DATABASE_CHOOSE_NAME', true);
 /* Configure a default username and password to access the CMS on all sites in this environment. */ 
 define('SS_DEFAULT_ADMIN_USERNAME', 'username'); 
 define('SS_DEFAULT_ADMIN_PASSWORD', 'password');
+
+$_FILE_TO_URL_MAPPING[dirname(__FILE__)] = 'http://localhost';
