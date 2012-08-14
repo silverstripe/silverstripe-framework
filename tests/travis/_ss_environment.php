04e3beddf55f8d994e28bfbc62033cc86b911cf4
Sam Minnee <sam@silverstripe.com>
2012-08-14 16:02:12 +1200

    Added support for Travis CI

---
+++ tests/travis/_ss_environment.php
@@ -0,0 +1,14 @@
+<?php 
+ob_start();
+
+define('SS_ENVIRONMENT_TYPE', 'dev');
+
+/* Database connection */ 
+define('SS_DATABASE_SERVER', 'localhost'); 
+define('SS_DATABASE_USERNAME', 'root'); 
+define('SS_DATABASE_PASSWORD', '');
+define('SS_DATABASE_CHOOSE_NAME', true);
+
+/* Configure a default username and password to access the CMS on all sites in this environment. */ 
+define('SS_DEFAULT_ADMIN_USERNAME', 'username'); 
+define('SS_DEFAULT_ADMIN_PASSWORD', 'password');
