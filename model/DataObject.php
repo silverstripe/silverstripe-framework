silverstripe/silverstripe-framework

commit ca1d38dffdb7e9657aafaf070c2d8de2c88f8208

BUG Localize DataObject->summaryFields()

model/DataObject.php
@@ -3156,6 +3156,10 @@ class DataObject extends ViewableData implements DataObjectInterface, i18nEntity
 		
 		// Final fail-over, just list ID field
 		if(!$fields) $fields['ID'] = 'ID';
+
+		// Localize fields (if possible)
+		$labels = $this->fieldLabels(false);
+		$fields = array_intersect_key($labels, $fields);
 		
 		return $fields;
 	}

commit f070f971cfd001743b0229085094e0d18a147482

BUG Allow custom getters in summaryFields() (fixes #7788)
Regression from ca1d38dffdb

model/DataObject.php
@@ -3135,7 +3135,6 @@ class DataObject extends ViewableData implements DataObjectInterface, i18nEntity
 	 * @return array
 	 */
 	public function summaryFields(){
-
 		$fields = $this->stat('summary_fields');
 
 		// if fields were passed in numeric array,
@@ -3158,8 +3157,9 @@ class DataObject extends ViewableData implements DataObjectInterface, i18nEntity
 		if(!$fields) $fields['ID'] = 'ID';
 
 		// Localize fields (if possible)
-		$labels = $this->fieldLabels(false);
-		$fields = array_intersect_key($labels, $fields);
+		foreach($this->fieldLabels(false) as $name => $label) {
+			if(isset($fields[$name])) $fields[$name] = $label;
+		}
 		
 		return $fields;
 	}