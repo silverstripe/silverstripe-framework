silverstripe/silverstripe-framework

commit d50341d8cd202e7d07d6b7e18ea4aa74917eac4a

Rendering attributes for CMSTabSet.ss
Make behaviour consistent with parent template, Tabet.ss

admin/templates/CMSTabSet.ss
@@ -1,4 +1,4 @@
-<div id="$id">
+<div $AttributesHTML>
 	<%-- Tab nav is rendered in CMSEditForm.ss --%>
 
 	<% loop Tabs %>

commit b842bfef91713b01b172988c61c5eff96737b700

BUG: This reverts commit d50341d which broke the Files TabSet

admin/templates/CMSTabSet.ss
@@ -1,4 +1,4 @@
-<div $AttributesHTML>
+<div id="$id">
 	<%-- Tab nav is rendered in CMSEditForm.ss --%>
 
 	<% loop Tabs %>