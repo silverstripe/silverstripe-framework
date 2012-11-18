9476fb4284fb629296a46f015356799e38f80ecd
Kirk Mayo <kirk@silverstripe.com>
2012-11-19 09:00:21 +1300

    Enhancement Setting tooltips so they only show on click

--- javascript/Tooltip.js
+++ javascript/Tooltip.js
@@ -7,12 +7,18 @@
 		 * Setup jquery ui tooltips
 		 */
 		$('.ss-tooltip').entwine({
-			onmatch : function() {
+			onmousedown : function() {
+				this.tooltip({disabled: false});
 				this.tooltip({
 					show: { effect: "fadeIn", duration: 300, delay: 300 },
 					hide: { effect: "fadeOut", duration: 300, delay: 0 },
 					position: { my: "left+15 top", at: "right top", collision: "flipfit"}
 				});
+				this.tooltip('open');
+			},
+			onmouseup : function() {
+				this.tooltip('close');
+				this.tooltip({disabled: true});
 			}
 		});
 }(jQuery));
