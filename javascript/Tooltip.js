5fb9e43436800ba2911c0800c652fa302179d541
Kirk Mayo <kirk@silverstripe.com>
2012-11-19 15:58:01 +1300

    Enhancement remove the default popup for the title on tooltips

--- javascript/Tooltip.js
+++ javascript/Tooltip.js
@@ -7,7 +7,16 @@
 		 * Setup jquery ui tooltips
 		 */
 		$('.ss-tooltip').entwine({
+			onmatch : function(e) {
+				// take of the title attribute because the default title popup looks nasty
+				$('.ss-tooltip').each(function() {
+					var title = $(this).attr('title');
+					$(this).data('title', title).removeAttr('title');
+				});
+			},
 			onmousedown : function() {
+				var title = $(this).data('title');
+				this.attr('title', title);
 				this.tooltip({disabled: false});
 				this.tooltip({
 					show: { effect: "fadeIn", duration: 300, delay: 300 },
