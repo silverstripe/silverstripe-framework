b0419958cc17c7154700799bdb379a29051bbf16
Kirk Mayo <kirk@silverstripe.com>
2012-11-26 12:58:40 +1300

    BUG fixed a tooltip bug where you had to long press for a tooltip

--- javascript/Tooltip.js
+++ javascript/Tooltip.js
@@ -6,6 +6,7 @@
 		 * Class: ss-tooltip.
 		 * Setup jquery ui tooltips
 		 */
+		var toggle = 'hide';
 		$('.ss-tooltip').entwine({
 			onmatch : function(e) {
 				// take of the title attribute because the default title popup looks nasty
@@ -15,27 +16,35 @@
 				});
 			},
 			onmousedown : function() {
-				this.tooltipshow();
+				this.tooltiptoggle();
 			},
-			tooltipshow : function() {
-				var title = $(this).data('title');
-				this.attr('title', title);
-				this.tooltip({disabled: false});
-				this.tooltip({
-					show: { effect: "fadeIn", duration: 300, delay: 300 },
-					hide: { effect: "fadeOut", duration: 300, delay: 0 },
-					position: { my: "left+15 top", at: "right top", collision: "flipfit"}
-				});
-				this.tooltip('open');
+			tooltiptoggle : function() {
+				if (toggle == 'hide') {
+					toggle = 'show';
+					var title = $(this).data('title');
+					this.attr('title', title);
+					this.tooltip({disabled: false});
+					this.tooltip({
+						show: { effect: "fadeIn", duration: 300, delay: 300 },
+						hide: { effect: "fadeOut", duration: 300, delay: 0 },
+						position: { my: "left+15 top", at: "right top", collision: "flipfit"}
+					});
+					this.tooltip('open');
+				} else {
+					toggle = 'hide';
+					$(this).data('title', title).removeAttr('title');
+					this.tooltip('close');
+				}
 			},
-			onmouseup : function() {
+			onmouseleave : function() {
+				toggle = 'hide';
 				this.tooltip('close');
 				this.tooltip({disabled: true});
 			},
 			onmouseenter : function() {
 				// handle touch screen devices
 				if (window.Touch) {
-					this.tooltipshow();
+					this.tooltiptoggle();
 				}
 			}
 		});
