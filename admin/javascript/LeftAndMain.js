silverstripe/silverstripe-framework

commit ff39f9ad38dad4e8a162230ab01ca3121ade9f19

Upgrade jQuery UI to 1.9
- Fixed jQuery.tabs remote tabs loading behaviour (see http://forum.jquery.com/topic/tabs-api-redesign)

admin/javascript/LeftAndMain.js
@@ -918,23 +918,21 @@ jQuery.noConflict();
 			redrawTabs: function() {
 				this.rewriteHashlinks();
 
-				var id = this.attr('id'), cookieId = 'ui-tabs-' + id, 
-					selectedTab = this.find('ul:first .ui-tabs-selected');
+				var id = this.attr('id'), selectedTab = this.find('ul:first .ui-tabs-active');
 
-				// Fix for wrong cookie storage of deselected tabs
-				if($.cookie && id && $.cookie(cookieId) == -1) $.cookie(cookieId, 0);
 				if(!this.data('tabs')) this.tabs({
-					cookie: ($.cookie && id) ? { expires: 30, path: '/', name: cookieId } : false,
-					ajaxOptions: {
-						// Overwrite ajax loading to use CMS logic instead
-						beforeSend: function(xhr, settings) {
-							if(!isSameUrl(document.location.href, settings.url)) {
-								$('.cms-container').loadPanel(settings.url);
-							}
-							return false;
-						}
-					},
 					selected: (selectedTab.index() != -1) ? selectedTab.index() : 0,
+					beforeLoad: function(e, settings) {
+						// Overwrite ajax loading to use CMS logic instead
+						var makeAbs = $.path.makeUrlAbsolute,
+							baseUrl = $('base').attr('href'),
+							isSame = (makeAbs(settings.url, baseUrl) == makeAbs(document.location.href));
+
+						if(!isSame) $('.cms-container').loadPanel(settings.url);
+						$(this).tabs('select', settings.tab.index());
+
+						return false;
+					},
 					show: function(e, ui) {
 						// Usability: Hide actions for "readonly" tabs (which don't contain any editable fields)
 						var actions = $(this).closest('form').find('.Actions');
@@ -948,15 +946,14 @@ jQuery.noConflict();
 			},
 		
 			/**
-			 * Replace prefixes for all hashlinks in tabs.
-			 * SSViewer rewrites them from "#Root_MyTab" to
-			 * e.g. "/admin/#Root_MyTab" which makes them
-			 * unusable for jQuery UI.
+			 * Ensure hash links are prefixed with the current page URL,
+			 * otherwise jQuery interprets them as being external.
 			 */
 			rewriteHashlinks: function() {
 				$(this).find('ul a').each(function() {
-					var href = $(this).attr('href');
-					if(href) $(this).attr('href', href.replace(/.*(#.*)/, '$1'));
+					var matches = $(this).attr('href').match(/#.*/);
+					if(!matches) return;
+					$(this).attr('href', document.location.href.replace(/#.*/, '') + matches[0]);
 				});
 			}
 		});

admin/code/SecurityAdmin.php
@@ -145,11 +145,11 @@ class SecurityAdmin extends LeftAndMain implements PermissionProvider {
 
 		$actionParam = $this->request->param('Action');
 		if($actionParam == 'groups') {
-			$groupsTab->addExtraClass('ui-state-selected');
+			$groupsTab->addExtraClass('ui-state-active');
 		} elseif($actionParam == 'users') {
-			$usersTab->addExtraClass('ui-state-selected');
+			$usersTab->addExtraClass('ui-state-active');
 		} elseif($actionParam == 'roles') {
-			$rolesTab->addExtraClass('ui-state-selected');
+			$rolesTab->addExtraClass('ui-state-active');
 		}
 
 		$actions = new FieldList();

admin/css/screen.css
@@ -119,7 +119,7 @@ body, html { font-size: 12px; line-height: 16px; font-family: Arial, sans-serif;
 .cms .ui-tabs { padding: 0; background: none; }
 .cms .ui-tabs .ui-widget-header { border: 0; background: none; }
 .cms .ui-tabs .ui-tabs-nav { margin: 0; padding: 0; }
-.cms .ui-tabs .ui-tabs-nav li { top: 0; }
+.cms .ui-tabs .ui-tabs-nav li { top: 0; border-bottom: 0 !important; }
 .cms .ui-tabs .ui-tabs-nav li a { padding: 0 15px; }
 .cms .ui-tabs .ui-tabs-nav.ui-state-active { border-color: gray; }
 .cms .ui-tabs .ui-tabs-panel { background: transparent; }

admin/javascript/ssui.core.js
@@ -1,35 +1,5 @@
 (function($) {
 
-	$.entwine('ss', function($) {
-		/**
-		 * Creates a jQuery UI tab navigation bar, detached from the container DOM structure.
-		 */
-		$('.ss-ui-tabs-nav').entwine({
-			onadd: function() {
-				this.redraw();
-			},
-			redraw: function() {
-				this.addClass('ui-tabs ui-widget ui-widget-content ui-corner-all ui-tabs-panel ui-corner-bottom');
-				this.find('ul').addClass('ui-tabs-nav ui-helper-reset ui-helper-clearfix ui-widget-header ui-corner-all');
-				this.find('li').addClass('ui-state-default ui-corner-top');
-				// TODO Figure out selected tab
-				var selected = this.find('li.current');
-				if(!selected.length) selected = this.find('li:first');
-				selected.selectIt();
-			}
-		});
-		
-		$('.ss-ui-tabs-nav li').entwine({
-			onclick: function() {
-				this.selectIt();
-			},
-			selectIt: function() {
-				var cls = 'ui-tabs-selected ui-state-active';
-				this.addClass(cls).siblings().not(this).removeClass(cls);
-			}
-		});
-	});
-
 	/**
 	 * Allows icon definition via HTML5 data attrs for easier handling in PHP
 	 */

admin/scss/_uitheme.scss
@@ -26,6 +26,7 @@
 			
 			li {
 				top: 0;
+				border-bottom: 0 !important;
 				
 				a {
 					padding: 0 15px;

admin/templates/Includes/ModelAdmin_Content.ss
@@ -1,4 +1,4 @@
-<div class="cms-content center $BaseCSSClasses" data-layout-type="border" data-pjax-fragment="Content">
+<div class="cms-content cms-tabset center $BaseCSSClasses" data-layout-type="border" data-pjax-fragment="Content">
 
 	<div class="cms-content-header north">
 		<div class="cms-content-header-info">
@@ -12,10 +12,10 @@
 			</h2>
 		</div>
 
-		<div class="cms-content-header-tabs ss-ui-tabs-nav">
+		<div class="cms-content-header-tabs">
 			<ul>
 			<% loop ManagedModelTabs %>
-				<li class="tab-$ClassName $LinkOrCurrent">
+				<li class="tab-$ClassName $LinkOrCurrent<% if $LinkOrCurrent == 'current' %> ui-tabs-active<% end_if %>">
 					<a href="$Link" class="cms-panel-link">$Title</a>
 				</li>
 			<% end_loop %>

css/GridField.css
@@ -38,7 +38,7 @@
 .cms table.ss-gridfield-table tbody td.col-buttons { width: 40px; padding: 0 8px; text-align: left; }
 .cms table.ss-gridfield-table tbody td.col-listChildrenLink { width: 16px; border-right: none; text-indent: -9999em; padding: 0; }
 .cms table.ss-gridfield-table tbody td.col-listChildrenLink .list-children-link { background: transparent url(../images/sitetree_ss_default_icons.png) no-repeat 3px -4px; display: block; }
-.cms table.ss-gridfield-table tbody td.col-getTreeTitle span.item { color: #1556b2; }
+.cms table.ss-gridfield-table tbody td.col-getTreeTitle span.item { color: #0073c1; }
 .cms table.ss-gridfield-table tbody td.col-getTreeTitle span.badge { clear: both; text-transform: uppercase; display: inline-block; padding: 0px 3px; font-size: 0.75em; line-height: 1em; margin-left: 10px; margin-right: 6px; margin-top: -1px; -webkit-border-radius: 2px 2px; -moz-border-radius: 2px / 2px; border-radius: 2px / 2px; }
 .cms table.ss-gridfield-table tbody td.col-getTreeTitle span.badge.modified { color: #7E7470; border: 1px solid #C9B800; background-color: #FFF0BC; }
 .cms table.ss-gridfield-table tbody td.col-getTreeTitle span.badge.addedtodraft { color: #7E7470; border: 1px solid #C9B800; background-color: #FFF0BC; }

javascript/TabSet.js
@@ -10,7 +10,7 @@
 				this._super();
 			},
 			onremove: function() {
-				this.tabs('destroy');
+				if(this.data('tabs')) this.tabs('destroy');
 				this._super();
 			},
 			redrawTabs: function() {
@@ -19,15 +19,14 @@
 			},
 		
 			/**
-			 * Replace prefixes for all hashlinks in tabs.
-			 * SSViewer rewrites them from "#Root_MyTab" to
-			 * e.g. "/admin/#Root_MyTab" which makes them
-			 * unusable for jQuery UI.
+			 * Ensure hash links are prefixed with the current page URL,
+			 * otherwise jQuery interprets them as being external.
 			 */
 			rewriteHashlinks: function() {
 				$(this).find('ul a').each(function() {
-					var href = $(this).attr('href');
-					if(href) $(this).attr('href', href.replace(/.*(#.*)/, '$1'));
+					var matches = $(this).attr('href').match(/#.*/);
+					if(!matches) return;
+					$(this).attr('href', document.location.href.replace(/#.*/, '') + matches[0]);
 				});
 			}
 		});

commit 34788130ef52c93eda81c62aab3fc0c869f68025

BUG Rewrite hashlinks failing on empty a tags
Check for presence of href in _a_ tags before trying to .match them to
prevent null pointer errors.

Some external JS libs (eg tag-it) use <ul><a> structures which get matched
and break

(fixes regression introduced by ff39f9ad38dad4e8a162230ab01ca3121ade9f19)

admin/javascript/LeftAndMain.js
@@ -951,6 +951,7 @@ jQuery.noConflict();
 			 */
 			rewriteHashlinks: function() {
 				$(this).find('ul a').each(function() {
+					if (!$(this).attr('href')) return;
 					var matches = $(this).attr('href').match(/#.*/);
 					if(!matches) return;
 					$(this).attr('href', document.location.href.replace(/#.*/, '') + matches[0]);