e3148c46fef43349596045b4b0b77e18e6623aab
Nicolaas <github@sunnysideup.co.nz>
2013-01-02 13:26:22 +1300

    small update to documentation of DateField.php
    updated documentation on how use the Datefield (DateField::setConfig)

--- forms/DateField.php
+++ forms/DateField.php
@@ -45,7 +45,7 @@ require_once 'Zend/Date.php';
  * 
  *   $f = new DateField('MyDate');
  *   $f->setLocale('de_DE');
- *   $f->setConfig('dmyfields');
+ *   $f->setConfig('dateformat', 'dmyfields');
  * 
  * # Validation
  * 
