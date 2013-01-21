silverstripe/silverstripe-framework

commit 729bcc95db06d554ff60f5984cf2f9b840898a24

BUGFIX: Don't clear form messages unless forTemplate() is actually called. BUGFIX: Clear session-stored form data as well as form error message.

forms/Form.php
@@ -974,9 +974,7 @@ class Form extends RequestHandler {
 	 */
 	public function Message() {
 		$this->getMessageFromSession();
-		$message = $this->message;
-		$this->clearMessage();
-		return $message;
+		return $this->message;
 	}
 	
 	/**
@@ -993,8 +991,6 @@ class Form extends RequestHandler {
 		}else{
 			$this->message = Session::get("FormInfo.{$this->FormName()}.formError.message");
 			$this->messageType = Session::get("FormInfo.{$this->FormName()}.formError.type");
-
-			Session::clear("FormInfo.{$this->FormName()}");
 		}
 	}
 
@@ -1030,9 +1026,11 @@ class Form extends RequestHandler {
 		$this->message  = null;
 		Session::clear("FormInfo.{$this->FormName()}.errors");
 		Session::clear("FormInfo.{$this->FormName()}.formError");
+		Session::clear("FormInfo.{$this->FormName()}.data");
 	}
 	public function resetValidation() {
 		Session::clear("FormInfo.{$this->FormName()}.errors");
+		Session::clear("FormInfo.{$this->FormName()}.data");
 	}
 
 	/**
@@ -1317,6 +1315,13 @@ class Form extends RequestHandler {
 			(array)$this->getTemplate(),
 			array('Form')
 		));
+
+		// Now that we're rendered, clear message
+		Session::clear("FormInfo.{$this->FormName()}.errors");
+		Session::clear("FormInfo.{$this->FormName()}.formError");
+		Session::clear("FormInfo.{$this->FormName()}.data");
+		
+		return $result;
 	}
 
 	/**

commit 5d37d55f3558424a9b284ead05acae6f73c3c03f

BUG Form session message clearing regression
Regression originally from 729bcc95, but made visible by 014f541a8

forms/Form.php
@@ -1319,15 +1319,15 @@ class Form extends RequestHandler {
 	 * than <% control FormObject %>
 	 */
 	public function forTemplate() {
-		// Now that we're rendered, clear message
-		Session::clear("FormInfo.{$this->FormName()}.errors");
-		Session::clear("FormInfo.{$this->FormName()}.formError");
-		Session::clear("FormInfo.{$this->FormName()}.data");
-
-		return $this->renderWith(array_merge(
+		$return = $this->renderWith(array_merge(
 			(array)$this->getTemplate(),
 			array('Form')
 		));
+
+		// Now that we're rendered, clear message
+		$this->clearMessage();
+
+		return $return;
 	}
 
 	/**