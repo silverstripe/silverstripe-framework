135c41e1a5b4307a0115a8e8373c823ba23639c9
Daniel Hensby <dan@hens.by>
2013-01-30 16:39:18 +0000

    Better cache key generation
    All of the arguments supplied to the request function can impact what is
    returned by a restful service.
    
    This takes account of that and makes the cache key more specific,
    including basic auth details, so we don't rely on *just* the absolute
    URL for caching.

--- api/RestfulService.php
+++ api/RestfulService.php
@@ -124,7 +124,16 @@ class RestfulService extends ViewableData {
 		assert(in_array($method, array('GET','POST','PUT','DELETE','HEAD','OPTIONS')));
 		
 		$cachedir = TEMP_FOLDER;	// Default silverstripe cache
-		$cache_file = md5($url);	// Encoded name of cache file
+		//use var export on potentially nested arrays
+		$cache_file_items = array(
+			$subURL,
+			$method,
+			var_export($data, true),
+			var_export(array_merge((array)$this->customHeaders, (array)$headers), true),
+			var_export($curlOptions, true),
+			"$this->authUsername:$this->authPassword"
+		);
+		$cache_file = md5(implode('-', $cache_file_items));	// Encoded name of cache file
 		$cache_path = $cachedir."/xmlresponse_$cache_file";
 		
 		// Check for unexpired cached feed (unless flush is set)
