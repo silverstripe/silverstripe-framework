36d3303e1b123ca8296643ef9e6a235650aac25c
Will Rossiter <will.rossiter@gmail.com>
2013-03-05 10:27:15 +1300

    FIX GridField export and print actions should preserve state.

--- forms/gridfield/GridFieldPrintButton.php
+++ forms/gridfield/GridFieldPrintButton.php
@@ -118,6 +118,7 @@ class GridFieldPrintButton implements GridField_HTMLProvider, GridField_ActionPr
 			}
 		}
 		
+		$items = $gridField->getManipulatedList();
 		$itemRows = new ArrayList();
 		foreach($items as $item) {
 			$itemRow = new ArrayList();
