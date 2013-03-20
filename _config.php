07809762deea1910a59c38012acd27f1ff095c6b
Ingo Schommer <ingo@silverstripe.com>
2013-03-20 11:53:09 +0100

    Increased deprecation notification version

--- _config.php
+++ _config.php
@@ -44,8 +44,8 @@ if (!is_dir($aggregatecachedir)) mkdir($aggregatecachedir);
 SS_Cache::add_backend('aggregatestore', 'File', array('cache_dir' => $aggregatecachedir));
 SS_Cache::pick_backend('aggregatestore', 'aggregate', 1000);
 
-// If you don't want to see deprecation errors for the new APIs, change this to 3.0.0-dev.
-Deprecation::notification_version('3.1.0');
+// If you don't want to see deprecation errors for the new APIs, change this to 3.2.0-dev.
+Deprecation::notification_version('3.2.0');
 
 // TODO Remove once new ManifestBuilder with submodule support is in place
 require_once('admin/_config.php');
