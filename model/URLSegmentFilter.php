859455135d8f4ad87ca2d71611bb9fb9a9240752
Zann St Pierre <mail@zann.com.au>
2013-03-28 08:52:01 +1000

    Revert "FIX: Don't include dots in URL segments"
    This reverts commit 0a06a536121c3eed9f76ef5b3cea59a5253f5423.
    
    Signed-off-by: Zann St Pierre <mail@zann.com.au>

--- model/URLSegmentFilter.php
+++ model/URLSegmentFilter.php
@@ -29,11 +29,7 @@ class URLSegmentFilter extends Object {
 		'/&/u' => '-and-',
 		'/\s/u' => '-', // remove whitespace
 		'/_/u' => '-', // underscores to dashes
-<<<<<<< HEAD
 		'/[^A-Za-z0-9-]+/u' => '', // remove non-ASCII chars, only allow alphanumeric, dashes and dots.
-=======
-		'/[^A-Za-z0-9]+/u' => '', // remove non-ASCII chars, only allow alphanumeric, dashes and dots.
->>>>>>> 377e79b... Incorrect RegEx
 		'/[\-]{2,}/u' => '-', // remove duplicate dashes
 		'/^[\.\-_]/u' => '', // Remove all leading dots, dashes or underscores
 	);
