silverstripe/silverstripe-framework

commit 5238e9cf48251f77f3fda095724990da3bee6803

API: add onBeforeRollback() and onAfterRollback() hooks.

model/Versioned.php
@@ -1077,8 +1077,12 @@ class Versioned extends DataExtension {
 	 * @param $version Either the string 'Live' or a version number
 	 */
 	public function doRollbackTo($version) {
+		$this->extend('onBeforeRollback', $version);
 		$this->publish($version, "Stage", true);
+
 		$this->owner->writeWithoutVersion();
+
+		$this->extend('onAfterRollback', $version);
 	}
 	
 	/**

commit e2b42ca4c211d840f56c6af83f68aba4a75212d6

Fixed Versioned hook regression
Introduced by 5238e9cf48251f77f3fda095724990da3bee6803

model/Versioned.php
@@ -1077,12 +1077,12 @@ class Versioned extends DataExtension {
 	 * @param $version Either the string 'Live' or a version number
 	 */
 	public function doRollbackTo($version) {
-		$this->extend('onBeforeRollback', $version);
+		$this->owner->extend('onBeforeRollback', $version);
 		$this->publish($version, "Stage", true);
 
 		$this->owner->writeWithoutVersion();
 
-		$this->extend('onAfterRollback', $version);
+		$this->owner->extend('onAfterRollback', $version);
 	}
 	
 	/**