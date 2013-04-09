silverstripe/silverstripe-framework

commit b645703eb9a46bb41d626eb0bb80ae7d3cdad378

Sort menu items according to priority descending, then title ascending

admin/code/CMSMenu.php
@@ -150,15 +150,14 @@ class CMSMenu extends Object implements IteratorAggregate, i18nEntityProvider
 			}
 		}
 		
-		// Sort menu items according to priority
+		// Sort menu items according to priority, then title asc
 		$menuPriority = array();
-		$i = 0;
+		$menuTitle    = array();
 		foreach($menuItems as $key => $menuItem) {
-			$i++;
-			// This funny litle formula ensures that the first item added with the same priority will be left-most.
-			$menuPriority[$key] = $menuItem->priority*100 - $i;
+			$menuPriority[$key] = $menuItem->priority;
+			$menuTitle[$key]    = $menuItem->title;
 		}
-		array_multisort($menuPriority, SORT_DESC, $menuItems);
+		array_multisort($menuPriority, SORT_DESC, $menuTitle, SORT_ASC, $menuItems);
 		
 		return $menuItems;
 	}

admin/code/ModelAdmin.php
@@ -54,6 +54,13 @@ abstract class ModelAdmin extends LeftAndMain {
 	 * @var array|string
 	 */
 	public static $managed_models = null;
+
+	/**
+	 * Override menu_priority so that ModelAdmin CMSMenu objects
+	 * are grouped together directly above the Help menu item.
+	 * @var float
+	 */
+	public static $menu_priority = -0.5;
 	
 	public static $allowed_actions = array(
 		'ImportForm',

commit 7d600f57d5014420171a382864c4e3bd30e8c154

CMSMenu sort order (fixes #1716)
Regression caused by b645703eb9a46bb41d626eb0bb80ae7d3cdad378

admin/code/CMSMenu.php
@@ -154,7 +154,7 @@ class CMSMenu extends Object implements IteratorAggregate, i18nEntityProvider
 		$menuPriority = array();
 		$menuTitle    = array();
 		foreach($menuItems as $key => $menuItem) {
-			$menuPriority[$key] = $menuItem->priority;
+			$menuPriority[$key] = is_numeric($menuItem->priority) ? $menuItem->priority : 0;
 			$menuTitle[$key]    = $menuItem->title;
 		}
 		array_multisort($menuPriority, SORT_DESC, $menuTitle, SORT_ASC, $menuItems);