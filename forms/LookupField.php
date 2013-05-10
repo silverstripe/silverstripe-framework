42cf2a95bd51a3f097d1177e61b190a3b2669d9a
Will Rossiter <will.rossiter@gmail.com>
2013-05-11 00:01:39 +1200

    FIX: Add support for multi dimensional source arrays in LookupField (open/6132)

--- forms/LookupField.php
+++ forms/LookupField.php
@@ -1,30 +1,51 @@
 <?php
+
 /**
  * Read-only complement of {@link DropdownField}.
- * Shows the "human value" of the dropdown field for the currently selected value.
+ *
+ * Shows the "human value" of the dropdown field for the currently selected 
+ * value.
+ *
  * @package forms
  * @subpackage fields-basic
  */
 class LookupField extends DropdownField {
 
+	/**
+	 * @var boolean $readonly
+	 */
 	protected $readonly = true;
 	
 	/**
 	 * Returns a readonly span containing the correct value.
+	 *
+	 * @param array $properties
+	 *
+	 * @return string
 	 */
 	public function Field($properties = array()) {
 		$source = $this->getSource();
 		
-		
 		// Normalize value to array to simplify further processing
-		$values = (is_array($this->value) || is_object($this->value)) ? $this->value : array(trim($this->value));
+		if(is_array($this->value) || is_object($this->value)) {
+			$values = $this->value;
+		} else {
+			$values = array(trim($this->value));
+		}
 
 		$mapped = array();
+
 		if($source instanceof SQLMap) {
-			foreach($values as $value) $mapped[] = $source->getItem($value);
-		} else if($source instanceof ArrayAccess || is_array($source)) {
 			foreach($values as $value) {
-				if(isset($source[$value])) $mapped[] = $source[$value];
+				$mapped[] = $source->getItem($value);
+			}
+		} else if($source instanceof ArrayAccess || is_array($source)) {
+			$source = ArrayLib::flatten($source);
+			
+			foreach($values as $value) {
+				if(isset($source[$value])) {
+					$mapped[] = $source[$value];
+				}
 			}
 		} else {
 			$mapped = array();
@@ -39,7 +60,11 @@ class LookupField extends DropdownField {
 		
 		if($mapped) {
 			$attrValue = implode(', ', array_values($mapped));
-			if(!$this->dontEscape) $attrValue = Convert::raw2xml($attrValue);
+			
+			if(!$this->dontEscape) {
+				$attrValue = Convert::raw2xml($attrValue);
+			}
+
 			$inputValue = implode(', ', array_values($values)); 
 		} else {
 			$attrValue = "<i>(none)</i>";
@@ -51,17 +76,26 @@ class LookupField extends DropdownField {
 			"\" value=\"" . $inputValue . "\" />";
 	}
 	
+	/**
+	 * @return LookupField
+	 */
 	public function performReadonlyTransformation() {
 		$clone = clone $this;
+
 		return $clone;
 	}
 
+	/**
+	 * @return string
+	 */
 	public function Type() {
 		return "lookup readonly";
 	}
 	
 	/**
-	 * Override parent behaviour by not merging arrays.
+	 * Override parent behavior by not merging arrays.
+	 *
+	 * @return array
 	 */
 	public function getSource() {
 		return $this->source;
