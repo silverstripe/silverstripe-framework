bb6283740a3f67ec71b35c6dbe3c048161dd8069
Sean Harvey <halkyon@gmail.com>
2013-05-13 23:39:52 +1200

    Fixing broken CoreTest when using silverstripe-cache for temp folder

--- tests/core/CoreTest.php
+++ tests/core/CoreTest.php
@@ -16,11 +16,11 @@ class CoreTest extends SapphireTest {
 	}
 
 	public function testGetTempPathInProject() {
-		if(file_exists($this->tempPath)) {
-			$this->assertEquals(getTempFolder(BASE_PATH), $this->tempPath);
-		} else {
-			$user = getTempFolderUsername();
+		$user = getTempFolderUsername();
 
+		if(file_exists($this->tempPath)) {
+			$this->assertEquals(getTempFolder(BASE_PATH), $this->tempPath . '/' . $user);
+		} else {
 			// A typical Windows location for where sites are stored on IIS
 			$this->assertEquals(sys_get_temp_dir() . '/silverstripe-cacheC--inetpub-wwwroot-silverstripe-test-project/' . $user,
 				getTempFolder('C:\\inetpub\\wwwroot\\silverstripe-test-project'));
