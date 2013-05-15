146b5a81cbc6b10d054cab3c0b7f8186d7bb48c2
Ingo Schommer <ingo@silverstripe.com>
2013-05-16 01:08:56 +0200

    Improved shortcode docs

--- parsers/ShortcodeParser.php
+++ parsers/ShortcodeParser.php
@@ -1,61 +1,11 @@
 <?php
 /**
  * A simple parser that allows you to map BBCode-like "shortcodes" to an arbitrary callback.
- *
- * * The Shortcode API (new in 2.4) is a simple regex based parser that allows you to replace simple bbcode-like tags
- * within a HTMLText or HTMLVarchar field when rendered into a template. It is inspired by and very similar to the
- * [Wordpress implementation](http://codex.wordpress.org/Shortcode_API) of shortcodes. Examples of shortcode tags are:
- *  
- * <code>
- *     [shortcode]
- *     [shortcode /]
- *     [shortcode parameter="value"]
- *     [shortcode parameter="value"]Enclosed Content[/shortcode]
- * </code>
+ * It is a simple regex based parser that allows you to replace simple bbcode-like tags
+ * within a HTMLText or HTMLVarchar field when rendered into a template. The API is inspired by and very similar to the
+ * [Wordpress implementation](http://codex.wordpress.org/Shortcode_API) of shortcodes.
  * 
- * <b>Defining Custom Shortcodes</b>
- * 
- * All you need to do to define a shortcode is to register a callback with the parser that will be called whenever a
- * shortcode is encountered. This callback will return a string to replace the shortcode with.
- * 
- * To register a shortcode you call:
- *
- * <code>
- *     ShortcodeParser::get('default')->register('shortcode_tag_name', 'callback');
- * </code> 
- * 
- * These parameters are passed to the callback:
- * - Any parameters attached to the shortcode as an associative array (keys are lower-case).
- * - Any content enclosed within the shortcode (if it is an enclosing shortcode). Note that any content within this
- *   will not have been parsed, and can optionally be fed back into the parser.
- * - The ShortcodeParser instance used to parse the content.
- * - The shortcode tag name that was matched within the parsed content.
- * 
- * <b>Inbuilt Shortcodes</b>
- * 
- * From 2.4 onwards links inserted via the CMS into a content field are in the form ''<a href="[sitetree_link id=n]">
- * '', and from 3.0 the comma is used as a separator instead ''<a href="[sitetree_link,id=n]">''. At runtime this is
- * replaced by a plain link to the page with the ID in question.
- * 
- * <b>Limitations</b>
- * 
- * Since the shortcode parser is based on a simple regular expression it cannot properly handle nested shortcodes. For
- * example the below code will not work as expected:
- * 
- * <code>
- *     [shortcode]
- *     [shortcode][/shortcode]
- *     [/shortcode]
- * </code>
- * 
- * The parser will recognise this as:
- * 
- * <code>
- *     [shortcode]
- *     [shortcode]
- *     [/shortcode]
- * </code>
- *
+ * @see http://doc.silverstripe.org/framework/en/topics/shortcodes
  * @package framework
  * @subpackage misc
  */
