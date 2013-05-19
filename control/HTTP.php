d21fd1f0bbd6c654dfa2a3e9286524b51d548964
Jeremy Shipman <jeremy@burnbright.net>
2013-05-20 11:59:04 +1200

    FIX: Don't rewrite urls to be absolute, if they are a URI with a protocol. This is determined in this fix by the existence of a colon ':', to show the uri has a protocol.

--- control/HTTP.php
+++ control/HTTP.php
@@ -62,7 +62,11 @@ class HTTP {
 	public static function absoluteURLs($html) {
 		$html = str_replace('$CurrentPageURL', $_SERVER['REQUEST_URI'], $html);
 		return HTTP::urlRewriter($html, function($url) {
-			if(stripos($url, 'mailto:') === 0) return $url;
+			//no need to rewrite, if uri has a protocol
+				//(determined here by existence of reserved URI character ":")
+			if(stripos($url, ":") !== false){
+				return $url;
+			}
 			return Director::absoluteURL($url, true);
 		});
 	}
