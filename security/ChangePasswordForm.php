06265c992ce5d1f313de8e0c50a6d2b59c82f96f
Sam Minnee <sam@silverstripe.com>
2013-05-25 16:27:17 +1200

    API: Refactor the authentication plugin system to make it easier to create new authenticators without so much copy & paste. NEW: Allow passing of default data to change password from from querystring. FIX: When re-setting your password from a lost password email, make sure that you're not already logged in.

--- security/ChangePasswordForm.php
+++ security/ChangePasswordForm.php
@@ -5,7 +5,8 @@
  * @subpackage security
  */
 class ChangePasswordForm extends Form {
-	
+	protected $authenticator;
+
 	/**
 	 * Constructor
 	 *
@@ -19,31 +20,30 @@ class ChangePasswordForm extends Form {
 	 * @param FieldList|FormAction $actions All of the action buttons in the
 	 *                                     form - a {@link FieldList} of
 	 */
-	public function __construct($controller, $name, $fields = null, $actions = null) {
+	function __construct($controller, $name, Authenticator $authenticator) {
+		$this->authenticator = $authenticator;
+
 		if(isset($_REQUEST['BackURL'])) {
 			$backURL = $_REQUEST['BackURL'];
 		} else {
 			$backURL = Session::get('BackURL');
 		}
 		
-		if(!$fields) {
-			$fields = new FieldList();
-			
-			// Security/changepassword?h=XXX redirects to Security/changepassword
-			// without GET parameter to avoid potential HTTP referer leakage.
-			// In this case, a user is not logged in, and no 'old password' should be necessary.
-			if(Member::currentUser()) {
-				$fields->push(new PasswordField("OldPassword",_t('Member.YOUROLDPASSWORD', "Your old password")));
-			}
+		$fields = new FieldList();
 
-			$fields->push(new PasswordField("NewPassword1", _t('Member.NEWPASSWORD', "New Password")));
-			$fields->push(new PasswordField("NewPassword2", _t('Member.CONFIRMNEWPASSWORD', "Confirm New Password")));
+		// Security/changepassword?h=XXX redirects to Security/changepassword
+		// without GET parameter to avoid potential HTTP referer leakage.
+		// In this case, a user is not logged in, and no 'old password' should be necessary.
+		if(Member::currentUser()) {
+			$fields->push(new PasswordField("OldPassword",_t('Member.YOUROLDPASSWORD', "Your old password")));
 		}
-		if(!$actions) {
-			$actions = new FieldList(
-				new FormAction("doChangePassword", _t('Member.BUTTONCHANGEPASSWORD', "Change Password"))
-			);
-		}
+
+		$fields->push(new PasswordField("NewPassword1", _t('Member.NEWPASSWORD', "New Password")));
+		$fields->push(new PasswordField("NewPassword2", _t('Member.CONFIRMNEWPASSWORD', "Confirm New Password")));
+
+		$actions = new FieldList(
+			new FormAction("doChangePassword", _t('Member.BUTTONCHANGEPASSWORD', "Change Password"))
+		);
 
 		if(isset($backURL)) {
 			$fields->push(new HiddenField('BackURL', 'BackURL', $backURL));
@@ -52,6 +52,13 @@ class ChangePasswordForm extends Form {
 		parent::__construct($controller, $name, $fields, $actions);
 	}
 
+	function getAuthenticator() {
+		return $this->authenticator;
+	}
+
+	function setAuthenticator(Authenticator $authenticator) {
+		$this->authenticator = $authenticator;
+	}
 
 	/**
 	 * Change the password
@@ -61,7 +68,7 @@ class ChangePasswordForm extends Form {
 	public function doChangePassword(array $data) {
 		if($member = Member::currentUser()) {
 			// The user was logged in, check the current password
-			if(empty($data['OldPassword']) || !$member->checkPassword($data['OldPassword'])->valid()) {
+			if(empty($data['OldPassword']) || !$this->authenticator->checkPassword($member, $data['OldPassword'])) {
 				$this->clearMessage();
 				$this->sessionMessage(
 					_t('Member.ERRORPASSWORDNOTMATCH', "Your current password does not match, please try again"), 
@@ -95,7 +102,7 @@ class ChangePasswordForm extends Form {
 			return;
 		}
 		else if($data['NewPassword1'] == $data['NewPassword2']) {
-			$isValid = $member->changePassword($data['NewPassword1']);
+			$isValid = $this->authenticator->changePassword($member, $data['NewPassword1']);
 			if($isValid->valid()) {
 				$member->logIn();
 				
@@ -113,7 +120,7 @@ class ChangePasswordForm extends Form {
 					// Redirect to default location - the login form saying "You are logged in as..."
 					$redirectURL = HTTP::setGetVar(
 						'BackURL',
-						Director::absoluteBaseURL(), $this->controller->Link('login')
+						Director::absoluteBaseURL(), Security::Link('login')
 					);
 					$this->controller->redirect($redirectURL);
 				}
