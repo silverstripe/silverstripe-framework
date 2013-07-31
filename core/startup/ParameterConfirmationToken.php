2f9689b8f80408a39f5b3c96dbcd65b7396f8e67
Hamish Friedlander <hamish@silverstripe.com>
2013-08-01 09:42:52 +1200

    FIX Flush on memory exhaustion and headers sent

--- core/startup/ParameterConfirmationToken.php
+++ core/startup/ParameterConfirmationToken.php
@@ -94,7 +94,14 @@ class ParameterConfirmationToken {
 		$location = "$proto://" . $host . BASE_URL . $url . ($params ? '?'.http_build_query($params) : '');
 
 		// And redirect
-		header('location: '.$location, true, 302);
+		if (headers_sent()) {
+			echo "
+<script>location.href='$location';</script>
+<noscript><meta http-equiv='refresh' content='0; url=$location'></noscript>
+You are being redirected. If you are not redirected soon, <a href='$location'>click here to continue the flush</a>
+";
+		}
+		else header('location: '.$location, true, 302);
 		die;
 	}
 }
