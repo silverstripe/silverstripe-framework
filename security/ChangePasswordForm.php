6c6ab7b0ce6e24ff48a4cd46c66044aec47ed4eb
Devlin <support@layon.de>
2013-10-30 09:46:39 +0100

    Bad redirect in ChangePasswordForm->doChangePassword()

--- security/ChangePasswordForm.php
+++ security/ChangePasswordForm.php
@@ -81,7 +81,7 @@ class ChangePasswordForm extends Form {
 			// The user is not logged in and no valid auto login hash is available
 			if(!$member) {
 				Session::clear('AutoLoginHash');
-				$this->controller->redirect('loginpage');
+				$this->controller->redirect($this->controller->Link('login'));
 				return;
 			}
 		}
