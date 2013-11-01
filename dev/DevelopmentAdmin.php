65b44073379de4c9657f8047da8064fd437247aa
Ingo Schommer <ingo@silverstripe.com>
2013-11-01 11:50:49 +0100

    FIX "Draft" stage to fix dev/build, Versioned docs (fixes #2619)

--- dev/DevelopmentAdmin.php
+++ dev/DevelopmentAdmin.php
@@ -72,6 +72,10 @@ class DevelopmentAdmin extends Controller {
 			}
 		}
 		
+		// Backwards compat: Default to "draft" stage, which is important
+		// for tasks like dev/build which call DataObject->requireDefaultRecords(),
+		// but also for other administrative tasks which have assumptions about the default stage.
+		Versioned::reading_stage('Stage');
 	}
 	
 	public function index() {
