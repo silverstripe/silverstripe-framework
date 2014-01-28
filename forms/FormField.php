93085b9660eeb4d90b2bc54d0cc6c6aeedc12c6f
Will Rossiter <will.rossiter@gmail.com>
2014-01-28 20:00:47 +1300

    Correct whitespace / phpdoc

--- forms/FormField.php
+++ forms/FormField.php
@@ -298,32 +298,33 @@ class FormField extends RequestHandler {
 	}
 	
 	/**
-	 * Add one or more CSS-classes to the formfield-container.
+	 * Add one or more CSS-classes to the formfield-container. Multiple class 
+	 * names should be space delimited.
 	 * 
-	 * @param $class String
+	 * @param string $class
 	 */
 	public function addExtraClass($class) {
-		//split at white space to extract all the classes
 		$classes = preg_split('/\s+/', $class);
+
 		foreach ($classes as $class) {
-			//add each class one by one
-		$this->extraClasses[$class] = $class;
+			$this->extraClasses[$class] = $class;
 		}
+
 		return $this;
 	}
 
 	/**
 	 * Remove one or more CSS-classes from the formfield-container.
 	 * 
-	 * @param $class String
+	 * @param string $class
 	 */
 	public function removeExtraClass($class) {
-		//split at white space to extract all the classes
 		$classes = preg_split('/\s+/', $class);
+
 		foreach ($classes as $class) {
-			//unset each class one by one
 			unset($this->extraClasses[$class]);
 		}
+		
 		return $this;
 	}
 
