silverstripe/silverstripe-framework

commit 3d24079d15dc327a27070833d57beaf2529cfd61

Use filtered file paths in upload (fixes #2836)

filesystem/Upload.php
@@ -129,17 +129,12 @@ class Upload extends Controller {
 		$base = Director::baseFolder();
 		$parentFolder = Folder::find_or_make($folderPath);
 
-		// Create a folder for uploading.
-		if(!file_exists(ASSETS_PATH . "/" . $folderPath)){
-			Filesystem::makeFolder(ASSETS_PATH . "/" . $folderPath);
-		}
-
 		// Generate default filename
 		$nameFilter = FileNameFilter::create();
 		$file = $nameFilter->filter($tmpFile['name']);
 		$fileName = basename($file);
 
-		$relativeFilePath = ASSETS_DIR . "/" . $folderPath . "/$fileName";
+		$relativeFilePath = $parentFolder->getRelativePath() . "/$fileName";
 		
 		// Create a new file record (or try to retrieve an existing one)
 		if(!$this->file) {

commit caecc05b0bc5fde4a4e7f80f0f86aa7cbf45ebc8

Upload path regression caused by 3d24079

filesystem/Upload.php
@@ -134,7 +134,7 @@ class Upload extends Controller {
 		$file = $nameFilter->filter($tmpFile['name']);
 		$fileName = basename($file);
 
-		$relativeFilePath = $parentFolder->getRelativePath() . "/$fileName";
+		$relativeFilePath = $parentFolder ? $parentFolder->getRelativePath() . "$fileName" : $fileName;		
 		
 		// Create a new file record (or try to retrieve an existing one)
 		if(!$this->file) {