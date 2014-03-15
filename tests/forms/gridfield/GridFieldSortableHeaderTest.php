dbf64399b81ec17177dd48f62cf47661412b684d
Simon Welsh <simon@simon.geek.nz>
2014-03-16 09:52:37 +1300

    Update expected ID value

--- tests/forms/gridfield/GridFieldSortableHeaderTest.php
+++ tests/forms/gridfield/GridFieldSortableHeaderTest.php
@@ -31,7 +31,7 @@ class GridFieldSortableHeaderTest extends SapphireTest {
 		// Check that the output shows name and hat as sortable fields, but not city
 		$this->assertContains('<span class="non-sortable">City</span>', $htmlFragment['header']);
 		$this->assertContains('value="Name" class="action ss-gridfield-sort" id="action_SetOrderName"', $htmlFragment['header']);
-		$this->assertContains('value="Cheerleader Hat" class="action ss-gridfield-sort" id="action_SetOrderCheerleader.Hat.Colour"', $htmlFragment['header']);
+		$this->assertContains('value="Cheerleader Hat" class="action ss-gridfield-sort" id="action_SetOrderCheerleader-Hat-Colour"', $htmlFragment['header']);
 		
 		// Check inverse of above
 		$this->assertNotContains('value="City" class="action ss-gridfield-sort" id="action_SetOrderCity"', $htmlFragment['header']);
