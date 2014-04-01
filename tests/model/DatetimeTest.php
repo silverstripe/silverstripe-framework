05e4d1af71261da85f61c246dc1199808384e621
Damian Mooyman <damian@silverstripe.com>
2014-04-01 17:03:10 +1300

    BUG Fix Date and SS_DateTime::FormatFromSettings This issue is caused by the odd default behaviour of Zend_Date, which attempts to parse yyyy-mm-dd format date and times as though they were yyyy-dd-mm.

--- tests/model/DatetimeTest.php
+++ tests/model/DatetimeTest.php
@@ -162,11 +162,12 @@ class SS_DatetimeTest extends SapphireTest {
 			'2000-12-31 1:11:01' => '31/12/2000 01:11:01',
 			'12/12/2000 1:11:01' => '12/12/2000 01:11:01',
 			'2000-12-31' => '31/12/2000 12:00:00',
+			'2014-04-01 10:11:01' => '01/04/2014 10:11:01',
 			'10:11:01' => date('d/m/Y').' 10:11:01'
 		);
 	
 		foreach($fixtures as $from => $to) {
-			$date = DBField::create_field('Datetime', $from);
+			$date = DBField::create_field('SS_Datetime', $from);
 			// With member
 			$this->assertEquals($to, $date->FormatFromSettings($member));
 			// Without member
