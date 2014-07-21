46ac2340aefe4fe19b288a6cc67657f789908132
Daniel Hensby <dan@hens.by>
2014-07-21 15:50:56 +0100

    allowing SS_ENVIRONMENT_FILE to be defined explicitly

--- core/Constants.php
+++ core/Constants.php
@@ -36,43 +36,50 @@
 /**
  * Include _ss_environment.php file
  */
-//define the name of the environment file
-$envFile = '_ss_environment.php';
-//define the dirs to start scanning from (have to add the trailing slash)
-// we're going to check the realpath AND the path as the script sees it
-$dirsToCheck = array(
-	realpath('.'),
-	dirname($_SERVER['SCRIPT_FILENAME'])
-);
-//if they are the same, remove one of them
-if ($dirsToCheck[0] == $dirsToCheck[1]) {
-	unset($dirsToCheck[1]);
-}
-foreach ($dirsToCheck as $dir) {
-	//check this dir and every parent dir (until we hit the base of the drive)
-	// or until we hit a dir we can't read
-	while(true) {
-		//if it's readable, go ahead
-		if (@is_readable($dir)) {
-			//if the file exists, then we include it, set relevant vars and break out
-			if (file_exists($dir . DIRECTORY_SEPARATOR . $envFile)) {
-				define('SS_ENVIRONMENT_FILE', $dir . DIRECTORY_SEPARATOR . $envFile);
-				include_once(SS_ENVIRONMENT_FILE);
-				//break out of BOTH loops because we found the $envFile
-				break(2);
+if (!defined('SS_ENVIRONMENT_FILE')) {
+	if (getenv('SS_ENVIRONMENT_FILE')) {
+		define('SS_ENVIRONMENT_FILE', getenv('SS_ENVIRONMENT_FILE'));
+	}
+	else {
+		//define the name of the environment file
+		$envFile = '_ss_environment.php';
+		//define the dirs to start scanning from (have to add the trailing slash)
+		// we're going to check the realpath AND the path as the script sees it
+		$dirsToCheck = array(
+			realpath('.'),
+			dirname($_SERVER['SCRIPT_FILENAME'])
+		);
+		//if they are the same, remove one of them
+		if ($dirsToCheck[0] == $dirsToCheck[1]) {
+			unset($dirsToCheck[1]);
+		}
+		foreach ($dirsToCheck as $dir) {
+			//check this dir and every parent dir (until we hit the base of the drive)
+			// or until we hit a dir we can't read
+			while(true) {
+				//if it's readable, go ahead
+				if (@is_readable($dir)) {
+					//if the file exists, then we include it, set relevant vars and break out
+					if (file_exists($dir . DIRECTORY_SEPARATOR . $envFile)) {
+						define('SS_ENVIRONMENT_FILE', $dir . DIRECTORY_SEPARATOR . $envFile);
+						include_once(SS_ENVIRONMENT_FILE);
+						//break out of BOTH loops because we found the $envFile
+						break(2);
+					}
+				}
+				else {
+					//break out of the while loop, we can't read the dir
+					break;
+				}
+				if (dirname($dir) == $dir) {
+					// here we need to check that the path of the last dir and the next one are
+					// not the same, if they are, we have hit the root of the drive
+					break;
+				}
+				//go up a directory
+				$dir = dirname($dir);
 			}
 		}
-		else {
-			//break out of the while loop, we can't read the dir
-			break;
-		}
-		if (dirname($dir) == $dir) {
-			// here we need to check that the path of the last dir and the next one are
-			// not the same, if they are, we have hit the root of the drive
-			break;
-		}
-		//go up a directory
-		$dir = dirname($dir);
 	}
 }
 
@@ -178,7 +185,7 @@ if(!isset($_SERVER['HTTP_HOST'])) {
 	 * Fix HTTP_HOST from reverse proxies
 	 */
 	if (TRUSTED_PROXY && isset($_SERVER['HTTP_X_FORWARDED_HOST'])) {
-		
+
 		// Get the first host, in case there's multiple separated through commas
 		$_SERVER['HTTP_HOST'] = strtok($_SERVER['HTTP_X_FORWARDED_HOST'], ',');
 	}
