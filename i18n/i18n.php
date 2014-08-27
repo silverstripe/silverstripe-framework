9d3565f87ea45605f239ba4c88706ddff9bd9870
ClayLennart <Lennart@my-clay.com>
2014-08-27 11:38:11 +0200

    add default injection variables i18n

--- i18n/i18n.php
+++ i18n/i18n.php
@@ -93,6 +93,12 @@ class i18n extends Object implements TemplateGlobalProvider {
 	private static $time_format;
 
 	/**
+	 * @config
+	 * @var array
+	 */
+	private static $default_injection_variables = array();
+
+	/**
 	 * @var array Array of priority keys to instances of Zend_Translate, mapped by name.
 	 */
 	protected static $translators;
@@ -2011,10 +2017,11 @@ class i18n extends Object implements TemplateGlobalProvider {
 		$argList = func_get_args();
 		$argNum = func_num_args();
 		//_t($entity, $string = "", $context (optional), $injectionArray (optional))
-		$injectionArray = null;
+		$injectionArray = self::config()->default_injection_variables;
 		for($i = 0; $i < $argNum; $i++) {
 			if (is_array($argList[$i])) {   //we have reached the injectionArray
-				$injectionArray = $argList[$i]; //any array in the args will be the injection array
+				// merge with the default injections with $argList as highest priotity
+				$injectionArray = array_merge($injectionArray ,$argList[$i]); //any array in the args will be the injection array
 			}
 		}
 
@@ -2057,7 +2064,7 @@ class i18n extends Object implements TemplateGlobalProvider {
 			}
 
 		// inject the variables from injectionArray (if present)
-		if($injectionArray) {
+		if(count($injectionArray)) {
 			$regex = '/\{[\w\d]*\}/i';
 			if(!preg_match($regex, $returnValue)) {
 				// Legacy mode: If no injection placeholders are found,
