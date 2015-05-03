eae7863fff87ca083903b09c5747a7484ca5ceec
Christopher Pitt <cgpitt@gmail.com>
2015-05-03 22:21:59 +1200

    DB filter by NULL

--- search/filters/ExactMatchFilter.php
+++ search/filters/ExactMatchFilter.php
@@ -1,20 +1,18 @@
 <?php
-/**
- * @package framework
- * @subpackage search
- */
 
 /**
- * Selects textual content with an exact match between columnname and keyword.
+ * Selects textual content with an exact match between column name and keyword.
  *
  * @todo case sensitivity switch
  * @todo documentation
- * 
+ *
  * @package framework
  * @subpackage search
  */
 class ExactMatchFilter extends SearchFilter {
-
+	/**
+	 * @param array $modifiers
+	 */
 	public function setModifiers(array $modifiers) {
 		if(($extras = array_diff($modifiers, array('not', 'nocase', 'case'))) != array()) {
 			throw new InvalidArgumentException(
@@ -27,36 +25,53 @@ class ExactMatchFilter extends SearchFilter {
 	/**
 	 * Applies an exact match (equals) on a field value.
 	 *
+	 * @param DataQuery $query
+	 *
 	 * @return DataQuery
 	 */
 	protected function applyOne(DataQuery $query) {
 		$this->model = $query->applyRelation($this->relation);
-		$modifiers = $this->getModifiers();
+
+		$value = $this->getValue();
+
+		if($value === null) {
+			return $query->where(
+				sprintf('%s IS NULL', $this->getDbName())
+			);
+		}
+
 		$where = DB::getConn()->comparisonClause(
 			$this->getDbName(),
-			Convert::raw2sql($this->getValue()),
+			Convert::raw2sql($value),
 			true, // exact?
 			false, // negate?
 			$this->getCaseSensitive()
 		);
+
 		return $query->where($where);
 	}
 
 	/**
-	 * Applies an exact match (equals) on a field value against multiple
-	 * possible values.
+	 * Applies an exact match (equals) on a field value against multiple possible values.
+	 *
+	 * @param DataQuery $query
 	 *
 	 * @return DataQuery
 	 */
 	protected function applyMany(DataQuery $query) {
 		$this->model = $query->applyRelation($this->relation);
+
 		$modifiers = $this->getModifiers();
+
 		$values = array();
-		foreach($this->getValue() as $value) {
+
+		foreach((array) $this->getValue() as $value) {
 			$values[] = Convert::raw2sql($value);
 		}
+
 		if(!in_array('case', $modifiers) && !in_array('nocase', $modifiers)) {
 			$valueStr = "'" . implode("', '", $values) . "'";
+
 			return $query->where(sprintf(
 				'%s IN (%s)',
 				$this->getDbName(),
@@ -72,7 +87,9 @@ class ExactMatchFilter extends SearchFilter {
 					$this->getCaseSensitive()
 				);
 			}
+
 			$where = implode(' OR ', $values);
+
 			return $query->where($where);
 		}
 	}
@@ -80,36 +97,53 @@ class ExactMatchFilter extends SearchFilter {
 	/**
 	 * Excludes an exact match (equals) on a field value.
 	 *
+	 * @param DataQuery $query
+	 *
 	 * @return DataQuery
 	 */
 	protected function excludeOne(DataQuery $query) {
 		$this->model = $query->applyRelation($this->relation);
-		$modifiers = $this->getModifiers();
+
+		$value = $this->getValue();
+
+		if($value === null) {
+			return $query->where(
+				sprintf('%s IS NOT NULL', $this->getDbName())
+			);
+		}
+
 		$where = DB::getConn()->comparisonClause(
 			$this->getDbName(),
-			Convert::raw2sql($this->getValue()),
+			Convert::raw2sql($value),
 			true, // exact?
 			true, // negate?
 			$this->getCaseSensitive()
 		);
+
 		return $query->where($where);
 	}
 
 	/**
-	 * Excludes an exact match (equals) on a field value against multiple
-	 * possible values.
+	 * Excludes an exact match (equals) on a field value against multiple possible values.
+	 *
+	 * @param DataQuery $query
 	 *
 	 * @return DataQuery
 	 */
 	protected function excludeMany(DataQuery $query) {
 		$this->model = $query->applyRelation($this->relation);
+
 		$modifiers = $this->getModifiers();
+
 		$values = array();
-		foreach($this->getValue() as $value) {
+
+		foreach((array) $this->getValue() as $value) {
 			$values[] = Convert::raw2sql($value);
 		}
+
 		if(!in_array('case', $modifiers) && !in_array('nocase', $modifiers)) {
 			$valueStr = "'" . implode("', '", $values) . "'";
+
 			return $query->where(sprintf(
 				'%s NOT IN (%s)',
 				$this->getDbName(),
@@ -125,11 +159,16 @@ class ExactMatchFilter extends SearchFilter {
 					$this->getCaseSensitive()
 				);
 			}
+
 			$where = implode(' OR ', $values);
+
 			return $query->where($where);
 		}
 	}
-	
+
+	/**
+	 * @return bool
+	 */
 	public function isEmpty() {
 		return $this->getValue() === array() || $this->getValue() === null || $this->getValue() === '';
 	}
