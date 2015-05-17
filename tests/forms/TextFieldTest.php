2a36639f9791b09f82caf94275ef132bf1a8a1ff
Kamran Vighio <kamran.usman@gmail.com>
2015-05-17 13:58:53 +0200

    Issue #4073: Updated code based on feedback from @kinglozzer

--- tests/forms/TextFieldTest.php
+++ tests/forms/TextFieldTest.php
@@ -5,43 +5,25 @@
  */
 class TextFieldTest extends SapphireTest {
 
-    private $fieldName = null;
-    private $fieldMaxLength = null;
+	/**
+	 * Tests the TextField Max Length Validation Failure
+	 */
+	public function testMaxLengthValidationFail() {
+		$textField = new TextField('TestField');
+		$textField->setMaxLength(5);
+		$textField->setValue("John Doe"); // 8 characters, so should fail
+		$result = $textField->validate(new RequiredFields());
+		$this->assertFalse($result);
+	}
 
-    /**
-     * Setup base values
-     */
-    public function setup() {
-        $this->fieldName = 'TestField';
-        $this->fieldMaxLength = 5;
-    }
-
-    /**
-     * Tests the TextField Max Length Validation Failure
-     */
-    public function testMaxLengthValidationFail() {
-        $textField = new TextField($this->fieldName);
-        $textField->setMaxLength($this->fieldMaxLength);
-        $textField->setValue("John Doe");
-        $validator = new TextFieldTestValidator();
-        $result = $textField->validate($validator);
-        $this->assertFalse($result);
-    }
-
-    /**
-     * Tests the TextField Max Length Validation Success
-     */
-    public function testMaxLengthValidationSuccess() {
-        $textField = new TextField($this->fieldName);
-        $textField->setMaxLength($this->fieldMaxLength);
-        $textField->setValue("John");
-        $validator = new TextFieldTestValidator();
-        $result = $textField->validate($validator);
-        $this->assertTrue($result);
-    }
-}
-
-class TextFieldTestValidator extends Validator {
-    public function validationError($fieldName, $message, $messageType='') {}
-    public function php($data) {}
+	/**
+	 * Tests the TextField Max Length Validation Success
+	 */
+	public function testMaxLengthValidationSuccess() {
+		$textField = new TextField('TestField');
+		$textField->setMaxLength(5);
+		$textField->setValue("John"); // 4 characters, so should pass
+		$result = $textField->validate(new RequiredFields());
+		$this->assertTrue($result);
+	}
 }
