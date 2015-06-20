987d829094630a580c8ceee53aa6937e59846087
Peter Thaleikis <petert@catalyst.net.nz>
2015-06-20 18:19:26 +1200

    minor adjustments to fix PHP and meet the codestyle in examples

--- forms/GroupedDropdownField.php
+++ forms/GroupedDropdownField.php
@@ -1,55 +1,55 @@
 <?php
 /**
  * Grouped dropdown, using <optgroup> tags.
- * 
+ *
  * $source parameter (from DropdownField) must be a two dimensional array.
  * The first level of the array is used for the <optgroup>, and the second
  * level are the <options> for each group.
- * 
+ *
  * Returns a <select> tag containing all the appropriate <option> tags, with
  * <optgroup> tags around the <option> tags as required.
- * 
+ *
  * <b>Usage</b>
- * 
+ *
  * <code>
- * new GroupedDropdownField(
- *    $name = "dropdown",
- *    $title = "Simple Grouped Dropdown",
- *    $source = array(
- *       "numbers" => array(
- *       		"1" => "1",
- *       		"2" => "2",
- *       		"3" => "3",
- *       		"4" => "4"
- *    		),
- *       "letters" => array(
- *       		"1" => "A",
- *       		"2" => "B",
- *       		"3" => "C",
- *       		"4" => "D",
- *       		"5" => "E",
- *       		"6" => "F"
- *    		)
- *    )
- * )
+ *	new GroupedDropdownField(
+ *		$name = "dropdown",
+ *		$title = "Simple Grouped Dropdown",
+ *		$source = array(
+ *			"numbers" => array(
+ *				"1" => "1",
+ *				"2" => "2",
+ *				"3" => "3",
+ *				"4" => "4"
+ *			),
+ *			"letters" => array(
+ *				"1" => "A",
+ *				"2" => "B",
+ *				"3" => "C",
+ *				"4" => "D",
+ *				"5" => "E",
+ *				"6" => "F"
+ *			)
+ *		)
+ *	);
  * </code>
- * 
+ *
  * <b>Disabling individual items</b>
- * 
+ *
  * <code>
- * $groupedDrDownField->setDisabledItems( 
- *    array(
- *       "numbers" => array(
- *       		"1" => "1",
- *       		"3" => "3"
- *    		),
- *       "letters" => array(
- *       		"3" => "C"
- *    		)
- *    )
- * )
+ *	$groupedDrDownField->setDisabledItems(
+ *		array(
+ *			"numbers" => array(
+ *					"1" => "1",
+ *					"3" => "3"
+ *				),
+ *			"letters" => array(
+ *					"3" => "C"
+ *				)
+ *		)
+ *	);
  * </code>
- * 
+ *
  * @package forms
  * @subpackage fields-basic
  */
@@ -63,7 +63,7 @@ class GroupedDropdownField extends DropdownField {
 				foreach($title as $value2 => $title2) {
 					$disabled = '';
 					if( array_key_exists($value, $this->disabledItems)
-							&& is_array($this->disabledItems[$value]) 
+							&& is_array($this->disabledItems[$value])
 							&& in_array($value2, $this->disabledItems[$value]) ){
 						$disabled = 'disabled="disabled"';
 					}
@@ -87,6 +87,6 @@ class GroupedDropdownField extends DropdownField {
 	public function Type() {
 		return 'groupeddropdown dropdown';
 	}
-	
+
 }
 
