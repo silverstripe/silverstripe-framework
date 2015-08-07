silverstripe/silverstripe-framework

commit ffbeac6b7d3ed1a6e02a150573ee4b2f9251cf9c

Ensuring classinfo is case insensitive

core/ClassInfo.php
@@ -3,8 +3,8 @@
 /**
  * Provides introspection information about the class tree.
  *
- * It's a cached wrapper around the built-in class functions.  SilverStripe uses 
- * class introspection heavily and without the caching it creates an unfortunate 
+ * It's a cached wrapper around the built-in class functions.  SilverStripe uses
+ * class introspection heavily and without the caching it creates an unfortunate
  * performance hit.
  *
  * @package framework
@@ -35,7 +35,7 @@ class ClassInfo {
 	 * @var Array Cache for {@link ancestry()}.
 	 */
 	private static $_cache_ancestry = array();
-	
+
 	/**
 	 * @todo Move this to SS_Database or DB
 	 */
@@ -52,17 +52,18 @@ class ClassInfo {
 			return false;
 		}
 	}
-	
+
 	public static function reset_db_cache() {
 		self::$_cache_all_tables = null;
 		self::$_cache_ancestry = array();
 	}
-	
+
 	/**
 	 * Returns the manifest of all classes which are present in the database.
 	 * @param string $class Class name to check enum values for ClassName field
 	 */
 	public static function getValidSubClasses($class = 'SiteTree', $includeUnbacked = false) {
+		$class = self::class_name($class);
 		$classes = DB::getConn()->enumValuesForField($class, 'ClassName');
 		if (!$includeUnbacked) $classes = array_filter($classes, array('ClassInfo', 'exists'));
 		return $classes;
@@ -71,7 +72,7 @@ class ClassInfo {
 	/**
 	 * Returns an array of the current class and all its ancestors and children
 	 * which have a DB table.
-	 * 
+	 *
 	 * @param string|object $class
 	 * @todo Move this into data object
 	 * @return array
@@ -79,9 +80,7 @@ class ClassInfo {
 	public static function dataClassesFor($class) {
 		$result = array();
 
-		if (is_object($class)) {
-			$class = get_class($class);
-		}
+		$class = self::class_name($class);
 
 		$classes = array_merge(
 			self::ancestry($class),
@@ -102,7 +101,7 @@ class ClassInfo {
 	 * @return string
 	 */
 	public static function baseDataClass($class) {
-		if (is_object($class)) $class = get_class($class);
+		$class = self::class_name($class);
 
 		if (!is_subclass_of($class, 'DataObject')) {
 			throw new InvalidArgumentException("$class is not a subclass of DataObject");
@@ -121,29 +120,48 @@ class ClassInfo {
 	 * Returns a list of classes that inherit from the given class.
 	 * The resulting array includes the base class passed
 	 * through the $class parameter as the first array value.
-	 * 
+	 *
 	 * Example usage:
 	 * <code>
 	 * ClassInfo::subclassesFor('BaseClass');
 	 * 	array(
-	 * 	0 => 'BaseClass',
+	 * 	'BaseClass' => 'BaseClass',
 	 * 	'ChildClass' => 'ChildClass',
 	 * 	'GrandChildClass' => 'GrandChildClass'
 	 * )
 	 * </code>
-	 * 
+	 *
 	 * @param mixed $class string of the classname or instance of the class
 	 * @return array Names of all subclasses as an associative array.
 	 */
 	public static function subclassesFor($class) {
+		//normalise class case
+		$className = self::class_name($class);
 		$descendants = SS_ClassLoader::instance()->getManifest()->getDescendantsOf($class);
-		$result      = array($class => $class);
+		$result      = array($className => $className);
 
 		if ($descendants) {
 			return $result + ArrayLib::valuekey($descendants);
 		} else {
 			return $result;
 		}
+	}
+
+	/**
+	 * Convert a class name in any case and return it as it was defined in PHP
+	 *
+	 * eg: self::class_name('dataobJEct'); //returns 'DataObject'
+	 *
+	 * @param string|object $nameOrObject The classname or object you want to normalise
+	 *
+	 * @return string The normalised class name
+	 */
+	public static function class_name($nameOrObject) {
+		if (is_object($nameOrObject)) {
+			return get_class($nameOrObject);
+		}
+		$reflection = new ReflectionClass($nameOrObject);
+		return $reflection->getName();
 	}
 
 	/**
@@ -155,9 +173,11 @@ class ClassInfo {
 	 * @return array
 	 */
 	public static function ancestry($class, $tablesOnly = false) {
-		if (!is_string($class)) $class = get_class($class);
+		$class = self::class_name($class);
 
-		$cacheKey = $class . '_' . (string)$tablesOnly;
+		$lClass = strtolower($class);
+
+		$cacheKey = $lClass . '_' . (string)$tablesOnly;
 		$parent = $class;
 		if(!isset(self::$_cache_ancestry[$cacheKey])) {
 			$ancestry = array();
@@ -166,7 +186,7 @@ class ClassInfo {
 					$ancestry[$parent] = $parent;
 				}
 			} while ($parent = get_parent_class($parent));
-			self::$_cache_ancestry[$cacheKey] = array_reverse($ancestry);	
+			self::$_cache_ancestry[$cacheKey] = array_reverse($ancestry);
 		}
 
 		return self::$_cache_ancestry[$cacheKey];
@@ -184,16 +204,16 @@ class ClassInfo {
 	 * Returns true if the given class implements the given interface
 	 */
 	public static function classImplements($className, $interfaceName) {
-		return in_array($className, SS_ClassLoader::instance()->getManifest()->getImplementorsOf($interfaceName));
+		return in_array($className, self::implementorsOf($interfaceName));
 	}
 
 	/**
 	 * Get all classes contained in a file.
 	 * @uses ManifestBuilder
-	 * 
+	 *
 	 * @todo Doesn't return additional classes that only begin
 	 *  with the filename, and have additional naming separated through underscores.
-	 * 
+	 *
 	 * @param string $filePath Path to a PHP file (absolute or relative to webroot)
 	 * @return array
 	 */
@@ -205,16 +225,16 @@ class ClassInfo {
 		foreach($manifest as $class => $compareFilePath) {
 			if($absFilePath == $compareFilePath) $matchedClasses[] = $class;
 		}
-		
+
 		return $matchedClasses;
 	}
-	
+
 	/**
 	 * Returns all classes contained in a certain folder.
 	 *
 	 * @todo Doesn't return additional classes that only begin
 	 *  with the filename, and have additional naming separated through underscores.
-	 * 
+	 *
 	 * @param string $folderPath Relative or absolute folder path
 	 * @return array Array of class names
 	 */
@@ -233,25 +253,28 @@ class ClassInfo {
 	private static $method_from_cache = array();
 
 	public static function has_method_from($class, $method, $compclass) {
-		if (!isset(self::$method_from_cache[$class])) self::$method_from_cache[$class] = array();
+		$lClass = strtolower($class);
+		$lMethod = strtolower($method);
+		$lCompclass = strtolower($compclass);
+		if (!isset(self::$method_from_cache[$lClass])) self::$method_from_cache[$lClass] = array();
 
-		if (!array_key_exists($method, self::$method_from_cache[$class])) {
-			self::$method_from_cache[$class][$method] = false;
+		if (!array_key_exists($lMethod, self::$method_from_cache[$lClass])) {
+			self::$method_from_cache[$lClass][$lMethod] = false;
 
 			$classRef = new ReflectionClass($class);
 
 			if ($classRef->hasMethod($method)) {
 				$methodRef = $classRef->getMethod($method);
-				self::$method_from_cache[$class][$method] = $methodRef->getDeclaringClass()->getName();
+				self::$method_from_cache[$lClass][$lMethod] = $methodRef->getDeclaringClass()->getName();
 			}
 		}
 
-		return self::$method_from_cache[$class][$method] == $compclass;
+		return strtolower(self::$method_from_cache[$lClass][$lMethod]) == $lCompclass;
 	}
-	
+
 
 	/**
-	 * Returns the table name in the class hierarchy which contains a given 
+	 * Returns the table name in the class hierarchy which contains a given
 	 * field column for a {@link DataObject}. If the field does not exist, this
 	 * will return null.
 	 *
@@ -261,23 +284,26 @@ class ClassInfo {
 	 * @return string
 	 */
 	public static function table_for_object_field($candidateClass, $fieldName) {
-		if(!$candidateClass || !$fieldName) {
+		if(!$candidateClass || !$fieldName || !is_subclass_of($candidateClass, 'DataObject')) {
 			return null;
 		}
 
-		$exists = class_exists($candidateClass);
+		//normalise class name
+		$candidateClass = self::class_name($candidateClass);
+
+		$exists = self::exists($candidateClass);
 
 		while($candidateClass && $candidateClass != 'DataObject' && $exists) {
 			if(DataObject::has_own_table($candidateClass)) {
 				$inst = singleton($candidateClass);
-				
+
 				if($inst->hasOwnTableDatabaseField($fieldName)) {
 					break;
 				}
 			}
 
 			$candidateClass = get_parent_class($candidateClass);
-			$exists = class_exists($candidateClass);
+			$exists = $candidateClass && self::exists($candidateClass);
 		}
 
 		if(!$candidateClass || !$exists) {

model/DataQuery.php
@@ -12,34 +12,34 @@
  * @package framework
  */
 class DataQuery {
-	
+
 	/**
 	 * @var string
 	 */
 	protected $dataClass;
-	
+
 	/**
 	 * @var SQLQuery
 	 */
 	protected $query;
-	
+
 	/**
 	 * @var array
 	 */
 	protected $collidingFields = array();
 
 	private $queriedColumns = null;
-	
+
 	/**
 	 * @var Boolean
 	 */
 	private $queryFinalised = false;
-	
+
 	// TODO: replace subclass_access with this
 	protected $querySubclasses = true;
 	// TODO: replace restrictclasses with this
 	protected $filterByClassName = true;
-	
+
 	/**
 	 * Create a new DataQuery.
 	 *
@@ -49,14 +49,14 @@ class DataQuery {
 		$this->dataClass = $dataClass;
 		$this->initialiseQuery();
 	}
-	
+
 	/**
 	 * Clone this object
 	 */
 	public function __clone() {
 		$this->query = clone $this->query;
 	}
-	
+
 	/**
 	 * Return the {@link DataObject} class that is being queried.
 	 */
@@ -71,8 +71,8 @@ class DataQuery {
 	public function query() {
 		return $this->getFinalisedQuery();
 	}
-	
-	
+
+
 	/**
 	 * Remove a filter from the query
 	 */
@@ -96,7 +96,7 @@ class DataQuery {
 
 		return $this;
 	}
-	
+
 	/**
 	 * Set up the simplest initial query
 	 */
@@ -104,7 +104,7 @@ class DataQuery {
 		// Get the tables to join to.
 		// Don't get any subclass tables - let lazy loading do that.
 		$tableClasses = ClassInfo::ancestry($this->dataClass, true);
-		
+
 		// Error checking
 		if(!$tableClasses) {
 			if(!SS_ClassLoader::instance()->hasManifest()) {
@@ -122,7 +122,7 @@ class DataQuery {
 		// Build our intial query
 		$this->query = new SQLQuery(array());
 		$this->query->setDistinct(true);
-		
+
 		if($sort = singleton($this->dataClass)->stat('default_sort')) {
 			$this->sort($sort);
 		}
@@ -169,7 +169,7 @@ class DataQuery {
 			$tableClasses = $ancestorTables;
 		}
 
-		$tableNames = array_keys($tableClasses);
+		$tableNames = array_values($tableClasses);
 		$baseClass = $tableNames[0];
 
 		// Iterate over the tables and check what we need to select from them. If any selects are made (or the table is
@@ -183,10 +183,10 @@ class DataQuery {
 				$tableFields = DataObject::database_fields($tableClass);
 				$selectColumns = array_intersect($queriedColumns, array_keys($tableFields));
 			}
-			
+
 			// If this is a subclass without any explicitly requested columns, omit this from the query
 			if(!in_array($tableClass, $ancestorTables) && empty($selectColumns)) continue;
-			
+
 			// Select necessary columns (unless an explicitly empty array)
 			if($selectColumns !== array()) {
 				$this->selectColumnsFromTable($query, $tableClass, $selectColumns);
@@ -197,7 +197,7 @@ class DataQuery {
 				$query->addLeftJoin($tableClass, "\"$tableClass\".\"ID\" = \"$baseClass\".\"ID\"", $tableClass, 10);
 			}
 		}
-		
+
 		// Resolve colliding fields
 		if($this->collidingFields) {
 			foreach($this->collidingFields as $k => $collisions) {
@@ -246,7 +246,7 @@ class DataQuery {
 
 	/**
 	 * Ensure that if a query has an order by clause, those columns are present in the select.
-	 * 
+	 *
 	 * @param SQLQuery $query
 	 * @return null
 	 */
@@ -259,7 +259,7 @@ class DataQuery {
 			$i = 0;
 			foreach($orderby as $k => $dir) {
 				$newOrderby[$k] = $dir;
-				
+
 				// don't touch functions in the ORDER BY or public function calls
 				// selected as fields
 				if(strpos($k, '(') !== false) continue;
@@ -275,26 +275,26 @@ class DataQuery {
 
 					continue;
 				}
-				
+
 				if(count($parts) == 1) {
 					$databaseFields = DataObject::database_fields($baseClass);
-	
-					// database_fields() doesn't return ID, so we need to 
+
+					// database_fields() doesn't return ID, so we need to
 					// manually add it here
 					$databaseFields['ID'] = true;
-				
+
 					if(isset($databaseFields[$parts[0]])) {
 						$qualCol = "\"$baseClass\".\"{$parts[0]}\"";
 					} else {
 						$qualCol = "\"$parts[0]\"";
 					}
-						
+
 					// remove original sort
 					unset($newOrderby[$k]);
 
 					// add new columns sort
 					$newOrderby[$qualCol] = $dir;
-							
+
 					// To-do: Remove this if block once SQLQuery::$select has been refactored to store getSelect()
 					// format internally; then this check can be part of selectField()
 					$selects = $query->getSelect();
@@ -306,7 +306,7 @@ class DataQuery {
 
 					if(!in_array($qualCol, $query->getSelect())) {
 						unset($newOrderby[$k]);
-						
+
 						$newOrderby["\"_SortColumn$i\""] = $dir;
 						$query->selectField($qualCol, "_SortColumn$i");
 
@@ -345,7 +345,7 @@ class DataQuery {
 
 	/**
 	 * Return the maximum value of the given field in this DataList
-	 * 
+	 *
 	 * @param String $field Unquoted database column name (will be escaped automatically)
 	 */
 	public function max($field) {
@@ -354,16 +354,16 @@ class DataQuery {
 
 	/**
 	 * Return the minimum value of the given field in this DataList
-	 * 
+	 *
 	 * @param String $field Unquoted database column name (will be escaped automatically)
 	 */
 	public function min($field) {
 		return $this->aggregate(sprintf('MIN("%s")', Convert::raw2sql($field)));
 	}
-	
+
 	/**
 	 * Return the average value of the given field in this DataList
-	 * 
+	 *
 	 * @param String $field Unquoted database column name (will be escaped automatically)
 	 */
 	public function avg($field) {
@@ -372,13 +372,13 @@ class DataQuery {
 
 	/**
 	 * Return the sum of the values of the given field in this DataList
-	 * 
+	 *
 	 * @param String $field Unquoted database column name (will be escaped automatically)
 	 */
 	public function sum($field) {
 		return $this->aggregate(sprintf('SUM("%s")', Convert::raw2sql($field)));
 	}
-	
+
 	/**
 	 * Runs a raw aggregate expression.  Please handle escaping yourself
 	 */
@@ -415,7 +415,7 @@ class DataQuery {
 				if($expressionForField = $query->expressionForField($k)) {
 					if(!isset($this->collidingFields[$k])) $this->collidingFields[$k] = array($expressionForField);
 					$this->collidingFields[$k][] = "\"$tableClass\".\"$k\"";
-				
+
 				} else {
 					$query->selectField("\"$tableClass\".\"$k\"", $k);
 				}
@@ -428,20 +428,20 @@ class DataQuery {
 			}
 		}
 	}
-	
+
 	/**
 	 * Append a GROUP BY clause to this query.
-	 * 
+	 *
 	 * @param String $groupby Escaped SQL statement
 	 */
 	public function groupby($groupby) {
 		$this->query->addGroupBy($groupby);
 		return $this;
 	}
-	
+
 	/**
 	 * Append a HAVING clause to this query.
-	 * 
+	 *
 	 * @param String $having Escaped SQL statement
 	 */
 	public function having($having) {
@@ -494,7 +494,7 @@ class DataQuery {
 
 	/**
 	 * Append a WHERE with OR.
-	 * 
+	 *
 	 * @example $dataQuery->whereAny(array("\"Monkey\" = 'Chimp'", "\"Color\" = 'Brown'"));
 	 * @see where()
 	 *
@@ -507,7 +507,7 @@ class DataQuery {
 		}
 		return $this;
 	}
-	
+
 	/**
 	 * Set the ORDER BY clause of this query
 	 *
@@ -524,10 +524,10 @@ class DataQuery {
 		} else {
 			$this->query->addOrderBy($sort, $direction);
 		}
-			
+
 		return $this;
 	}
-	
+
 	/**
 	 * Reverse order by clause
 	 *
@@ -537,10 +537,10 @@ class DataQuery {
 		$this->query->reverseOrderBy();
 		return $this;
 	}
-	
+
 	/**
 	 * Set the limit of this query.
-	 * 
+	 *
 	 * @param int $limit
 	 * @param int $offset
 	 */
@@ -562,7 +562,7 @@ class DataQuery {
 
 	/**
 	 * Add an INNER JOIN clause to this query.
-	 * 
+	 *
 	 * @param String $table The unquoted table name to join to.
 	 * @param String $onClause The filter for the join (escaped SQL statement)
 	 * @param String $alias An optional alias name (unquoted)
@@ -576,7 +576,7 @@ class DataQuery {
 
 	/**
 	 * Add a LEFT JOIN clause to this query.
-	 * 
+	 *
 	 * @param String $table The unquoted table to join to.
 	 * @param String $onClause The filter for the join (escaped SQL statement).
 	 * @param String $alias An optional alias name (unquoted)
@@ -592,18 +592,18 @@ class DataQuery {
 	 * Traverse the relationship fields, and add the table
 	 * mappings to the query object state. This has to be called
 	 * in any overloaded {@link SearchFilter->apply()} methods manually.
-	 * 
+	 *
 	 * @param String|array $relation The array/dot-syntax relation to follow
 	 * @return The model class of the related item
 	 */
 	public function applyRelation($relation) {
 		// NO-OP
 		if(!$relation) return $this->dataClass;
-		
+
 		if(is_string($relation)) $relation = explode(".", $relation);
 
 		$modelClass = $this->dataClass;
-		
+
 		foreach($relation as $rel) {
 			$model = singleton($modelClass);
 			if ($component = $model->has_one($rel)) {
@@ -612,7 +612,7 @@ class DataQuery {
 					$realModelClass = ClassInfo::table_for_object_field($modelClass, "{$foreignKey}ID");
 					$this->query->addLeftJoin($component,
 						"\"$component\".\"ID\" = \"{$realModelClass}\".\"{$foreignKey}ID\"");
-				
+
 					/**
 					 * add join clause to the component's ancestry classes so that the search filter could search on
 					 * its ancestor fields.
@@ -667,15 +667,15 @@ class DataQuery {
 
 			}
 		}
-		
+
 		return $modelClass;
 	}
-	
+
 	/**
 	 * Removes the result of query from this query.
-	 * 
+	 *
 	 * @param DataQuery $subtractQuery
-	 * @param string $field 
+	 * @param string $field
 	 */
 	public function subtract(DataQuery $subtractQuery, $field='ID') {
 		$fieldExpression = $subtractQuery->expressionForField($field);
@@ -690,15 +690,15 @@ class DataQuery {
 
 	/**
 	 * Select the given fields from the given table.
-	 * 
+	 *
 	 * @param String $table Unquoted table name (will be escaped automatically)
 	 * @param Array $fields Database column names (will be escaped automatically)
 	 */
 	public function selectFromTable($table, $fields) {
 		$table = Convert::raw2sql($table);
-		$fieldExpressions = array_map(create_function('$item', 
+		$fieldExpressions = array_map(create_function('$item',
 			"return '\"$table\".\"' . Convert::raw2sql(\$item) . '\"';"), $fields);
-		
+
 		$this->query->setSelect($fieldExpressions);
 
 		return $this;
@@ -706,7 +706,7 @@ class DataQuery {
 
 	/**
 	 * Query the given field column from the database and return as an array.
-	 * 
+	 *
 	 * @param string $field See {@link expressionForField()}.
 	 * @return array List of column values for the specified column
 	 */
@@ -720,31 +720,31 @@ class DataQuery {
 
 		return $query->execute()->column($field);
 	}
-	
+
 	/**
 	 * @param  String $field Select statement identifier, either the unquoted column name,
 	 * the full composite SQL statement, or the alias set through {@link SQLQuery->selectField()}.
 	 * @return String The expression used to query this field via this DataQuery
 	 */
 	protected function expressionForField($field) {
-		
+
 		// Prepare query object for selecting this field
 		$query = $this->getFinalisedQuery(array($field));
-		
+
 		// Allow query to define the expression for this field
 		$expression = $query->expressionForField($field);
 		if(!empty($expression)) return $expression;
-		
+
 		// Special case for ID, if not provided
 		if($field === 'ID') {
 			$baseClass = ClassInfo::baseDataClass($this->dataClass);
-			return "\"$baseClass\".\"ID\"";	
+			return "\"$baseClass\".\"ID\"";
 		}
 	}
 
 	/**
 	 * Select the given field expressions.
-	 * 
+	 *
 	 * @param $fieldExpression String The field to select (escaped SQL statement)
 	 * @param $alias String The alias of that field (escaped SQL statement)
 	 */
@@ -759,7 +759,7 @@ class DataQuery {
 	 * @todo This will probably be made obsolete if we have subclasses of DataList and/or DataQuery.
 	 */
 	private $queryParams;
-	
+
 	/**
 	 * Set an arbitrary query parameter, that can be used by decorators to add additional meta-data to the query.
 	 * It's expected that the $key will be namespaced, e.g, 'Versioned.stage' instead of just 'stage'.
@@ -767,7 +767,7 @@ class DataQuery {
 	public function setQueryParam($key, $value) {
 		$this->queryParams[$key] = $value;
 	}
-	
+
 	/**
 	 * Set an arbitrary query parameter, that can be used by decorators to add additional meta-data to the query.
 	 */
@@ -788,7 +788,7 @@ class DataQuery {
 /**
  * Represents a subgroup inside a WHERE clause in a {@link DataQuery}
  *
- * Stores the clauses for the subgroup inside a specific {@link SQLQuery} 
+ * Stores the clauses for the subgroup inside a specific {@link SQLQuery}
  * object.
  *
  * All non-where methods call their DataQuery versions, which uses the base
@@ -833,7 +833,7 @@ class DataQuery_SubGroup extends DataQuery {
 
 	/**
 	 * Set a WHERE with OR.
-	 * 
+	 *
 	 * @example $dataQuery->whereAny(array("\"Monkey\" = 'Chimp'", "\"Color\" = 'Brown'"));
 	 * @see where()
 	 *
@@ -855,10 +855,10 @@ class DataQuery_SubGroup extends DataQuery {
 		}
 
 		$sql = DB::getConn()->sqlWhereToString(
-			$this->whereQuery->getWhere(), 
+			$this->whereQuery->getWhere(),
 			$this->whereQuery->getConnective()
 		);
-		
+
 		$sql = preg_replace('[^\s*WHERE\s*]', '', $sql);
 
 		return $sql;

control/injector/SilverStripeServiceConfigurationLocator.php
@@ -1,36 +1,36 @@
 <?php
 
 /**
- * Use the SilverStripe configuration system to lookup config for a 
+ * Use the SilverStripe configuration system to lookup config for a
  * particular service.
  *
  * @package framework
  * @subpackage injector
  */
 class SilverStripeServiceConfigurationLocator extends ServiceConfigurationLocator {
-	
+
 	/**
 	 * List of Injector configurations cached from Config in class => config format.
-	 * If any config is false, this denotes that this class and all its parents 
+	 * If any config is false, this denotes that this class and all its parents
 	 * have no configuration specified.
-	 * 
+	 *
 	 * @var array
 	 */
 	protected $configs = array();
-	
+
 	public function locateConfigFor($name) {
-		
+
 		// Check direct or cached result
 		$config = $this->configFor($name);
 		if($config !== null) return $config;
-		
+
 		// do parent lookup if it's a class
 		if (class_exists($name)) {
-			$parents = array_reverse(array_keys(ClassInfo::ancestry($name)));
+			$parents = array_reverse(array_values(ClassInfo::ancestry($name)));
 			array_shift($parents);
 
 			foreach ($parents as $parent) {
-				// have we already got for this? 
+				// have we already got for this?
 				$config = $this->configFor($parent);
 				if($config !== null) {
 					// Cache this result
@@ -39,27 +39,27 @@ class SilverStripeServiceConfigurationLocator extends ServiceConfigurationLocato
 				}
 			}
 		}
-		
+
 		// there is no parent config, so we'll record that as false so we don't do the expensive
 		// lookup through parents again
 		$this->configs[$name] = false;
 	}
-	
+
 	/**
 	 * Retrieves the config for a named service without performing a hierarchy walk
-	 * 
+	 *
 	 * @param string $name Name of service
-	 * @return mixed Returns either the configuration data, if there is any. A missing config is denoted 
+	 * @return mixed Returns either the configuration data, if there is any. A missing config is denoted
 	 * by a value of either null (there is no direct config assigned and a hierarchy walk is necessary)
-	 * or false (there is no config for this class, nor within the hierarchy for this class). 
+	 * or false (there is no config for this class, nor within the hierarchy for this class).
 	 */
 	protected function configFor($name) {
-		
+
 		// Return cached result
 		if (isset($this->configs[$name])) {
 			return $this->configs[$name]; // Potentially false
 		}
-		
+
 		$config = Config::inst()->get('Injector', $name);
 		if ($config) {
 			$this->configs[$name] = $config;

commit 1f0602d42fd9e1c0a4268f3a51aa7f483100a935

FIX: Fixed regression from ClassInfo case-sensitivity fix.
This fixes a bug introduced by ffbeac6b7d3ed1a6e02a150573ee4b2f9251cf9c.
ClassInfo::subclassesFor() didn't previously throw an Exception if passed
an invalid class; it just returned no values. This will annoy minor-release
upgrades, and so I've made it return null instead in these situation.

core/ClassInfo.php
@@ -61,6 +61,8 @@ class ClassInfo {
 	 * @return array List of subclasses
 	 */
 	public static function getValidSubClasses($class = 'SiteTree', $includeUnbacked = false) {
+		if(is_string($class) && !class_exists($class)) return null;
+
 		$class = self::class_name($class);
 		$classes = DB::get_schema()->enumValuesForField($class, 'ClassName');
 		if (!$includeUnbacked) $classes = array_filter($classes, array('ClassInfo', 'exists'));
@@ -76,6 +78,8 @@ class ClassInfo {
 	 * @return array
 	 */
 	public static function dataClassesFor($class) {
+		if(is_string($class) && !class_exists($class)) return null;
+
 		$result = array();
 
 		$class = self::class_name($class);
@@ -100,6 +104,8 @@ class ClassInfo {
 	 * @return string
 	 */
 	public static function baseDataClass($class) {
+		if(is_string($class) && !class_exists($class)) return null;
+
 		$class = self::class_name($class);
 
 		if (!is_subclass_of($class, 'DataObject')) {
@@ -134,6 +140,8 @@ class ClassInfo {
 	 * @return array Names of all subclasses as an associative array.
 	 */
 	public static function subclassesFor($class) {
+		if(is_string($class) && !class_exists($class)) return null;
+
 		//normalise class case
 		$className = self::class_name($class);
 		$descendants = SS_ClassLoader::instance()->getManifest()->getDescendantsOf($class);
@@ -159,6 +167,7 @@ class ClassInfo {
 		if (is_object($nameOrObject)) {
 			return get_class($nameOrObject);
 		}
+
 		$reflection = new ReflectionClass($nameOrObject);
 		return $reflection->getName();
 	}
@@ -172,6 +181,8 @@ class ClassInfo {
 	 * @return array
 	 */
 	public static function ancestry($class, $tablesOnly = false) {
+		if(is_string($class) && !class_exists($class)) return null;
+
 		$class = self::class_name($class);
 
 		$lClass = strtolower($class);

model/DataQuery.php
@@ -232,10 +232,12 @@ class DataQuery {
 				foreach($collisions as $collision) {
 					if(preg_match('/^"([^"]+)"/', $collision, $matches)) {
 						$collisionBase = $matches[1];
-						$collisionClasses = ClassInfo::subclassesFor($collisionBase);
-						$collisionClasses = Convert::raw2sql($collisionClasses, true);
-						$caseClauses[] = "WHEN \"$baseClass\".\"ClassName\" IN ("
-							. implode(", ", $collisionClasses) . ") THEN $collision";
+						if(class_exists($collisionBase)) {
+							$collisionClasses = ClassInfo::subclassesFor($collisionBase);
+							$collisionClasses = Convert::raw2sql($collisionClasses, true);
+							$caseClauses[] = "WHEN \"$baseClass\".\"ClassName\" IN ("
+								. implode(", ", $collisionClasses) . ") THEN $collision";
+						}
 					} else {
 						user_error("Bad collision item '$collision'", E_USER_WARNING);
 					}