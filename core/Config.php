c820d06b34e4c3f087af0cd8be987bebafd95f1e
Silbinary Wolf <doogie1012@gmail.com>
2016-01-07 17:42:18 +1100

    Updated deprecation message as instructed, Made ClassLoader::classExists call ClassInfo::exists so changes to ClassInfo::exists stay consistent and profiled vanilla SS page load to see if ClassLoader::classExists is ever called and it wasn't. (+1 squashed commits)
    Squashed commits:
    
    [c060de1] Removed Config class changes as they broke functionality (isset isn't true if value is null)

--- core/Config.php
+++ core/Config.php
@@ -514,7 +514,7 @@ class Config {
 		// Include extensions only if not flagged not to, and some have been set
 		if (($sourceOptions & self::EXCLUDE_EXTRA_SOURCES) != self::EXCLUDE_EXTRA_SOURCES) {
 			// If we don't have a fresh list of extra sources, get it from the class itself
-			if (!isset($class[$this->extraConfigSources]) || $class[$this->extraConfigSources] === null) {
+			if (!array_key_exists($class, $this->extraConfigSources)) {
 				$this->extraConfigSources[$class] = Object::get_extra_config_sources($class);
 			}
 
@@ -617,7 +617,7 @@ class Config {
 		} else {
 			if (!isset($this->overrides[0][$class])) $this->overrides[0][$class] = array();
 
-			if (!isset($name[$this->overrides[0][$class]]) || $name[$this->overrides[0][$class]] === null) {
+			if (!array_key_exists($name, $this->overrides[0][$class])) {
 				$this->overrides[0][$class][$name] = $val;
 			} else {
 				self::merge_high_into_low($this->overrides[0][$class][$name], $val);
