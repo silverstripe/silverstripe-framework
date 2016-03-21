05d3da543d7fd6edcb8651432058d66a971c7a15
Paul Clarke <paul@silverstripe.com>
2016-03-22 12:25:23 +1300

    Add generic action component

---
+++ admin/javascript/src/components/action/index.js
@@ -0,0 +1,109 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import SilverStripeComponent from 'silverstripe-component';
+
+class ActionComponent extends SilverStripeComponent {
+    constructor(props) {
+        super(props);
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+    
+    render() {
+        return (
+            <button className={this.getButtonClasses()} onClick={this.handleClick}>
+                {this.getLoadingIcon()}
+                {this.props.text}
+            </button>
+        );
+    }
+
+    /**
+     * Returns the necessary button classes based on the given props
+     *
+     * @returns string
+     */
+    getButtonClasses() {
+        var buttonClasses = 'btn';
+
+        // If there is no text
+        if (typeof this.props.text === 'undefined') {
+        	buttonClasses += ' no-text';
+        }
+
+        // Add 'type' class
+        if (this.props.type === 'danger') {
+            buttonClasses += ' btn-danger';
+        } else if (this.props.type === 'success') {
+            buttonClasses += ' btn-success';
+        } else if (this.props.type === 'primary') {
+            buttonClasses += ' btn-primary';
+        } else if (this.props.type === 'link') {
+            buttonClasses += ' btn-link';
+        } else if (this.props.type === 'secondary') {
+            buttonClasses += ' btn-secondary';
+        } else if (this.props.type === 'complete') {
+            buttonClasses += ' btn-success-outline';
+        } 
+
+        // Add icon class
+        if (typeof this.props.icon !== 'undefined') {
+            buttonClasses += ` font-icon-${this.props.icon}`;
+        }
+
+        // Add loading class
+        if (this.props.loading === true) {
+            buttonClasses += ' btn--loading';
+        }
+
+        // Add disabled class
+        if (this.props.disabled === true) {
+            buttonClasses += ' disabled';
+        }
+
+        return buttonClasses;
+    }
+
+    /**
+     * Returns markup for the loading icon
+     *
+     * @returns object|null
+     */
+    getLoadingIcon() {
+        if (this.props.loading) {
+            return (
+                <div className="btn__loading-icon" >
+                    <svg viewBox="0 0 44 12"> 
+                        <circle cx="6" cy="6" r="6" />
+                        <circle cx="22" cy="6" r="6" />
+                        <circle cx="38" cy="6" r="6" />
+                    </svg>
+                </div>
+            );
+        }
+
+        return null;
+    }
+
+    /**
+     * Event handler triggered when a user clicks the button.
+     *
+     * @param object event
+     * @returns null
+     */
+    handleClick(event) {
+        if (typeof this.props.handleClick === 'function' && this.props.disabled !== true) {
+            this.props.handleClick(event);
+        }
+
+        return null;
+    }
+}
+
+ActionComponent.propTypes = {
+    type: React.PropTypes.string,
+    icon: React.PropTypes.string,
+    text: React.PropTypes.string
+};
+
+export default ActionComponent;
--- admin/javascript/src/sections/campaign-admin/controller.js
+++ admin/javascript/src/sections/campaign-admin/controller.js
@@ -3,18 +3,37 @@ import { connect } from 'react-redux';
 import SilverStripeComponent from 'silverstripe-component';
 import NorthHeader from '../../components/north-header';
 import GridField from '../grid-field';
+import GridFieldHeader from '../../components/grid-field-header';
+import GridFieldHeaderCell from '../../components/grid-field-header-cell';
+import GridFieldRow from '../../components/grid-field-row';
+import GridFieldCell from '../../components/grid-field-cell';
+import Action from '../../components/action';
+import i18n from 'i18n';
 
 class CampaignAdminContainer extends SilverStripeComponent {
+    constructor(props) {
+        super(props);
+
+        this.addCampaign = this.addCampaign.bind(this);
+    }
 
     render() {
         return (
             <div>
-                <NorthHeader></NorthHeader>
-                <GridField></GridField>
+                <NorthHeader />
+                <Action 
+                    text={i18n._t('Campaigns.ADDCAMPAIGN')} 
+                    type={'secondary'} 
+                    icon={'plus-circled'} 
+                    handleClick={this.addCampaign} />
+                <GridField />
             </div>
         );
     }
 
+    addCampaign() {
+        //Add campaign
+    }
 }
 
 CampaignAdminContainer.propTypes = {
