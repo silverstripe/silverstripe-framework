6aa22c38ed6ec9e7c2ea8425d27e7c2e3cfdf27e
scott1702 <scott@silverstripe.com>
2016-03-31 16:49:12 +1300

    Convert react grid-field from flexbox to table-based

--- admin/javascript/src/components/grid-field/header-cell.js
+++ admin/javascript/src/components/grid-field/header-cell.js
@@ -5,24 +5,8 @@ class GridFieldHeaderCellComponent extends SilverStripeComponent {
 
     render() {
         return (
-            <div className={this.getHeaderCellClassNames()}>{this.props.children}</div>
+            <div className='grid-field-header-cell-component'>{this.props.children}</div>
         );
-    }
-
-    getHeaderCellClassNames() {
-        var headerCellClassNames = 'grid-field-header-cell-component ';
-
-        if (typeof this.props.width !== 'number') {
-            headerCellClassNames += 'cell-width-5';
-        } else if (this.props.width > 10) {
-            headerCellClassNames += 'cell-width-10';
-        } else if (this.props.width < 1) {
-            headerCellClassNames += 'cell-width-1';
-        } else {
-            headerCellClassNames += `cell-width-${this.props.width}`;
-        }
-
-        return headerCellClassNames;
     }
 
 }
