silverstripe/silverstripe-framework

commit c4e5074f67b0929319dbc8c28df2d276f71cc698

Partially reverted b1b85b5
We still require copying thirdparty components from node_modules on tiny_mce

gulpfile.js
@@ -91,6 +91,18 @@ var jquerySizesConfig = {
     files: ['/lib/jquery.sizes.js']
 };
 
+var tinymceConfig = {
+    src: PATHS.MODULES + '/tinymce',
+    dest: PATHS.FRAMEWORK_THIRDPARTY + '/tinymce',
+    files: [
+        '/tinymce.min.js', // Exclude unminified file to keep repository size down
+        '/jquery.tinymce.min.js',
+        '/themes/**',
+        '/skins/**',
+        '/plugins/**'
+    ]
+};
+
 /**
  * Copies files from a source directory to a destination directory.
  *
@@ -177,10 +189,8 @@ gulp.task('bundle-leftandmain', function bundleLeftAndMain() {
             ignore: /(thirdparty)/,
             comments: false
         }))
-        .external('config')
         .external('jQuery')
         .external('i18n')
-        .external('router')
         .bundle()
         .on('update', bundleLeftAndMain)
         .on('error', notify.onError({ message: 'Error: <%= error.message %>' }))
@@ -199,10 +209,8 @@ gulp.task('bundle-lib', function bundleLib() {
             ignore: /(thirdparty)/,
             comments: false
         }))
-        .require(PATHS.ADMIN_JAVASCRIPT_SRC + '/config.js',     { expose: 'config' })
         .require(PATHS.FRAMEWORK_JAVASCRIPT_SRC + '/jQuery.js', { expose: 'jQuery' })
         .require(PATHS.FRAMEWORK_JAVASCRIPT_SRC + '/i18n.js', { expose: 'i18n' })
-        .require(PATHS.FRAMEWORK_JAVASCRIPT_SRC + '/router.js', { expose: 'router' })
         .bundle()
         .on('update', bundleLib)
         .on('error', notify.onError({ message: 'Error: <%= error.message %>' }))
@@ -236,6 +244,7 @@ gulp.task('sanity', function () {
     diffFiles(blueimpLoadImageConfig);
     diffFiles(blueimpTmplConfig);
     diffFiles(jquerySizesConfig);
+    diffFiles(tinymceConfig);
 });
 
 gulp.task('thirdparty', function () {
@@ -283,7 +292,7 @@ gulp.task('css', ['compile:css'], function () {
         rootCompileFolders.forEach(function (folder) {
             gulp.watch(folder + '/scss/**/*.scss', ['compile:css']);
         });
-        
+
         // Watch the .scss files in react components
         gulp.watch('./admin/javascript/src/**/*.scss', ['compile:css']);
     }

commit 6926e85fffabc0fe653f3b6e631ab4e77a7527ec

Reinstate route externals in Gulpfile
Accidental regression from c4e5074f67 (see aa1aebd0079)

gulpfile.js
@@ -189,8 +189,10 @@ gulp.task('bundle-leftandmain', function bundleLeftAndMain() {
             ignore: /(thirdparty)/,
             comments: false
         }))
+        .external('config')
         .external('jQuery')
         .external('i18n')
+        .external('router')
         .bundle()
         .on('update', bundleLeftAndMain)
         .on('error', notify.onError({ message: 'Error: <%= error.message %>' }))
@@ -209,8 +211,10 @@ gulp.task('bundle-lib', function bundleLib() {
             ignore: /(thirdparty)/,
             comments: false
         }))
+        .require(PATHS.ADMIN_JAVASCRIPT_SRC + '/config.js',     { expose: 'config' })
         .require(PATHS.FRAMEWORK_JAVASCRIPT_SRC + '/jQuery.js', { expose: 'jQuery' })
         .require(PATHS.FRAMEWORK_JAVASCRIPT_SRC + '/i18n.js', { expose: 'i18n' })
+        .require(PATHS.FRAMEWORK_JAVASCRIPT_SRC + '/router.js', { expose: 'router' })
         .bundle()
         .on('update', bundleLib)
         .on('error', notify.onError({ message: 'Error: <%= error.message %>' }))