23bf410897ac96a1e3d50a39f3dc64e142038626
Sam Minnee <sam@silverstripe.com>
2016-04-11 18:21:33 +1200

    NEW: Add createEndpointFetcher to backend
    SilverStripeBackend now has a createEndpointFetcher method, described
    in its docblock in more detail.
    
    It’s based the asset-admin code for apiCallerFromEndpoint(). It’s
    refactored and generalised into framework in order to provide a
    general-purpose API for tying Javascript components to backend APIs.
    
    @todo: tests

--- admin/javascript/dist/silverstripe-backend.js
+++ admin/javascript/dist/silverstripe-backend.js
@@ -1,16 +1,16 @@
 (function (global, factory) {
   if (typeof define === "function" && define.amd) {
-    define('ss.silverstripe-backend', ['exports', 'isomorphic-fetch', 'es6-promise'], factory);
+    define('ss.silverstripe-backend', ['exports', 'isomorphic-fetch', 'es6-promise', 'qs'], factory);
   } else if (typeof exports !== "undefined") {
-    factory(exports, require('isomorphic-fetch'), require('es6-promise'));
+    factory(exports, require('isomorphic-fetch'), require('es6-promise'), require('qs'));
   } else {
     var mod = {
       exports: {}
     };
-    factory(mod.exports, global.isomorphicFetch, global.es6Promise);
+    factory(mod.exports, global.isomorphicFetch, global.es6Promise, global.qs);
     global.ssSilverstripeBackend = mod.exports;
   }
-})(this, function (exports, _isomorphicFetch, _es6Promise) {
+})(this, function (exports, _isomorphicFetch, _es6Promise, _qs) {
   'use strict';
 
   Object.defineProperty(exports, "__esModule", {
@@ -20,6 +20,8 @@
   var _isomorphicFetch2 = _interopRequireDefault(_isomorphicFetch);
 
   var _es6Promise2 = _interopRequireDefault(_es6Promise);
+
+  var _qs2 = _interopRequireDefault(_qs);
 
   function _interopRequireDefault(obj) {
     return obj && obj.__esModule ? obj : {
@@ -75,6 +77,96 @@
     }
 
     _createClass(SilverStripeBackend, [{
+      key: 'createEndpointFetcher',
+      value: function createEndpointFetcher(endpointSpec) {
+        var _this = this;
+
+        function encode(contentType, data) {
+          switch (contentType) {
+            case 'application/x-www-form-url-encoded':
+              return _qs2.default.stringify(data);
+
+            case 'application/json':
+            case 'application/x-json':
+            case 'application/x-javascript':
+            case 'text/javascript':
+            case 'text/x-javascript':
+            case 'text/x-json':
+              return JSON.stringify(data);
+
+            default:
+              throw new Error('Can\'t encode format: ' + contentType);
+          }
+        }
+
+        function decode(contentType, text) {
+          switch (contentType) {
+            case 'application/x-www-form-url-encoded':
+              return _qs2.default.parse(text);
+
+            case 'application/json':
+            case 'application/x-json':
+            case 'application/x-javascript':
+            case 'text/javascript':
+            case 'text/x-javascript':
+            case 'text/x-json':
+              return JSON.parse(text);
+
+            default:
+              throw new Error('Can\'t decode format: ' + contentType);
+          }
+        }
+
+        function addQuerystring(url, querystring) {
+          if (url.match(/\?/)) return url + '&' + querystring;
+          return url + '?' + querystring;
+        }
+
+        function parseResponse(response) {
+          return response.text().then(function (body) {
+            return decode(response.headers.get('Content-Type'), body);
+          });
+        }
+
+        var refinedSpec = Object.assign({
+          method: 'get',
+          payloadFormat: 'application/x-www-form-url-encoded',
+          responseFormat: 'application/json'
+        }, endpointSpec);
+
+        var formatShortcuts = {
+          json: 'application/json',
+          urlencoded: 'application/x-www-form-url-encoded'
+        };
+        ['payloadFormat', 'responseFormat'].forEach(function (key) {
+          if (formatShortcuts[refinedSpec[key]]) refinedSpec[key] = formatShortcuts[refinedSpec[key]];
+        });
+
+        if (refinedSpec.payloadFormat === 'querystring') {
+          return function (data) {
+            var headers = {
+              Accept: refinedSpec.responseFormat
+            };
+
+            var encodedData = encode('application/x-www-form-url-encoded', data);
+            var url = addQuerystring(endpointSpec.url, encodedData);
+
+            return _this[refinedSpec.method](url, null, headers).then(parseResponse);
+          };
+        }
+
+        return function (data) {
+          var headers = {
+            Accept: refinedSpec.responseFormat,
+            'Content-Type': refinedSpec.payloadFormat
+          };
+
+          var encodedData = encode(refinedSpec.payloadFormat, data);
+
+          return _this[refinedSpec.method](endpointSpec.url, encodedData, headers).then(parseResponse);
+        };
+      }
+    }, {
       key: 'get',
       value: function get(url) {
         return this.fetch(url, { method: 'get', credentials: 'same-origin' }).then(checkStatus);
