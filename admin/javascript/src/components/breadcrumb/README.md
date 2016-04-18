3c8960d88b438d9eadfd5c1dc7b3138c6dfe56c1
Ingo Schommer <ingo@silverstripe.com>
2016-04-18 22:54:52 +1200

    Simplified breadcrumb component
    The "multiline" variation was the only one actually used at the moment.
    If we need a "singleline" one, we could make a "condensed" variation instead

--- admin/javascript/src/components/breadcrumb/README.md
+++ admin/javascript/src/components/breadcrumb/README.md
@@ -9,27 +9,17 @@ The breadcrumbs for the current section of the CMS.
 An array of objects, each object should have a `text` and `href` key.
 
 ```
-import BreadcrumbComponent from 'breadcrumb';
-
-...
-
-getBreadcrumbs() {
-    var breadcrumbs = [
-        {
-            text: 'Pages',
-            href: 'admin/pages'
-        },
-        {
-            text: 'About us',
-            href: 'admin/pages/show/2'
-        }
-    ];
-    
-    return breadcrumbs;
-}
-
-render() {
-    return <BreadcrumbComponent crumbs={this.getBreadcrumbs()} />
+let breadcrumbs = [
+    {
+        text: 'Pages',
+        href: 'admin/pages'
+    },
+    {
+        text: 'About us',
+        href: 'admin/pages/show/2'
+    }
+];
+<BreadcrumbComponent crumbs={breadcrumbs} />
 }
 
 ...
--- admin/javascript/src/components/breadcrumb/index.js
+++ admin/javascript/src/components/breadcrumb/index.js
@@ -4,14 +4,8 @@ import SilverStripeComponent from 'silverstripe-component';
 class BreadcrumbComponent extends SilverStripeComponent {
 
   render() {
-    const classNames = ['breadcrumb'];
-    if (this.props.multiline) {
-      classNames.push('breadcrumb--multiline');
-    }
-    const classNamesStr = classNames.join(' ');
-
     return (
-      <ol className={classNamesStr}>
+      <ol className="breadcrumb">
         {this.getBreadcrumbs()}
       </ol>
     );
@@ -24,16 +18,24 @@ class BreadcrumbComponent extends SilverStripeComponent {
 
     return [].concat(
      this.props.crumbs.slice(0, -1).map((crumb, index) => [
-       <li><a key={index} className="" href={crumb.href}>{crumb.text}</a></li>,
+       <li className="breadcrumb__item">
+         <a key={index} className="breadcrumb__item-title" href={crumb.href}>{crumb.text}</a>
+        </li>,
      ]),
        this.props.crumbs.slice(-1).map((crumb, index) => [
-         <li className="active">
-           <h2 className="breadcrumb__crumb--last" key={index}>{crumb.text}</h2>
+         <li className="breadcrumb__item breadcrumb__item--last">
+           <h2 className="breadcrumb__item-title breadcrumb__item-title--last" key={index}>
+             {crumb.text}
+           </h2>
          </li>,
        ])
     );
   }
 
 }
+
+BreadcrumbComponent.propTypes = {
+  crumbs: React.PropTypes.array,
+};
 
 export default BreadcrumbComponent;
