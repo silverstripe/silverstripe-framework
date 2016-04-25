25f1f992a9a445fe0e8a7bebf8d43ae6fdee0905
David Craig <david@silverstripe.com>
2016-04-26 11:25:27 +1200

    Made FormBuilder.mergeFieldData do deep merge

--- admin/client/src/components/FormBuilder/tests/FormBuilder-test.js
+++ admin/client/src/components/FormBuilder/tests/FormBuilder-test.js
@@ -1,27 +1,56 @@
-/* global jest, describe, beforeEach */
+/* global jest, describe, expect, it, beforeEach */
 
+jest.unmock('merge');
 jest.unmock('lib/SilverStripeComponent');
 jest.unmock('../FormBuilder');
 
-// import { FormBuilder } from '../';
+import { FormBuilderComponent } from '../FormBuilder';
 
-describe('FormBuilder', () => {
-  describe('getFormSchema()', () => {
-    /*
+describe('FormBuilderComponent', () => {
+  describe('mergeFieldData()', () => {
     let formBuilder;
 
     beforeEach(() => {
       const props = {
-        store: {
-          getState: () => {},
-        },
-        actions: {},
+        forms: {},
+        formsActions: {},
+        schemas: {},
+        schemaActions: {},
         schemaUrl: 'admin/assets/schema/1',
-        schema: { forms: [{ schema: { id: '1', schema_url: 'admin/assets/schema/1' } }] },
       };
 
-      formBuilder = new FormBuilder(props);
+      formBuilder = new FormBuilderComponent(props);
     });
-    */
+
+    it('should deep merge properties on the origional object', () => {
+      const fieldStructure = {
+        component: 'TextField',
+        data: {
+          someCustomData: {
+            x: 1,
+          },
+        },
+      };
+
+      const fieldState = {
+        data: {
+          someCustomData: {
+            y: 2,
+          },
+        },
+        messages: [{ type: 'good' }],
+        valid: true,
+        value: 'My test field',
+      };
+
+      const field = formBuilder.mergeFieldData(fieldStructure, fieldState);
+
+      expect(field.component).toBe('TextField');
+      expect(field.data.someCustomData.x).toBe(1);
+      expect(field.data.someCustomData.y).toBe(2);
+      expect(field.messages[0].type).toBe('good');
+      expect(field.valid).toBe(true);
+      expect(field.value).toBe('My test field');
+    });
   });
 });
