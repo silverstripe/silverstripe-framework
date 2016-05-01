8d2f063f0c40cbb1ec39c05fc653486efe7c867f
Damian Mooyman <damian@silverstripe.com>
2016-05-02 08:51:31 +1200

    Fix eslint errors (#5411)

--- admin/client/dist/js/AddToCampaignForm.js
+++ admin/client/dist/js/AddToCampaignForm.js
@@ -1,109 +1,114 @@
 (function (global, factory) {
-    if (typeof define === "function" && define.amd) {
-        define('ss.AddToCampaignForm', ['jQuery', 'i18n'], factory);
-    } else if (typeof exports !== "undefined") {
-        factory(require('jQuery'), require('i18n'));
-    } else {
-        var mod = {
-            exports: {}
-        };
-        factory(global.jQuery, global.i18n);
-        global.ssAddToCampaignForm = mod.exports;
-    }
-})(this, function (_jQuery, _i18n) {
-    'use strict';
+  if (typeof define === "function" && define.amd) {
+    define('ss.AddToCampaignForm', ['jQuery'], factory);
+  } else if (typeof exports !== "undefined") {
+    factory(require('jQuery'));
+  } else {
+    var mod = {
+      exports: {}
+    };
+    factory(global.jQuery);
+    global.ssAddToCampaignForm = mod.exports;
+  }
+})(this, function (_jQuery) {
+  'use strict';
 
-    var _jQuery2 = _interopRequireDefault(_jQuery);
+  var _jQuery2 = _interopRequireDefault(_jQuery);
 
-    var _i18n2 = _interopRequireDefault(_i18n);
+  function _interopRequireDefault(obj) {
+    return obj && obj.__esModule ? obj : {
+      default: obj
+    };
+  }
 
-    function _interopRequireDefault(obj) {
-        return obj && obj.__esModule ? obj : {
-            default: obj
-        };
-    }
+  _jQuery2.default.entwine('ss', function ($) {
+    $('.add-to-campaign-action, #add-to-campaign__action').entwine({
+      onclick: function onclick() {
+        var dialog = $('#add-to-campaign__dialog');
 
-    _jQuery2.default.entwine('ss', function ($) {
-        $('.add-to-campaign-action, #add-to-campaign__action').entwine({
-            onclick: function onclick() {
-                var dialog = $('#add-to-campaign__dialog');
+        if (dialog.length) {
+          dialog.open();
+        } else {
+          dialog = $('<div id="add-to-campaign__dialog" class="add-to-campaign__dialog" />');
+          $('body').append(dialog);
+        }
 
-                if (dialog.length) {
-                    dialog.open();
-                } else {
-                    dialog = $('<div id="add-to-campaign__dialog" class="add-to-campaign__dialog" />');
-                    $('body').append(dialog);
-                }
+        if (dialog.children().length === 0) dialog.addClass('loading');
 
-                if (dialog.children().length == 0) dialog.addClass('loading');
+        var form = this.closest('form');
+        var button = this;
 
-                var form = this.closest('form');
-                var button = this;
-
-                var formData = form.serializeArray();
-                formData.push({ name: button.attr('name'), value: '1' });
-
-                $.ajax({
-                    url: form.attr('action'),
-                    data: formData,
-                    type: 'POST',
-                    global: false,
-                    complete: function complete() {
-                        dialog.removeClass('loading');
-                    },
-                    success: function success(data, status, xhr) {
-                        if (xhr.getResponseHeader('Content-Type').indexOf('text/plain') === 0) {
-                            var container = $('<div class="add-to-campaign__response add-to-campaign__response--good"><span></span></div>');
-                            container.find('span').text(data);
-                            dialog.append(container);
-                        } else {
-                            dialog.html(data);
-                        }
-                    },
-                    error: function error(xhr, status) {
-                        var error = xhr.responseText || "Something went wrong. Please try again in a few minutes.";
-                        var container = $('<div class="add-to-campaign__response add-to-campaign__response--error"><span></span></div>');
-                        container.find('span').text(error);
-                        dialog.append(container);
-                    }
-                });
-
-                return false;
-            }
-        }), $('#add-to-campaign__dialog').entwine({
-            onadd: function onadd() {
-                if (!this.is('.ui-dialog-content')) {
-                    this.ssdialog({
-                        autoOpen: true,
-                        minHeight: 200,
-                        maxHeight: 200,
-                        minWidth: 200,
-                        maxWidth: 500
-                    });
-                }
-
-                this._super();
-            },
-
-            open: function open() {
-                this.ssdialog('open');
-            },
-
-            close: function close() {
-                this.ssdialog('close');
-            },
-
-            'onssdialogclose': function onssdialogclose() {
-                this.empty();
-            },
-
-            'onchosen:showing_dropdown': function onchosenShowing_dropdown() {
-                this.css({ overflow: 'visible' });
-            },
-
-            'onchosen:hiding_dropdown': function onchosenHiding_dropdown() {
-                this.css({ overflow: '' });
-            }
+        var formData = form.serializeArray();
+        formData.push({
+          name: button.attr('name'),
+          value: '1'
         });
+
+        $.ajax({
+          url: form.attr('action'),
+          data: formData,
+          type: 'POST',
+          global: false,
+          complete: function complete() {
+            dialog.removeClass('loading');
+          },
+          success: function success(data, status, xhr) {
+            if (xhr.getResponseHeader('Content-Type').indexOf('text/plain') === 0) {
+              var container = $('<div class="add-to-campaign__response add-to-campaign__response--good">' + '<span></span></div>');
+              container.find('span').text(data);
+              dialog.append(container);
+            } else {
+              dialog.html(data);
+            }
+          },
+          error: function error(xhr) {
+            var error = xhr.responseText || 'Something went wrong. Please try again in a few minutes.';
+            var container = $('<div class="add-to-campaign__response add-to-campaign__response--error">' + '<span></span></div>');
+            container.find('span').text(error);
+            dialog.append(container);
+          }
+        });
+
+        return false;
+      }
     });
+
+    $('#add-to-campaign__dialog').entwine({
+      onadd: function onadd() {
+        if (!this.is('.ui-dialog-content')) {
+          this.ssdialog({
+            autoOpen: true,
+            minHeight: 200,
+            maxHeight: 200,
+            minWidth: 200,
+            maxWidth: 500
+          });
+        }
+
+        this._super();
+      },
+      open: function open() {
+        this.ssdialog('open');
+      },
+      close: function close() {
+        this.ssdialog('close');
+      },
+      onssdialogclose: function onssdialogclose() {
+        this.empty();
+      },
+
+
+      'onchosen:showing_dropdown': function onchosenShowing_dropdown() {
+        this.css({
+          overflow: 'visible'
+        });
+      },
+
+      'onchosen:hiding_dropdown': function onchosenHiding_dropdown() {
+        this.css({
+          overflow: ''
+        });
+      }
+    });
+  });
 });
--- admin/client/src/legacy/AddToCampaignForm.js
+++ admin/client/src/legacy/AddToCampaignForm.js
@@ -1,90 +1,102 @@
-import $ from 'jQuery';
-import i18n from 'i18n';
+import jQuery from 'jQuery';
 
-$.entwine('ss', function($){
-    $('.add-to-campaign-action, #add-to-campaign__action').entwine({
-        onclick: function() {
-            var dialog = $('#add-to-campaign__dialog');
+jQuery.entwine('ss', ($) => {
+  $('.add-to-campaign-action, #add-to-campaign__action').entwine({
+    onclick() {
+      let dialog = $('#add-to-campaign__dialog');
 
-            if (dialog.length) {
-                dialog.open();
-            } else {
-                dialog = $('<div id="add-to-campaign__dialog" class="add-to-campaign__dialog" />');
-                $('body').append(dialog);
-            }
+      if (dialog.length) {
+        dialog.open();
+      } else {
+        dialog = $('<div id="add-to-campaign__dialog" class="add-to-campaign__dialog" />');
+        $('body').append(dialog);
+      }
 
-            if (dialog.children().length == 0) dialog.addClass('loading');
+      if (dialog.children().length === 0) dialog.addClass('loading');
 
-            var form = this.closest('form');
-            var button = this;
+      const form = this.closest('form');
+      const button = this;
 
-            var formData = form.serializeArray();
-            formData.push({name: button.attr('name'), value:'1'});
+      const formData = form.serializeArray();
+      formData.push({
+        name: button.attr('name'),
+        value: '1',
+      });
 
-            $.ajax({
-                url: form.attr('action'),
-                data: formData,
-                type: 'POST',
-                global: false,
-                complete: function() {
-                    dialog.removeClass('loading');
-                },
-                success: function(data, status, xhr) {
-                    if (xhr.getResponseHeader('Content-Type').indexOf('text/plain') === 0) {
-                        var container = $('<div class="add-to-campaign__response add-to-campaign__response--good"><span></span></div>');
-                        container.find('span').text(data);
-                        dialog.append(container);
-                    } else {
-                        dialog.html(data);
-                    }
-                },
-                error: function(xhr, status) {
-                    var error = xhr.responseText || "Something went wrong. Please try again in a few minutes.";
-                    var container = $('<div class="add-to-campaign__response add-to-campaign__response--error"><span></span></div>');
-                    container.find('span').text(error);
-                    dialog.append(container);
-                }
-            });
-
-            return false;
-        }
-    }),
-
-    $('#add-to-campaign__dialog').entwine({
-        onadd: function() {
-            // Create jQuery dialog
-            if (!this.is('.ui-dialog-content')) {
-                this.ssdialog({
-                    autoOpen: true,
-                    minHeight: 200,
-                    maxHeight: 200,
-                    minWidth: 200,
-                    maxWidth: 500
-                });
-            }
-
-            this._super();
+      $.ajax({
+        url: form.attr('action'),
+        data: formData,
+        type: 'POST',
+        global: false,
+        complete() {
+          dialog.removeClass('loading');
         },
-
-        open: function() {
-            this.ssdialog('open');
+        success(data, status, xhr) {
+          if (xhr.getResponseHeader('Content-Type').indexOf('text/plain') === 0) {
+            const container = $(
+              '<div class="add-to-campaign__response add-to-campaign__response--good">' +
+              '<span></span></div>'
+            );
+            container.find('span').text(data);
+            dialog.append(container);
+          } else {
+            dialog.html(data);
+          }
         },
-
-        close: function() {
-            this.ssdialog('close');
+        error(xhr) {
+          const error = xhr.responseText
+            || 'Something went wrong. Please try again in a few minutes.';
+          const container = $(
+            '<div class="add-to-campaign__response add-to-campaign__response--error">' +
+            '<span></span></div>'
+          );
+          container.find('span').text(error);
+          dialog.append(container);
         },
+      });
 
-        'onssdialogclose': function() {
-            this.empty();
-        },
+      return false;
+    },
+  });
 
-        'onchosen:showing_dropdown': function() {
-            this.css({overflow: 'visible'});
-        },
+  $('#add-to-campaign__dialog').entwine({
+    onadd() {
+      // Create jQuery dialog
+      if (!this.is('.ui-dialog-content')) {
+        this.ssdialog({
+          autoOpen: true,
+          minHeight: 200,
+          maxHeight: 200,
+          minWidth: 200,
+          maxWidth: 500,
+        });
+      }
 
-        'onchosen:hiding_dropdown': function() {
-            this.css({overflow: ''});
-        }
-    });
+      this._super();
+    },
 
-})
+    open() {
+      this.ssdialog('open');
+    },
+
+    close() {
+      this.ssdialog('close');
+    },
+
+    onssdialogclose() {
+      this.empty();
+    },
+
+    'onchosen:showing_dropdown': function () {  // eslint-disable-line
+      this.css({
+        overflow: 'visible',
+      });
+    },
+
+    'onchosen:hiding_dropdown': function () {  // eslint-disable-line
+      this.css({
+        overflow: '',
+      });
+    },
+  });
+});
