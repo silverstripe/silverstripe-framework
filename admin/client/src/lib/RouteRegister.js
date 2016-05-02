a8db5621c49dbe8b5c847724f8bb7df2db59062c
David Craig <david@silverstripe.com>
2016-05-03 11:26:29 +1200

    Add docs to RouteRegister

--- admin/client/src/lib/RouteRegister.js
+++ admin/client/src/lib/RouteRegister.js
@@ -2,10 +2,41 @@ import deepFreeze from 'deep-freeze';
 
 /**
  * The register of routes
+ *
  * @private
  */
 let register = deepFreeze({});
 
+/**
+ * RouteRegister is the iterface developers should use to register routes with
+ * the main client application. Routes should not be registered with Page.js directly.
+ *
+ * Register routes using the `DOMContentLoaded` event in your controller file.
+ *
+ * __controller.js__
+ * ```
+ * import routeRegister from 'lib/RouteRegister';
+ *
+ * document.addEventListener('DOMContentLoaded', () => {
+ *   routeRegister.add('', (ctx, next) => {
+ *     // Do stuff.
+ *   });
+ * }
+ * ```
+ *
+ * Any route callback you register will invoked _after_ passing through to top level wildcard route.
+ * This route adds some custom properties to the `ctx` object which will be useful in your callback.
+ *
+ * `ctx.store` - [Redux store](http://redux.js.org/docs/api/Store.html) for the client application.
+ *
+ * All routes registered with `RouteRegister` are applied to Page.js by `appBoot()`
+ * see `/admin/client/src/boot/index.js`.
+ *
+ * Page.js doesn't provide a way to inspect which routes are registered
+ * so you can use `RouteRegister` to do this using the `get` or `getAll` methods.
+ *
+ * @class
+ */
 class RouteRegister {
   /**
    * Adds a route to the register.
@@ -27,7 +58,7 @@ class RouteRegister {
   }
 
   /**
-   * Revoves a route from the register.
+   * Removes a route from the register.
    *
    * @param {string} - The route to remove.
    * @return {object}
