faffdf4981106c8d2f763e380974a6c0537c7ecf
Cochrane, Mike <Mike.Cochrane@airnz.co.nz>
2016-05-09 20:12:29 +1200

    Explictly set the FileNameFilter replacements before relying on them

--- tests/filesystem/FolderTest.php
+++ tests/filesystem/FolderTest.php
@@ -21,6 +21,15 @@ class FolderTest extends SapphireTest {
 		// Set backend root to /FolderTest
 		AssetStoreTest_SpyStore::activate('FolderTest');
 
+		// Set the File Name Filter replacements so files have the expected names
+        Config::inst()->update('FileNameFilter', 'default_replacements', array(
+            '/\s/' => '-', // remove whitespace
+            '/_/' => '-', // underscores to dashes
+            '/[^A-Za-z0-9+.\-]+/' => '', // remove non-ASCII chars, only allow alphanumeric plus dash and dot
+            '/[\-]{2,}/' => '-', // remove duplicate dashes
+            '/^[\.\-_]+/' => '', // Remove all leading dots, dashes or underscores
+        ));
+
 		// Create a test folders for each of the fixture references
 		foreach(Folder::get() as $folder) {
 			$path = AssetStoreTest_SpyStore::getLocalPath($folder);
