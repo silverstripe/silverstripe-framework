413e3d74167d3508d7e309a911bcac3d5a6ca680
Cochrane, Mike <Mike.Cochrane@airnz.co.nz>
2016-05-09 21:08:30 +1200

    Remember the basic details about a user when they are deleted

---
+++ security/DeletedMember.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * The delete member class which represents a user that has been deleted from the system
+ */
+class DeletedMember extends DataObject {
+
+    private static $db = array(
+        'FirstName' => 'Varchar',
+        'Surname' => 'Varchar',
+        'Email' => 'Varchar',
+        'NumVisit' => 'Int',
+        'LastVisited' => 'SS_Datetime',
+        'Locale' => 'Varchar(6)',
+        // In ISO format
+        'DateFormat' => 'Varchar(30)',
+        'TimeFormat' => 'Varchar(30)',
+    );
+
+    function getFirstName() {
+        return '(deleted) ' . $this->getField('FirstName');
+    }
+
+    public function getTitle() {
+        return '(deleted) ' . $this->getField('Surname') . ', ' . $this->getField('FirstName');
+    }
+
+    public function getEmail() {
+        return '(deleted) ' . $this->getField('Email');
+    }
+
+    /**
+     * Get the complete name of the member
+     *
+     * @return string Returns the first- and surname of the member.
+     */
+    public function getName() {
+        return ($this->data()->Surname) ? trim($this->data()->FirstName . ' ' . $this->data()->Surname) : $this->data()->FirstName;
+    }
+
+}
