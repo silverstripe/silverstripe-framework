f4c2a89d5a1943c6578ec0be9352590938ba1794
Damian Mooyman <damian@silverstripe.com>
2016-05-11 11:05:57 +1200

    Update translations

--- admin/javascript/lang/src/nl.js
+++ admin/javascript/lang/src/nl.js
@@ -1,17 +1,17 @@
 {
-    "CMSMAIN.SELECTONEPAGE": "Selecteer minstens 1 pagina.",
-    "CMSMAIN.BATCH_UNPUBLISH_PROMPT": "Je hebt {num} pagina(s) geselecteerd.\n\nWeet je zeker dat je de publicatie wilt intrekken?",
-    "CMSMAIN.BATCH_PUBLISH_PROMPT": "Je hebt {num} pagina(s) geselecteerd.\n\nWeet je zeker dat je de pagina(s) wilt publiceren?",
-    "CMSMAIN.BATCH_DELETE_PROMPT": "Je hebt {num} pagina(s) geselecteerd.\n\nWeet je zeker dat je de pagina(s) wilt verwijderen?",
     "CMSMAIN.BATCH_ARCHIVE_PROMPT": "Je hebt {num} pagina(s) geselecteerd.\n\nWeet je zeker dat je de pagina(s) wilt archiveren?\n\nDe publicatie van deze en onderliggende pagina's wordt ingetrokken en gearchiveerd.",
-    "CMSMAIN.BATCH_RESTORE_PROMPT": "Je hebt {num} pagina(s) geselecteerd.\n\nWeet je zeker dat je de pagina(s) wilt herstellen? Onderliggende pagina's worden naar het top level hersteld, tenzij deze pagina's ook worden hersteld.",
     "CMSMAIN.BATCH_DELETELIVE_PROMPT": "Je hebt {num} pagina(s) geselecteerd.\n\nWeet je zeker dat je de publicatie van deze pagina's wilt intrekken?",
+    "CMSMAIN.BATCH_DELETE_PROMPT": "Je hebt {num} pagina(s) geselecteerd.\n\nWeet je zeker dat je de pagina(s) wilt verwijderen?",
+    "CMSMAIN.BATCH_PUBLISH_PROMPT": "Je hebt {num} pagina(s) geselecteerd.\n\nWeet je zeker dat je de pagina(s) wilt publiceren?",
+    "CMSMAIN.BATCH_RESTORE_PROMPT": "Je hebt {num} pagina(s) geselecteerd.\n\nWeet je zeker dat je de pagina(s) wilt herstellen? Onderliggende pagina's worden naar het top level hersteld, tenzij deze pagina's ook worden hersteld.",
+    "CMSMAIN.BATCH_UNPUBLISH_PROMPT": "Je hebt {num} pagina(s) geselecteerd.\n\nWeet je zeker dat je de publicatie wilt intrekken?",
+    "CMSMAIN.SELECTONEPAGE": "Selecteer minstens 1 pagina.",
     "LeftAndMain.CONFIRMUNSAVED": "Weet u zeker dat u deze pagina wilt verlaten?\nWAARSCHUWING: Uw veranderingen zijn niet opgeslagen.\n\nKies OK om te verlaten, of Cancel om op de huidige pagina te blijven.",
     "LeftAndMain.CONFIRMUNSAVEDSHORT": "WAARSCHUWING: Uw veranderingen zijn niet opgeslagen",
-    "SecurityAdmin.BATCHACTIONSDELETECONFIRM": "Weet u zeker dat u deze groep %s wilt verwijderen?",
-    "ModelAdmin.SAVED": "Opgeslagen",
-    "ModelAdmin.REALLYDELETE": "Weet u zeker dat u wilt verwijderen?",
+    "LeftAndMain.PAGEWASDELETED": "Deze pagina is verwijderd. Om een pagina aan te passen, selecteer deze aan de linkerkant.",
     "ModelAdmin.DELETED": "Verwijderd",
+    "ModelAdmin.REALLYDELETE": "Weet u zeker dat u wilt verwijderen?",
+    "ModelAdmin.SAVED": "Opgeslagen",
     "ModelAdmin.VALIDATIONERROR": "Validatiefout",
-    "LeftAndMain.PAGEWASDELETED": "Deze pagina is verwijderd. Om een pagina aan te passen, selecteer deze aan de linkerkant."
+    "SecurityAdmin.BATCHACTIONSDELETECONFIRM": "Weet u zeker dat u deze groep %s wilt verwijderen?"
 }
