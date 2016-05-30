5e634c2a163a5af89eb2610a9820f81a36999a3a
Paul Clarke <paul@silverstripe.com>
2016-05-31 10:20:45 +1200

    Update Form component to just Form Convert form and Textfield  styles to use Bootstrap Split out btn styles a bit more clearly defined (BEM) Toolbar modifier to improve spacing for smaller screens Use bootstrap spacer styles .m-t-1 (margin-top-1 x spacer) instead of custom spacer Added a few typography helpers Tab styles continued although they are hidden (used on AssetAdmin editor panel)

--- admin/client/src/components/TextField/TextField.js
+++ admin/client/src/components/TextField/TextField.js
@@ -23,16 +23,18 @@ class TextField extends SilverStripeComponent {
 
     // The extraClass property is defined on both the holder and element
     // for legacy reasons (same behaviour as PHP rendering)
-    const classNames = ['field', 'text', 'form-group', this.props.extraClass].join(' ');
+    const classNames = ['form-group', this.props.extraClass].join(' ');
 
     return (
       <div className={classNames}>
         {labelText &&
-          <label className="left" htmlFor={`gallery_${this.props.name}`}>
+          <label className="form__field-label" htmlFor={`gallery_${this.props.name}`}>
             {labelText}
           </label>
         }
+        <div className="form__field-holder">
         {field}
+        </div>
       </div>
     );
   }
@@ -41,7 +43,7 @@ class TextField extends SilverStripeComponent {
     return {
       // The extraClass property is defined on both the holder and element
       // for legacy reasons (same behaviour as PHP rendering)
-      className: ['text', 'form-control', this.props.extraClass].join(' '),
+      className: ['form-control', this.props.extraClass].join(' '),
       id: `gallery_${this.props.name}`,
       name: this.props.name,
       onChange: this.handleChange,
