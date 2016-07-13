d54f4dc9c6a31ea41cc3d558db1a6ec20afff9a6
Damian Mooyman <damian@silverstripe.com>
2016-07-13 14:33:19 +1200

    Convert tabs to spaces in legacy script files

--- admin/client/src/legacy/LeftAndMain.TreeDropdownField.js
+++ admin/client/src/legacy/LeftAndMain.TreeDropdownField.js
@@ -2,15 +2,15 @@ import $ from 'jQuery';
 
 $.entwine('ss', function($){
 
-	// Any TreeDowndownField needs to refresh it's contents after a form submission,
-	// because the tree on the backend might have changed
-	$('.TreeDropdownField').entwine({
-		'from .cms-container form': {
-			onaftersubmitform: function(e){
-				this.find('.tree-holder').empty();
-				this._super();
-			}
-		}
-	});
+  // Any TreeDowndownField needs to refresh it's contents after a form submission,
+  // because the tree on the backend might have changed
+  $('.TreeDropdownField').entwine({
+    'from .cms-container form': {
+      onaftersubmitform: function(e){
+        this.find('.tree-holder').empty();
+        this._super();
+      }
+    }
+  });
 
 });
--- admin/client/src/legacy/MemberDatetimeOptionsetField.js
+++ admin/client/src/legacy/MemberDatetimeOptionsetField.js
@@ -2,18 +2,18 @@ import $ from 'jQuery';
 
 $.entwine('ss', function($){
 
-	$('.memberdatetimeoptionset').entwine({
-		onmatch: function() {
-			this.find('.description .toggle-content').hide();
-			this._super();
-		}
-	});
+  $('.memberdatetimeoptionset').entwine({
+    onmatch: function() {
+      this.find('.description .toggle-content').hide();
+      this._super();
+    }
+  });
 
-	$('.memberdatetimeoptionset .toggle').entwine({
-		onclick: function(e) {
-			jQuery(this).closest('.description').find('.toggle-content').toggle();
-			return false;
-		}
-	});
+  $('.memberdatetimeoptionset .toggle').entwine({
+    onclick: function(e) {
+      jQuery(this).closest('.description').find('.toggle-content').toggle();
+      return false;
+    }
+  });
 
 });
