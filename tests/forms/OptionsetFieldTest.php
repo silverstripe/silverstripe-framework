62a242154ec3508fe9b174a40713c8520ac1684c
Damian Mooyman <damian@silverstripe.com>
2016-08-03 11:23:17 +1200

    [ss-2016-015] Fix value / title escaping in CheckboxSetField and OptionsetField

--- tests/forms/OptionsetFieldTest.php
+++ tests/forms/OptionsetFieldTest.php
@@ -6,11 +6,11 @@
 class OptionsetFieldTest extends SapphireTest {
 	public function testSetDisabledItems() {
 		$f = new OptionsetField(
-			'Test', 
-			false, 
+			'Test',
+			false,
 			array(0 => 'Zero', 1 => 'One')
 		);
-		
+
 		$f->setDisabledItems(array(0));
 		$p = new CSSContentParser($f->Field());
 		$item0 = $p->getBySelector('#Test_0');
@@ -33,5 +33,19 @@ class OptionsetFieldTest extends SapphireTest {
 		$readonlyField = $field->performReadonlyTransformation();
 		preg_match('/Yes/', $field->Field(), $matches);
 		$this->assertEquals($matches[0], 'Yes');
+	}
+
+	public function testSafelyCast() {
+		$field1 = new OptionsetField('Options', 'Options', array(
+			1 => 'One',
+			2 => 'Two & Three',
+			3 => DBField::create_field('HTMLText', 'Four &amp; Five &amp; Six')
+		));
+		$fieldHTML = (string)$field1->Field();
+		$this->assertContains('One', $fieldHTML);
+		$this->assertContains('Two &amp; Three', $fieldHTML);
+		$this->assertNotContains('Two & Three', $fieldHTML);
+		$this->assertContains('Four &amp; Five &amp; Six', $fieldHTML);
+		$this->assertNotContains('Four & Five & Six', $fieldHTML);
 	}
 }
