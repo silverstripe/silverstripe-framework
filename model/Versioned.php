0c7264eca6dd4b11b7f0f7fb38606a17980940c8
Rob Ingram <rob@headcrash.org>
2016-09-01 08:37:50 +1200

    DOCS Update PHP doc on latestPublished method (#5905)
    * Update doc comment on  method to be more accurate

--- model/Versioned.php
+++ model/Versioned.php
@@ -896,9 +896,9 @@ class Versioned extends DataExtension implements TemplateGlobalProvider {
 	}
 
 	/**
-	 * Get the latest published version of this object.
+	 * Is the latest version of the object published?
 	 *
-	 * @return DataObject
+	 * @return bool
 	 */
 	public function latestPublished() {
 		// Get the root data object class - this will have the version field
