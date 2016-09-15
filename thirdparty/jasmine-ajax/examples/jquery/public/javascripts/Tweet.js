7d67b24c257c515ee758ca7df11aba81766d82fd
Ingo Schommer <ingo@silverstripe.com>
2016-09-15 14:37:35 +1200

    API Removed legacy Jasmine JS Unit tests
    They were used for TreeDropdownField JavaScript tests but were never run
    on CI, hence have been a bit of a wasted effort.
    
    We're using a different JS unit testing solution now (geared towards React),
    and running Jasmine in parallel on Travis for a few test cases
    doesn't warrant the setup effort involved.
    
    The TreeDropdownField component will eventually move to a React solution
    which can be developed in a test-driven fashion.

--- thirdparty/jasmine-ajax/examples/jquery/public/javascripts/Tweet.js
+++
@@ -1,6 +0,0 @@
-function Tweet(tweet){
-  this.postedAt = tweet.created_at;
-  this.text = tweet.text;
-  this.imageUrl = tweet.profile_image_url;
-  this.user = tweet.from_user;
-}
--- thirdparty/jasmine-ajax/examples/jquery/spec/javascripts/helpers/tweets.js
+++
@@ -1,3 +0,0 @@
-var Tweets = {
-  noAtReply: '{"profile_image_url":"http://a2.twimg.com/profile_images/1014111170/06212010155_normal.jpg","created_at":"Thu, 29 Jul 2010 02:18:53 +0000","from_user":"_wbrodrigues","metadata":{"result_type":"recent"},"to_user_id":null,"text":"Pres Obama on stage with the Foo fighters, jonas brothers and a whole lot of ppl..nice..","id":19789985947,"from_user_id":139732299,"geo":null,"iso_language_code":"en","source":"&lt;a href=&quot;http://www.tweetdeck.com&quot; rel=&quot;nofollow&quot;&gt;TweetDeck&lt;/a&gt;"}'
-}
