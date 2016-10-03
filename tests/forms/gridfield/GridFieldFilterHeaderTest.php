efbf14be63d668d55749b819304bc2615b2b5ddc
Thomas Portelange <thomas@lekoala.be>
2016-10-03 10:23:58 +0200

    Allow filtering if a relation is defined or a formatting

---
+++ tests/forms/gridfield/GridFieldFilterHeaderTest.php
@@ -0,0 +1,27 @@
+<?php
+
+class GridFieldFilterHeaderTest extends SapphireTest {
+
+	protected $extraDataObjects = array(
+		'GridFieldFilterHeaderTest_DataObject',
+	);
+
+	public function testColumnToFilterField() {
+		$class = 'GridFieldFilterHeaderTest_DataObject';
+		$header = new GridFieldFilterHeader();
+		$method = new ReflectionMethod($header, 'columnToFilterField');
+		$method->setAccessible(true);
+		$this->assertEquals('Title', $method->invoke($header, $class,'Title.ATT'));
+		$this->assertEquals('isTest', $method->invoke($header, $class, 'isTest.Nice'));
+	}
+
+}
+
+class GridFieldFilterHeaderTest_DataObject extends DataObject implements TestOnly {
+
+	private static $db = array(
+		'Title' => 'Varchar',
+	    'isTest' => 'Boolean',
+	);
+
+}
