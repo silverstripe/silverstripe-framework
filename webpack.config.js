8fbfe6b5c53ba6dc886411a1f6f0ca42e766d2f4
Christopher Joe <cjoe@silverstripe.com>
2016-10-06 20:43:59 +1300

    move admin JS src folder to resolve as root, rather than as modules

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const webpack = require('webpack');
 const autoprefixer = require('autoprefixer');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const path = require('path');
 // const SprityWebpackPlugin = require('sprity-webpack-plugin');
 
 const PATHS = {
@@ -48,7 +49,8 @@ const config = [
       TinyMCE_SSPlugin: `${PATHS.ADMIN_JS_SRC}/legacy/TinyMCE_SSPlugin.js`,
     },
     resolve: {
-      modulesDirectories: [PATHS.ADMIN_JS_SRC, PATHS.MODULES],
+      root: [__dirname, path.resolve(__dirname, PATHS.ADMIN_JS_SRC)],
+      modulesDirectories: [PATHS.MODULES],
     },
     output: {
       path: 'admin/client/dist',
