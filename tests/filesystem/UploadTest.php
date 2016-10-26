148070c697745065b87defcb1b2d8223e24170d5
Damian Mooyman <damian@silverstripe.com>
2016-10-26 15:06:50 +1300

    API Shorten overly-verbose invalid extension error Fixes #6198

--- tests/filesystem/UploadTest.php
+++ tests/filesystem/UploadTest.php
@@ -831,10 +831,8 @@ class UploadTest_Validator extends Upload_Validator implements TestOnly {
 		// extension validation
 		if(!$this->isValidExtension()) {
 			$this->errors[] = _t(
-				'File.INVALIDEXTENSION',
-				'Extension is not allowed (valid: {extensions})',
-				'Argument 1: Comma-separated list of valid extensions',
-				array('extensions' => implode(',', $this->allowedExtensions))
+				'File.INVALIDEXTENSION_SHORT',
+				'Extension is not allowed'
 			);
 			return false;
 		}
