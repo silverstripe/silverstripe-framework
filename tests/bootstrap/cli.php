9adfba8f303709098ffeb2c2afd90485e09c9445
Damian Mooyman <damian@silverstripe.com>
2016-10-27 14:39:00 +1300

    Refactor bootstrap.php Bootstrap CMS behat tests

---
+++ tests/bootstrap/cli.php
@@ -0,0 +1,33 @@
+<?php
+
+// Fake the script name and base
+global $_SERVER;
+if (!$_SERVER) $_SERVER = array();
+
+$frameworkPath = dirname(dirname(__FILE__));
+$frameworkDir = basename($frameworkPath);
+
+$_SERVER['SCRIPT_FILENAME'] = $frameworkPath . DIRECTORY_SEPARATOR . 'cli-script.php';
+$_SERVER['SCRIPT_NAME'] = '.' . DIRECTORY_SEPARATOR . $frameworkDir . DIRECTORY_SEPARATOR . 'cli-script.php';
+
+// Copied from cli-script.php, to enable same behaviour through phpunit runner.
+if(isset($_SERVER['argv'][2])) {
+	$args = array_slice($_SERVER['argv'],2);
+	if(!isset($_GET)) $_GET = array();
+	if(!isset($_REQUEST)) $_REQUEST = array();
+	foreach($args as $arg) {
+		if(strpos($arg,'=') == false) {
+			$_GET['args'][] = $arg;
+		} else {
+			$newItems = array();
+			parse_str( (substr($arg,0,2) == '--') ? substr($arg,2) : $arg, $newItems );
+			$_GET = array_merge($_GET, $newItems);
+		}
+	}
+	$_REQUEST = array_merge($_REQUEST, $_GET);
+}
+
+// Ensure Director::protocolAndHost() works
+if (empty($_SERVER['HTTP_HOST'])) {
+	$_SERVER['HTTP_HOST'] = 'localhost';
+}
---
+++ tests/bootstrap/init.php
@@ -0,0 +1,21 @@
+<?php
+
+if(!defined('BASE_PATH')) {
+	echo "BASE_PATH hasn't been defined. This probably means that framework/Core/Constants.php hasn't been " .
+		"included by Composer's autoloader.\n" .
+		"Make sure the you are running your tests via vendor/bin/phpunit and your autoloader is up to date.\n";
+	exit(1);
+}
+
+
+/**
+ * This bootstraps the SilverStripe system so that phpunit can be run directly on SilverStripe tests.
+ */
+
+// Make sure display_errors is on
+ini_set('display_errors', 1);
+
+// Asset folder
+if(!file_exists(BASE_PATH . '/assets')) {
+	mkdir(BASE_PATH . '/assets', 02775);
+}
