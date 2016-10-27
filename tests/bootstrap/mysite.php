e386c6a1534f1184bbf9480e67294eb910a1fa5c
Damian Mooyman <damian@silverstripe.com>
2016-10-28 10:53:11 +1300

    FIX: Refactor bootstrap.php to allow for code sharing with cms bootstrap

---
+++ tests/bootstrap/mysite.php
@@ -0,0 +1,10 @@
+<?php
+
+// Default database settings
+global $project;
+$project = 'mysite';
+
+global $database;
+$database = '';
+
+require_once(__DIR__ . '/../../conf/ConfigureFromEnv.php');
---
+++ tests/bootstrap/phpunit.php
@@ -0,0 +1,29 @@
+<?php
+
+// Bootstrap for running SapphireTests
+
+// Connect to database
+use SilverStripe\Dev\SapphireTest;
+use SilverStripe\ORM\DB;
+
+require_once __DIR__ . '/../../Core/Core.php';
+require_once __DIR__ . '/../FakeController.php';
+
+global $databaseConfig;
+DB::connect($databaseConfig);
+
+// Now set a fake REQUEST_URI
+$_SERVER['REQUEST_URI'] = BASE_URL;
+
+// Fake a session
+$_SESSION = null;
+
+// Prepare manifest autoloader
+$controller = new FakeController();
+
+SapphireTest::use_test_manifest();
+
+SapphireTest::set_is_running_test(true);
+
+// Remove the error handler so that PHPUnit can add its own
+restore_error_handler();
