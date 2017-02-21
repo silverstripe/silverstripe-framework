1c55948fc1d72c4d4c5f55a6b2786496a6ced21a
Daniel Hensby <dhensby@silverstripe.com>
2017-02-21 20:09:20 +0000

    Ensure test objects implement TestOnly

--- tests/php/Forms/FormFactoryTest/TestObject.php
+++ tests/php/Forms/FormFactoryTest/TestObject.php
@@ -2,13 +2,14 @@
 
 namespace SilverStripe\Forms\Tests\FormFactoryTest;
 
+use SilverStripe\Dev\TestOnly;
 use SilverStripe\ORM\DataObject;
 use SilverStripe\ORM\Versioning\Versioned;
 
 /**
  * @mixin Versioned
  */
-class TestObject extends DataObject
+class TestObject extends DataObject implements TestOnly
 {
     private static $table_name = 'FormFactoryTest_TestObject';
 
