1ca51eb57a933964e9640c02697a78c068a8c462
Robbie Averill <robbie@silverstripe.com>
2017-04-11 13:31:58 +1200

    FIX Ensure that mysite test boostrap configuration is loaded after core and before the database connection

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,5 +3,4 @@
 require __DIR__ . '/bootstrap/init.php';
 require __DIR__ . '/bootstrap/cli.php';
 require __DIR__ . '/bootstrap/environment.php';
-require __DIR__ . '/bootstrap/mysite.php';
 require __DIR__ . '/bootstrap/phpunit.php';
