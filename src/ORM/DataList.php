cc813e656f7afe4bc1fdea8ec17fb2b26677d3d5
Sam Minnee <sam@silverstripe.com>
2017-06-29 11:47:42 +1200

    FIX: Re-set finalisedQuery to allow recreation of iterator.

--- src/ORM/DataList.php
+++ src/ORM/DataList.php
@@ -853,6 +853,9 @@ class DataList extends ViewableData implements SS_List, Filterable, Sortable, Li
         foreach ($this->getFinalisedQuery() as $row) {
             yield $this->createDataObject($row);
         }
+
+        // Re-set the finaliseQuery so that it can be re-executed
+        $this->finalisedQuery = null;
     }
 
     /**
