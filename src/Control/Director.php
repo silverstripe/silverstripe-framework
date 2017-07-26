11f137e09f4120e65aef53c3d148abb16e1cf625
Andrew Aitken-Fincham <andrew@silverstripe.com>
2017-07-26 14:23:10 +0100

    check for current_request so _config.php doesn't infinite loop

--- src/Control/Director.php
+++ src/Control/Director.php
@@ -843,7 +843,7 @@ class Director implements TemplateGlobalProvider
     /**
      * Force the site to run on SSL.
      *
-     * To use, call from _config.php. For example:
+     * To use, call from the init() method of your PageController. For example:
      * <code>
      * if (Director::isLive()) Director::forceSSL();
      * </code>
@@ -873,6 +873,11 @@ class Director implements TemplateGlobalProvider
      */
     public static function forceSSL($patterns = null, $secureDomain = null)
     {
+        // if no request i.e. has been set in _config.php - prevents loops
+        if (!static::currentRequest()) {
+            return false;
+        }
+
         // Already on SSL
         if (static::is_https()) {
             return true;
@@ -915,6 +920,11 @@ class Director implements TemplateGlobalProvider
      */
     public static function forceWWW()
     {
+        // if no request i.e. has been set in _config.php - prevents loops
+        if (!static::currentRequest()) {
+            return false;
+        }
+
         if (!Director::isDev() && !Director::isTest() && strpos(static::host(), 'www') !== 0) {
             $destURL = str_replace(
                 Director::protocol(),
