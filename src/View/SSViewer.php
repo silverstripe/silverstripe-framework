6613826ed8022c6feb112524b7835ea2bee73d74
Andrew Aitken-Fincham <andrew@silverstripe.com>
2017-09-12 12:32:28 +0100

    FIX SSViewer::add_themes() to properly prepend

--- src/View/SSViewer.php
+++ src/View/SSViewer.php
@@ -165,7 +165,10 @@ class SSViewer implements Flushable
 
     public static function add_themes($themes = [])
     {
-        SSViewer::config()->merge('themes', $themes);
+        $currentThemes = SSViewer::get_themes();
+        $finalThemes = array_merge($themes, $currentThemes);
+        // array_values is used to ensure sequential array keys as array_unique can leave gaps
+        SSViewer::set_themes(array_values(array_unique($finalThemes)));
     }
 
     public static function get_themes()
