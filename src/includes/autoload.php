31b213be7b7359cc2ada96863c699ad49c743316
Ingo Schommer <me@chillu.com>
2017-10-02 22:14:35 +1300

    Allow autoloading from vendor path

--- src/includes/autoload.php
+++ src/includes/autoload.php
@@ -3,8 +3,13 @@
 // Init composer autoload
 call_user_func(function () {
     $candidates = [
+        // module in vendor
+        __DIR__ . '/../../../../autoload.php',
+        // module in webroot
         __DIR__ . '/../../vendor/autoload.php',
+        // module in webroot
         __DIR__ . '/../../../vendor/autoload.php',
+        // module is webroot (usually during CI installs)
         getcwd() . '/vendor/autoload.php',
     ];
     foreach ($candidates as $candidate) {
