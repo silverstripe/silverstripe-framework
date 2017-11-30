e23fcda6b60f63d67948c5854ce0d2a05f4258e7
Daniel Hensby <dhensby@silverstripe.com>
2017-11-30 18:11:59 +0000

    Remove usage of asserts with strings

--- core/Diff.php
+++ core/Diff.php
@@ -413,7 +413,7 @@ class _DiffEngine
 	$i = 0;
 	$j = 0;
 
-	USE_ASSERTS && assert('sizeof($lines) == sizeof($changed)');
+	USE_ASSERTS && assert(sizeof($lines) == sizeof($changed));
 	$len = sizeof($lines);
 	$other_len = sizeof($other_changed);
 
@@ -433,7 +433,7 @@ class _DiffEngine
 		$j++;
 
 		while ($i < $len && ! $changed[$i]) {
-		USE_ASSERTS && assert('$j < $other_len && ! $other_changed[$j]');
+		USE_ASSERTS && assert($j < $other_len && ! $other_changed[$j]);
 		$i++; $j++;
 		while ($j < $other_len && $other_changed[$j])
 			$j++;
@@ -465,10 +465,10 @@ class _DiffEngine
 			$changed[--$i] = false;
 			while ($start > 0 && $changed[$start - 1])
 			$start--;
-			USE_ASSERTS && assert('$j > 0');
+			USE_ASSERTS && assert($j > 0);
 			while ($other_changed[--$j])
 			continue;
-			USE_ASSERTS && assert('$j >= 0 && !$other_changed[$j]');
+			USE_ASSERTS && assert($j >= 0 && !$other_changed[$j]);
 				}
 
 		/*
@@ -491,7 +491,7 @@ class _DiffEngine
 			while ($i < $len && $changed[$i])
 			$i++;
 
-			USE_ASSERTS && assert('$j < $other_len && ! $other_changed[$j]');
+			USE_ASSERTS && assert($j < $other_len && ! $other_changed[$j]);
 			$j++;
 			if ($j < $other_len && $other_changed[$j]) {
 			$corresponding = $i;
@@ -508,10 +508,10 @@ class _DiffEngine
 		while ($corresponding < $i) {
 		$changed[--$start] = 1;
 		$changed[--$i] = 0;
-		USE_ASSERTS && assert('$j > 0');
+		USE_ASSERTS && assert($j > 0);
 		while ($other_changed[--$j])
 			continue;
-		USE_ASSERTS && assert('$j >= 0 && !$other_changed[$j]');
+		USE_ASSERTS && assert($j >= 0 && !$other_changed[$j]);
 			}
 		}
 	}
