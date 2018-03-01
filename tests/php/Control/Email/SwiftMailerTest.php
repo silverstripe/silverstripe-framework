27cb966359ada32f358d210fb1c41382d24ef560
Daniel Hensby <git@hens.by>
2018-03-01 15:57:17 +0000

    Move to SendmailTransport

--- tests/php/Control/Email/SwiftMailerTest.php
+++ tests/php/Control/Email/SwiftMailerTest.php
@@ -6,10 +6,10 @@ use SilverStripe\Control\Email\Email;
 use SilverStripe\Control\Email\SwiftMailer;
 use SilverStripe\Dev\SapphireTest;
 use Swift_Mailer;
-use Swift_MailTransport;
 use Swift_Message;
 use Swift_NullTransport;
 use Swift_Plugins_AntiFloodPlugin;
+use Swift_SendmailTransport;
 
 class SwiftMailerTest extends SapphireTest
 {
@@ -23,8 +23,8 @@ class SwiftMailerTest extends SapphireTest
         SwiftMailer::config()->remove('swift_plugins');
         SwiftMailer::config()->update('swift_plugins', array(Swift_Plugins_AntiFloodPlugin::class));
 
-        /** @var Swift_MailTransport $transport */
-        $transport = $this->getMockBuilder(Swift_MailTransport::class)->getMock();
+        /** @var Swift_SendmailTransport $transport */
+        $transport = $this->getMockBuilder(Swift_SendmailTransport::class)->getMock();
         $transport
             ->expects($this->once())
             ->method('registerPlugin')
