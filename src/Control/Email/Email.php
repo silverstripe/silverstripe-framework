057150f1da0a798faa21c0bdd48c155e00649fa9
Daniel Hensby <git@hens.by>
2018-03-01 16:00:02 +0000

    Update setDate to pass in DateTime object

--- src/Control/Email/Email.php
+++ src/Control/Email/Email.php
@@ -273,7 +273,7 @@ class Email extends ViewableData
      */
     public function setSwiftMessage($swiftMessage)
     {
-        $swiftMessage->setDate(DBDatetime::now()->getTimestamp());
+        $swiftMessage->setDate((new \DateTime())->setTimestamp(DBDatetime::now()->getTimestamp()));
         if (!$swiftMessage->getFrom() && ($defaultFrom = $this->config()->get('admin_email'))) {
             $swiftMessage->setFrom($defaultFrom);
         }
