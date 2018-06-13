432d20c8d0e3855273ad396b5a1a6e91df98d6f3
Loz Calver <lozcalver@bigfork.co.uk>
2018-06-13 16:47:38 +0100

    Testing switching PDOQuery to a generator

--- src/ORM/Connect/PDOQuery.php
+++ src/ORM/Connect/PDOQuery.php
@@ -17,8 +17,6 @@ class PDOQuery extends Query
      */
     protected $statement = null;
 
-    protected $results = null;
-
     /**
      * Hook the result-set given into a Query class, suitable for use by SilverStripe.
      * @param PDOStatement $statement The internal PDOStatement containing the results
@@ -26,20 +24,19 @@ class PDOQuery extends Query
     public function __construct(PDOStatement $statement)
     {
         $this->statement = $statement;
-        // Since no more than one PDOStatement for any one connection can be safely
-        // traversed, each statement simply requests all rows at once for safety.
-        // This could be re-engineered to call fetchAll on an as-needed basis
-        $this->results = $statement->fetchAll(PDO::FETCH_ASSOC);
-        $statement->closeCursor();
     }
 
     public function getIterator()
     {
-        return new ArrayIterator($this->results);
+        if (is_object($this->statement)) {
+            while ($data = $this->statement->fetch(PDO::FETCH_ASSOC)) {
+                yield $data;
+            }
+        }
     }
 
     public function numRecords()
     {
-        return count($this->results);
+        return $this->statement->rowCount();
     }
 }
