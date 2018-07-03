a8853504b4be9288fcbfdc71c5949fc7c612c599
Robbie Averill <robbie@silverstripe.com>
2018-07-04 11:55:31 +1200

    API API MonologErrorHandler::setLogger is deprecated, use MonologErrorHandler::pushLogger instead

--- tests/php/Logging/MonologErrorHandlerTest.php
+++ tests/php/Logging/MonologErrorHandlerTest.php
@@ -2,6 +2,7 @@
 
 namespace SilverStripe\Logging\Tests;
 
+use Psr\Log\LoggerInterface;
 use SilverStripe\Dev\SapphireTest;
 use SilverStripe\Logging\MonologErrorHandler;
 
@@ -15,5 +16,21 @@ class MonologErrorHandlerTest extends SapphireTest
     {
         $handler = new MonologErrorHandler();
         $handler->start();
+    }
+
+    public function testSetLoggerResetsStack()
+    {
+        /** @var LoggerInterface $logger */
+        $logger = $this->createMock(LoggerInterface::class);
+
+        $handler = new MonologErrorHandler();
+        $handler->pushLogger($logger)->pushLogger($logger);
+        $this->assertCount(2, $handler->getLoggers(), 'Loggers are pushed to the stack');
+
+        $handler->setLogger($logger);
+        $this->assertCount(1, $handler->getLoggers(), 'setLogger resets stack and pushes');
+
+        $handler->setLoggers([]);
+        $this->assertCount(0, $handler->getLoggers(), 'setLoggers overwrites all configured loggers');
     }
 }
