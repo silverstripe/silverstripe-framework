6410efc216e4bd25dc3052d41cf1f6289a6cfab6
Robbie Averill <robbie@silverstripe.com>
2018-10-15 17:23:25 +0200

    FIX Correct PHPUnit error class name in SQLSelect test

--- tests/php/ORM/SQLSelectTest.php
+++ tests/php/ORM/SQLSelectTest.php
@@ -273,7 +273,7 @@ class SQLSelectTest extends SapphireTest
     }
 
     /**
-     * @expectedException \PHPUnit\Framework\Error
+     * @expectedException \PHPUnit\Framework\Error\Error
      */
     public function testZeroLimit()
     {
@@ -284,7 +284,7 @@ class SQLSelectTest extends SapphireTest
     }
 
     /**
-     * @expectedException \PHPUnit\Framework\Error
+     * @expectedException \PHPUnit\Framework\Error\Error
      */
     public function testZeroLimitWithOffset()
     {
