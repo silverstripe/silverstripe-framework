c06cf4820e02a923a683419b03979bd3677f38db
Robbie Averill <robbie@silverstripe.com>
2018-10-20 17:41:41 +0200

    BUG Readonly and disabled CurrencyFields no longer always returns dollar currency sign, now respect config

---
+++ tests/php/Forms/CurrencyField_DisabledTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace SilverStripe\Forms\Tests;
+
+use SilverStripe\Dev\SapphireTest;
+use SilverStripe\Forms\CurrencyField_Disabled;
+use SilverStripe\ORM\FieldType\DBCurrency;
+
+class CurrencyField_DisabledTest extends SapphireTest
+{
+    public function testFieldWithValue()
+    {
+        $field = new CurrencyField_Disabled('Test', '', '$5.00');
+        $result = $field->Field();
+
+        $this->assertContains('<input', $result, 'An input should be rendered');
+        $this->assertContains('disabled', $result, 'The input should be disabled');
+        $this->assertContains('$5.00', $result, 'The value should be rendered');
+    }
+
+    /**
+     * @todo: Update the expectation when intl for currencies is implemented
+     */
+    public function testFieldWithCustomisedCurrencySymbol()
+    {
+        DBCurrency::config()->update('currency_symbol', '€');
+        $field = new CurrencyField_Disabled('Test', '', '€5.00');
+        $result = $field->Field();
+
+        $this->assertContains('<input', $result, 'An input should be rendered');
+        $this->assertContains('disabled', $result, 'The input should be disabled');
+        $this->assertContains('€5.00', $result, 'The value should be rendered');
+    }
+}
---
+++ tests/php/Forms/CurrencyField_ReadonlyTest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace SilverStripe\Forms\Tests;
+
+use SilverStripe\Dev\SapphireTest;
+use SilverStripe\Forms\CurrencyField_Readonly;
+use SilverStripe\ORM\FieldType\DBCurrency;
+
+class CurrencyField_ReadonlyTest extends SapphireTest
+{
+    public function testPerformReadonlyTransformation()
+    {
+        $field = new CurrencyField_Readonly('Test', '', '$5.00');
+        $result = $field->performReadonlyTransformation();
+        $this->assertInstanceOf(CurrencyField_Readonly::class, $result);
+        $this->assertNotSame($result, $field, 'Should return a clone of the field');
+    }
+
+    public function testFieldWithValue()
+    {
+        $field = new CurrencyField_Readonly('Test', '', '$5.00');
+        $result = $field->Field();
+
+        $this->assertContains('<input', $result, 'An input should be rendered');
+        $this->assertContains('readonly', $result, 'The input should be readonly');
+        $this->assertContains('$5.00', $result, 'The value should be rendered');
+    }
+
+    public function testFieldWithOutValue()
+    {
+        DBCurrency::config()->update('currency_symbol', 'AUD');
+        $field = new CurrencyField_Readonly('Test', '', null);
+        $result = $field->Field();
+
+        $this->assertContains('<input', $result, 'An input should be rendered');
+        $this->assertContains('readonly', $result, 'The input should be readonly');
+        $this->assertContains('AUD0.00', $result, 'The value should be rendered');
+    }
+
+    /**
+     * @todo: Update the expectation when intl for currencies is implemented
+     */
+    public function testFieldWithCustomisedCurrencySymbol()
+    {
+        DBCurrency::config()->update('currency_symbol', '€');
+        $field = new CurrencyField_Readonly('Test', '', '€5.00');
+        $result = $field->Field();
+
+        $this->assertContains('<input', $result, 'An input should be rendered');
+        $this->assertContains('readonly', $result, 'The input should be readonly');
+        $this->assertContains('€5.00', $result, 'The value should be rendered');
+    }
+}
