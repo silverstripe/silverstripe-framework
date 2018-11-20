80885fc23111e1ebee9bc1540cabc30c7398555e
Serge Latyntcev <dnsl48@gmail.com>
2018-11-20 14:04:12 +1300

    ADD php test TreeMultiselectField::testEmptyChoiceReadonly

--- tests/php/Forms/TreeMultiselectFieldTest.php
+++ tests/php/Forms/TreeMultiselectFieldTest.php
@@ -10,14 +10,43 @@ class TreeMultiselectFieldTest extends SapphireTest
 {
     protected static $fixture_file = 'TreeDropdownFieldTest.yml';
 
+    public function testEmptyChoiceReadonly()
+    {
+        $field = new TreeMultiselectField('TestTree', 'Test tree', File::class);
+
+        $asdf = $this->objFromFixture(File::class, 'asdf');
+        $subfolderfile1 = $this->objFromFixture(File::class, 'subfolderfile1');
+
+        $schemaStateDefaults = $field->getSchemaStateDefaults();
+        $this->assertArraySubset(['id' => 'TestTree', 'name' => 'TestTree', 'value' => 'unchanged'], $schemaStateDefaults, $strict = true);
+
+        $html = $field->performReadonlyTransformation()->Field();
+
+        $this->assertEquals(
+            <<<"HTML"
+<span id="TestTree_ReadonlyValue" class="readonly">
+	<i>('none')</i>
+</span><input type="hidden" name="TestTree" class="hidden" id="TestTree" />
+HTML
+            ,
+            $html
+        );
+    }
+
     public function testReadonly()
     {
         $field = new TreeMultiselectField('TestTree', 'Test tree', File::class);
+
         $asdf = $this->objFromFixture(File::class, 'asdf');
         $subfolderfile1 = $this->objFromFixture(File::class, 'subfolderfile1');
+
         $field->setValue(implode(',', [$asdf->ID, $subfolderfile1->ID]));
 
-        $readonlyField = $field->performReadonlyTransformation();
+        $schemaStateDefaults = $field->getSchemaStateDefaults();
+        $this->assertArraySubset(['id' => 'TestTree', 'name' => 'TestTree', 'value' => [$asdf->ID, $subfolderfile1->ID]], $schemaStateDefaults, $strict = true);
+
+        $html = (string)$field->performReadonlyTransformation()->Field();
+
         $this->assertEquals(
             <<<"HTML"
 <span id="TestTree_ReadonlyValue" class="readonly">
@@ -25,7 +54,7 @@ class TreeMultiselectFieldTest extends SapphireTest
 </span><input type="hidden" name="TestTree" value="{$asdf->ID},{$subfolderfile1->ID}" class="hidden" id="TestTree" />
 HTML
             ,
-            (string)$readonlyField->Field()
+            $html
         );
     }
 }
