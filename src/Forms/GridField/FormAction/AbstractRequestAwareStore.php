c7b5b1efd2e37cc0b5f732f461bd37fafe14bd83
Guy Marriott <guy@scopey.co.nz>
2018-11-28 16:00:51 +1300

    FIX Switching to use Controller::curr as it was using previously

--- src/Forms/GridField/FormAction/AbstractRequestAwareStore.php
+++ src/Forms/GridField/FormAction/AbstractRequestAwareStore.php
@@ -1,34 +1,17 @@
 <?php
 namespace SilverStripe\Forms\GridField\FormAction;
 
+use SilverStripe\Control\Controller;
 use SilverStripe\Control\HTTPRequest;
 
 abstract class AbstractRequestAwareStore implements StateStore
 {
-    private static $dependencies = [
-        'request' => '%$' . HTTPRequest::class,
-    ];
-
-    /**
-     * @var HTTPRequest
-     */
-    protected $request;
-
     /**
      * @return HTTPRequest
      */
     public function getRequest()
     {
-        return $this->request;
-    }
-
-    /**
-     * @param HTTPRequest $request
-     * @return $this
-     */
-    public function setRequest($request)
-    {
-        $this->request = $request;
-        return $this;
+        // Replicating existing functionality from GridField_FormAction
+        return Controller::curr()->getRequest();
     }
 }
