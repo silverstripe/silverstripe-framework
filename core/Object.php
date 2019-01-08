1bc51a1c3949260e488b88fc2dd1fcf38b14bf39
Joe Chenevey <jchenevey@users.noreply.github.com>
2019-01-08 15:28:15 -0500

    Update Object.php
    Switch to an early `continue` rather than wrapping contents of `foreach` in an `if` and indenting.

--- core/Object.php
+++ core/Object.php
@@ -908,12 +908,14 @@ abstract class SS_Object {
 		$methods = $this->findMethodsFromExtension($extension);
 		if ($methods) {
 			foreach ($methods as $method) {
-				if (isset(self::$extra_methods[$this->class][$method])) {
-					$methodInfo = self::$extra_methods[$this->class][$method];
+				if (!isset(self::$extra_methods[$this->class][$method])) {
+					continue;
+				}
+				
+				$methodInfo = self::$extra_methods[$this->class][$method];
 
-					if ($methodInfo['property'] === $property && $methodInfo['index'] === $index) {
-						unset(self::$extra_methods[$this->class][$method]);
-					}
+				if ($methodInfo['property'] === $property && $methodInfo['index'] === $index) {
+					unset(self::$extra_methods[$this->class][$method]);
 				}
 			}
 
