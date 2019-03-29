898fab293769e3b8f1cd3bbec6cd210c7ac205d1
Maxime Rainville <maxime@silverstripe.com>
2019-03-29 18:24:12 +1300

    BUG Add test to test various domain format

--- tests/php/Control/Middleware/TrustedProxyMiddlewareTest.php
+++ tests/php/Control/Middleware/TrustedProxyMiddlewareTest.php
@@ -8,6 +8,36 @@ use SilverStripe\Dev\SapphireTest;
 
 class TrustedProxyMiddlewareTest extends SapphireTest
 {
+    public function goodHostnames()
+    {
+        return [
+            ['127.0.0.1'],
+            ['test1.com'],
+            ['localhost'],
+            ['www.test.com'],
+            ['www.test.com'],
+            ['2001:0db8:85a3:0000:0000:8a2e:0370:7334'],
+            ['localhost:8080'],
+            ['127.0.0.1:8080'],
+            ['vAlId-hOsT'],
+            ['www.monkey.intranet.silverstripe.com'],
+            ['はじめよう.みんな'],
+        ];
+    }
+
+    public function badHostnames()
+    {
+        return [
+            ['">invalid-test.com'],
+            ['invalid_host'],
+            ['invalid host'],
+            ['invalid-char.com#$!#^%$&^&'],
+            ['www.invalid-char.com/some-path'],
+            ['https://www.invalid-char.com'],
+            ['-invalid-host.com'],
+        ];
+    }
+
     public function testIgnoresHostHeaderOnUntrustedIP()
     {
         $m = new TrustedProxyMiddleware();
@@ -29,12 +59,29 @@ class TrustedProxyMiddlewareTest extends SapphireTest
         $this->assertEquals('test1.com', $response->getHeader('Host'));
     }
 
-    public function testFiltersHostHeader()
+    /**
+     * @dataProvider goodHostnames
+     */
+    public function testSetSepecificHostheader($host)
     {
         $m = new TrustedProxyMiddleware();
         $m->setTrustedProxyIPs('*');
         $request = (new HTTPRequest('GET', ''))
-            ->addHeader('X-Forwarded-Host', '">invalid-test.com');
+            ->addHeader('X-Forwarded-Host', $host);
+
+        $response = $m->process($request, $this->getIdentityFn());
+        $this->assertEquals($host, $response->getHeader('Host'));
+    }
+
+    /**
+     * @dataProvider badHostnames
+     */
+    public function testFiltersHostHeader($host)
+    {
+        $m = new TrustedProxyMiddleware();
+        $m->setTrustedProxyIPs('*');
+        $request = (new HTTPRequest('GET', ''))
+            ->addHeader('X-Forwarded-Host', '$host');
 
         $response = $m->process($request, $this->getIdentityFn());
         $this->assertEquals('', $response->getHeader('Host'));
