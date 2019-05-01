0696045e59fa2e62b5021fe1045763c433fb13aa
Ingo Schommer <me@chillu.com>
2019-05-02 09:33:54 +1200

    NEW Legacy thumbnail migration task (#8924)
    * NEW Legacy thumbnail migration task
    
    See https://github.com/silverstripe/silverstripe-assets/issues/235
    Makes a start at https://github.com/silverstripe/silverstripe-assets/issues/219 as well
    
    * API Removed migrate_legacy_file support
    
    For the vast majority of sites, you really don't want to run your file migration as part of dev build.
    The step is involved enough to warrant it's own task.
    I don't think this is an API change, since the setting won't have affect
    for anyone who has already enabled it - they would've already done the one-off migration.
    
    See https://github.com/silverstripeltd/open-sourcerers/issues/91
    and https://github.com/silverstripe/silverstripe-assets/issues/235

--- src/Dev/Tasks/MigrateFileTask.php
+++ src/Dev/Tasks/MigrateFileTask.php
@@ -6,11 +6,12 @@ use Monolog\Handler\StreamHandler;
 use Monolog\Logger;
 use Psr\Log\LoggerInterface;
 use SilverStripe\AssetAdmin\Helper\ImageThumbnailHelper;
+use SilverStripe\Assets\Dev\Tasks\LegacyThumbnailMigrationHelper;
+use SilverStripe\Assets\FileMigrationHelper;
+use SilverStripe\Assets\Storage\AssetStore;
 use SilverStripe\Control\Director;
 use SilverStripe\Core\Injector\Injector;
 use SilverStripe\Logging\PreformattedEchoHandler;
-use SilverStripe\ORM\DB;
-use SilverStripe\Assets\FileMigrationHelper;
 use SilverStripe\Dev\BuildTask;
 
 /**
@@ -20,38 +21,118 @@ class MigrateFileTask extends BuildTask
 {
     private static $segment = 'MigrateFileTask';
 
-    protected $title = 'Migrate File dataobjects from 3.x';
+    protected $title = 'Migrate File dataobjects from 3.x and successive iterations in 4.x';
 
-    protected $description =
-        'Imports all files referenced by File dataobjects into the new Asset Persistence Layer introduced in 4.0. ' .
-        'If the task fails or times out, run it again and it will start where it left off.';
+    protected $defaultSubtasks = [
+        'move-files',
+        'move-thumbnails',
+        'generate-cms-thumbnails'
+    ];
+
+    private static $dependencies = [
+        'logger' => '%$' . LoggerInterface::class,
+    ];
+
+    /** @var Logger */
+    private $logger;
 
     public function run($request)
     {
         $this->addLogHandlers();
 
-        if (!class_exists(FileMigrationHelper::class)) {
-            DB::alteration_message("No file migration helper detected", "notice");
-            return;
+        $args = $request->getVars();
+        $this->validateArgs($args);
+
+        $subtasks = !empty($args['only']) ? explode(',', $args['only']) : $this->defaultSubtasks;
+
+        if (in_array('move-files', $subtasks)) {
+            if (!class_exists(FileMigrationHelper::class)) {
+                $this->logger->error("No file migration helper detected");
+                return;
+            }
+
+            $this->logger->info('### Migrating filesystem and database records (move-files)');
+
+            $this->logger->info('If the task fails or times out, run it again and it will start where it left off.');
+
+            $migrated = FileMigrationHelper::singleton()->run();
+            if ($migrated) {
+                $this->logger->info("{$migrated} File DataObjects upgraded");
+            } else {
+                $this->logger->info("No File DataObjects need upgrading");
+            }
         }
 
-        DB::alteration_message(
-            'If the task fails or times out, run it again and it will start where it left off.',
-            "info"
-        );
+        if (in_array('move-thumbnails', $subtasks)) {
+            if (!class_exists(LegacyThumbnailMigrationHelper::class)) {
+                $this->logger->error("LegacyThumbnailMigrationHelper not found");
+                return;
+            }
 
-        $migrated = FileMigrationHelper::singleton()->run();
-        if ($migrated) {
-            DB::alteration_message("{$migrated} File DataObjects upgraded", "changed");
-        } else {
-            DB::alteration_message("No File DataObjects need upgrading", "notice");
+            $this->logger->info('### Migrating existing thumbnails (move-thumbnails)');
+
+            $moved = LegacyThumbnailMigrationHelper::singleton()
+                ->setLogger($this->logger)
+                ->run($this->getStore());
+
+            if ($moved) {
+                $this->logger->info(sprintf("%d thumbnails moved", count($moved)));
+            } else {
+                $this->logger->info("No thumbnails moved");
+            }
         }
 
-        if (!class_exists(ImageThumbnailHelper::class)) {
-            DB::alteration_message("No image thumbnail helper detected", "notice");
-            return;
+        if (in_array('generate-cms-thumbnails', $subtasks)) {
+            if (!class_exists(ImageThumbnailHelper::class)) {
+                $this->logger->error("ImageThumbnailHelper not found");
+                return;
+            }
+
+            $this->logger->info('### Generating new CMS UI thumbnails (generate-cms-thumbnails)');
+
+            ImageThumbnailHelper::singleton()->run();
         }
-        ImageThumbnailHelper::singleton()->run();
+    }
+
+    public function getDescription()
+    {
+        return <<<TXT
+Imports all files referenced by File dataobjects into the new Asset Persistence Layer introduced in 4.0.
+Moves existing thumbnails, and generates new thumbnail sizes for the CMS UI.
+If the task fails or times out, run it again and it will start where it left off.
+See https://docs.silverstripe.org/en/4/developer_guides/files/file_migration/.
+TXT;
+    }
+
+    /**
+     * @param LoggerInterface $logger
+     */
+    public function setLogger(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+
+        return $this;
+    }
+
+    /**
+     * @return AssetStore
+     */
+    protected function getStore()
+    {
+        return singleton(AssetStore::class);
+    }
+
+    /**
+     * @param array $args
+     * @throws \InvalidArgumentException
+     */
+    protected function validateArgs($args)
+    {
+        if (!empty($args['only'])) {
+            if (array_diff(explode(',', $args['only']), $this->defaultSubtasks)) {
+                throw new \InvalidArgumentException('Invalid subtasks detected: ' . $args['only']);
+            }
+        }
     }
 
     /**
