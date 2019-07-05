494139394cfc937f72317e5f96073837092b051d
Sam Minnee <sam@silverstripe.com>
2019-07-05 17:39:54 +1200

    FIX: MemebeAuthenticator error message

--- src/Security/MemberAuthenticator/MemberAuthenticator.php
+++ src/Security/MemberAuthenticator/MemberAuthenticator.php
@@ -189,7 +189,8 @@ class MemberAuthenticator implements Authenticator
         /** @skipUpgrade */
         $email = isset($data['Email']) ? $data['Email'] : null;
         if (is_array($email)) {
-            throw new InvalidArgumentException("Bad email passed to MemberAuthenticator::authenticate(): $email");
+            throw new InvalidArgumentException("Bad email passed to MemberAuthenticator::authenticate(): "
+                . json_encode($email));
         }
 
         $attempt = LoginAttempt::create();
