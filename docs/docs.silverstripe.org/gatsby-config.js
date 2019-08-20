3154977ab6dc6119fd64cb4ff59869175ff37a12
Aaron Carlino <unclecheese@leftandmain.com>
2019-08-20 23:40:49 +1200

    new styled components plugin

--- docs/docs.silverstripe.org/gatsby-config.js
+++ docs/docs.silverstripe.org/gatsby-config.js
@@ -51,7 +51,13 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    `gatsby-plugin-typescript`
+    `gatsby-plugin-typescript`,
+    {
+      resolve: `gatsby-plugin-styled-components`,
+      options: {
+        // Add any options here
+      },
+    },    
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
