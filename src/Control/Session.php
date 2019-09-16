569237c0f4d16ac6f927aeb0ed8c9b8787490080
Serge Latyntcev <dnsl48@gmail.com>
2019-09-16 16:29:42 +1200

    [CVE-2019-12203] Session fixation in "change password" form
    A potential account hijacking may happen if an attacker has physical access to
    victim's computer to perform session fixation. Also possible if the targeted application contains an XSS vulnerability.
    Requires the victim to click the password reset link sent to their email.
    If all the above happens, attackers may reset the password before the actual user does that.

--- src/Control/Session.php
+++ src/Control/Session.php
@@ -218,7 +218,6 @@ class Session
      */
     public function init(HTTPRequest $request)
     {
-
         if (!$this->isStarted() && $this->requestContainsSessionId($request)) {
             $this->start($request);
         }
@@ -633,6 +632,18 @@ class Session
                 $sourceVal = $this->nestedValue($key, $source);
                 $this->recursivelyApplyChanges($changed, $sourceVal, $destVal);
             }
+        }
+    }
+
+    /**
+     * Regenerate session id
+     *
+     * @internal This is for internal use only. Isn't a part of public API.
+     */
+    public function regenerateSessionId()
+    {
+        if (!headers_sent()) {
+            session_regenerate_id(true);
         }
     }
 }
