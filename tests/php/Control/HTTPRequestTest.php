b94b65cafaeb95172792f7f2145840f3e04c0617
Dan Hensby <git@hens.by>
2020-03-23 21:20:11 +0000

    Extra tests for wildcard parameters

--- tests/php/Control/HTTPRequestTest.php
+++ tests/php/Control/HTTPRequestTest.php
@@ -38,6 +38,19 @@ class HTTPRequestTest extends SapphireTest
         $this->assertEquals(['_matched' => true], $request->match('admin/$*', true));
         $this->assertTrue($request->allParsed());
         $this->assertEquals('crm/test', $request->remaining());
+
+        $request = new HTTPRequest('GET', 'admin/crm/test/part1/part2');
+        $this->assertEquals(['Action' => 'crm', '$1' => 'test', '$2' => 'part1', '$3' => 'part2'], $request->match('admin/$Action/$@', true));
+        $this->assertTrue($request->allParsed());
+
+        $request = new HTTPRequest('GET', 'admin/crm/test/part1/part2');
+        $this->assertEquals(['Action' => 'crm'], $request->match('admin/$Action/$*', true));
+        $this->assertTrue($request->allParsed());
+        $this->assertEquals('test/part1/part2', $request->remaining());
+
+        $request = new HTTPRequest('GET', 'admin/crm/test');
+        // all parameters after the first wildcard parameter are ignored
+        $this->assertEquals(['Action' => 'crm', '$1' => 'test'], $request->match('admin/$Action/$@/$Other/$*', true));
     }
 
     public function testHttpMethodOverrides()
