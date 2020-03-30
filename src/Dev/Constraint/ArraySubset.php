d82b1b14bc55f06c9be3018c09a4e1555a9df5c2
Dan Hensby <git@hens.by>
2020-03-30 22:13:50 +0100

    Change evaluate signature for phpunit9 compat

--- src/Dev/Constraint/ArraySubset.php
+++ src/Dev/Constraint/ArraySubset.php
@@ -56,7 +56,7 @@ final class ArraySubset extends Constraint
      * @throws ExpectationFailedException
      * @throws \SebastianBergmann\RecursionContext\InvalidArgumentException
      */
-    public function evaluate($other, string $description = '', bool $returnResult = false)
+    public function evaluate($other, string $description = '', bool $returnResult = false): ?bool
     {
         //type cast $other & $this->subset as an array to allow
         //support in standard array functions.
