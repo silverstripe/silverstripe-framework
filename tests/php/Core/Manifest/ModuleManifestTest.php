520fac9b77ca608177d9db4a2f2eff2c462cfd5d
Nicolaas Thiemen Francken <github@sunnysideup.co.nz>
2020-07-13 21:50:15 +1200

    PATCH: lint

--- tests/php/Core/Manifest/ModuleManifestTest.php
+++ tests/php/Core/Manifest/ModuleManifestTest.php
@@ -136,8 +136,8 @@ class ModuleManifestTest extends SapphireTest
 
         // it is testing time!
         $this->assertEquals($expectedModuleName, $actualModuleName);
-	
-	// bye bye bogus manifest
+
+        // bye bye bogus manifest
         ModuleLoader::inst()->popManifest();
     }
 }
