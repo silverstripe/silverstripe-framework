e4e4b0924deeb6e2f254de1b44a8e72ce9af9cf7
GuySartorelli <36352093+GuySartorelli@users.noreply.github.com>
2021-06-29 17:45:01 +1200

    ENH Use text field's title for validation messages.
    Where a field has a title, this is preferred over the field name in validation messages, as that is the "name" of the field as presented to the user.

--- src/Forms/TextField.php
+++ src/Forms/TextField.php
@@ -142,12 +142,13 @@ class TextField extends FormField implements TippableFieldInterface
     public function validate($validator)
     {
         if (!is_null($this->maxLength) && mb_strlen($this->value) > $this->maxLength) {
+            $name = strip_tags($this->Title() ? $this->Title() : $this->getName());
             $validator->validationError(
                 $this->name,
                 _t(
                     'SilverStripe\\Forms\\TextField.VALIDATEMAXLENGTH',
                     'The value for {name} must not exceed {maxLength} characters in length',
-                    ['name' => $this->getName(), 'maxLength' => $this->maxLength]
+                    ['name' => $name, 'maxLength' => $this->maxLength]
                 ),
                 "validation"
             );
