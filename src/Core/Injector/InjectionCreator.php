d4a33a437ab6ed0ac637f2625fa8b6a3e02863ab
Steve Boyd <emteknetnz@gmail.com>
2021-09-05 22:37:41 +1200

    ENH Performance optimisations

--- src/Core/Injector/InjectionCreator.php
+++ src/Core/Injector/InjectionCreator.php
@@ -2,29 +2,21 @@
 
 namespace SilverStripe\Core\Injector;
 
-use ReflectionClass;
-use ReflectionException;
-
 /**
  * A class for creating new objects by the injector.
  */
 class InjectionCreator implements Factory
 {
-
     public function create($class, array $params = [])
     {
-        try {
-            $reflector = new ReflectionClass($class);
-        } catch (ReflectionException $e) {
-            throw new InjectorNotFoundException($e);
+        if (!class_exists($class)) {
+            throw new InjectorNotFoundException(sprintf('Class "%s" does not exist', $class));
         }
-
-        if (count($params)) {
-            // Remove named keys to ensure that PHP7 and PHP8 interpret these the same way
-            $params = array_values($params);
-            return $reflector->newInstanceArgs($params);
+        if (empty($params)) {
+            return new $class();
         }
-
-        return $reflector->newInstance();
+        // Remove named keys to ensure that PHP7 and PHP8 interpret these the same way
+        $args = array_values($params);
+        return new $class(...$args);
     }
 }
