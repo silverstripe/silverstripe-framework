a3d964868732d035043f4b1c53dcf2404f119597
Steve Boyd <emteknetnz@gmail.com>
2021-11-25 15:52:51 +1300

    FIX Ensure tmpItem has a value before calling method_exists on it

--- src/Forms/GridField/GridFieldSortableHeader.php
+++ src/Forms/GridField/GridFieldSortableHeader.php
@@ -146,7 +146,7 @@ class GridFieldSortableHeader implements GridField_HTMLProvider, GridField_DataM
                     if ($tmpItem instanceof SS_List) {
                         // It's impossible to sort on a HasManyList/ManyManyList
                         break;
-                    } elseif (method_exists($tmpItem, 'hasMethod') && $tmpItem->hasMethod($methodName)) {
+                    } elseif ($tmpItem && method_exists($tmpItem, 'hasMethod') && $tmpItem->hasMethod($methodName)) {
                         // The part is a relation name, so get the object/list from it
                         $tmpItem = $tmpItem->$methodName();
                     } elseif ($tmpItem instanceof DataObject
