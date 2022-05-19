a9a6b0f114b73b407ed16d191c70e22d359c73f8
Mojmir Fendek <mfendek@silverstripe.com>
2022-05-19 14:09:04 +1200

    PR fixes.

--- tests/php/ORM/DBDatetimeTest.php
+++ tests/php/ORM/DBDatetimeTest.php
@@ -62,14 +62,14 @@ class DBDatetimeTest extends SapphireTest
     {
         DBDatetime::set_mock_now('2010-01-01 10:00:00');
 
-        DBDatetime::mockSleep(1);
+        $this->mockSleep(1);
         $this->assertEquals(
             '2010-01-01 10:00:01',
             DBDatetime::now()->Rfc2822(),
             'We expect the time to move forward by 1 second'
         );
 
-        DBDatetime::mockSleep(10);
+        $this->mockSleep(10);
         $this->assertEquals(
             '2010-01-01 10:00:11',
             DBDatetime::now()->Rfc2822(),
