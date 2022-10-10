c0230b72a6196dd6eb33cd2bd6f6fcd79ba7f0ba
Nate Devereux <nate@daveclark.co.nz>
2022-10-11 10:04:31 +1300

    Simply return the class, without creating a new instance as it serves no point

--- src/Core/Injector/InjectionCreator.php
+++ src/Core/Injector/InjectionCreator.php
@@ -9,16 +9,16 @@ class InjectionCreator implements Factory
 {
     public function create($class, array $params = [])
     {
-        // Ensure there are no string keys as they cannot be unpacked with the `...` operator
-        $values = array_values($params ?? []);
-
         // Allow anonymous classes or other classes to pass without ReflectionClass
         if (is_object($class)) {
-            return new $class(...$values);
+            return $class;
         }
         elseif(!class_exists($class ?? '')) {
             throw new InjectorNotFoundException("Class {$class} does not exist");
         }
+
+        // Ensure there are no string keys as they cannot be unpacked with the `...` operator
+        $values = array_values($params ?? []);
 
         return new $class(...$values);
     }
