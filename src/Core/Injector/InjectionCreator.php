b5fad7f91d407dde72bfc70eda9f5a2c0db36078
Nate Devereux <nate@daveclark.co.nz>
2022-10-11 09:58:32 +1300

    Shift position of array handling to remove new private function

--- src/Core/Injector/InjectionCreator.php
+++ src/Core/Injector/InjectionCreator.php
@@ -9,30 +9,17 @@ class InjectionCreator implements Factory
 {
     public function create($class, array $params = [])
     {
+        // Ensure there are no string keys as they cannot be unpacked with the `...` operator
+        $values = array_values($params ?? []);
+
         // Allow anonymous classes or other classes to pass without ReflectionClass
         if (is_object($class)) {
-            $values = $this->removeStringKeys($params);
-
             return new $class(...$values);
         }
         elseif(!class_exists($class ?? '')) {
             throw new InjectorNotFoundException("Class {$class} does not exist");
         }
 
-        $values = $this->removeStringKeys($params);
-
         return new $class(...$values);
-    }
-
-    /**
-     * @param $params
-     *
-     * Ensure there are no string keys as they cannot be unpacked with the `...` operator
-     *
-     * @return array
-     */
-    private function removeStringKeys($params): array
-    {
-        return array_values($params ?? []);
     }
 }
