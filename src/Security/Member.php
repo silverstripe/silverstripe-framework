7b21b38ac4532d06565dfcefad50540ebd2b50f4
Steve Boyd <emteknetnz@gmail.com>
2023-05-12 12:24:12 +1200

    [CVE-2023-32302] Require password field to be non-empty

--- src/Security/Member.php
+++ src/Security/Member.php
@@ -731,7 +731,7 @@ class Member extends DataObject
             $password->setRequireExistingPassword(true);
         }
 
-        $password->setCanBeEmpty(true);
+        $password->setCanBeEmpty(false);
         $this->extend('updateMemberPasswordField', $password);
 
         return $password;
