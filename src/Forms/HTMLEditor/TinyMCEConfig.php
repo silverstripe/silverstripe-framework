2bdc24c86a4474106a85f9e648379290408f057e
Guy Sartorelli <guy.sartorelli@silverstripe.com>
2024-04-18 14:38:02 +1200

    ENH Set default valid_elements for new TinyMCE config

--- src/Forms/HTMLEditor/TinyMCEConfig.php
+++ src/Forms/HTMLEditor/TinyMCEConfig.php
@@ -311,6 +311,20 @@ class TinyMCEConfig extends HTMLEditorConfig implements i18nEntityProvider
         'promotion' => false,
         'upload_folder_id' => null, // Set folder ID for insert media dialog
         'link_default_target' => '_blank', // https://www.tiny.cloud/docs/tinymce/6/autolink/#example-using-link_default_target
+        // Default set of valid_elements which apply for all new configurations
+        'valid_elements' => "@[id|class|style|title],a[id|rel|rev|dir|tabindex|accesskey|type|name|href|target|title"
+        . "|class],-strong/-b[class],-em/-i[class],-strike[class],-u[class],#p[id|dir|class|align|style],-ol[class],"
+        . "-ul[class],-li[class],br,img[id|dir|longdesc|usemap|class|src|border|alt=|title|hspace|vspace|width|height|align|name|data*],"
+        . "-sub[class],-sup[class],-blockquote[dir|class],-cite[dir|class|id|title],"
+        . "-table[cellspacing|cellpadding|width|height|class|align|summary|dir|id|style],"
+        . "-tr[id|dir|class|rowspan|width|height|align|valign|bgcolor|background|bordercolor|style],"
+        . "tbody[id|class|style],thead[id|class|style],tfoot[id|class|style],"
+        . "#td[id|dir|class|colspan|rowspan|width|height|align|valign|scope|style],"
+        . "-th[id|dir|class|colspan|rowspan|width|height|align|valign|scope|style],caption[id|dir|class],"
+        . "-div[id|dir|class|align|style],-span[class|align|style],-pre[class|align],address[class|align],"
+        . "-h1[id|dir|class|align|style],-h2[id|dir|class|align|style],-h3[id|dir|class|align|style],"
+        . "-h4[id|dir|class|align|style],-h5[id|dir|class|align|style],-h6[id|dir|class|align|style],hr[class],"
+        . "dd[id|class|title|dir],dl[id|class|title|dir],dt[id|class|title|dir],"
     ];
 
     /**
