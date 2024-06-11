6d42a3a2b796c522d227d8a2840859c69e737134
Guy Sartorelli <guy.sartorelli@silverstripe.com>
2024-06-12 11:53:25 +1200

    API Deprecate references to installation tool

--- src/Core/BaseKernel.php
+++ src/Core/BaseKernel.php
@@ -27,6 +27,7 @@ use SilverStripe\View\SSViewer;
 use SilverStripe\View\ThemeManifest;
 use SilverStripe\View\ThemeResourceLoader;
 use Exception;
+use SilverStripe\Dev\Deprecation;
 
 /**
  * Simple Kernel container
@@ -308,10 +309,13 @@ abstract class BaseKernel implements Kernel
      * If missing configuration, redirect to install.php if it exists.
      * Otherwise show a server error to the user.
      *
+     * @deprecated 5.3.0 Will be removed without equivalent functionality
+     *
      * @param string $msg Optional message to show to the user on an installed project (install.php missing).
      */
     protected function redirectToInstaller($msg = '')
     {
+        Deprecation::notice('5.3.0', 'Will be removed without equivalent functionality');
         // Error if installer not available
         if (!file_exists(Director::publicFolder() . '/install.php')) {
             throw new HTTPResponse_Exception(
