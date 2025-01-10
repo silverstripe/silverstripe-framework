8ebbe73cbaa2a39a80b0d389db85244de48b0c0e
Thomas Portelange <thomas@lekoala.be>
2025-01-10 10:56:12 +0100

    Use filesCache

--- src/Core/Manifest/ClassManifest.php
+++ src/Core/Manifest/ClassManifest.php
@@ -610,7 +610,7 @@ class ClassManifest
         // Note: $classes, $interfaces and $traits arrays have correct-case keys, not lowercase
         $changed = false;
         if ($this->cache
-            && ($data = $this->cache->get($key))
+            && ($data = $this->filesCache[$key] ?? null)
             && $this->validateItemCache($data)
         ) {
             $classes = $data['classes'];
