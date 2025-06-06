439e411c3f87c1c4f96564caed986c2d1f3525df
Thomas Portelange <thomas@lekoala.be>
2025-06-06 11:10:27 +0200

    Create ObjectK.php

---
+++ tests/php/ORM/DataQueryTest/ObjectK.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace SilverStripe\ORM\Tests\DataQueryTest;
+
+use SilverStripe\Dev\TestOnly;
+use SilverStripe\ORM\DataObject;
+
+class ObjectK extends DataObject implements TestOnly
+{
+    private static $table_name = 'DataQueryTest_K';
+
+    private static $db = [
+        'Name' => 'Varchar',
+    ];
+
+    private static $has_one = [
+        'TestI' => ObjectJ::class,
+    ];
+}
