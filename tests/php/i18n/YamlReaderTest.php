39764ce090ac18aa6159e044a86a8e055d7fc1b2
Guy Sartorelli <guy.sartorelli@gmail.com>
2025-12-01 10:02:06 +1300

    MNT Don't fail test for unrelated line number change
    symfony/yaml changed which line it thinks the error is on - arguably the
    change is wrong, but arguably the line number was already wrong anyway
    (the actual error is on line 4, but we used to expect line 5, and it
    changed to reporting line 6).
    
    We shouldn't care what line symfony is reporting about - what really
    matters is that the error we expect is the error that is thrown.

--- tests/php/i18n/YamlReaderTest.php
+++ tests/php/i18n/YamlReaderTest.php
@@ -43,7 +43,7 @@ class YamlReaderTest extends SapphireTest
         $path = __DIR__ . '/i18nTest/_fakewebroot/i18ntestmodule/lang/en_corrupt.yml';
         $this->expectException(InvalidResourceException::class);
         $regex_path = str_replace('.', '\.', $path ?? '');
-        $this->expectExceptionMessageMatches('@^Error parsing YAML, invalid file "' . $regex_path . '"\. Message: ([\w ].*) line 5 @');
+        $this->expectExceptionMessageMatches('@^Error parsing YAML, invalid file "' . $regex_path . '"\. Message: ([\w ].*) line \d@');
         $reader = new YamlReader();
         $reader->read('en', $path);
     }
