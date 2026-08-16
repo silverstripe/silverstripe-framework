21f73178081f563f14fd6a3b19abd700d79d22cb
Robbie Averill <robbie@averill.co.nz>
2026-08-16 22:55:27 +1200

    FIX Resolve table class when multiple modifiers are appended
    For example, when using Versioned and Fluent together with
    `DBSchemaManager::$exclude_models_from_db_checks` enabled.

--- tests/php/ORM/DataObjectSchemaTest.php
+++ tests/php/ORM/DataObjectSchemaTest.php
@@ -84,6 +84,15 @@ class DataObjectSchemaTest extends SapphireTest
             WithCustomTable::class,
             $schema->tableClass('DOSTWithCustomTable')
         );
+
+        $this->assertEquals(
+            WithRelation::class,
+            $schema->tableClass('DataObjectSchemaTest_WithRelation_Localised_Live')
+        );
+        $this->assertEquals(
+            WithRelation::class,
+            $schema->tableClass('DataObjectSchemaTest_WithRelation_Localised_Versions')
+        );
     }
 
     public function testTableForObjectField()
